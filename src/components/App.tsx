7ed483a9978d7906c22be2dbe48e5cb9b0bb9a7e
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-16 16:15:30 -0600

    prettier format changes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -90,7 +90,7 @@ function App() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
