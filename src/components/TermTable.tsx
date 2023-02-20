893739e22bc731c2b8bf3391733d9147c18b019b
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-20 14:18:53 -0600

    Create TermTable.tsx

---
+++ src/components/TermTable.tsx
@@ -0,0 +1,52 @@
+export function TermTable() {
+  return (
+    <table className="text-left text-white border-separate border-spacing-y-4">
+      <thead>
+        <th>Token</th>
+        <th>Yield Source</th>
+        <th>Fixed APR</th>
+        <th>Variable APR</th>
+        <th>LP APR</th>
+        <th>TVL</th>
+      </thead>
+
+      <tbody>
+        <tr className="text-black bg-lean">
+          <td>USDC</td>
+          <td>Aave</td>
+          <td>8.0%</td>
+          <td>8.0%</td>
+          <td>11.0%</td>
+          <td>$1M</td>
+        </tr>
+
+        <tr className="text-black bg-lean">
+          <td>USDC</td>
+          <td>Aave</td>
+          <td>8.0%</td>
+          <td>8.0%</td>
+          <td>11.0%</td>
+          <td>$1M</td>
+        </tr>
+
+        <tr className="text-black bg-lean">
+          <td>USDC</td>
+          <td>Aave</td>
+          <td>8.0%</td>
+          <td>8.0%</td>
+          <td>11.0%</td>
+          <td>$1M</td>
+        </tr>
+
+        <tr className="text-black bg-lean">
+          <td>USDC</td>
+          <td>Aave</td>
+          <td>8.0%</td>
+          <td>8.0%</td>
+          <td>11.0%</td>
+          <td>$1M</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
