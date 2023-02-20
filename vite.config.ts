41ad4399672cb64803786ea652ee13204a94d26f
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-20 15:41:28 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  publicDir: "/",
+  // publicDir: "public",
 });
