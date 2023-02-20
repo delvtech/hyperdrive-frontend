c02bc97773c2d463d3538d29a2d293c39e3019b4
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-20 14:19:17 -0600

    wagmi client local host chain setup

--- src/wallet/wagmiClient.ts
+++ src/wallet/wagmiClient.ts
@@ -1,7 +1,7 @@
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
-import { configureChains, createClient, goerli } from "wagmi";
+import { Chain, configureChains, createClient, goerli } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 
 const ALCHEMY_GOERLI_RPC_KEY = import.meta.env.VITE_ALCHEMY_GOERLI_RPC_KEY;
 
@@ -9,9 +9,31 @@ if (!ALCHEMY_GOERLI_RPC_KEY) {
   throw new Error("Provide an ALCHEMY_GOERLI_RPC_KEY variable in .env");
 }
 
+const _localhost: Chain = {
+  name: "Localhost",
+  network: "localhost",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ethereum",
+    symbol: "ETH",
+  },
+  id: 31337,
+  rpcUrls: {
+    public: { http: ["http://localhost:8545/"] },
+    default: { http: ["http://localhost:8545/"] },
+  },
+};
+
 export const { chains, provider } = configureChains(
-  [goerli],
-  [alchemyProvider({ apiKey: ALCHEMY_GOERLI_RPC_KEY }), publicProvider()],
+  [goerli, _localhost],
+  [
+    alchemyProvider({ apiKey: ALCHEMY_GOERLI_RPC_KEY }),
+    jsonRpcProvider({
+      rpc: () => ({
+        http: `http://localhost:8545`,
+      }),
+    }),
+  ],
 );
 
 const { connectors } = getDefaultWallets({
