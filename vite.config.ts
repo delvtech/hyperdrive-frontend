58d57c536c44456f6ac74497486a6a86a10e2587
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-20 14:17:23 -0600

    cleanup vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: "happy-dom",
-    setupFiles: ".vitest/setup",
-    include: ["**/test.{ts,tsx}"],
-  },
+  publicDir: "/",
 });
