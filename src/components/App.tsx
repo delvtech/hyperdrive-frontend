467407b82f469c8f7c795051368c6d6c14215a64
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-20 14:18:45 -0600

    started custom rk theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { midnightTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { lightTheme, RainbowKitProvider, Theme } from "@rainbow-me/rainbowkit";
 import { Index } from "pages/Index";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -6,19 +6,33 @@ import { WagmiConfig } from "wagmi";
 import { chains, wagmiClient } from "wallet/wagmiClient";
 import { Navbar } from "./Navbar";
 
+import merge from "lodash.merge";
+
 const queryClient = new QueryClient();
 
 export function App() {
   return (
-    <div className="h-screen">
+    <div className="h-screen bg-zinc-900">
       <QueryClientProvider client={queryClient}>
         <WagmiConfig client={wagmiClient}>
-          <RainbowKitProvider chains={chains} theme={midnightTheme()}>
+          <RainbowKitProvider
+            chains={chains}
+            theme={merge(lightTheme(), {
+              fonts: {
+                body: "Akira",
+              },
+              colors: {
+                connectButtonBackground: "#D5FF90",
+                closeButtonBackground: "#D5FF90",
+
+                // TODO custom rainbow kit theme
+              },
+            } as Theme)}
+          >
             <Navbar />
             <BrowserRouter>
               <Routes>
                 <Route path="/" element={<Index />} />
-                <Route path="/a" element={<Index />} />
               </Routes>
             </BrowserRouter>
           </RainbowKitProvider>
