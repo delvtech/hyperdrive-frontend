29737b930deefc8e163dfa970ba8d9603580aaa7
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-21 13:31:35 -0600

    remove dup classnames

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,3 @@
-export function classNames(...classes: unknown[]): string {
-  return classes.filter(Boolean).join(" ");
-}
-
 import { format } from "d3-format";
 import { commify } from "ethers/lib/utils";
 
