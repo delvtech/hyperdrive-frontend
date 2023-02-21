283d609f9be6ef5805dbbd3dd23bc246708c71ef
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-21 13:31:01 -0600

    hyperdrive config init

---
+++ src/hyperdrive/config.ts
@@ -0,0 +1,9 @@
+import { Token } from "./types";
+
+interface Markets {
+  baseToken: Token;
+}
+
+export interface HyperdriveConfig {}
+
+export const config: HyperdriveConfig = {};
---
+++ src/hyperdrive/types.ts
@@ -0,0 +1,5 @@
+export interface Token {
+  name: string;
+  symbol: string;
+  logoUrl: string;
+}
