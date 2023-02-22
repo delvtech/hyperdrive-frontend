2cdfb2c15eed9ceda9532cfa34cbbcd9a9b3fd30
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-22 09:36:07 -0600

    tokeninput file

--- src/components/LongPositionForm.tsx
+++ src/components/LongPositionForm.tsx
@@ -1,84 +1,13 @@
-import { formatUnits } from "ethers/lib/utils.js";
-import { Market, OrderType, Token } from "hyperdrive/types";
-import { useState } from "react";
+import { Market, OrderType } from "hyperdrive/types";
 import { match } from "ts-pattern";
-import { formatBalance } from "utils";
 import { useAccount, useBalance } from "wagmi";
 import { Receipt } from "./Receipt";
 import { Tag } from "./Tag";
+import { TokenInput } from "./TokenInput";
 
 interface LongPositionFormProps {
   order: OrderType;
   market: Market;
-}
-
-interface TokenInputProps {
-  token: Token;
-  currentBalance: string;
-  onChange: (value: string) => void;
-}
-
-function TokenInput({ token, onChange, currentBalance }: TokenInputProps) {
-  const [input, setInput] = useState("");
-
-  return (
-    <div className="flex flex-col p-4 border-2 rounded border-lean gap-y-2">
-      <div className="flex items-center text-white gap-x-2">
-        <div className="grow basis-0">
-          <input
-            value={input}
-            className="w-full text-5xl font-bold bg-transparent"
-            placeholder="0"
-            onChange={(event) => {
-              const value = event.target.value;
-              setInput(value);
-              onChange(value);
-            }}
-          />
-        </div>
-        <Tag text={token.symbol}>
-          <img
-            className="inline mr-1"
-            src={token.logoUrl}
-            height={16}
-            width={16}
-          />
-        </Tag>
-      </div>
-
-      <div className="flex w-full text-white">
-        {currentBalance === input ? (
-          <button
-            className="mr-auto underline disabled:opacity-50"
-            // disabled={!currentBalance}
-            onClick={() => {
-              setInput("0");
-              onChange("0");
-            }}
-          >
-            Clear
-          </button>
-        ) : (
-          <button
-            className="mr-auto underline disabled:opacity-50"
-            disabled={!currentBalance}
-            onClick={() => {
-              setInput(formatUnits(currentBalance, token.decimals));
-              onChange(currentBalance);
-            }}
-          >
-            Max
-          </button>
-        )}
-        <div className="flex items-center">
-          <span className="mr-1">Balance:</span>
-          <span className="font-bold">
-            {formatBalance(formatUnits(currentBalance, token.decimals))}
-          </span>
-        </div>
-      </div>
-    </div>
-  );
 }
 
 export function LongPositionForm({ order, market }: LongPositionFormProps) {
---
+++ src/components/TokenInput.tsx
@@ -0,0 +1,78 @@
+import { formatUnits } from "ethers/lib/utils.js";
+import { Token } from "hyperdrive/types";
+import { useState } from "react";
+import { formatBalance } from "utils";
+import { Tag } from "./Tag";
+
+interface TokenInputProps {
+  token: Token;
+  currentBalance: string;
+  onChange: (value: string) => void;
+}
+
+export function TokenInput({
+  token,
+  onChange,
+  currentBalance,
+}: TokenInputProps) {
+  const [input, setInput] = useState("");
+
+  return (
+    <div className="flex flex-col p-4 border-2 rounded border-lean gap-y-2">
+      <div className="flex items-center text-white gap-x-2">
+        <div className="grow basis-0">
+          <input
+            value={input}
+            className="w-full text-5xl font-bold bg-transparent"
+            placeholder="0"
+            onChange={(event) => {
+              const value = event.target.value;
+              setInput(value);
+              onChange(value);
+            }}
+          />
+        </div>
+        <Tag text={token.symbol}>
+          <img
+            className="inline mr-1"
+            src={token.logoUrl}
+            height={16}
+            width={16}
+          />
+        </Tag>
+      </div>
+
+      <div className="flex w-full text-white">
+        {currentBalance === input ? (
+          <button
+            className="mr-auto underline disabled:opacity-50"
+            // disabled={!currentBalance}
+            onClick={() => {
+              setInput("0");
+              onChange("0");
+            }}
+          >
+            Clear
+          </button>
+        ) : (
+          <button
+            className="mr-auto underline disabled:opacity-50"
+            disabled={!currentBalance}
+            onClick={() => {
+              setInput(formatUnits(currentBalance, token.decimals));
+              onChange(currentBalance);
+            }}
+          >
+            Max
+          </button>
+        )}
+        <div className="flex items-center">
+          <span className="mr-1">Balance:</span>
+          <span className="font-bold">
+            {formatBalance(formatUnits(currentBalance, token.decimals))}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
