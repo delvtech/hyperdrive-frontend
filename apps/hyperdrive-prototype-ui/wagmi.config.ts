a1156a8f4e2551a9c0da527a4c68d9e60b833c0d
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-23 14:13:13 -0600

    added env var for hyperdrive project path

--- apps/hyperdrive-prototype-ui/wagmi.config.ts
+++ apps/hyperdrive-prototype-ui/wagmi.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "@wagmi/cli";
 import { foundry, react } from "@wagmi/cli/plugins";
 
+const HYPERDRIVE_PROJECT_PATH = import.meta.env.VITE_HYPERDRIVE_PROJECT_PATH;
+
+if (!HYPERDRIVE_PROJECT_PATH) {
+  throw new Error("Provide an HYPERDRIVE_PROJECT_PATH variable in .env");
+}
+
 export default defineConfig({
   out: "src/generated.ts",
   contracts: [],
   plugins: [
     foundry({
-      project: "/Users/cashd/workspace/delve/hyperdrive",
+      project: HYPERDRIVE_PROJECT_PATH,
       exclude: [
         "IERC20.sol",
         "Common.sol/**",
