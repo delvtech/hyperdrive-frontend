c730cea6f4849f2a8ad7449cefd3b104f6bd7424
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-23 15:25:22 -0600

    OPEN SHORT!!

---
+++ apps/hyperdrive-prototype-ui/src/hyperdrive/hooks/usePreviewOpenShort.ts
@@ -0,0 +1,48 @@
+import { BigNumber, constants, Contract } from "ethers";
+import { parseUnits } from "ethers/lib/utils.js";
+import { hyperdriveABI } from "generated";
+import { Market } from "hyperdrive/types";
+import { useQuery, UseQueryResult } from "react-query";
+import { isValidTokenAmount } from "utils";
+import { Address, useProvider, useSigner } from "wagmi";
+
+export function usePreviewOpenShort(
+  account: Address | undefined,
+  market: Market,
+  bondAmount: string,
+): UseQueryResult<BigNumber> {
+  const provider = useProvider();
+
+  const { data: signer } = useSigner();
+
+  return useQuery({
+    queryKey: [account, market, bondAmount],
+    enabled:
+      !!account && !!provider && !!signer && isValidTokenAmount(bondAmount),
+    queryFn: async () => {
+      const bondAmountBN = parseUnits(bondAmount, market.baseToken.decimals);
+
+      if (bondAmountBN.isZero()) {
+        return BigNumber.from(0);
+      }
+
+      const hyperdriveContract = new Contract(
+        market.address,
+        hyperdriveABI,
+        provider,
+      );
+
+      const out = await hyperdriveContract
+        .connect(signer!)
+        .callStatic.openShort(
+          bondAmountBN,
+          // todo support slippage
+          constants.MaxUint256,
+          account,
+          false,
+        );
+
+      return out;
+    },
+  });
+}
