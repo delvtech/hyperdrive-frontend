8c94e99ab49a85727eb9cdb5a2299007933533da
Danny Delott <dannydelott@gmail.com>
2023-02-28 10:18:52 -0800

    Add rainbow styles, update pacakage name

--- apps/fixed-borrow/src/main.tsx
+++ apps/fixed-borrow/src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./ui/App";
+
+import "@rainbow-me/rainbowkit/styles.css";
 import "./ui/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
