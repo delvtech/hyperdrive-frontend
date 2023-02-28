6465faa79b668e93c5229bad09ba0e6f6fc505e1
Danny Delott <dannydelott@gmail.com>
2023-02-27 19:34:25 -0800

    Stub out UI

--- apps/fixed-borrow/src/App.tsx
+++ apps/fixed-borrow/src/App.tsx
@@ -1,24 +1,126 @@
-import { ReactElement } from "react";
+import classNames from "classnames";
+import { ReactElement, useState } from "react";
 
-function App(): ReactElement {
+type TermDuration = "90_DAYS" | "180_DAYS" | "270_DAYS";
+
+export default function App(): ReactElement {
+  const [duration, setDuration] = useState<TermDuration | undefined>();
   return (
-    <div className="flex h-screen flex-col items-center p-8">
-      <h4 className="text-xl font-bold">Built by Delve</h4>
+    <div className="space-y-14 p-8">
+      <div className="space-y-2 text-center">
+        <h1 className="text-5xl font-bold">Fixed Borrow</h1>
+        <h4 className="text-xl">Built by Delve</h4>
+      </div>
 
-      <div className="daisy-hero h-full">
-        <div className="daisy-hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Fixed Borrow</h1>
-            <p className="py-6">
-              Get a fixed rate loan by borrowing DAI on Spark, then hedging the
-              borrow rate with a Hyperdrive Short.
-            </p>
-            <button className="daisy-btn-primary daisy-btn">Get Started</button>
+      <div className="flex h-full justify-center">
+        <div className="flex flex-col items-center gap-12">
+          <div className="daisy-stats daisy-stats-vertical justify-center text-center lg:daisy-stats-horizontal lg:text-start">
+            <div className="daisy-stat">
+              <div className="daisy-stat-title">Collat. Ratio</div>
+              <div className="daisy-stat-value">152%</div>
+              <div className="daisy-stat-desc text-sm">Min. ratio: 100%</div>
+            </div>
+
+            <div className="daisy-stat">
+              <div className="daisy-stat-title">Liquidation Price</div>
+              <div className="daisy-stat-value">$1,200</div>
+              <div className="daisy-stat-desc text-sm">
+                Current price: $1600
+              </div>
+            </div>
+
+            <div className="daisy-stat">
+              <div className="daisy-stat-title">Borrow Rate (APY)</div>
+              <div className="daisy-stat-value">1.25%</div>
+              <div className="daisy-stat-desc text-sm">
+                View on{" "}
+                <a href="#" className="daisy-link">
+                  Spark
+                </a>
+              </div>
+            </div>
           </div>
+          {/* Collateral */}
+          <div className="daisy-form-control w-full">
+            <label className="daisy-label">
+              <span className="daisy-label-text">Collateral</span>
+            </label>
+            <label className="daisy-input-group">
+              <span>ETH</span>
+              <input
+                type="text"
+                placeholder="10"
+                className="daisy-input-bordered daisy-input w-full"
+              />
+            </label>
+          </div>
+
+          {/* Debt */}
+          <div className="daisy-form-control w-full">
+            <label className="daisy-label">
+              <span className="daisy-label-text">Debt</span>
+            </label>
+            <label className="daisy-input-group">
+              <span>DAI</span>
+              <input
+                type="text"
+                placeholder="10"
+                className="daisy-input-bordered daisy-input w-full"
+              />
+            </label>
+          </div>
+
+          {/* Hyperdrive Short */}
+          <div className="flex w-full flex-col gap-4">
+            <div className="daisy-form-control">
+              <label className="daisy-label">
+                <span className="daisy-label-text">Hyperdrive Short</span>
+              </label>
+              <label className="daisy-input-group">
+                <span className="shrink-0">You Pay</span>
+                <input
+                  type="text"
+                  disabled
+                  className="daisy-input-bordered daisy-input w-full"
+                />
+                <span>DAI</span>
+              </label>
+            </div>
+            <div className="flex justify-end gap-2">
+              <button
+                onClick={() => setDuration("90_DAYS")}
+                className={classNames("daisy-btn ", {
+                  "daisy-btn-outline daisy-btn-active":
+                    duration && duration === "90_DAYS",
+                })}
+              >
+                3 months
+              </button>
+              <button
+                onClick={() => setDuration("180_DAYS")}
+                className={classNames("daisy-btn", {
+                  "daisy-btn-outline daisy-btn-active":
+                    duration && duration === "180_DAYS",
+                })}
+              >
+                6 months
+              </button>
+              <button
+                onClick={() => setDuration("270_DAYS")}
+                className={classNames("daisy-btn", {
+                  "daisy-btn-outline daisy-btn-active":
+                    duration && duration === "270_DAYS",
+                })}
+              >
+                9 months
+              </button>
+            </div>
+          </div>
+          <button className="daisy-btn daisy-btn-info daisy-btn-wide">
+            Submit transaction
+          </button>
         </div>
       </div>
     </div>
   );
 }
-
-export default App;
