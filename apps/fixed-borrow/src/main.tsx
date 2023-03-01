a98a235d3bfa9039b58d53d6844ac8c1d58c123e
Danny Delott <dannydelott@gmail.com>
2023-03-01 10:02:33 -0800

    Refactor import paths (#18)

--- apps/fixed-borrow/src/main.tsx
+++ apps/fixed-borrow/src/main.tsx
@@ -1,17 +1,16 @@
-// TODO: Find out if vite will let us move this to ui/ instead
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./ui/App";
-
-import { QueryClientProvider } from "@tanstack/react-query";
-import { WagmiConfig } from "wagmi";
-import { chains, wagmiClient } from "./network/wagmiClient";
-import { darkTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { queryClient } from "./network/queryClient";
+// TODO: Find out if vite will let us move main.tsx to ui/ instead
 
 import "@rainbow-me/rainbowkit/styles.css";
-import "./ui/index.css";
+import "src/ui/index.css";
+
+import { darkTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { queryClient } from "src/network/queryClient";
+import { chains, wagmiClient } from "src/network/wagmiClient";
+import App from "src/ui/App";
+import { WagmiConfig } from "wagmi";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
