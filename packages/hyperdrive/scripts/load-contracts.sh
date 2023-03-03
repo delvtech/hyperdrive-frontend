4e55b3b0ee2267853efd7a4d1e7d433f9563c4fc
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-03 10:56:10 -0600

    Create load-contracts.sh

---
+++ packages/hyperdrive/scripts/load-contracts.sh
@@ -0,0 +1,17 @@
+echo "Downloading contracts..."
+
+if [[ -z "${GITHUB_TOKEN}" ]]; then
+  git clone git@github.com:element-fi/hyperdrive.git hyperdrive-src
+else
+ git clone https://$GITHUB_TOKEN@github.com/element-fi/hyperdrive.git hyperdrive-src
+fi
+
+# blow away old-contracts
+rm -rf contracts
+mkdir contracts
+
+echo "Copying latest contracts..."
+cp -R hyperdrive-src/contracts/ contracts
+rm -rf hyperdrive-src
+
+echo "Done!"
