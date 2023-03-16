833fd8967449714c6ca3504b5fb8c654e68b0215
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-16 18:13:04 -0500

    hyperdrive trading init

---
+++ apps/hyperdrive-trading/src/main.tsx
@@ -0,0 +1,9 @@
+import "@rainbow-me/rainbowkit/styles.css";
+import { App } from "components/App";
+import { createRoot } from "react-dom/client";
+import "tailwindcss/tailwind.css";
+
+const container = document.getElementById("root") as HTMLDivElement;
+const root = createRoot(container);
+
+root.render(<App />);
---
+++ apps/hyperdrive-trading/src/pages/Index.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from "react";
+
+export function Index(): ReactElement {
+  return <div></div>;
+}
