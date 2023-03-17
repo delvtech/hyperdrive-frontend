93f6b87434fde6a7d28689877639432e53c00e55
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-16 20:03:23 -0500

    Update vite.config.ts

--- apps/hyperdrive-trading/vite.config.ts
+++ apps/hyperdrive-trading/vite.config.ts
@@ -4,5 +4,15 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
+  // Node.js global to browser globalThis
+  define: {
+    global: "globalThis",
+  },
+  resolve: {
+    alias: {
+      process: "process/browser",
+      util: "util",
+    },
+  },
   plugins: [react(), tsconfigPaths()],
 });
