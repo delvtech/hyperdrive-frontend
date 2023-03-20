e2d3a3e4f09f60a79fc188e6e1f4625e3bfb5f6f
Danny Delott <dannydelott@gmail.com>
2023-03-20 10:31:49 -0700

    Remove daisy-ui

--- apps/fixed-borrow/tailwind.config.js
+++ apps/fixed-borrow/tailwind.config.js
@@ -59,21 +59,5 @@ module.exports = {
         lineHeight,
       },
     },
-    daisyui: {
-      prefix: "daisy-",
-      themes: [
-        {
-          black: {
-            // eslint-disable-next-line @typescript-eslint/no-var-requires
-            ...require("daisyui/src/colors/themes")["[data-theme=black]"],
-            primary: "#ff00ff", // Collateral is pink, because in fast-and-the-furious they use pink slips as collateral
-            secondary: "#ffff00", // Debt is yellow
-
-            info: "#00ffff", // lighter shade of blue so outlined buttons are legible
-            success: "#00ff7f", // lighter shade of green so outlined buttons are legible
-          },
-        },
-      ],
-    },
   },
 };
