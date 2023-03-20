14fa18232b6839bfb167c1bd00e0fd2dfa3a96f8
Ryan Goree <goree.ryan@gmail.com>
2023-03-20 10:04:09 -0500

    Add typescale

--- apps/fixed-borrow/tailwind.config.js
+++ apps/fixed-borrow/tailwind.config.js
@@ -1,5 +1,36 @@
-/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { gudTypeScale, rounder } = require("gud-type");
 
+const typeScale = gudTypeScale(
+  ["sm", "body", "lg", "h6", "h5", "h4", "h3", "h2", "h1"],
+  {
+    // make "body" the 0 index
+    startingIndex: -1,
+    // set the base size (size at 0) to 13
+    base: 13,
+    // Set the scale multiplier/ratio to 2
+    multiplier: 2,
+    // Make it take 5 steps to reach each multiple
+    // for example, if starting at 4 with 5 steps to reach it's next multiple of
+    // 2 (8), the scale would look like: 4, 4.75, 5.5, 6.25, 7, 8
+    steps: 5,
+    // round each font size up to the closest whole number
+    round: rounder(1, "up"),
+    unit: "px",
+  },
+);
+
+console.log("Type scale:", typeScale);
+
+// Split up the styles by font size and line height
+const fontSize = {};
+const lineHeight = {};
+for (const [name, styles] of Object.entries(typeScale)) {
+  fontSize[name] = styles.fontSize;
+  lineHeight[name] = styles.lineHeight;
+}
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("daisyui")],
@@ -24,6 +55,8 @@ module.exports = {
           start: "#E9ED73",
           end: "#FFA977",
         },
+        fontSize,
+        lineHeight,
       },
     },
     daisyui: {
