bb6ae28b3f5c2efd8e64efe1b2fbd77f4b99ba58
Danny Delott <dannydelott@gmail.com>
2023-03-20 15:56:44 -0700

    Scaffold loan card header

---
+++ apps/fixed-borrow/src/ui/loans/LoanCard/LoanCardHeader.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+interface LoanCardHeaderProps {}
+
+export function LoanCardHeader({}: LoanCardHeaderProps): ReactElement {
+  return <div className="flex w-full justify-between"></div>;
+}
