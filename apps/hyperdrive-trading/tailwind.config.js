2d3869e8592b5f044173d3600932582bb7cb4b63
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-20 16:22:04 -0500

    added custom fonts and colors to tailwind config

--- apps/hyperdrive-trading/tailwind.config.js
+++ apps/hyperdrive-trading/tailwind.config.js
@@ -3,7 +3,20 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Akira: ["Akira", "sans-serif"],
+        Quantico: ["Quantico"],
+      },
+      colors: {
+        "bg-base": "#171B2C",
+        "bg-base-200": "#0E111B",
+        "hyper-pink": "#F4B1FF",
+        "hyper-green": "#61E4A3",
+        "off-white": "#F0F0F0",
+        "giga-blue-100": "99A1D3",
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
