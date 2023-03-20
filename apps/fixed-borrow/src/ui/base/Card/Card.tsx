d0063b4b8c1c9701ed209d336caf7cf21b41d540
Ryan Goree <goree.ryan@gmail.com>
2023-03-20 16:29:02 -0500

    Use PropsWithChildren generic

--- apps/fixed-borrow/src/ui/base/Card/Card.tsx
+++ apps/fixed-borrow/src/ui/base/Card/Card.tsx
@@ -1,10 +1,8 @@
-import { ReactElement, ReactNode } from "react";
+import { PropsWithChildren, ReactElement } from "react";
 
-interface CardProps {
-  children: ReactNode;
-}
+interface CardProps {}
 
-export function Card({ children }: CardProps): ReactElement {
+export function Card({ children }: PropsWithChildren<CardProps>): ReactElement {
   return (
     <div className="rounded-lg bg-dawn py-10 px-12 text-white shadow-[0_4px_14px_rgb(4,9,26)]">
       {children}
