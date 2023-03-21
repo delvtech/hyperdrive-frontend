51fe7a396748d2309afd7b324d7eeb02b0c58c1e
Ryan Goree <goree.ryan@gmail.com>
2023-03-20 20:35:57 -0500

    Add InfoTooltip component

---
+++ apps/fixed-borrow/src/ui/base/Tooltip/InfoTooltip.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from "react";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import { Tooltip, TooltipProps } from "./Tooltip";
+
+interface InfoTooltipProps extends Exclude<TooltipProps, "children"> {}
+
+export function InfoTooltip(props: InfoTooltipProps): ReactElement {
+  return (
+    <Tooltip {...props}>
+      <InformationCircleIcon width={18} className="stroke-current" />
+    </Tooltip>
+  );
+}
