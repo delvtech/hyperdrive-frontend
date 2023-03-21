921646fb0125d01f12d91d0de7ac12e886ba2b3b
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-21 18:37:46 -0500

    Add new scaffold components to market page

--- apps/hyperdrive-trading/src/pages/Markets.tsx
+++ apps/hyperdrive-trading/src/pages/Markets.tsx
@@ -1,5 +1,12 @@
+import { FeaturedMarkets } from "markets/components/FeaturedMarkets";
+import { ProtocolStatsBar } from "protocol/components/ProtocolStatsBar";
 import { ReactElement } from "react";
 
 export function Markets(): ReactElement {
-  return <div></div>;
+  return (
+    <div className="m-auto flex max-w-5xl flex-col gap-y-12 px-6 pt-[5vh]">
+      <ProtocolStatsBar />
+      <FeaturedMarkets />
+    </div>
+  );
 }
