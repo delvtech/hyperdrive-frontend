263148848d09272bdf4aec3b218688bc50e6d96b
Danny Delott <dannydelott@gmail.com>
2023-03-21 14:12:01 -0700

    Create SupplyInput

---
+++ apps/fixed-borrow/src/ui/loans/SupplyInput.tsx
@@ -0,0 +1,48 @@
+import { ReactElement } from "react";
+import { formatBalance } from "src/ui/base/formatting/formatBalance";
+import { Address, useAccount, useBalance, useToken } from "wagmi";
+import { NumericInput } from "src/ui/base/NumericInput/NumericInput";
+import { AssetIcon } from "src/ui/token/AssetIcon";
+
+interface SupplyInputProps {
+  tokenAddress: Address;
+  value?: string;
+  onInputChange?: (newAmount: string | undefined) => void;
+}
+
+export function SupplyInput({
+  tokenAddress,
+  value,
+  onInputChange,
+}: SupplyInputProps): ReactElement {
+  const { address: account } = useAccount();
+  const { data: tokenMetadata } = useToken({
+    address: tokenAddress,
+  });
+  const { data: balanceOf } = useBalance({
+    token: tokenAddress,
+    address: account,
+  });
+
+  const availableToDepositLabel = balanceOf
+    ? `Available to deposit: ${formatBalance(balanceOf.formatted, 2)} ${
+        tokenMetadata?.symbol
+      }`
+    : undefined;
+
+  return (
+    <NumericInput
+      showMaxButton={balanceOf?.value.gt(0)}
+      value={value}
+      maxValue={balanceOf?.formatted}
+      primaryLabel={"Supply"}
+      icon={
+        // TODO: Don't hardcode this to dai
+        <AssetIcon assetId="dai" />
+      }
+      placeholderText="0"
+      secondaryLabel={availableToDepositLabel}
+      onChange={onInputChange}
+    />
+  );
+}
