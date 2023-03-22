2ad25a6c16cc14cea0cbd5c259ced4e5faaaf567
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-22 15:46:40 -0500

    Update Markets.tsx

--- apps/hyperdrive-trading/src/pages/Markets.tsx
+++ apps/hyperdrive-trading/src/pages/Markets.tsx
@@ -1,12 +1,14 @@
-import { FeaturedMarkets } from "src/markets/components/FeaturedMarkets";
+import { FeaturedMarkets } from "src/ui/markets/components/FeaturedMarkets";
 import { ProtocolStatsBar } from "src/protocol/components/ProtocolStatsBar";
 import { ReactElement } from "react";
+import { MarketsTable } from "src/ui/markets/components/MarketsTable";
 
 export function Markets(): ReactElement {
   return (
-    <div className="m-auto flex max-w-5xl flex-col gap-y-12">
+    <div className="m-auto flex max-w-6xl flex-col gap-y-12">
       <ProtocolStatsBar />
       <FeaturedMarkets />
+      <MarketsTable />
     </div>
   );
 }
