4d0f1b4aceb8106b81a4701b6d58b9ccbfd05460
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-22 15:33:09 -0500

    Added sortable grid table compnent

---
+++ apps/hyperdrive-trading/src/ui/base/tables/GridTableRowLink.tsx
@@ -0,0 +1,30 @@
+import classNames from "classnames";
+import { Link } from "react-router-dom";
+import { PropsWithChildren, ReactElement } from "react";
+import { UrlObject } from "url";
+
+/**
+ * A div with `display: grid` pretending to be a table row :).
+ * To customize column widths use the `grid-cols-[<col-widths>]` className.
+ * @see https://tailwindcss.com/docs/grid-template-columns#using-custom-values
+ */
+export function GridTableRowLink({
+  href,
+  className,
+  children,
+}: PropsWithChildren<{
+  href: string;
+  className?: string;
+}>): ReactElement {
+  return (
+    <Link
+      to={href}
+      className={classNames(
+        "grid auto-cols-fr grid-flow-col transition-all last:rounded-b-lg odd:bg-base-200 hover:bg-base-300 [&>*]:overflow-hidden [&>*]:text-ellipsis [&>*]:p-4",
+        className,
+      )}
+    >
+      {children}
+    </Link>
+  );
+}
