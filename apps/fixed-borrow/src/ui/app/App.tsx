ee4a2dc1f71014121a9fad49b8cd8754fccc6655
Danny Delott <dannydelott@gmail.com>
2023-03-23 08:46:13 -0700

    Add header w/ connect buttton

--- apps/fixed-borrow/src/ui/app/App.tsx
+++ apps/fixed-borrow/src/ui/app/App.tsx
@@ -55,9 +55,15 @@ export default function App(): ReactElement {
   );
 
   return (
-    <div className="flex min-h-[75vh] w-full flex-col items-center justify-center py-[10vh] px-[4vw]">
-      <div className="max-w-4xl">
-        <LoanCard />
+    <div className="col-span-2 grid">
+      <div className="flex justify-between gap-2 p-4">
+        <h4 className="font-bold text-white">Fixed Borrow Demo</h4>
+        <ConnectButton showBalance={false} />
+      </div>
+      <div className="flex min-h-[75vh] w-full flex-col items-center justify-center py-[10vh] px-[4vw]">
+        <div className="max-w-4xl">
+          <LoanCard />
+        </div>
       </div>
     </div>
   );
