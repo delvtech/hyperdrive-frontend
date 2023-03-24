e01a183bbd76659d9c5a18ff176aaf3f3599af21
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-24 15:00:34 -0500

    useMarketRowData hook

---
+++ apps/hyperdrive-trading/src/ui/markets/hooks/useMarketRowData.ts
@@ -0,0 +1,36 @@
+import { useQuery, UseQueryResult } from "react-query";
+import { HyperdriveMarket } from "src/config/HyperdriveConfig";
+import { MarketStatistics, MarketTableRowData } from "src/ui/markets/types";
+
+// TODO: stubbed function for now
+// ideally fetched from an api
+function getMarketStatistics(
+  market: HyperdriveMarket,
+): Promise<MarketStatistics> {
+  return Promise.resolve({
+    liquidity: "$100M",
+    longAPR: "1.25%",
+    shortAPR: "1.25%",
+    lpAPR: "1.25%",
+  });
+}
+
+export function useMarketRowData(
+  markets: HyperdriveMarket[],
+): UseQueryResult<MarketTableRowData[]> {
+  return useQuery<MarketTableRowData[]>({
+    queryKey: markets,
+    queryFn: async () => {
+      return await Promise.all(
+        markets.map(async (market) => {
+          const stats = await getMarketStatistics(market);
+          return {
+            market,
+            ...stats,
+            liquidity: stats.liquidity.toString(),
+          };
+        }),
+      );
+    },
+  });
+}
