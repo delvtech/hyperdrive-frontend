e3b34dadc2ec5af3cc6c83dd8dd3ccec419f6bb0
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-28 01:11:46 -0500

    Update Stat.tsx

--- apps/hyperdrive-trading/src/ui/base/components/Stat.tsx
+++ apps/hyperdrive-trading/src/ui/base/components/Stat.tsx
@@ -11,7 +11,7 @@ export function Stat({ label, value }: StatProps): ReactElement {
       <p className="mb-1 whitespace-nowrap font-quantico text-hyper-blue-300">
         {label}
       </p>
-      <p className="font-bold whitespace-nowrap font-rubik text-hyper-blue-100">
+      <p className="font-bold whitespace-nowrap font-quantico text-hyper-blue-100">
         {value}
       </p>
     </div>
