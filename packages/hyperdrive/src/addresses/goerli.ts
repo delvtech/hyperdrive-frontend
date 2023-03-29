b8b180558b64322a6f32369801ca9b24686ea784
Danny Delott <dannydelott@gmail.com>
2023-03-29 13:20:51 -0700

    Add comment

--- packages/hyperdrive/src/addresses/goerli.ts
+++ packages/hyperdrive/src/addresses/goerli.ts
@@ -1,5 +1,7 @@
 // See: https://github.com/element-fi/hyperdrive/blob/main/addresses.json
 export const HyperdriveGoerliAddresses = {
   makerDsrHyperdrive: "0x8C9Ccd26fFB734658171EE2B0C6d087e72752c20",
+  // fixed borrow action is deployed with a reference to the makerDsrHyperdrive
+  // https://github.com/element-fi/hyperdrive/blob/main/contracts/src/actions/AaveFixedBorrow.sol
   aaveFixedBorrowAction: "0x6F97F6662eF2BDD2b468E1a5eB1956729648A9b8",
 } as const;
