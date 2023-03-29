5307569429cb99f44bcd97a8e7f755ddc6de62f4
Danny Delott <dannydelott@gmail.com>
2023-03-29 11:37:36 -0700

    Add AaveFixedBorrowAction to hyperdrive package (#70)

--- packages/hyperdrive/src/addresses/goerli.ts
+++ packages/hyperdrive/src/addresses/goerli.ts
@@ -1,4 +1,5 @@
 // See: https://github.com/element-fi/hyperdrive/blob/main/addresses.json
 export const HyperdriveGoerliAddresses = {
-  makerDsrHyperdrive: "0x27b8C295f59f313898b49AfAde92CB430F8b4074",
+  makerDsrHyperdrive: "0x8C9Ccd26fFB734658171EE2B0C6d087e72752c20",
+  aaveFixedBorrowAction: "0x6F97F6662eF2BDD2b468E1a5eB1956729648A9b8",
 } as const;
