68b75f710ecdc0525a859aa98d756cc1f8ebe42c
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-29 16:12:03 -0500

    Update hyperdrive.config.ts

--- apps/hyperdrive-trading/src/config/hyperdrive.config.ts
+++ apps/hyperdrive-trading/src/config/hyperdrive.config.ts
@@ -1,7 +1,8 @@
 import { goerliHyperdriveConfig } from "./chains/goerli";
 import { HyperdriveConfig } from "./HyperdriveConfig";
 
-export type SupportedChainId = 5;
+export const supportedChainIds = [5] as const;
+export type SupportedChainId = (typeof supportedChainIds)[number];
 
 export const hyperdriveConfigs: Record<SupportedChainId, HyperdriveConfig> = {
   5: goerliHyperdriveConfig,
