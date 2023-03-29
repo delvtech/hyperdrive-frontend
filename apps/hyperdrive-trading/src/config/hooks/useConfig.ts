ba6806cc958517d1d5b50fc6d47dae126f93f061
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-29 16:11:53 -0500

    useConfig hook

---
+++ apps/hyperdrive-trading/src/config/hooks/useConfig.ts
@@ -0,0 +1,18 @@
+import {
+  SupportedChainId,
+  supportedChainIds,
+} from "src/config/hyperdrive.config";
+import { HyperdriveConfig } from "src/config/HyperdriveConfig";
+import { getHyperdriveConfig } from "src/config/utils/getHyperdriveConfig";
+import { useChainId } from "wagmi";
+
+export function useConfig(): HyperdriveConfig {
+  const chainId = useChainId();
+  if (!supportedChainIds.includes(chainId as SupportedChainId)) {
+    // This should never happen with a properly configured wagmi config
+    throw new Error(
+      `Cannot fetch hyperdrive config with chain id: ${chainId}.`,
+    );
+  }
+  return getHyperdriveConfig(chainId as SupportedChainId);
+}
