ddf2b1a5052dbd9422ab883faa6ca3a87cb1a48e
Cash Baller <cashd@users.noreply.github.com>
2023-03-29 13:02:02 -0500

    [Hyperdrive Trading] Button Variants and Trade page theming (#69)
    * new base color
    
    * better responivness in app container
    
    * market page padding
    
    * Button variants
    
    * trade page theming and responsiveness refresh
    
    * address comments

--- apps/hyperdrive-trading/src/ui/base/components/Button.tsx
+++ apps/hyperdrive-trading/src/ui/base/components/Button.tsx
@@ -2,27 +2,32 @@ import classNames from "classnames";
 import { MouseEventHandler, PropsWithChildren, ReactElement } from "react";
 
 interface ButtonProps extends PropsWithChildren {
+  variant?: ButtonVariant;
   className?: string;
   active?: boolean;
   onClick: MouseEventHandler<HTMLButtonElement>;
 }
 
+type ButtonVariant = "Trade" | "Default";
+
+const ButtonVariantStyles: Record<ButtonVariant, string> = {
+  Trade: "text-xl font-bold bg-hyper-green text-base-300 font-quantico",
+  Default:
+    "whitespace-nowrap border border-hyper-blue-400 px-4 py-2 font-quantico text-hyper-green hover:bg-hyper-blue-300",
+};
+
 export default function Button({
   children,
   active,
+  variant = "Default",
   className,
   onClick,
 }: ButtonProps): ReactElement {
   return (
     <button
-      className={classNames(
-        {
-          "bg-hyper-blue-400": active,
-          "bg-transparent": !active,
-        },
-        "whitespace-nowrap border border-hyper-blue-400 px-6 py-2 font-quantico text-hyper-green hover:bg-giga-blue-300",
-        className,
-      )}
+      className={classNames(ButtonVariantStyles[variant], className, {
+        "bg-hyper-blue-400 font-bold": active,
+      })}
       onClick={onClick}
     >
       {children}
