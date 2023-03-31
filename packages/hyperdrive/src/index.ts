264c84280a596c6f997372e03b6ebeebf29c8273
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-30 19:31:34 -0500

    add ERC20 ABI to hyperdrive package

--- packages/hyperdrive/src/index.ts
+++ packages/hyperdrive/src/index.ts
@@ -2,3 +2,4 @@ export { HyperdriveGoerliAddresses } from "src/addresses/goerli";
 
 export { HyperdriveABI } from "src/abis/Hyperdrive";
 export { AaveFixedBorrowActionABI } from "src/abis/AaveFixedBorrrowAction";
+export { ERC20_ABI } from "src/abis/ERC20";
