b4185103478fc512de79b7db7d1446b327d276f4
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-30 23:12:43 -0500

    PositionOverview component

--- apps/hyperdrive-trading/src/ui/trading/components/PositionOverviewWell.tsx
+++ apps/hyperdrive-trading/src/ui/trading/components/PositionOverviewWell.tsx
@@ -1,30 +1,63 @@
 import { ReactElement } from "react";
+import { HyperdriveMarket } from "src/config/HyperdriveConfig";
 import { Pill } from "src/ui/base/components/Pill";
+import { formatBalance } from "src/ui/base/formatting/formatBalance";
+import { formatBigInt } from "src/ui/base/formatting/formatBigInt";
 
-export function PositionOverviewWell(): ReactElement {
+interface PositionOverviewWellProps {
+  market: HyperdriveMarket;
+  costBasis: bigint;
+  fixedApr: number;
+  claimableAtMaturity: bigint;
+}
+
+export function PositionOverviewWell({
+  market,
+  costBasis,
+  fixedApr,
+  claimableAtMaturity,
+}: PositionOverviewWellProps): ReactElement {
+  const current = new Date();
+  const formattedMaturityDate = new Date(
+    current.setMonth(current.getMonth() + market.termLength),
+  ).toLocaleDateString();
+
   return (
     <div className="flex flex-col p-4 bg-transparent border rounded gap-y-4 border-hyper-blue-300">
       <div className="flex items-center">
-        <h3 className="mr-4 text-2xl font-semibold">Maker DSR Long</h3>
-        <Pill className="h-6">6 months</Pill>
+        <h3 className="mr-4 text-2xl font-semibold">{market.name}</h3>
+        <Pill className="h-6">{market.termLength} months</Pill>
       </div>
 
       <div className="flex flex-col tracking-wide gap-y-1">
         <div className="flex">
           <p className="mr-auto">Cost Basis</p>
-          <p className="font-semibold tracking-wide">120000 DAI</p>
+          <p className="font-semibold tracking-wide">
+            {formatBigInt(costBasis, market.baseToken.decimals)}{" "}
+            {market.baseToken.symbol}
+          </p>
         </div>
+
         <div className="flex">
           <p className="mr-auto">Matures</p>
-          <p className="font-semibold tracking-wide">July 23rd, 2023</p>
+          <p className="font-semibold tracking-wide">{formattedMaturityDate}</p>
         </div>
+
         <div className="flex">
           <p className="mr-auto">Fixed APR</p>
           <p className="font-semibold tracking-wide">1.50%</p>
         </div>
+
         <div className="flex">
           <p className="mr-auto">Claimable at Maturity</p>
-          <p className="font-semibold tracking-wide">120500 DAI</p>
+          <p className="font-semibold tracking-wide">
+            {formatBalance(
+              formatBigInt(claimableAtMaturity, market.baseToken.decimals),
+              4,
+              false,
+            )}
+            {market.baseToken.symbol}
+          </p>
         </div>
       </div>
     </div>
