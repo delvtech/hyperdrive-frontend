ef7486b83130511d4979dc6dc612a71cd264e484
Danny Delott <dannydelott@gmail.com>
2023-04-03 13:31:19 -0700

    Add VariableDebtToken abi to spark package

--- packages/spark/src/index.ts
+++ packages/spark/src/index.ts
@@ -5,6 +5,7 @@ export { ATokenABI } from "src/abis/AToken";
 export { DSTokenABI } from "src/abis/DSToken";
 export { FaucetABI } from "src/abis/Faucet";
 export { PoolABI } from "src/abis/Pool";
+export { VariableDebtTokenABI } from "src/abis/VariableDebtToken";
 export { UiPoolDataProviderABI } from "src/abis/UiPoolDataProvider";
 
 export type { UserReserveData } from "src/types/UserReserveData";
