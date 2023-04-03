63509ace63b5a47790c078fb389ff47d28745999
Cash DeLeon <cashd@users.noreply.github.com>
2023-04-03 14:07:51 -0500

    Market page h elements and small design change

--- apps/hyperdrive-trading/src/pages/Markets.tsx
+++ apps/hyperdrive-trading/src/pages/Markets.tsx
@@ -4,22 +4,22 @@ import { MarketsTable } from "src/ui/markets/components/MarketsTable";
 
 export function Markets(): ReactElement {
   return (
-    <div className="mx-auto flex flex-col gap-y-16 px-[4vw] py-[5vh] w-full max-w-7xl">
-      <div className="space-y-8">
+    <div className="mx-auto flex flex-col gap-y-20 px-[4vw] py-[5vh] w-full max-w-7xl">
+      <div className="space-y-12">
         <div>
-          <h2 className="text-4xl font-bold sm:text-6xl font-akira text-hyper-blue-100">
+          <h2 className="font-bold font-akira text-hyper-blue-100 text-h3 sm:text-h2 leading-h3 sm:leading-h2">
             Markets
           </h2>
-          <p className="max-w-2xl text-xl font-medium sm:text-xl text-hyper-blue-200 font-dm-sans">
+          <h5 className="max-w-2xl font-medium text-hyper-blue-200 font-dm-sans text-h6 sm:text-h5 leading-h6 sm:leading-h5">
             Advanced yield markets featuring no preset expiration dates, no
             fragmented liquidity, and no LP rollovers — aka everlasting
             liquidity.
-          </p>
+          </h5>
         </div>
 
-        <button className="flex items-center px-4 py-2 text-sm font-bold rounded sm:text-md gap-x-1 text-base-100 font-dm-sans bg-gradient-to-r from-hyper-pink to-hyper-green">
+        <button className="flex items-center px-4 py-2 font-bold rounded gap-x-1 text-base-100 font-dm-sans bg-gradient-to-r from-hyper-pink to-hyper-green">
           <ArrowTopRightOnSquareIcon className="w-4" />
-          Learn more
+          <p>Learn more</p>
         </button>
       </div>
 
