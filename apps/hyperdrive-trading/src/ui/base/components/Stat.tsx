3412cd406fbbc0692238effe0f6313d10336be6f
Cash DeLeon <cashd@users.noreply.github.com>
2023-04-04 15:08:08 -0500

    classNames for stat prop

--- apps/hyperdrive-trading/src/ui/base/components/Stat.tsx
+++ apps/hyperdrive-trading/src/ui/base/components/Stat.tsx
@@ -3,15 +3,16 @@ import { ReactElement } from "react";
 interface StatProps {
   label: string;
   value: string;
+  className?: string;
 }
 
-export function Stat({ label, value }: StatProps): ReactElement {
+export function Stat({ label, value, className }: StatProps): ReactElement {
   return (
-    <div>
-      <p className="mb-1 whitespace-nowrap font-quantico text-hyper-blue-300">
+    <div className={className}>
+      <p className="mb-1 whitespace-nowrap font-dm-sans text-hyper-blue-300">
         {label}
       </p>
-      <p className="font-bold whitespace-nowrap font-quantico text-hyper-blue-100">
+      <p className="font-bold whitespace-nowrap font-dm-sans text-hyper-blue-100">
         {value}
       </p>
     </div>
