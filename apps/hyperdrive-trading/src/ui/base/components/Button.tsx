b93d95aa841dbff2fdd4cb2255b46ff1c3055873
Cash DeLeon <cashd@users.noreply.github.com>
2023-04-05 17:33:35 -0500

    Short button variant

--- apps/hyperdrive-trading/src/ui/base/components/Button.tsx
+++ apps/hyperdrive-trading/src/ui/base/components/Button.tsx
@@ -9,15 +9,17 @@ interface ButtonProps extends PropsWithChildren {
   onClick: MouseEventHandler<HTMLButtonElement>;
 }
 
-type ButtonVariant = "Trade" | "Default" | "Approve";
+type ButtonVariant = "Trade" | "Default" | "Approve" | "Short";
 
 const ButtonVariantStyles: Record<ButtonVariant, string> = {
   Trade:
     "text-xl font-bold bg-hyper-green text-base-300 font-quantico hover:bg-opacity-60 rounded-sm disabled:bg-opacity-50",
+  Short:
+    "text-xl font-bold bg-hyper-red text-base-300 font-quantico hover:bg-opacity-60 rounded-sm disabled:bg-opacity-50",
   Approve:
     "text-xl font-bold bg-hyper-orange text-base-300 font-quantico hover:bg-opacity-60 rounded-sm disabled:bg-opacity-50",
   Default:
-    "whitespace-nowrap border border-hyper-blue-400 px-4 py-2 font-quantico text-hyper-green hover:bg-hyper-blue-400 hover:bg-opacity-50 bg-base-200 disabled:bg-base-400 disabled:text-opacity-50",
+    "whitespace-nowrap border border-hyper-blue-400 px-4 py-2 font-quantico text-hyper-blue-100 hover:bg-hyper-blue-400 hover:bg-opacity-50 bg-base-200 disabled:bg-base-400 disabled:text-opacity-50",
 };
 
 export default function Button({
