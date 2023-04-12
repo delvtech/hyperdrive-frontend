37f185ea12f7203059ddf0cd11ea8bc009303053
Cash DeLeon <cashd@users.noreply.github.com>
2023-04-12 13:41:49 -0500

    Update LongPositionForm.tsx

--- apps/hyperdrive-trading/src/ui/trading/components/LongPositionForm.tsx
+++ apps/hyperdrive-trading/src/ui/trading/components/LongPositionForm.tsx
@@ -3,6 +3,7 @@ import { HyperdriveMarket } from "src/config/HyperdriveConfig";
 import { OrderType } from "src/ui/hyperdrive/types";
 import { OpenLongPositionForm } from "src/ui/trading/components/OpenLongPositionForm";
 import { match } from "ts-pattern";
+import { CloseLongPositionForm } from "./CloseLongPositionForm";
 
 interface LongPositionFormProps {
   market: HyperdriveMarket;
@@ -15,6 +16,6 @@ export function LongPositionForm({
 }: LongPositionFormProps): ReactElement {
   return match(order)
     .with("Open", () => <OpenLongPositionForm market={market} />)
-    .with("Close", () => <></>)
+    .with("Close", () => <CloseLongPositionForm market={market} />)
     .exhaustive();
 }
