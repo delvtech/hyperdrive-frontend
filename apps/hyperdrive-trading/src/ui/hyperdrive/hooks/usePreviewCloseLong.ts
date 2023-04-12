5ca899a0e108797309f103c11e9ee366f7b1cdff
Cash DeLeon <cashd@users.noreply.github.com>
2023-04-12 13:41:34 -0500

    usePreviewCloseLong hook

---
+++ apps/hyperdrive-trading/src/ui/hyperdrive/hooks/usePreviewCloseLong.ts
@@ -0,0 +1,73 @@
+import { BigNumber } from "@ethersproject/bignumber";
+import { HyperdriveABI } from "@hyperdrive/core";
+import { useQuery } from "react-query";
+import { HyperdriveMarket } from "src/config/HyperdriveConfig";
+import { WagmiHookStatusType } from "src/ui/base/types";
+import { Address, useContract, useSigner } from "wagmi";
+
+interface UsePreviewCloseLongOptions {
+  market: HyperdriveMarket;
+  tokenID: bigint | undefined;
+  bondAmountIn: bigint | undefined;
+  minBaseAmountOut: bigint | undefined;
+  destination: Address | undefined;
+  asUnderlying?: boolean;
+  enabled?: boolean;
+}
+
+interface UsePreviewCloseLongResult {
+  previewCloseLongStatus: WagmiHookStatusType;
+  baseAmountOut: bigint | undefined;
+}
+
+export function usePreviewCloseLong({
+  market,
+  tokenID,
+  bondAmountIn,
+  minBaseAmountOut,
+  destination,
+  asUnderlying = true,
+  enabled = true,
+}: UsePreviewCloseLongOptions): UsePreviewCloseLongResult {
+  const { data: signer } = useSigner();
+
+  const hyperdriveContract = useContract({
+    abi: HyperdriveABI,
+    address: market.address,
+    signerOrProvider: signer,
+  });
+
+  const queryEnabled =
+    !!bondAmountIn &&
+    !!minBaseAmountOut &&
+    !!destination &&
+    !!hyperdriveContract &&
+    enabled;
+
+  const { data, status } = useQuery({
+    queryKey: [
+      "preview-close-long",
+      market.address,
+      {
+        baseAmount: bondAmountIn?.toString(),
+        bondAmountOut: minBaseAmountOut?.toString(),
+        destination: destination?.toString(),
+      },
+    ],
+    enabled: queryEnabled,
+    queryFn: queryEnabled
+      ? async () => {
+          const openLongResult = (await hyperdriveContract.callStatic.closeLong(
+            BigNumber.from(tokenID),
+            BigNumber.from(bondAmountIn),
+            BigNumber.from(minBaseAmountOut),
+            destination,
+            asUnderlying,
+          )) as unknown as BigNumber;
+          return openLongResult.toBigInt();
+        }
+      : undefined,
+  });
+
+  return { baseAmountOut: data, previewCloseLongStatus: status };
+}
