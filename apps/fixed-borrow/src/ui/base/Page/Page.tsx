3f17a5dd68e12c8d1a11f3d1799ecde836a7f442
Danny Delott <dannydelott@gmail.com>
2023-04-13 10:06:19 -0700

    remove extra tw class

--- apps/fixed-borrow/src/ui/base/Page/Page.tsx
+++ apps/fixed-borrow/src/ui/base/Page/Page.tsx
@@ -9,7 +9,7 @@ export default function Page({ header, content }: PageProps): ReactElement {
   return (
     <div className="col-span-2 grid">
       {header}
-      <div className="flex min-h-[75vh] w-full flex-col items-center justify-center py-[10vh] px-[4vw]">
+      <div className="flex min-h-[75vh] w-full items-center justify-center py-[10vh] px-[4vw]">
         <div className="max-w-4xl">{content}</div>
       </div>
     </div>
