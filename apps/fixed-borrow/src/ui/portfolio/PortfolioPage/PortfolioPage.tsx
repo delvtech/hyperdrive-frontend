8785a666f43043f8ff1beb3175f11222eaae645c
Danny Delott <dannydelott@gmail.com>
2023-04-17 08:16:51 -0700

    Use constant

--- apps/fixed-borrow/src/ui/portfolio/PortfolioPage/PortfolioPage.tsx
+++ apps/fixed-borrow/src/ui/portfolio/PortfolioPage/PortfolioPage.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from "react";
 import { formatBigInt } from "src/base/bigint/formatBigInt";
+import { NETWORK_BASE_TOKEN_DECIMALS } from "src/pools/networkBaseToken";
 import { StatWell } from "src/ui/base/StatWell/StatWell";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
 import { useUserAccountData } from "src/ui/loans/hooks/useUserAccountData";
@@ -12,8 +13,11 @@ export function PortfolioPage(): ReactElement {
 
   let loanToValueStat = "";
   if (userAccountData) {
-    // TODO: Is this formatting correct?
-    loanToValueStat = `${formatBalance(Number(userAccountData.ltv), 4)}%`;
+    loanToValueStat = `${formatBalance(
+      // TODO: Is this calc correct?
+      Number(userAccountData.ltv) * 0.01,
+      2,
+    )}%`;
   }
 
   return (
@@ -25,21 +29,30 @@ export function PortfolioPage(): ReactElement {
             <StatWell
               label="Collateral"
               stat={`$${formatBalance(
-                formatBigInt(userAccountData?.totalCollateralBase || 0n, 8),
+                formatBigInt(
+                  userAccountData?.totalCollateralBase || 0n,
+                  NETWORK_BASE_TOKEN_DECIMALS,
+                ),
                 2,
               )}`}
             />
             <StatWell
               label="Total debt"
               stat={`$${formatBalance(
-                formatBigInt(userAccountData?.totalDebtBase || 0n, 8),
+                formatBigInt(
+                  userAccountData?.totalDebtBase || 0n,
+                  NETWORK_BASE_TOKEN_DECIMALS,
+                ),
                 2,
               )}`}
             />
             <StatWell
               label="Borrow limit"
               stat={`$${formatBalance(
-                formatBigInt(userAccountData?.availableBorrowBase || 0n, 8),
+                formatBigInt(
+                  userAccountData?.availableBorrowBase || 0n,
+                  NETWORK_BASE_TOKEN_DECIMALS,
+                ),
                 2,
               )}`}
             />
