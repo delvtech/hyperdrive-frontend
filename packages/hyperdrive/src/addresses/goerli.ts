f4ea3e7f526d97e9bb6d7d97c1dfad19ff21204f
Cash DeLeon <cashd@users.noreply.github.com>
2023-04-25 14:23:04 -0500

    [Hyperdrive] HyperdriveMath ABI and goerli deploy

--- packages/hyperdrive/src/addresses/goerli.ts
+++ packages/hyperdrive/src/addresses/goerli.ts
@@ -4,4 +4,5 @@ export const HyperdriveGoerliAddresses = {
   // fixed borrow action is deployed with a reference to the makerDsrHyperdrive
   // https://github.com/element-fi/hyperdrive/blob/main/contracts/src/actions/AaveFixedBorrow.sol
   aaveFixedBorrowAction: "0xdD029B122C15a313B8599348a2c472fe24541ECB",
+  hyperdriveMath: "0xB2144A34fD0495A75813C76B4380511FB753e44e",
 } as const;
