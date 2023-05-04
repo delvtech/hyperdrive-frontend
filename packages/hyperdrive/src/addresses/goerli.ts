b16b8fe6f0749a94bd9d92ad9ae5cd6031931d48
Ryan Goree <goree.ryan@gmail.com>
2023-05-03 22:27:38 -0500

    Change GH org name

--- packages/hyperdrive/src/addresses/goerli.ts
+++ packages/hyperdrive/src/addresses/goerli.ts
@@ -1,8 +1,8 @@
-// See: https://github.com/delv-tech/hyperdrive/blob/main/addresses.json
+// See: https://github.com/delvtech/hyperdrive/blob/main/addresses.json
 export const HyperdriveGoerliAddresses = {
   makerDsrHyperdrive: "0xB311B825171AF5A60d69aAD590B857B1E5ed23a2",
   // fixed borrow action is deployed with a reference to the makerDsrHyperdrive
-  // https://github.com/delv-tech/hyperdrive/blob/main/contracts/src/actions/AaveFixedBorrow.sol
+  // https://github.com/delvtech/hyperdrive/blob/main/contracts/src/actions/AaveFixedBorrow.sol
   aaveFixedBorrowAction: "0xdD029B122C15a313B8599348a2c472fe24541ECB",
   hyperdriveMath: "0xB2144A34fD0495A75813C76B4380511FB753e44e",
 } as const;
