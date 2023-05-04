23d11c3cfb39bb82c76b4fed1ed6df6cf0b276d1
Cash DeLeon <cashd@users.noreply.github.com>
2023-05-04 12:56:22 -0500

    PositionsTable component and ClosedOrdersTable component

---
+++ apps/hyperdrive-trading/src/ui/orders/components/PositionsTable.tsx
@@ -0,0 +1,41 @@
+import { ReactElement, useState } from "react";
+import { HyperdriveMarket } from "src/config/HyperdriveConfig";
+import { OpenOrdersTable } from "src/ui/orders/components/OpenOrdersTable";
+import { match } from "ts-pattern";
+import { ClosedOrdersTable } from "./ClosedOrdersTable";
+
+interface PositionsTableProps {
+  market: HyperdriveMarket;
+}
+
+type SelectedTable = "Open" | "Closed" | "Recent";
+
+export function PositionsTable({ market }: PositionsTableProps): ReactElement {
+  const [selectedTable, setSelectedTable] = useState<SelectedTable>("Open");
+
+  return (
+    <div className="flex flex-col overflow-hidden py-4">
+      <div className="flex w-full flex-wrap gap-2 py-2 px-8">
+        <button className="btn" onClick={() => setSelectedTable("Open")}>
+          Open
+        </button>
+        <button className="btn" onClick={() => setSelectedTable("Closed")}>
+          Closed
+        </button>
+        <button className="btn" onClick={() => setSelectedTable("Recent")}>
+          Recent Trades
+        </button>
+      </div>
+
+      <div className="flex-col gap-y-4 px-4 pt-4">
+        <div className="overflow-scroll">
+          {match(selectedTable)
+            .with("Open", () => <OpenOrdersTable market={market} />)
+            .with("Closed", () => <ClosedOrdersTable market={market} />)
+            .with("Recent", () => <OpenOrdersTable market={market} />)
+            .exhaustive()}
+        </div>
+      </div>
+    </div>
+  );
+}
