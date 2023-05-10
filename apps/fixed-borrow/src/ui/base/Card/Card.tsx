41baa9554065347569c73940b66e2e827cdedacd
Danny Delott <dannydelott@gmail.com>
2023-05-09 18:01:07 -0700

    Add InfoDisclosure component, stub out FAQ section

--- apps/fixed-borrow/src/ui/base/Card/Card.tsx
+++ apps/fixed-borrow/src/ui/base/Card/Card.tsx
@@ -4,8 +4,6 @@ interface CardProps {}
 
 export function Card({ children }: PropsWithChildren<CardProps>): ReactElement {
   return (
-    <div className="rounded-lg bg-dawn py-10 px-12 text-white shadow-[0_4px_14px_rgb(4,9,26)]">
-      {children}
-    </div>
+    <div className="rounded-lg bg-dawn py-10 px-12 text-white">{children}</div>
   );
 }
--- apps/fixed-borrow/src/ui/fixedborrow/FixedBorrowPage/FixedBorrowPage.tsx
+++ apps/fixed-borrow/src/ui/fixedborrow/FixedBorrowPage/FixedBorrowPage.tsx
@@ -1,13 +1,37 @@
 import { ReactElement } from "react";
+import { InfoDisclosure } from "src/ui/base/InfoDisclosure/InfoDisclosure";
 import { CoverageTable } from "src/ui/fixedborrow/CoverageTable/CoverageTable";
 
 export function FixedBorrowPage(): ReactElement {
   return (
     <div className="flex flex-col">
       <div className="space-y-2">
-        <h4 className="font-bold text-white">Spark Borrows</h4>
-        <div className="max-w-4xl">
-          <CoverageTable />
+        <div className="flex max-w-4xl flex-col gap-28">
+          <div>
+            <h4 className="mb-4 font-bold text-white">Spark Borrows</h4>
+            <CoverageTable />
+          </div>
+          <div>
+            <h4 className="mb-4 font-bold text-white">FAQ</h4>
+            <InfoDisclosure heading={"How does fixed rate coverage work?"}>
+              By opening a Hyperdrive short against the variable rate, you
+              effectively lock in a fixed rate for however long the short is
+              good for.
+            </InfoDisclosure>
+            <InfoDisclosure
+              heading={"How is the current available fixed rate calculated?"}
+            >
+              The fixed rate is calculated by first getting the upfront cost of
+              the Hyperdrive short and converting it to an APY percentage.
+            </InfoDisclosure>
+            <InfoDisclosure
+              heading={"What happens when my fixed rate coverage expires?"}
+            >
+              When the coverage expires, your debt will automatically switch
+              back to the variable rate. You can close expired coverage and
+              re-open new coverage to lock in a fixed rate again.
+            </InfoDisclosure>
+          </div>
         </div>
       </div>
     </div>
