23a5752eb1a5f2a3833fa4467887d8843e3936ad
Danny Delott <dannydelott@gmail.com>
2023-05-11 16:03:16 -0700

    Add disabled prop

--- apps/fixed-borrow/src/ui/base/Well/Well.tsx
+++ apps/fixed-borrow/src/ui/base/Well/Well.tsx
@@ -3,22 +3,27 @@ import { PropsWithChildren, ReactElement } from "react";
 
 interface WellProps {
   interactive?: boolean;
+  disabled?: boolean;
   onClick?: () => void;
 }
 
 export function Well({
+  disabled,
   interactive,
   children,
   onClick,
 }: PropsWithChildren<WellProps>): ReactElement {
   const className = classNames(
-    "rounded-lg bg-midnight p-4 ring-1 ring-dawnAccent",
-    { "hover:cursor-pointer hover:bg-inputBg": interactive || onClick },
+    "rounded-lg bg-midnight p-4 ring-1 ring-dawnAccent h-full",
+    {
+      "hover:cursor-pointer hover:bg-inputBg":
+        !disabled && (interactive || onClick),
+    },
   );
 
   if (onClick) {
     return (
-      <button className={className} onClick={onClick}>
+      <button className={className} onClick={onClick} disabled={disabled}>
         {children}
       </button>
     );
