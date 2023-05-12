59e0530b3717b69e43349da0ddcb8b51db71af12
Danny Delott <dannydelott@gmail.com>
2023-05-11 17:54:18 -0700

    Fix broken production deploy of Fixed Borrow (#143)

--- apps/fixed-borrow/vite.config.ts
+++ apps/fixed-borrow/vite.config.ts
@@ -5,10 +5,4 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  build: {
-    // Update the entry point to match the new location of the main.tsx file
-    rollupOptions: {
-      input: "/src/ui/main.tsx",
-    },
-  },
 });
