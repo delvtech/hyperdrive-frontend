23e18b4afa45b6dddb14d7831e9054190de24e7d
Danny Delott <dannydelott@gmail.com>
2023-05-18 10:49:43 -0700

    Make Supply quickstart button open a modal w/ options for approve + supply

---
+++ apps/fixed-borrow/src/ui/quickstart/SupplyQuickStartButton/SupplyModal.tsx
@@ -0,0 +1,126 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import { SparkGoerliAddresses } from "@hyperdrive/spark";
+import { Fragment, ReactElement } from "react";
+import { formatBigInt } from "src/base/bigint/formatBigInt";
+import { parseBigInt } from "src/base/bigint/parseBigInt";
+import { Button } from "src/ui/base/Button/Button";
+import { InfoTooltip } from "src/ui/base/Tooltip/InfoTooltip";
+import { Well } from "src/ui/base/Well/Well";
+import { ApproveAllowanceButton } from "src/ui/token/ApproveAllowanceButton";
+import { useSpenderAllowance } from "src/ui/token/useSpenderAllowance";
+import { useTokenApprove } from "src/ui/token/useTokenApprove";
+import { Address, useBalance, useToken } from "wagmi";
+
+interface SupplyModalProps {
+  tokenAddress: Address;
+  amount: bigint;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export function SupplyModal({
+  tokenAddress,
+  amount,
+  isOpen,
+  onClose,
+}: SupplyModalProps): ReactElement {
+  const { data: token } = useToken({ address: tokenAddress });
+  const { data: balance } = useBalance({ address: tokenAddress });
+  const { allowance } = useSpenderAllowance({
+    tokenAddress: tokenAddress,
+    spender: SparkGoerliAddresses.pool,
+  });
+
+  const { approve } = useTokenApprove({
+    tokenAddress,
+    amount: balance?.value,
+    spender: SparkGoerliAddresses.pool,
+  });
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        onClose={onClose}
+      >
+        <div className="min-h-screen px-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black opacity-60" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="inline-block w-full max-w-md overflow-hidden rounded-2xl bg-inputBg px-8 py-7 text-left align-middle shadow-xl ring-1 ring-dawn transition-all">
+              <Dialog.Title
+                as="h4"
+                className="font-semi mb-4 font-bold text-lightText"
+              >
+                Supply Collateral to Spark
+              </Dialog.Title>
+              <div className="space-y-6">
+                <p className="text-h6 text-lightText">
+                  You must supply collateral in order to borrow DAI.
+                </p>
+                <Well>
+                  <div className="flex flex-col gap-4 bg-midnight ">
+                    <div className={"flex justify-between font-bold"}>
+                      <span className=" text-h6 font-semibold uppercase text-secondaryText">
+                        Supply
+                      </span>
+                      <span className=" text-h6 text-lightText">
+                        {formatBigInt(amount, token?.decimals)} {token?.symbol}
+                      </span>
+                    </div>
+                  </div>
+                </Well>
+                <div className="flex w-full justify-center gap-4 pt-2">
+                  <ApproveAllowanceButton
+                    tokenAddress={tokenAddress}
+                    amount={amount}
+                    spender={SparkGoerliAddresses.pool}
+                  />
+                  <Button
+                    size="md"
+                    variant="sun"
+                    onClick={onClose}
+                    block
+                    disabled={allowance === undefined || allowance < amount}
+                  >
+                    Supply
+                  </Button>
+                  <Button size="md" variant="dark" onClick={onClose} block>
+                    Cancel
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
--- apps/fixed-borrow/src/ui/quickstart/SupplyQuickStartButton/SupplyQuickStartButton.tsx
+++ apps/fixed-borrow/src/ui/quickstart/SupplyQuickStartButton/SupplyQuickStartButton.tsx
@@ -1,9 +1,7 @@
-import { SparkGoerliAddresses } from "@hyperdrive/spark";
-import { ReactElement } from "react";
-import { StatWell } from "src/ui/base/StatWell/StatWell";
+import { ReactElement, useState } from "react";
 import { Well } from "src/ui/base/Well/Well";
 import { useSupplyCollateral } from "src/ui/loans/hooks/useSupplyCollateral";
-import { ApproveAllowanceButton } from "src/ui/token/ApproveAllowanceButton";
+import { SupplyModal } from "src/ui/quickstart/SupplyQuickStartButton/SupplyModal";
 import { useAccount, Address, useBalance } from "wagmi";
 
 export function SupplyQuickStartButton({
@@ -18,17 +16,25 @@ export function SupplyQuickStartButton({
   stat: string;
 }): ReactElement {
   const { address: account } = useAccount();
-  const { data: balanceOf } = useBalance({
-    token: tokenAddress,
-    address: account,
-  });
   const { supply } = useSupplyCollateral(tokenAddress, amount, account);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <Well block onClick={() => supply?.()}>
-      <div className="flex items-center justify-between">
-        <span className="font-bold uppercase text-secondaryText">{label}</span>
-        <span className="text-h6 text-lightText">{stat}</span>
-      </div>
-    </Well>
+    <>
+      <Well block onClick={() => setIsModalOpen(true)}>
+        <div className="flex items-center justify-between">
+          <span className="font-bold uppercase text-secondaryText">
+            {label}
+          </span>
+          <span className="text-h6 text-lightText">{stat}</span>
+        </div>
+      </Well>
+      <SupplyModal
+        tokenAddress={tokenAddress}
+        amount={amount}
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
+    </>
   );
 }
