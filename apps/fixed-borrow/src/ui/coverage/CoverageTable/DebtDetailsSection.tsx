a3b33f2b532f1c62e4aef77e458071546830191a
Danny Delott <dannydelott@gmail.com>
2023-05-22 14:39:44 -0700

    Wire up ui to useShorts

--- apps/fixed-borrow/src/ui/coverage/CoverageTable/DebtDetailsSection.tsx
+++ apps/fixed-borrow/src/ui/coverage/CoverageTable/DebtDetailsSection.tsx
@@ -1,9 +1,5 @@
-import { Popover } from "@headlessui/react";
-import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
-import { XCircleIcon } from "@heroicons/react/24/outline";
 import { ReactElement, useState } from "react";
 import { Tabs } from "src/ui/base/Tabs/Tabs";
-import { ClosePositionModal } from "./ClosePositionModal";
 import { ExistingCoverageTab } from "src/ui/coverage/CoverageTable/ExistingCoverageTab";
 import { UncoveredDebtTab } from "src/ui/coverage/CoverageTable/UncoveredDebtTab";
 
@@ -29,38 +25,5 @@ export function DebtDetailsSection(): ReactElement {
 
       {activeTab === 0 ? <ExistingCoverageTab /> : <UncoveredDebtTab />}
     </div>
-  );
-}
-
-export function ShortActionsMenu(): ReactElement {
-  const [isOpen, setIsOpen] = useState(false);
-
-  function handleOpenModal() {
-    setIsOpen(true);
-  }
-
-  function handleCloseModal() {
-    setIsOpen(false);
-  }
-
-  return (
-    <>
-      <Popover>
-        <Popover.Button className="rounded-md hover:bg-darkButton-hover">
-          <EllipsisVerticalIcon className="h-5 w-5 hover:text-white" />
-        </Popover.Button>
-
-        <Popover.Panel className="bg-background absolute -right-24 flex w-52 flex-col justify-start gap-1 rounded-lg bg-inputBg p-1 shadow-2xl shadow-midnight ring-1 ring-inset ring-dawn">
-          <button
-            onClick={handleOpenModal}
-            className="flex w-full items-center gap-3 rounded-lg p-2 text-left hover:bg-darkButton hover:text-white"
-          >
-            <XCircleIcon className="h-5 w-5" /> Close coverage
-          </button>
-        </Popover.Panel>
-      </Popover>
-
-      <ClosePositionModal isOpen={isOpen} onClose={handleCloseModal} />
-    </>
   );
 }
