c4a4fe0802d105f7f709cca3c15cd71c8f52943e
Danny Delott <dannydelott@gmail.com>
2023-05-22 16:25:19 -0700

    Cleanup formatted, add tooltips

--- apps/fixed-borrow/src/ui/coverage/CoverageTable/UncoveredDebtTab.tsx
+++ apps/fixed-borrow/src/ui/coverage/CoverageTable/UncoveredDebtTab.tsx
@@ -9,36 +9,56 @@ export function UncoveredDebtTab(): ReactElement {
       <div className="space-y-6">
         <Well>
           <div className="flex flex-col gap-4 bg-midnight ">
-            <div className="flex justify-between font-bold">
+            <div className="flex justify-between">
               <span className="text-h6 font-semibold uppercase text-secondaryText">
-                Coverage Amount
+                Current Fixed Rate
               </span>
-              <span className="text-h6 text-lightText">{"2,300.23 DAI"}</span>
+              <span className="text-h6 text-lightText">{"1.5% APR"}</span>
             </div>
-            <div className="flex justify-between font-bold">
-              <span className="text-h6 font-semibold uppercase text-secondaryText">
-                Coverage expires on
-              </span>
+            <div className="flex justify-between">
+              <div className="flex items-center gap-1 ">
+                <span className="text-h6 font-semibold uppercase text-secondaryText">
+                  Rate expires on
+                </span>
+                <InformationCircleIcon
+                  title="After expiration, this position will no longer provide any fixed rate coverage and will need to be redeemed. You can redeem coverage and re-open new coverage at any time."
+                  width={18}
+                  className="stroke-secondaryText"
+                />
+              </div>
               <span className=" text-h6 text-lightText">
                 {"December 31, 2023"}
               </span>
             </div>
-            <div className="flex justify-between font-bold">
-              <span className="text-h6 font-semibold uppercase text-secondaryText">
-                Fixed Rate
+            <div className="flex justify-between">
+              <div className="flex items-center gap-1 ">
+                <span className="text-h6 font-bold uppercase text-secondaryText">
+                  Total uncovered debt
+                </span>
+                <InformationCircleIcon
+                  title="Your total debt balance minus any existing coverage."
+                  width={18}
+                  className="stroke-secondaryText"
+                />
+              </div>
+              <span className="text-h6 font-bold text-lightText">
+                {"2,300.23 DAI"}
               </span>
-              <span className="text-h6 text-lightText">{"1.5% APY"}</span>
             </div>
             <div className="flex justify-between font-bold">
-              <div className="flex items-center gap-1 text-h6 font-bold uppercase text-secondaryText">
-                <span>Cost of Coverage</span>{" "}
+              <div className="flex items-center gap-1 ">
+                <span className="text-h6 font-bold uppercase text-secondaryText">
+                  Cost of Coverage
+                </span>{" "}
                 <InformationCircleIcon
                   title="The value of the Hyperdrive short, which acts as a guarantee for the fixed rate coverage, will be transfered to you upon closing coverage."
                   width={18}
-                  className="stroke-current"
+                  className="stroke-secondaryText"
                 />
               </div>
-              <span className="text-h6 font-bold text-pinkSlip">32.14 DAI</span>
+              <span className="text-h6 font-bold text-lightText ">
+                32.14 DAI
+              </span>
             </div>
           </div>
         </Well>
