3bd0c8494f86826cea22a042c168923a59898540
Danny Delott <dannydelott@gmail.com>
2023-05-22 21:33:12 -0700

    Add opacity

--- apps/fixed-borrow/src/ui/base/Banner/Banner.tsx
+++ apps/fixed-borrow/src/ui/base/Banner/Banner.tsx
@@ -8,7 +8,7 @@ interface BannerProps {
 export function Banner({ children }: BannerProps): ReactElement {
   return (
     <div className="flex w-full items-center justify-center rounded-md bg-gradient-to-r from-pinkSlipBurst-start via-pinkSlip to-pinkSlipBurst-end p-[1px]">
-      <div className="flex w-full items-center justify-center rounded-md bg-midnight py-3 px-4">
+      <div className="flex w-full items-center justify-center rounded-md bg-midnight/95 py-3 px-4">
         <span className="bg-gradient-to-r from-pinkSlipBurst-start to-pinkSlipBurst-end bg-clip-text text-transparent">
           {children}
         </span>
