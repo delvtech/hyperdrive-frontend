a9ff3fe7f36f27f5a4a407ea0f788eeb19ac94f4
Danny Delott <dannydelott@gmail.com>
2023-05-25 15:24:21 -0700

    Remove stubbed markets

--- apps/hyperdrive-trading/src/config/chains/goerli.ts
+++ apps/hyperdrive-trading/src/config/chains/goerli.ts
@@ -5,21 +5,7 @@ const MakerProtocol: Protocol = {
   name: "Maker",
   iconUrl: "https://cryptologos.cc/logos/maker-mkr-logo.png?v=024",
 };
-
-const MockProtocol: Protocol = {
-  name: "Mock",
-};
-
-const YearnProtocol: Protocol = {
-  name: "Yearn",
-  iconUrl: "https://cryptologos.cc/logos/yearn-finance-yfi-logo.png?v=024",
-};
-
-const supportedProtocols: readonly Protocol[] = [
-  MakerProtocol,
-  MockProtocol,
-  YearnProtocol,
-] as const;
+const supportedProtocols: readonly Protocol[] = [MakerProtocol] as const;
 
 const daiBaseToken: Token = {
   name: "DAI",
@@ -28,14 +14,6 @@ const daiBaseToken: Token = {
   decimals: 18,
   iconUrl:
     "https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png?v=024",
-};
-
-const usdcBaseToken: Token = {
-  name: "USDC",
-  symbol: "USDC",
-  address: "0x07865c6e87b9f70255377e024ace6630c1eaa37f",
-  decimals: 6,
-  iconUrl: "https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=024",
 };
 
 export const goerliHyperdriveConfig: HyperdriveConfig<
@@ -48,28 +26,6 @@ export const goerliHyperdriveConfig: HyperdriveConfig<
       address: "0x8C9Ccd26fFB734658171EE2B0C6d087e72752c20",
       baseToken: daiBaseToken,
       protocol: MakerProtocol,
-      termLength: 12,
-    },
-    // Stubbed below
-    {
-      name: "DAI Vault",
-      address: "0x7C9Ccd26fFB734658171EE2B0C6d087e72752c20",
-      baseToken: usdcBaseToken,
-      protocol: YearnProtocol,
-      termLength: 3,
-    },
-    {
-      name: "DAI Vault",
-      address: "0x9C9Ccd26fFB734658171EE2B0C6d087e72752c20",
-      baseToken: usdcBaseToken,
-      protocol: YearnProtocol,
-      termLength: 6,
-    },
-    {
-      name: "DAI Vault",
-      address: "0x4C9Ccd26fFB734658171EE2B0C6d087e72752c20",
-      baseToken: usdcBaseToken,
-      protocol: YearnProtocol,
       termLength: 12,
     },
   ],
