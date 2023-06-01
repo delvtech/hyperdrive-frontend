87aaa015d322e6858284ee7bf41ceacd74bd451a
Danny Delott <dannydelott@gmail.com>
2023-06-01 10:32:52 -0700

    Put import back after eslint autofix removed it

--- apps/hyperdrive-trading/src/wallet/wagmiClient.ts
+++ apps/hyperdrive-trading/src/wallet/wagmiClient.ts
@@ -1,11 +1,14 @@
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 // Something very strange is happening with the types, where if you don't import
 // Chain from wagmi then the return type of createConfig cannot be inferred.
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { configureChains, createConfig } from "wagmi";
+import { Chain, configureChains, createConfig } from "wagmi";
 import { goerli } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 const ALCHEMY_GOERLI_RPC_KEY = import.meta.env.VITE_ALCHEMY_GOERLI_RPC_KEY;
+
+// Hack: An unused reference so that eslint doesn't automatically fixup the
+// Chain import.
+type UnusedChain = Chain;
 
 if (!ALCHEMY_GOERLI_RPC_KEY) {
   throw new Error("Provide an ALCHEMY_GOERLI_RPC_KEY variable in .env");
