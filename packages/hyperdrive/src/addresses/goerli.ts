6f7ba58aece3b49ddb7c432f104e722fae1e8d42
Danny Delott <dannydelott@gmail.com>
2023-06-01 09:54:56 -0700

    Update hyperdrive math address

--- packages/hyperdrive/src/addresses/goerli.ts
+++ packages/hyperdrive/src/addresses/goerli.ts
@@ -4,5 +4,5 @@ export const HyperdriveGoerliAddresses = {
   // fixed borrow action is deployed with a reference to the makerDsrHyperdrive
   // https://github.com/delvtech/hyperdrive/blob/main/contracts/src/actions/AaveFixedBorrow.sol
   aaveFixedBorrowAction: "0xdD029B122C15a313B8599348a2c472fe24541ECB",
-  hyperdriveMath: "0xB2144A34fD0495A75813C76B4380511FB753e44e",
+  hyperdriveMath: "0x2D5CC1a6de5863A3E24EB48a758FE20d9253b3B2",
 } as const;
