b3fe651f46d60c7b9c3380a4e6041930034d3bb5
Danny Delott <dannydelott@gmail.com>
2023-06-01 18:36:15 -0700

    Add TODO

--- apps/hyperdrive-trading/src/ui/appconfig/useAppConfig.ts
+++ apps/hyperdrive-trading/src/ui/appconfig/useAppConfig.ts
@@ -1,27 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { goerliAppConfig } from "src/appconfig/chains/goerli";
-import {
-  SupportedChainId,
-  supportedChainIds,
-} from "src/appconfig/supportedChains";
+import { SupportedChainId } from "src/appconfig/supportedChains";
 import { AppConfig } from "src/appconfig/types";
 import { useChainId } from "wagmi";
 
 const LOCALHOST_ADDRESSES_URL = import.meta.env.VITE_LOCALHOST_ADDRESSES_URL;
-// const LOCALHOST_ADDRESSES_URL = '/localAddresses'
 
 export function useAppConfig(): {
   appConfig: AppConfig | undefined;
   appConfigStatus: "idle" | "error" | "loading" | "success";
 } {
   const chainId = useChainId();
-  if (!supportedChainIds.includes(chainId as SupportedChainId)) {
-    // This should never happen with a properly configured wagmi config
-    throw new Error(
-      `Cannot fetch hyperdrive config with chain id: ${chainId}.`,
-    );
-  }
-
   const { data: appConfig, status: appConfigStatus } = useQuery({
     queryKey: ["app-config", { chainId }],
     queryFn: async () => {
@@ -29,16 +18,19 @@ export function useAppConfig(): {
         case 5:
           return goerliAppConfig;
         case 31337:
-          return getLocalAddresses();
+          return getLocalAppConfig();
       }
     },
+    // The config never changes, so it can be cached forever
+    staleTime: Infinity,
   });
 
   return { appConfig, appConfigStatus };
 }
 
-async function getLocalAddresses(): Promise<AppConfig> {
+// TODO: Move this to src/appconfig/chains/local.ts
+async function getLocalAppConfig(): Promise<AppConfig> {
   const addresses = await fetch(LOCALHOST_ADDRESSES_URL);
   console.log(addresses);
-  return null as any;
+  return { chainId: 31337, markets: [] } as AppConfig;
 }
