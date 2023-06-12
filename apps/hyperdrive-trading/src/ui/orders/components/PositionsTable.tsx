83bce3d839f0094b65376894128bc87f51ab1614
Danny Delott <dannydelott@gmail.com>
2023-06-12 09:33:30 -0700

    Remove recent trades tab

--- apps/hyperdrive-trading/src/ui/orders/components/PositionsTable.tsx
+++ apps/hyperdrive-trading/src/ui/orders/components/PositionsTable.tsx
@@ -8,7 +8,7 @@ interface PositionsTableProps {
   hyperdrive: Hyperdrive;
 }
 
-type SelectedTable = "Open" | "Closed" | "Recent";
+type SelectedTable = "Open" | "Closed";
 
 export function PositionsTable({
   hyperdrive,
@@ -24,9 +24,6 @@ export function PositionsTable({
         <button className="btn" onClick={() => setSelectedTable("Closed")}>
           Closed
         </button>
-        <button className="btn" onClick={() => setSelectedTable("Recent")}>
-          Recent Trades
-        </button>
       </div>
 
       <div className="flex-col gap-y-4 px-4 pt-4">
@@ -34,7 +31,6 @@ export function PositionsTable({
           {match(selectedTable)
             .with("Open", () => <OpenOrdersTable hyperdrive={hyperdrive} />)
             .with("Closed", () => <ClosedOrdersTable market={hyperdrive} />)
-            .with("Recent", () => <></>)
             .exhaustive()}
         </div>
       </div>
