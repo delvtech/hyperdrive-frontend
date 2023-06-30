a38829f7a81e3a385db4304fbd9f8df333cb35d3
Danny Delott <dannydelott@gmail.com>
2023-06-30 09:31:39 -0700

    Trim whitespace

--- apps/hyperdrive-trading/src/pages/Markets.tsx
+++ apps/hyperdrive-trading/src/pages/Markets.tsx
@@ -10,14 +10,14 @@ export function Markets(): ReactElement {
           <h2 className="font-lato text-h3 font-bold leading-h3 sm:text-h2 sm:leading-h2">
             Markets
           </h2>
-          <h5 className=" max-w-2xl text-h6 font-medium leading-h6 text-neutral-content sm:text-h5 sm:leading-h5">
+          <h5 className="max-w-2xl text-h6 font-medium leading-h6 text-neutral-content sm:text-h5 sm:leading-h5">
             Advanced yield markets featuring no preset expiration dates, no
             fragmented liquidity, and no LP rollovers — aka everlasting
             liquidity.
           </h5>
         </div>
 
-        <button className=" flex items-center gap-x-1 rounded bg-gradient-to-r from-primary via-[#afd5ff] to-success px-4 py-2 font-bold text-base-100">
+        <button className="flex items-center gap-x-1 rounded bg-gradient-to-r from-primary via-[#afd5ff] to-success px-4 py-2 font-bold text-base-100">
           <ArrowTopRightOnSquareIcon className="w-4" />
           <p>Learn more</p>
         </button>
