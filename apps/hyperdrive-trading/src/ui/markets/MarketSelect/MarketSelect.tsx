43dc2c5bcf55bbbcc24f25570c2f5f9501e4b85e
Danny Delott <dannydelott@gmail.com>
2023-07-03 21:42:19 -0700

    Fix invalid dom nesting

--- apps/hyperdrive-trading/src/ui/markets/MarketSelect/MarketSelect.tsx
+++ apps/hyperdrive-trading/src/ui/markets/MarketSelect/MarketSelect.tsx
@@ -16,7 +16,7 @@ export function MarketSelect({
       <div className="flex justify-between">
         <label
           htmlFor={MARKETS_MODAL_KEY}
-          className="glass daisy-btn-xs daisy-btn w-full justify-between"
+          className="glass daisy-btn-xs daisy-btn w-full"
         >
           <span className="inline-flex items-center gap-2 text-sm">
             Select market <ChevronDownIcon className="h-5" />
