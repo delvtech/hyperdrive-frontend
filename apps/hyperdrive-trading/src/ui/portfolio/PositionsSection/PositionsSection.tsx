e6e20d1700d17644a0bedd133124aaa2b35cf6f0
Danny Delott <dannydelott@gmail.com>
2023-07-06 09:59:09 -0700

    Wire up open/closed tabs

--- apps/hyperdrive-trading/src/ui/portfolio/PositionsSection/PositionsSection.tsx
+++ apps/hyperdrive-trading/src/ui/portfolio/PositionsSection/PositionsSection.tsx
@@ -2,16 +2,28 @@ import assertNever from "assert-never";
 import classNames from "classnames";
 import { ReactElement, useState } from "react";
 import { Hyperdrive } from "src/appconfig/types";
+import { Well } from "src/ui/base/components/Well/Well";
 import { ClosedOrdersTable } from "src/ui/portfolio/ClosedOrdersTable/ClosedOrdersTable";
+import {
+  OpenOrClosedTab,
+  OpenOrClosedTabs,
+} from "src/ui/portfolio/OpenOrClosedTabs/OpenOrClosedTabs";
 import { OpenOrdersTable } from "src/ui/portfolio/OpenOrdersTable/OpenOrdersTable";
 
-type TabId = "Open" | "Closed";
+type PositionTab = "Longs" | "Shorts" | "LP";
+
 export function PositionsSection({
   hyperdrive,
 }: {
   hyperdrive: Hyperdrive;
 }): ReactElement {
-  const [activeTab, setActiveTab] = useState<TabId>("Open");
+  // TODO: Move this state into router
+  const [activePositionTab, setActivePositionTab] =
+    useState<PositionTab>("Longs");
+  // TODO: Move this state into router
+  const [activeOpenOrClosedTab, setActiveOpenOrClosedTab] =
+    useState<OpenOrClosedTab>("Open");
+
   return (
     <div>
       <div className="mb-2 flex w-full items-center justify-between border-b border-neutral-content/30">
@@ -20,34 +32,54 @@ export function PositionsSection({
         </span>
         <div className="daisy-tabs">
           <a
-            onClick={() => setActiveTab("Open")}
+            onClick={() => setActivePositionTab("Longs")}
             className={classNames("daisy-tab-lifted daisy-tab daisy-tab-lg", {
-              "daisy-tab-active !bg-neutral-content/30": activeTab === "Open",
+              "daisy-tab-active !bg-secondary/30":
+                activePositionTab === "Longs",
             })}
           >
-            Open
+            Longs
           </a>
           <a
-            onClick={() => setActiveTab("Closed")}
+            onClick={() => setActivePositionTab("Shorts")}
             className={classNames("daisy-tab-lifted daisy-tab daisy-tab-lg", {
               "daisy-tab-active  !bg-neutral-content/30":
-                activeTab === "Closed",
+                activePositionTab === "Shorts",
             })}
           >
-            Closed
+            Shorts
+          </a>
+          <a
+            onClick={() => setActivePositionTab("LP")}
+            className={classNames("daisy-tab-lifted daisy-tab daisy-tab-lg", {
+              "daisy-tab-active  !bg-neutral-content/30":
+                activePositionTab === "LP",
+            })}
+          >
+            LP
           </a>
         </div>
       </div>
 
       <div className="flex flex-col">
+        <OpenOrClosedTabs
+          setActiveOpenOrClosedTab={setActiveOpenOrClosedTab}
+          activeOpenOrClosedTab={activeOpenOrClosedTab}
+        />
+
         {(() => {
-          switch (activeTab) {
-            case "Open":
+          switch (activePositionTab) {
+            case "Longs":
               return <OpenOrdersTable hyperdrive={hyperdrive} />;
-            case "Closed":
+            case "Shorts":
               return <ClosedOrdersTable hyperdrive={hyperdrive} />;
+            case "LP":
+              {
+                /* TODO: Wire this up */
+              }
+              return <Well>Under construction</Well>;
             default:
-              assertNever(activeTab);
+              assertNever(activePositionTab);
           }
         })()}
       </div>
