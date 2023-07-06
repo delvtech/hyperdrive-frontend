40c157dc9e721e7101e1c4e0ba639e30fd9d07aa
Danny Delott <dannydelott@gmail.com>
2023-07-06 14:12:58 -0700

    Simplify styles

--- apps/hyperdrive-trading/src/ui/base/components/Well/Well.tsx
+++ apps/hyperdrive-trading/src/ui/base/components/Well/Well.tsx
@@ -20,22 +20,11 @@ export function Well({
   const isInteractive = !disabled && (interactive || onClick);
   const className = classNames("rounded-lg p-4", {
     "w-full": block,
-    "hover:cursor-pointer hover:ring-opacity-50": isInteractive,
-
-    "bg-base-300/50": !variant,
-    "hover:ring-neutral-content hover:ring-1": isInteractive && !variant,
-
+    "hover:cursor-pointer hover:glass": isInteractive,
+    "bg-base-300/50 ring-neutral-content/20 ring-1": !variant,
     "bg-primary/5 ring-primary/20 ring-1": variant === "primary",
-    "hover:bg-primary/10 hover:ring-primary/50":
-      isInteractive && variant === "primary",
-
     "bg-secondary/5 ring-secondary/20 ring-1": variant === "secondary",
-    "hover:bg-secondary/10 hover:ring-secondary/50":
-      isInteractive && variant === "secondary",
-
     "bg-accent/5 ring-accent/20 ring-1": variant === "accent",
-    "hover:bg-accent/10 hover:ring-accent/50":
-      isInteractive && variant === "accent",
   });
 
   if (onClick || interactive) {
