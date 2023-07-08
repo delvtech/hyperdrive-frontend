ee0a1f09cb27b7807bf03ce2b5920f035ba57ff0
Danny Delott <dannydelott@gmail.com>
2023-07-07 19:05:05 -0700

    Add getFixedAPR method and query

---
+++ packages/hyperdrive/src/amm/longs/getFixedAPR.ts
@@ -0,0 +1,87 @@
+import { PublicClient, Address, Transport, Chain } from "viem";
+import { HyperdriveMathABI } from "src/abis/HyperdriveMath";
+import { HyperdriveABI } from "src/abis/Hyperdrive";
+import { QueryObserverOptions } from "@tanstack/query-core";
+
+export interface GetFixedAPROptions {
+  traderAddress: Address;
+  hyperdriveAddress: Address;
+  hyperdriveMathAddress: Address;
+  publicClient: PublicClient<Transport, Chain>;
+}
+
+export async function getFixedAPR({
+  hyperdriveAddress,
+  hyperdriveMathAddress,
+  publicClient,
+}: GetFixedAPROptions): Promise<bigint> {
+  // TODO: Move to own method
+  const poolConfig = await publicClient.readContract({
+    address: hyperdriveAddress,
+    abi: HyperdriveABI,
+    functionName: "getPoolConfig",
+  });
+
+  // TODO: Move to own method
+  const poolInfo = await publicClient.readContract({
+    address: hyperdriveAddress,
+    abi: HyperdriveABI,
+    functionName: "getPoolInfo",
+  });
+
+  const apr = await publicClient.readContract({
+    address: hyperdriveMathAddress,
+    abi: HyperdriveMathABI,
+    functionName: "calculateAPRFromReserves",
+    args: [
+      poolInfo.shareReserves,
+      poolInfo.shareReserves,
+      poolConfig.initialSharePrice,
+      poolConfig.positionDuration,
+      poolConfig.timeStretch,
+    ],
+  });
+
+  return apr;
+}
+
+/**
+ * A query wrapper for consumers who want easy caching via @tanstack/query
+ *
+ * TODO: Piloting this idea here for now as proof-of-concept. Ultimately
+ * @hyperdrive/core should not know about caching and just be pure hyperdrive
+ * bindings. If this works well in practice we can move this to a
+ * @hyperdrive/queries package.
+ */
+export function getFixedAPRQuery({
+  hyperdriveAddress,
+  hyperdriveMathAddress,
+  publicClient,
+  traderAddress: account,
+}: Partial<GetFixedAPROptions>): QueryObserverOptions<
+  Awaited<ReturnType<typeof getFixedAPR>>
+> {
+  const queryEnabled =
+    !!account &&
+    !!hyperdriveAddress &&
+    !!publicClient &&
+    !!hyperdriveMathAddress;
+
+  return {
+    enabled: queryEnabled,
+    queryKey: [
+      "@hyperdrive/core",
+      "calculateAPR",
+      { hyperdriveAddress, hyperdriveMathAddress, account },
+    ],
+    queryFn: queryEnabled
+      ? () =>
+          getFixedAPR({
+            traderAddress: account,
+            hyperdriveAddress,
+            hyperdriveMathAddress,
+            publicClient,
+          })
+      : undefined,
+  };
+}
