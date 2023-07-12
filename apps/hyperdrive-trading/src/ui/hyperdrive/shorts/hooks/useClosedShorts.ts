0d6c0558116b8710fd09106fe03ad111f2297c0e
Danny Delott <dannydelott@gmail.com>
2023-07-12 12:48:53 -0700

    Wire up Closed Shorts table

---
+++ apps/hyperdrive-trading/src/ui/hyperdrive/shorts/hooks/useClosedShorts.ts
@@ -0,0 +1,31 @@
+import { ClosedShort, getClosedShortsQuery } from "@hyperdrive/core";
+import { QueryStatus, useQuery } from "@tanstack/react-query";
+import { Address } from "viem";
+import { usePublicClient } from "wagmi";
+
+interface UseClosedShortsOptions {
+  account: Address | undefined;
+  hyperdriveAddress: Address | undefined;
+}
+
+/**
+ * Returns the list of shorts that the account has closed out of
+ */
+export function useClosedShorts({
+  account,
+  hyperdriveAddress,
+}: UseClosedShortsOptions): {
+  closedShorts: ClosedShort[] | undefined;
+  closedShortsStatus: QueryStatus;
+} {
+  const publicClient = usePublicClient();
+  const { data: closedShorts, status: closedShortsStatus } = useQuery(
+    getClosedShortsQuery({
+      traderAddress: account,
+      hyperdriveAddress,
+      publicClient: publicClient as any,
+    }),
+  );
+
+  return { closedShorts, closedShortsStatus };
+}
