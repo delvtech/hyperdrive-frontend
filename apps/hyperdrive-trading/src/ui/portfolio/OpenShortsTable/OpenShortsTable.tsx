c9803582c5c37c46cd52c62ac52fea7157fa688d
Danny Delott <dannydelott@gmail.com>
2023-07-13 23:02:52 -0700

    Rename to Bonds (#230)

--- apps/hyperdrive-trading/src/ui/portfolio/OpenShortsTable/OpenShortsTable.tsx
+++ apps/hyperdrive-trading/src/ui/portfolio/OpenShortsTable/OpenShortsTable.tsx
@@ -24,7 +24,7 @@ export function OpenShortsTable({
       headingRowClassName="grid-cols-4 text-neutral-content"
       bodyRowClassName="grid-cols-4 text-base-content items-center text-sm md:text-h6 even:bg-accent/5 h-16"
       // Blank col added for actions
-      cols={["Position", "Balance", "Value", "Matures on", ""]}
+      cols={["Position", "Bonds", "Value", "Matures on", ""]}
       rows={openShortRows}
     />
   );
