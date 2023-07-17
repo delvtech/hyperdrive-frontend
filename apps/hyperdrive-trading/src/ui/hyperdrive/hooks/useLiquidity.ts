3933d1812e54b46eea06448ef4da312a02557ad0
jack <jhburrus3@gmail.com>
2023-07-17 22:45:45 +0200

    begrudgingly use any type

--- apps/hyperdrive-trading/src/ui/hyperdrive/hooks/useLiquidity.ts
+++ apps/hyperdrive-trading/src/ui/hyperdrive/hooks/useLiquidity.ts
@@ -1,12 +1,11 @@
 import { getLiquidity } from "@hyperdrive/core";
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import { GetLiquidityResponse } from "src/ui/base/types";
-import { PublicClient } from "viem";
 import { usePublicClient } from "wagmi";
 export function useLiquidity(
   hyperdriveAddress: `0x${string}`,
 ): UseQueryResult<GetLiquidityResponse> {
-  const publicClient = usePublicClient() as PublicClient;
+  const publicClient = usePublicClient() as any;
   return useQuery({
     queryKey: ["liquidity", { hyperdriveAddress, publicClient }],
     queryFn: () => getLiquidity(hyperdriveAddress, publicClient),
