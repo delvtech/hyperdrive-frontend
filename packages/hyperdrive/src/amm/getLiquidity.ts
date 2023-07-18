2c2670dc7a77b8941a97fb0c1f783273ac4499c3
Ryan Goree <goree.ryan@gmail.com>
2023-07-18 01:37:23 -0500

    Refactor getLiquidity to return a bigint and formatted

--- packages/hyperdrive/src/amm/getLiquidity.ts
+++ packages/hyperdrive/src/amm/getLiquidity.ts
@@ -4,32 +4,31 @@ import { makeQueryKey } from "src/makeQueryKey";
 import { QueryObserverOptions } from "@tanstack/query-core";
 import { getPoolInfo } from "src/amm/getPoolInfo";
 
+interface GetLiquidityResult {
+  liquidity: bigint;
+  formatted: string;
+}
+
 /**
  * This function retrieves the market liquidity by using the following formula:
  * marketLiquidity = sharePrice * shareReserves - longsOutstanding
  *
  * @param hyperdriveAddress - The address of the hyperdrive
  * @param publicClient - The public client
- * @returns Promise that resolves with an object containing the market liquidity
  */
 export async function getLiquidity(
   hyperdriveAddress: Address,
   publicClient: PublicClient<Transport, Chain>,
-): Promise<{ marketLiquidity: string }> {
-  const poolInfo = await getPoolInfo({
+): Promise<GetLiquidityResult> {
+  const { sharePrice, shareReserves, longsOutstanding } = await getPoolInfo({
     hyperdriveAddress,
     publicClient,
   });
 
-  const sharePrice = parseFloat(formatUnits(poolInfo.sharePrice, 18));
-  const shareReserves = parseFloat(formatUnits(poolInfo.shareReserves, 18));
-  const longsOutstanding = parseFloat(
-    formatUnits(poolInfo.longsOutstanding, 18),
-  );
+  const liquidity =
+    (sharePrice * shareReserves) / 10n ** 18n - longsOutstanding;
 
-  const marketLiquidity = sharePrice * shareReserves - longsOutstanding;
-
-  return { marketLiquidity: marketLiquidity.toString() };
+  return { liquidity, formatted: formatUnits(liquidity, 18) };
 }
 
 interface GetLiquidityQueryOptions {
