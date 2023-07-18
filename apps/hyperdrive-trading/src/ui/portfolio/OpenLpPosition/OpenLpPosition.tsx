70c91e70d94be302630404cca3d8739315947480
Ryan Goree <goree.ryan@gmail.com>
2023-07-18 18:12:57 -0500

    Remove log

--- apps/hyperdrive-trading/src/ui/portfolio/OpenLpPosition/OpenLpPosition.tsx
+++ apps/hyperdrive-trading/src/ui/portfolio/OpenLpPosition/OpenLpPosition.tsx
@@ -2,7 +2,6 @@ import { ReactElement } from "react";
 import { Hyperdrive } from "src/appconfig/types";
 import { SortableGridTable } from "src/ui/base/components/tables/SortableGridTable";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
-import { useHyperdrivePoolInfo } from "src/ui/hyperdrive/hooks/useHyperdrivePoolInfo";
 import { useLpShares } from "src/ui/hyperdrive/lp/hooks/useLpShares";
 import { useWithdrawalShares } from "src/ui/hyperdrive/lp/hooks/useWithdrawalShares";
 import { RemoveLiquidityModalButton } from "src/ui/hyperdrive/lp/RemoveLiquidityModalButton/RemoveLiquidityModalButton";
@@ -28,30 +27,6 @@ export function OpenLpPosition({
     account,
   });
 
-  const { poolInfo } = useHyperdrivePoolInfo(hyperdrive.address);
-
-  if (poolInfo) {
-    console.log("poolInfo", {
-      shareReserves: formatUnits(poolInfo.shareReserves, 18),
-      bondReserves: formatUnits(poolInfo.bondReserves, 18),
-      lpTotalSupply: formatUnits(poolInfo.lpTotalSupply, 18),
-      sharePrice: formatUnits(poolInfo.sharePrice, 18),
-      longsOutstanding: formatUnits(poolInfo.longsOutstanding, 18),
-      longAverageMaturityTime: poolInfo.longAverageMaturityTime.toString(),
-      shortsOutstanding: formatUnits(poolInfo.shortsOutstanding, 18),
-      shortAverageMaturityTime: poolInfo.shortAverageMaturityTime.toString(),
-      shortBaseVolume: formatUnits(poolInfo.shortBaseVolume, 18),
-      withdrawalSharesReadyToWithdraw: formatUnits(
-        poolInfo.withdrawalSharesReadyToWithdraw,
-        18,
-      ),
-      withdrawalSharesProceeds: formatUnits(
-        poolInfo.withdrawalSharesProceeds,
-        18,
-      ),
-    });
-  }
-
   return (
     <SortableGridTable
       headingRowClassName="grid-cols-4 text-start text-neutral-content"
@@ -59,7 +34,7 @@ export function OpenLpPosition({
       // Blank col added for actions
       cols={["Position", "Shares", "Value", "Withdrawable", ""]}
       rows={[
-        lpShares && poolInfo
+        lpShares
           ? [
               <span key="type" className="font-semibold uppercase text-primary">
                 LP
