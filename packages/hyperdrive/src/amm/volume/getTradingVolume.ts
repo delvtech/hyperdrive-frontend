19837ffd1f40a75bc0a2cbc0f1350e66a5b478a4
jack <jhburrus3@gmail.com>
2023-07-19 13:19:58 +0200

    adds types for events

--- packages/hyperdrive/src/amm/volume/getTradingVolume.ts
+++ packages/hyperdrive/src/amm/volume/getTradingVolume.ts
@@ -1,40 +1,63 @@
+import { QueryObserverOptions } from "@tanstack/query-core";
+import { HyperdriveABI } from "src/abis/Hyperdrive";
 import { getOpenLongEvents } from "src/amm/longs/getOpenLongEvents";
 import { getOpenShortEvents } from "src/amm/shorts/getOpenShortEvents";
-import { Address, Chain, PublicClient, Transport, formatUnits } from "viem";
+import {
+  Address,
+  Chain,
+  DecodeEventLogReturnType,
+  GetFilterLogsReturnType,
+  PublicClient,
+  Transport,
+  formatUnits,
+} from "viem";
+interface OpenShortEvent {
+  eventData: DecodeEventLogReturnType<
+    typeof HyperdriveABI,
+    "OpenShort"
+  >["args"];
+  eventLog: GetFilterLogsReturnType<typeof HyperdriveABI, "OpenShort">[number];
+}
+interface OpenLongEvent {
+  eventData: DecodeEventLogReturnType<typeof HyperdriveABI, "OpenLong">["args"];
+  eventLog: GetFilterLogsReturnType<typeof HyperdriveABI, "OpenLong">[number];
+}
 
+/**
+ * Calculates the total trading volume based on long and short events.
+ *
+ * @param hyperdriveAddress - Address of hyperdrive.
+ * @param publicClient - Client to fetch data.
+ * @param currentBlockNumber - Current block number.
+ * @returns Total trading volume as a string.
+ */
 export async function getTradingVolume(
   hyperdriveAddress: Address,
   publicClient: PublicClient<Transport, Chain>,
   currentBlockNumber: bigint,
 ): Promise<string> {
-  console.log(7137n.toString(), "currentBlockNumber");
-
+  // 7137 is the average number of blocks in 24h
   let blockNumber24hAgo = currentBlockNumber - 7137n;
   // If the block number 24h ago is negative, set it to 0. Useful for anvil because blocknumbers are low. Should remove this for mainnet
   if (blockNumber24hAgo < 0n) {
     blockNumber24hAgo = 0n;
   }
 
-  const shortEvents = await getOpenShortEvents({
-    fromBlock: blockNumber24hAgo,
-    hyperdriveAddress,
-    publicClient,
-  });
-  const longEvents = await getOpenLongEvents({
-    fromBlock: blockNumber24hAgo,
-    hyperdriveAddress,
-    publicClient,
-  });
-  const longEventAmounts = longEvents.map((event) =>
-    parseInt(formatUnits(event.eventData.bondAmount, 18)),
-  );
+  const [shortEvents, longEvents] = await Promise.all([
+    getOpenShortEvents({
+      fromBlock: blockNumber24hAgo,
+      hyperdriveAddress,
+      publicClient,
+    }),
+    getOpenLongEvents({
+      fromBlock: blockNumber24hAgo,
+      hyperdriveAddress,
+      publicClient,
+    }),
+  ]);
 
-  const shortEventAmounts = shortEvents.map((event) =>
-    parseInt(formatUnits(event.eventData.bondAmount, 18)),
-  );
-  const summedLongs = longEventAmounts.reduce((a, b) => a + b, 0);
-  const summedShorts = shortEventAmounts.reduce((a, b) => a + b, 0);
-  const totalVolume = summedLongs + summedShorts;
+  const totalVolume =
+    calculateTotalAmount(longEvents) + calculateTotalAmount(shortEvents);
 
   return totalVolume.toString();
 }
@@ -44,17 +67,22 @@ export function getTradingVolumeQuery({
   publicClient,
   currentBlockNumber,
 }: {
-  hyperdriveAddress: Address | undefined;
-  publicClient: any;
+  hyperdriveAddress: Address;
+  publicClient: PublicClient<Transport, Chain>;
   currentBlockNumber: bigint;
-}): any {
+}): QueryObserverOptions<Awaited<ReturnType<typeof getTradingVolume>>> {
   return {
     queryKey: ["tradingVolume", { hyperdriveAddress, publicClient }],
     queryFn: () =>
-      getTradingVolume(
-        hyperdriveAddress as Address,
-        publicClient,
-        currentBlockNumber,
-      ),
+      getTradingVolume(hyperdriveAddress, publicClient, currentBlockNumber),
+    enabled: !!hyperdriveAddress && !!publicClient && !!currentBlockNumber,
   };
+}
+
+function calculateTotalAmount(
+  events: OpenShortEvent[] | OpenLongEvent[],
+): number {
+  return events
+    .map((event) => parseInt(formatUnits(event.eventData.bondAmount, 18)))
+    .reduce((a, b) => a + b, 0);
 }
