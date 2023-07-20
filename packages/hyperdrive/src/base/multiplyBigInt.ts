b0ca76e97a6f1c5c044dc6f6d2186a9f7ccf9556
Ryan Goree <goree.ryan@gmail.com>
2023-07-20 16:31:44 -0500

    Remove multiple decimal value support

--- packages/hyperdrive/src/base/multiplyBigInt.ts
+++ packages/hyperdrive/src/base/multiplyBigInt.ts
@@ -2,11 +2,8 @@
  * Multiply an array of bigints together and preserve the correct scaling for
  * decimals.
  *
- * @param values - The bigints to multiply. Can be a bigint or an object with a
- * bigint `value` and a `decimals` property.
- * @param decimals - The number of decimal places the bigints are scaled
- * to. If a value in the `values` has a `decimals` property, that value will be
- * used instead. This is also the number of decimal places used for the result.
+ * @param values - The bigints to multiply.
+ * @param decimals - The number of decimal places the bigints are scaled to.
  *
  * @example
  *
@@ -14,58 +11,20 @@
  * // 5.00 * .50 = 2.50
  * multiplyBigInt([500n, 50n], 2);
  * // 250n
- *
- * // With different decimals
- * // 5.00 * .500 = 2.50
- * multiplyBigInt(
- *   [
- *     500n,
- *     {
- *       value: 500n,
- *       decimals: 3
- *     }
- *   ],
- *   2
- * );
- * // 250n
- * ```
  */
-export function multiplyBigInt(
-  values: (
-    | bigint
-    | {
-        value: bigint;
-        decimals: number;
-      }
-  )[],
-  decimals: number,
-): bigint {
+export function multiplyBigInt(values: bigint[], decimals: number): bigint {
   if (values.length === 0) {
     return 0n;
   }
 
-  let productValue: bigint;
-  let productDecimals: number;
-
-  // Initialize the total value and decimals to the first value in the array.
-  if (typeof values[0] === "bigint") {
-    productValue = values[0];
-    productDecimals = decimals;
-  } else {
-    productValue = values[0].value;
-    productDecimals = values[0].decimals;
-  }
+  let productValue = values[0];
+  let productDecimals = decimals;
 
   // Multiply the total value by each subsequent value in the array. And add the
   // decimals together.
   for (const value of values.slice(1)) {
-    if (typeof value === "bigint") {
-      productValue *= value;
-      productDecimals += decimals;
-    } else {
-      productValue *= value.value;
-      productDecimals += value.decimals;
-    }
+    productValue *= value;
+    productDecimals += decimals;
   }
 
   // If the total decimals is less than the desired decimals, return 0.
