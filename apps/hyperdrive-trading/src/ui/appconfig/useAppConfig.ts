e078c22d711b3af58786e7c6c1d3ee8d4a75fcc3
Ryan Goree <goree.ryan@gmail.com>
2023-07-21 16:12:46 -0500

    Add custom chain config

--- apps/hyperdrive-trading/src/ui/appconfig/useAppConfig.ts
+++ apps/hyperdrive-trading/src/ui/appconfig/useAppConfig.ts
@@ -1,4 +1,3 @@
-import { HyperdriveGoerliAddresses } from "@hyperdrive/core";
 import { useQuery } from "@tanstack/react-query";
 import assertNever from "assert-never";
 import { SupportedChainId } from "src/appconfig/chains/supportedChains";
@@ -8,6 +7,9 @@ import { Address } from "viem";
 import { useChainId, usePublicClient } from "wagmi";
 
 const LOCALHOST_ADDRESSES_URL = import.meta.env.VITE_LOCALHOST_ADDRESSES_URL;
+const CUSTOM_CHAIN_ADDRESSES_URL = import.meta.env
+  .VITE_CUSTOM_CHAIN_ADDRESSES_URL;
+
 export function useAppConfig(): {
   appConfig: AppConfig | undefined;
   appConfigStatus: "idle" | "error" | "loading" | "success";
@@ -18,11 +20,8 @@ export function useAppConfig(): {
     queryKey: ["app-config", { chainId }],
     queryFn: async () => {
       switch (chainId) {
-        case 5:
-          return getAppConfig(HyperdriveGoerliAddresses, publicClient);
-
         case 31337:
-          const localAddresses = await fetchLocalAddresses();
+          const localAddresses = await fetchLocalhostAddresses();
           return getAppConfig(
             // TODO: This is a temporary "cross-walk" object to use until SC
             // team unifies the shape of the addresses.json file across the
@@ -30,6 +29,19 @@ export function useAppConfig(): {
             {
               dsrHyperdrive: localAddresses.mockHyperdrive,
               mockHyperdriveMath: localAddresses.mockHyperdriveMath,
+            },
+            publicClient,
+          );
+
+        case 42069:
+          const addresses = await fetchCustomChainAddresses();
+          return getAppConfig(
+            // TODO: This is a temporary "cross-walk" object to use until SC
+            // team unifies the shape of the addresses.json file across the
+            // different chains.
+            {
+              dsrHyperdrive: addresses.mockHyperdrive,
+              mockHyperdriveMath: addresses.mockHyperdriveMath,
             },
             publicClient,
           );
@@ -45,9 +57,15 @@ export function useAppConfig(): {
   return { appConfig, appConfigStatus };
 }
 
-async function fetchLocalAddresses() {
+async function fetchLocalhostAddresses() {
   return await fetch(LOCALHOST_ADDRESSES_URL).then(
-    (res) => res.json() as Promise<LocalAddressesJson>,
+    (res) => res.json() as Promise<AddressesJson>,
+  );
+}
+
+async function fetchCustomChainAddresses() {
+  return await fetch(CUSTOM_CHAIN_ADDRESSES_URL).then(
+    (res) => res.json() as Promise<AddressesJson>,
   );
 }
 
@@ -56,7 +74,7 @@ async function fetchLocalAddresses() {
  * addresses.json file and the local one (see Docker image in infra repo) share
  * the same shape. Smart Contract team will implement this.
  */
-interface LocalAddressesJson {
+interface AddressesJson {
   baseToken: Address;
   mockHyperdrive: Address;
   mockHyperdriveMath: Address;
