330093d65aec065e8d806c4310bd1e5293eb94d6
jack <jhburrus3@gmail.com>
2023-07-24 20:56:22 +0200

    remove test from stubs

--- packages/hyperdrive/src/test/multiplyBigInt.test.ts
+++ packages/hyperdrive/src/test/multiplyBigInt.test.ts
@@ -1,29 +1,29 @@
 import { multiplyBigInt } from "src/base/multiplyBigInt";
-import { test, assert } from "vitest";
+import { expect, test } from "vitest";
 
 test("multiplyBigInt: simple multiplication", async () => {
   const result = multiplyBigInt([500n, 50n], 2);
-  assert.equal(result, 250n, "5.00 * 0.50 should equal 2.50");
+  expect(result).toEqual(250n);
 });
 
 test("multiplyBigInt: when values array is empty", async () => {
   const result = multiplyBigInt([], 2);
-  assert.equal(result, 0n, "Multiplying empty array should return 0");
+  expect(result).toEqual(0n);
 });
 
 test("multiplyBigInt: single value input", async () => {
   const result = multiplyBigInt([10n], 2);
-  assert.equal(result, 10n, "Single value should be returned as-is");
+  expect(result).toEqual(10n);
 });
 
 test("multiplyBigInt: multiplying with zero", async () => {
   const result = multiplyBigInt([500n, 0n], 2);
-  assert.equal(result, 0n, "Any value multiplied by 0 should be 0");
+  expect(result).toEqual(0n);
 });
 
 test("multiplyBigInt: multiplying multiple bigint values", async () => {
   // Test case: multiplying three values
   // Let's say our values are [2000n, 300n, 50n] and for 2 decimals, this translates to 20.00 * 3.00 * 0.50 = 30.00
   const result = multiplyBigInt([2000n, 300n, 50n], 2);
-  assert.equal(result, 3000n, "20.00 * 3.00 * 0.50 should equal 30.00");
+  expect(result).toEqual(3000n);
 });
