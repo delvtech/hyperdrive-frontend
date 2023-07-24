6298b263fddda9d4367e59281769b98b60964503
Danny Delott <dannydelott@gmail.com>
2023-07-24 16:45:58 -0700

    Remove LP APY (#270)

--- apps/hyperdrive-trading/src/network/wagmiClient.ts
+++ apps/hyperdrive-trading/src/network/wagmiClient.ts
@@ -1,56 +1,39 @@
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { cloudChain, cloudChainRpcProvider } from "src/network/cloudChain";
 import { Chain, ChainProviderFn, configureChains, createConfig } from "wagmi";
 import { foundry } from "wagmi/chains";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 const {
-  VITE_LOCALHOST_NODE_RPC_URL: LOCALHOST_NODE_RPC_URL,
-  VITE_CUSTOM_CHAIN_NODE_RPC_URL: CUSTOM_CHAIN_NODE_RPC_URL,
-  VITE_CUSTOM_CHAIN_ADDRESSES_URL: CUSTOM_CHAIN_NODE_ADDRESSES_URL,
-  VITE_CUSTOM_CHAIN_CHAIN_ID: CUSTOM_CHAIN_CHAIN_ID,
-  VITE_WALLET_CONNECT_PROJECT_ID: PROJECT_ID,
+  VITE_LOCALHOST_NODE_RPC_URL,
+  VITE_CUSTOM_CHAIN_NODE_RPC_URL,
+  VITE_CUSTOM_CHAIN_ADDRESSES_URL,
+  VITE_CUSTOM_CHAIN_CHAIN_ID,
+  VITE_WALLET_CONNECT_PROJECT_ID,
 } = import.meta.env;
 
 const chainsToConfigure: Chain[] = [];
 const providersToConfigure: ChainProviderFn[] = [];
 
-if (LOCALHOST_NODE_RPC_URL) {
+// Localhost devnet
+if (VITE_LOCALHOST_NODE_RPC_URL) {
   chainsToConfigure.push(foundry);
   providersToConfigure.push(
     jsonRpcProvider({
       rpc: () => ({
-        http: LOCALHOST_NODE_RPC_URL,
+        http: VITE_LOCALHOST_NODE_RPC_URL,
       }),
     }),
   );
 }
 
+// CloudChain (if proper env is set)
 if (
-  CUSTOM_CHAIN_NODE_RPC_URL &&
-  CUSTOM_CHAIN_CHAIN_ID &&
-  CUSTOM_CHAIN_NODE_ADDRESSES_URL
+  VITE_CUSTOM_CHAIN_NODE_RPC_URL &&
+  VITE_CUSTOM_CHAIN_CHAIN_ID &&
+  VITE_CUSTOM_CHAIN_ADDRESSES_URL
 ) {
-  chainsToConfigure.push({
-    id: +CUSTOM_CHAIN_CHAIN_ID,
-    name: "☁️ \u00A0 Chain",
-    network: "custom-chain",
-    nativeCurrency: {
-      decimals: 18,
-      name: "Ether",
-      symbol: "ETH",
-    },
-    rpcUrls: {
-      public: { http: [CUSTOM_CHAIN_NODE_RPC_URL] },
-      default: { http: [CUSTOM_CHAIN_NODE_RPC_URL] },
-    },
-  });
-
-  providersToConfigure.push(
-    jsonRpcProvider({
-      rpc: () => ({
-        http: CUSTOM_CHAIN_NODE_RPC_URL,
-      }),
-    }),
-  );
+  chainsToConfigure.push(cloudChain);
+  providersToConfigure.push(cloudChainRpcProvider);
 }
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
@@ -62,7 +45,7 @@ export const wagmiChains = chains;
 
 const { connectors } = getDefaultWallets({
   appName: "Hyperdrive",
-  projectId: PROJECT_ID || undefined,
+  projectId: VITE_WALLET_CONNECT_PROJECT_ID || undefined,
   chains,
 });
 
