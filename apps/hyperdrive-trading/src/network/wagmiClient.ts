0f685d552db5507638d1d99481cd9bb1c0949120
Danny Delott <dannydelott@gmail.com>
2023-07-24 16:04:52 -0700

    Don't rename env variables

--- apps/hyperdrive-trading/src/network/wagmiClient.ts
+++ apps/hyperdrive-trading/src/network/wagmiClient.ts
@@ -3,34 +3,34 @@ import { Chain, ChainProviderFn, configureChains, createConfig } from "wagmi";
 import { foundry } from "wagmi/chains";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 const {
-  VITE_LOCALHOST_NODE_RPC_URL: LOCALHOST_NODE_RPC_URL,
-  VITE_CUSTOM_CHAIN_NODE_RPC_URL: CUSTOM_CHAIN_NODE_RPC_URL,
-  VITE_CUSTOM_CHAIN_ADDRESSES_URL: CUSTOM_CHAIN_NODE_ADDRESSES_URL,
-  VITE_CUSTOM_CHAIN_CHAIN_ID: CUSTOM_CHAIN_CHAIN_ID,
-  VITE_WALLET_CONNECT_PROJECT_ID: PROJECT_ID,
+  VITE_LOCALHOST_NODE_RPC_URL,
+  VITE_CUSTOM_CHAIN_NODE_RPC_URL,
+  VITE_CUSTOM_CHAIN_ADDRESSES_URL,
+  VITE_CUSTOM_CHAIN_CHAIN_ID,
+  VITE_WALLET_CONNECT_PROJECT_ID,
 } = import.meta.env;
 
 const chainsToConfigure: Chain[] = [];
 const providersToConfigure: ChainProviderFn[] = [];
 
-if (LOCALHOST_NODE_RPC_URL) {
+if (VITE_LOCALHOST_NODE_RPC_URL) {
   chainsToConfigure.push(foundry);
   providersToConfigure.push(
     jsonRpcProvider({
       rpc: () => ({
-        http: LOCALHOST_NODE_RPC_URL,
+        http: VITE_LOCALHOST_NODE_RPC_URL,
       }),
     }),
   );
 }
 
 if (
-  CUSTOM_CHAIN_NODE_RPC_URL &&
-  CUSTOM_CHAIN_CHAIN_ID &&
-  CUSTOM_CHAIN_NODE_ADDRESSES_URL
+  VITE_CUSTOM_CHAIN_NODE_RPC_URL &&
+  VITE_CUSTOM_CHAIN_CHAIN_ID &&
+  VITE_CUSTOM_CHAIN_ADDRESSES_URL
 ) {
   chainsToConfigure.push({
-    id: +CUSTOM_CHAIN_CHAIN_ID,
+    id: +VITE_CUSTOM_CHAIN_CHAIN_ID,
     name: "☁️ \u00A0 Chain",
     network: "custom-chain",
     nativeCurrency: {
@@ -39,15 +39,15 @@ if (
       symbol: "ETH",
     },
     rpcUrls: {
-      public: { http: [CUSTOM_CHAIN_NODE_RPC_URL] },
-      default: { http: [CUSTOM_CHAIN_NODE_RPC_URL] },
+      public: { http: [VITE_CUSTOM_CHAIN_NODE_RPC_URL] },
+      default: { http: [VITE_CUSTOM_CHAIN_NODE_RPC_URL] },
     },
   });
 
   providersToConfigure.push(
     jsonRpcProvider({
       rpc: () => ({
-        http: CUSTOM_CHAIN_NODE_RPC_URL,
+        http: VITE_CUSTOM_CHAIN_NODE_RPC_URL,
       }),
     }),
   );
@@ -62,7 +62,7 @@ export const wagmiChains = chains;
 
 const { connectors } = getDefaultWallets({
   appName: "Hyperdrive",
-  projectId: PROJECT_ID || undefined,
+  projectId: VITE_WALLET_CONNECT_PROJECT_ID || undefined,
   chains,
 });
 
