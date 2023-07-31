d1f43af63b6d32361d55963e588b0ac675adcdf2
Danny Delott <dannydelott@gmail.com>
2023-07-31 15:18:42 -0700

    Add comments

--- apps/hyperdrive-trading/src/network/wagmiClient.ts
+++ apps/hyperdrive-trading/src/network/wagmiClient.ts
@@ -1,7 +1,8 @@
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { cloudChain, cloudChainRpcProvider } from "src/network/cloudChain";
 import { Chain, ChainProviderFn, configureChains, createConfig } from "wagmi";
-import { foundry } from "wagmi/chains";
+import { foundry, goerli } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 const {
   VITE_LOCALHOST_NODE_RPC_URL,
@@ -9,10 +10,19 @@ const {
   VITE_CUSTOM_CHAIN_ADDRESSES_URL,
   VITE_CUSTOM_CHAIN_CHAIN_ID,
   VITE_WALLET_CONNECT_PROJECT_ID,
+  VITE_ALCHEMY_GOERLI_RPC_KEY,
 } = import.meta.env;
 
 const chainsToConfigure: Chain[] = [];
 const providersToConfigure: ChainProviderFn[] = [];
+
+// Goerli
+if (VITE_ALCHEMY_GOERLI_RPC_KEY) {
+  chainsToConfigure.push(goerli);
+  providersToConfigure.push(
+    alchemyProvider({ apiKey: VITE_ALCHEMY_GOERLI_RPC_KEY }),
+  );
+}
 
 // Localhost devnet
 if (VITE_LOCALHOST_NODE_RPC_URL) {
@@ -26,7 +36,7 @@ if (VITE_LOCALHOST_NODE_RPC_URL) {
   );
 }
 
-// CloudChain (if proper env is set)
+// CloudChain
 if (
   VITE_CUSTOM_CHAIN_NODE_RPC_URL &&
   VITE_CUSTOM_CHAIN_CHAIN_ID &&
