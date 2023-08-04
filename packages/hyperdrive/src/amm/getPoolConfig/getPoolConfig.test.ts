2ad9ba3ac65221b63d959fb8994a6484940a4556
Danny Delott <dannydelott@gmail.com>
2023-08-03 21:33:59 -0700

    Configure vitest w/ an anvil node, add first test

--- packages/hyperdrive/src/amm/getPoolConfig/getPoolConfig.test.ts
+++ packages/hyperdrive/src/amm/getPoolConfig/getPoolConfig.test.ts
@@ -1,21 +1,29 @@
-/**
- * @vitest-environment jsdom
- */
-import { getPoolConfig } from "src/amm/getPoolConfig/getPoolConfig";
-import { createTestClient, http, publicActions } from "viem";
-import { foundry } from "viem/chains";
 import { expect, test } from "vitest";
 
-const testClient = createTestClient({
-  chain: foundry,
-  mode: "anvil",
-  transport: http(),
-}).extend(publicActions);
+import { publicClient } from "src/testing/utils";
+import { getPoolConfig } from "src/amm/getPoolConfig/getPoolConfig";
 
-test("multiplyBigInt: simple multiplication", async () => {
-  const result = await getPoolConfig({
+test("gets the poolConfig", async () => {
+  const poolConfig = await getPoolConfig({
+    publicClient,
     hyperdriveAddress: "0xd8058efe0198ae9dD7D563e1b4938Dcbc86A1F81",
-    publicClient: testClient,
   });
-  expect(result).toEqual(250n);
+
+  expect(poolConfig).toStrictEqual({
+    baseToken: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    checkpointDuration: 3600n,
+    feeCollector: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    fees: {
+      curve: 100000000000000000n,
+      flat: 500000000000000n,
+      governance: 150000000000000000n,
+    },
+    governance: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    initialSharePrice: 1000000000000000000n,
+    minimumShareReserves: 10000000000000000000n,
+    oracleSize: 10n,
+    positionDuration: 604800n,
+    timeStretch: 44463125629060298n,
+    updateGap: 3600n,
+  });
 });
--- packages/hyperdrive/vite.config.ts
+++ packages/hyperdrive/vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from "vitest/config";
-import type { UserConfig as VitestUserConfigInterface } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-const vitestConfig: VitestUserConfigInterface = {
-  test: {
-    // https://vitest.dev/config
-  },
-};
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  test: vitestConfig.test,
+  test: {
+    environment: "happy-dom",
+    globalSetup: ["src/testing/globalSetup.ts"],
+    setupFiles: ["src/testing/setup.ts"],
+  },
 });
