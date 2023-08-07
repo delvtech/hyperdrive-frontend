461dfcb3a5b3abfadd984a1ee275b88533fb4455
Jack Burrus <jhburrus3@gmail.com>
2023-08-07 08:43:48 +0200

    adds hover animation to faq (#323)

--- apps/hyperdrive-trading/src/ui/base/components/Disclosure/Disclosure.tsx
+++ apps/hyperdrive-trading/src/ui/base/components/Disclosure/Disclosure.tsx
@@ -10,9 +10,11 @@ export function Disclosure({
   description,
 }: DisclosureProps): ReactElement {
   return (
-    <div className="daisy-collapse daisy-collapse-plus border border-neutral-content/30">
+    <div className="group daisy-collapse-plus daisy-collapse border border-neutral-content/30">
       <input type="checkbox" />
-      <div className="text-xl daisy-collapse-title font-medium">{title}</div>
+      <div className="text-xl daisy-collapse-title font-medium opacity-70 group-hover:opacity-100">
+        {title}
+      </div>
       <div className="daisy-collapse-content">{description}</div>
     </div>
   );
