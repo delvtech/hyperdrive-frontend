a0fef80d4aa886d4708f2a445cd938408c6ac822
jack <jhburrus3@gmail.com>
2023-08-09 14:22:03 +0200

    update tests

--- packages/hyperdrive/src/amm/getPoolInfo.test.ts
+++ packages/hyperdrive/src/amm/getPoolInfo.test.ts
@@ -3,62 +3,67 @@ import { publicClient, testClient, walletClient } from "src/testing/utils";
 import { expect, test } from "vitest";
 import { ERC20MintableABI, HyperdriveABI, getPoolInfo } from "..";
 
-test("gets the poolInfo", async () => {
+test("poolInfo changes after a trade", async () => {
   await testClient.setLoggingEnabled(true);
-
-  const poolInfoStart = await getPoolInfo({
-    publicClient,
-    hyperdriveAddress: "0xd8058efe0198ae9dD7D563e1b4938Dcbc86A1F81",
-  });
 
   const [account] = await walletClient.getAddresses();
 
-  // Define the amount
-  const amount = parseUnits("100", 18);
+  // Constants
+  const ERC20_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const HYPERDRIVE_ADDRESS = "0xd8058efe0198ae9dD7D563e1b4938Dcbc86A1F81";
+  const AMOUNT = parseUnits("100", 18);
+  const MIN_AMOUNT = 1n;
 
-  // Make the minAmount to account for slippage
-  const minAmount = 1n;
-
+  // Approve ERC20 tokens
   await publicClient.writeContract({
     abi: ERC20MintableABI,
-    address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    address: ERC20_ADDRESS,
     account,
     functionName: "approve",
-    args: ["0xd8058efe0198ae9dD7D563e1b4938Dcbc86A1F81", amount],
-    // gas: 1391291n,
+    args: [HYPERDRIVE_ADDRESS, AMOUNT],
   });
 
+  // Mint ERC20 tokens
   await publicClient.writeContract({
     abi: ERC20MintableABI,
-    address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    address: ERC20_ADDRESS,
     account,
     functionName: "mint",
-    args: [account, amount],
+    args: [account, AMOUNT],
   });
 
-  await publicClient.simulateContract({
-    abi: HyperdriveABI,
-    address: "0xd8058efe0198ae9dD7D563e1b4938Dcbc86A1F81",
-    account,
-    functionName: "openLong",
-    args: [amount, minAmount, account, true],
-    value: 0n,
-  });
-
+  // Conduct initial trade
   await publicClient.writeContract({
     abi: HyperdriveABI,
-    address: "0xd8058efe0198ae9dD7D563e1b4938Dcbc86A1F81",
+    address: HYPERDRIVE_ADDRESS,
     account,
     functionName: "openLong",
-    args: [amount, minAmount, account, true],
+    args: [AMOUNT, MIN_AMOUNT, account, true],
     value: 0n,
   });
 
-  const poolInfoFinish = await getPoolInfo({
+  // Get initial poolInfo
+  const poolInfoStart = await getPoolInfo({
     publicClient,
-    hyperdriveAddress: "0xd8058efe0198ae9dD7D563e1b4938Dcbc86A1F81",
+    hyperdriveAddress: HYPERDRIVE_ADDRESS,
   });
 
-  //expoect poolinfo to be different
+  // Conduct another trade
+  await publicClient.writeContract({
+    abi: HyperdriveABI,
+    address: HYPERDRIVE_ADDRESS,
+    account,
+    functionName: "openLong",
+    args: [AMOUNT, MIN_AMOUNT, account, true],
+    value: 0n,
+  });
+
+  // Get updated poolInfo
+  const poolInfoFinish = await getPoolInfo({
+    publicClient,
+    hyperdriveAddress: HYPERDRIVE_ADDRESS,
+  });
+
+  // Expect poolInfo to have changed after the trade
   expect(poolInfoStart).not.toStrictEqual(poolInfoFinish);
 });
