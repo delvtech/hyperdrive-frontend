86f2b462df783dae63a5581df4e746a092265635
jack <jhburrus3@gmail.com>
2023-08-10 14:31:21 -0600

    cleanup pool info test

--- packages/hyperdrive/src/amm/getPoolInfo.test.ts
+++ packages/hyperdrive/src/amm/getPoolInfo.test.ts
@@ -1,71 +1,50 @@
-import { parseUnits } from "src/base/parseUnits";
-import { publicClient, testClient, walletClient } from "src/testing/utils";
-import { expect, test } from "vitest";
-import { ERC20MintableABI, HyperdriveABI, getPoolInfo } from "..";
-import { Address } from "viem";
 import { TestAddresses } from "src/addresses/test";
-import { MAX_UINT256 } from "src/base/numbers";
+import { parseUnits } from "src/base/parseUnits";
+import { ALICE } from "src/testing/accounts";
+import { publicClient } from "src/testing/utils";
+import { expect, test } from "vitest";
+import { HyperdriveABI, getPoolInfo } from "..";
+import { setupMintTokensAndApproveHyperdrive } from "./testing/setupMintTokensAndApproveHyperdrive";
+
 test("poolInfo changes after a trade", async () => {
-  await testClient.setLoggingEnabled(true);
-
-  const [account] = await walletClient.getAddresses();
-
   // Constants
-  const HYPERDRIVE_ADDRESS = "0xd8058efe0198ae9dD7D563e1b4938Dcbc86A1F81";
-  const AMOUNT = parseUnits("100", 18);
-  const MIN_AMOUNT = 1n;
+  const baseAmountIn = parseUnits("100", 18);
+  const minAmountOut = 1n;
 
-  await setupMintTokensAndApproveHyperdrive(account);
+  await setupMintTokensAndApproveHyperdrive(ALICE);
 
   // Conduct initial trade
   await publicClient.writeContract({
     abi: HyperdriveABI,
-    address: HYPERDRIVE_ADDRESS,
-    account,
+    address: TestAddresses.mockHyperdrive,
+    account: ALICE,
     functionName: "openLong",
-    args: [AMOUNT, MIN_AMOUNT, account, true],
+    args: [baseAmountIn, minAmountOut, ALICE, true],
     value: 0n,
   });
 
   // Get initial longsOutstanding
   const { longsOutstanding: longsOutstandingStart } = await getPoolInfo({
     publicClient,
-    hyperdriveAddress: HYPERDRIVE_ADDRESS,
+    hyperdriveAddress: TestAddresses.mockHyperdrive,
   });
 
   // Conduct another trade
   await publicClient.writeContract({
     abi: HyperdriveABI,
-    address: HYPERDRIVE_ADDRESS,
-    account,
+    address: TestAddresses.mockHyperdrive,
+    account: ALICE,
     functionName: "openLong",
-    args: [AMOUNT, MIN_AMOUNT, account, true],
+    args: [baseAmountIn, minAmountOut, ALICE, true],
     value: 0n,
   });
 
   // Get updated longsOutstanding
   const { longsOutstanding: longsOutstandingFinish } = await getPoolInfo({
     publicClient,
-    hyperdriveAddress: HYPERDRIVE_ADDRESS,
+    hyperdriveAddress: TestAddresses.mockHyperdrive,
   });
 
   // Expect poolInfo to have changed after the trade
   expect(longsOutstandingStart).not.toStrictEqual(longsOutstandingFinish);
 });
-
-async function setupMintTokensAndApproveHyperdrive(account: Address) {
-  await publicClient.writeContract({
-    abi: ERC20MintableABI,
-    address: TestAddresses.baseToken,
-    functionName: "mint",
-    args: [account, parseUnits("100000", 18)],
-    account,
-  });
-  await publicClient.writeContract({
-    functionName: "approve",
-    account,
-    address: TestAddresses.baseToken,
-    args: [TestAddresses.mockHyperdrive, MAX_UINT256],
-    abi: ERC20MintableABI,
-  });
-}
