a19fe9da6036ba9d8ce7199c152bd1a61b92689e
jackburrus <jhburrus3@gmail.com>
2023-08-18 12:42:41 -0600

    adds initial tags

--- apps/hyperdrive-trading/src/ui/app/Head/CommonHeadTags.tsx
+++ apps/hyperdrive-trading/src/ui/app/Head/CommonHeadTags.tsx
@@ -1,11 +1,28 @@
 import Helmet from "react-helmet";
-export function CommonHeadTags(): JSX.Element {
+
+interface CommonHeadTagsProps {
+  title: string;
+  description: string;
+}
+
+const defaultTitle = "Hyperdrive Trading";
+const defaultDescription =
+  "Hyperdrive is the next research leap from the Delv team on variable and fixed rate primitives. No preset expiration dates, no fragmented liquidity, and no LP rollovers, aka everlasting liquidity.";
+
+export function CommonHeadTags({
+  title = defaultTitle,
+  description = defaultDescription,
+}: CommonHeadTagsProps): JSX.Element {
   return (
     <Helmet>
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, minimal-ui"
       />
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:site_name" content={defaultTitle} />
+      {/* TODO: Add og:image, og:url, og:title, og:description twitter:image twitter:card twitter:site icon */}
     </Helmet>
   );
 }
