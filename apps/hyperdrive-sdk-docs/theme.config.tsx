81b673bb04feea54e2ce390a1a3033c281d21b12
jackburrus <jhburrus3@gmail.com>
2023-08-21 09:47:58 -0400

    updates links

--- apps/hyperdrive-sdk-docs/theme.config.tsx
+++ apps/hyperdrive-sdk-docs/theme.config.tsx
@@ -1,14 +1,25 @@
 const config = {
+  logo: <span>Hyperdrive SDK</span>,
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/delvtech/hyperdrive-monorepo",
   },
   chat: {
-    link: "https://discord.com",
+    link: "https://discord.gg/atwMje4P",
   },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  docsRepositoryBase: "https://github.com/delvtech/hyperdrive-monorepo",
   footer: {
-    text: "Nextra Docs Template",
+    text: "Hyperdrive SDK",
   },
+  head: (
+    <>
+      <title>Hyperdrive SDK</title>
+      <meta name="og:title" content="Hyperdrive SDK" />
+      <meta
+        name="og:description"
+        content="Hyperdrive is the next research leap from the Delv team on variable and fixed rate primitives. No preset expiration dates, no fragmented liquidity, and no LP rollovers, aka everlasting liquidity."
+      />
+    </>
+  ),
 };
 
 export default config;
