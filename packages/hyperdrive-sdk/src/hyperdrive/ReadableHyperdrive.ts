add2112a21c1bc799f9e6de264f7f4e7e0b2468c
Danny Delott <dannydelott@gmail.com>
2023-08-29 16:56:54 -0700

    Oops, remove caching in ReadableHyperdrive

--- packages/hyperdrive-sdk/src/hyperdrive/ReadableHyperdrive.ts
+++ packages/hyperdrive-sdk/src/hyperdrive/ReadableHyperdrive.ts
@@ -1,21 +1,16 @@
-import { QueryClient } from "@tanstack/query-core";
 import { ReadCallOptions } from "src/base/abitype";
-import { makeQueryKey } from "src/caching/makeQueryKey";
 import { HyperdriveContract } from "src/hyperdrive/HyperdriveContract";
 import { PoolConfig } from "src/pool/PoolConfig";
 
 interface ReadableHyperdriveConstructorOptions {
   contract: HyperdriveContract;
-  queryClient?: QueryClient;
 }
 
 export class ReadableHyperdrive {
   private contract: HyperdriveContract;
-  private queryClient: QueryClient;
 
-  constructor({ contract, queryClient }: ReadableHyperdriveConstructorOptions) {
+  constructor({ contract }: ReadableHyperdriveConstructorOptions) {
     this.contract = contract;
-    this.queryClient = queryClient ? queryClient : new QueryClient();
   }
 
   /**
@@ -23,15 +18,7 @@ export class ReadableHyperdrive {
    * @param options
    * @returns PoolConfig
    */
-  getPoolConfig(options: ReadCallOptions): Promise<PoolConfig> {
-    return this.queryClient.fetchQuery({
-      queryKey: makeQueryKey("getPoolConfig", {
-        address: this.contract.address,
-        options,
-      }),
-      queryFn: () => this.contract.read("getPoolConfig", [], options),
-      // Pool config can never change, it's safe to cache forever
-      staleTime: Infinity,
-    });
+  getPoolConfig(options?: ReadCallOptions): Promise<PoolConfig> {
+    return this.contract.read("getPoolConfig", [], options);
   }
 }
