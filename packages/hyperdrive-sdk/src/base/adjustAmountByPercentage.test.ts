88a7ba5d1b1cfb7beb1f7fa8b5a1ceba2393dbaa
Danny Delott <dannydelott@gmail.com>
2023-08-30 09:45:45 -0700

    Remove test

--- packages/hyperdrive-sdk/src/base/adjustAmountByPercentage.test.ts
+++ packages/hyperdrive-sdk/src/base/adjustAmountByPercentage.test.ts
@@ -1,11 +1,6 @@
 import { expect, test } from "vitest";
 import { adjustAmountByPercentage } from "src/base/adjustAmountByPercentage";
 import { parseUnits } from "viem";
-import { ALICE } from "src/testing/accounts";
-import { setupMintTokensAndApproveHyperdrive } from "src/amm/testing/setupMintTokensAndApproveHyperdrive";
-import { publicClient } from "src/testing/utils";
-import { TestAddresses } from "src/testing/test";
-import { HyperdriveABI } from "@hyperdrive/core";
 
 test("should return adjusted amount when given a basic input", () => {
   const amount = parseUnits("100", 18);
@@ -39,34 +34,4 @@ test("should throw an error when given a negative input amount", () => {
   expect(() => {
     adjustAmountByPercentage({ amount: negativeAmount, decimals: 18 });
   }).toThrow(Error("Negative amounts are not allowed"));
-});
-
-test("should simulate and execute 'open long' operation correctly when mocking an open long operation", async () => {
-  await setupMintTokensAndApproveHyperdrive(ALICE);
-  const baseAmountIn = parseUnits("100", 18);
-  const minAmountOut = adjustAmountByPercentage({
-    amount: baseAmountIn,
-    decimals: 18,
-  });
-
-  const { request } = await publicClient.simulateContract({
-    abi: HyperdriveABI,
-    functionName: "openLong",
-    account: ALICE,
-    address: TestAddresses.mockHyperdrive,
-    args: [baseAmountIn, minAmountOut, ALICE, true],
-    value: 0n,
-  });
-  expect(request.args[1]).toBe(minAmountOut);
-
-  const hash = await publicClient.writeContract({
-    abi: HyperdriveABI,
-    functionName: "openLong",
-    account: ALICE,
-    address: TestAddresses.mockHyperdrive,
-    args: [baseAmountIn, minAmountOut, ALICE, true],
-    value: 0n,
-  });
-  const { status } = await publicClient.getTransactionReceipt({ hash });
-  expect(status).toEqual("success");
 });
