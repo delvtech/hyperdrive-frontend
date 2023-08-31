f3679f245d37eb7660a02e1d658854a9176c8bae
Danny Delott <dannydelott@gmail.com>
2023-08-31 09:30:12 -0700

    Add getFixedApr to ReadableHyperdrive

--- packages/hyperdrive-sdk/src/hyperdrive/ReadableHyperdrive.ts
+++ packages/hyperdrive-sdk/src/hyperdrive/ReadableHyperdrive.ts
@@ -1,17 +1,25 @@
+import { formatRate } from "src/base/formatRate";
 import { ContractReadOptions } from "src/contract/Contract";
 import { ReadableHyperdriveContract } from "src/hyperdrive/HyperdriveContract";
+import { HyperdriveMathContract } from "src/hyperdrive/HyperdriveMathContract";
 import { PoolConfig } from "src/pool/PoolConfig";
 import { PoolInfo } from "src/pool/PoolInfo";
 
 interface ReadableHyperdriveConstructorOptions {
   contract: ReadableHyperdriveContract;
+  mathContract: HyperdriveMathContract;
 }
 
 export class ReadableHyperdrive {
   private readonly contract: ReadableHyperdriveContract;
+  private readonly mathContract: HyperdriveMathContract;
 
-  constructor({ contract }: ReadableHyperdriveConstructorOptions) {
+  constructor({
+    contract,
+    mathContract,
+  }: ReadableHyperdriveConstructorOptions) {
     this.contract = contract;
+    this.mathContract = mathContract;
   }
 
   /**
@@ -31,5 +39,36 @@ export class ReadableHyperdrive {
    */
   getPoolInfo(options?: ContractReadOptions): Promise<PoolInfo> {
     return this.contract.read("getPoolInfo", [], options);
+  }
+
+  /**
+   * Gets the pool's fixed APR on bonds.
+   * @param options
+   * @returns
+   */
+  async getFixedApr(
+    options?: ContractReadOptions,
+  ): Promise<{ apr: bigint; formatted: string }> {
+    const { positionDuration, initialSharePrice, timeStretch } =
+      await this.getPoolConfig(options);
+    const { shareReserves, bondReserves } = await this.getPoolInfo(options);
+    const apr = await this.mathContract.read(
+      "calculateAPRFromReserves",
+      [
+        shareReserves,
+        bondReserves,
+        initialSharePrice,
+        positionDuration,
+        timeStretch,
+      ],
+      options,
+    );
+
+    const formatted = formatRate(apr);
+
+    return {
+      apr,
+      formatted,
+    };
   }
 }
