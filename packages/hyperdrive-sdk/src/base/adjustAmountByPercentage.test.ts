3e893439ad116bc91f0b1d12ba2c7f46191bd0fc
Danny Delott <dannydelott@gmail.com>
2023-08-31 10:56:14 -0700

    Add dnum package, remove use of parseUnits from viem in sdk code

--- packages/hyperdrive-sdk/src/base/adjustAmountByPercentage.test.ts
+++ packages/hyperdrive-sdk/src/base/adjustAmountByPercentage.test.ts
@@ -1,17 +1,17 @@
 import { expect, test } from "vitest";
 import { adjustAmountByPercentage } from "src/base/adjustAmountByPercentage";
-import { parseUnits } from "viem";
+import * as dnum from "dnum";
 
 test("should return adjusted amount when given a basic input", () => {
-  const amount = parseUnits("100", 18);
-  const expectedAmount = parseUnits("99", 18);
+  const [amount] = dnum.from("100", 18);
+  const [expectedAmount] = dnum.from("99", 18);
   expect(adjustAmountByPercentage({ amount, decimals: 18 })).toBe(
     expectedAmount,
   );
 });
 
 test("should handle precision accurately when given precise input amounts", () => {
-  const preciseAmount = parseUnits("100.123456", 18);
+  const [preciseAmount] = dnum.from("100.123456", 18);
   const expectedPreciseAmount = preciseAmount - preciseAmount / 100n;
   expect(
     adjustAmountByPercentage({ amount: preciseAmount, decimals: 18 }),
@@ -19,7 +19,7 @@ test("should handle precision accurately when given precise input amounts", () =
 });
 
 test("should round down to zero when given the smallest possible positive value", () => {
-  const roundingAmount = parseUnits("0.000000000000000001", 18);
+  const [roundingAmount] = dnum.from("0.000000000000000001", 18);
   expect(
     adjustAmountByPercentage({ amount: roundingAmount, decimals: 18 }),
   ).toBe(0n);
@@ -30,7 +30,7 @@ test("should return zero when input amount is zero", () => {
 });
 
 test("should throw an error when given a negative input amount", () => {
-  const negativeAmount = parseUnits("-100", 18);
+  const [negativeAmount] = dnum.from("-100", 18);
   expect(() => {
     adjustAmountByPercentage({ amount: negativeAmount, decimals: 18 });
   }).toThrow(Error("Negative amounts are not allowed"));
