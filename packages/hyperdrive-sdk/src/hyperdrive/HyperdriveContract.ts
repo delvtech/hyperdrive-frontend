94791e7c84743d3249ee67ccaf8e9bf3794c504f
Danny Delott <dannydelott@gmail.com>
2023-09-01 15:57:42 -0700

    These moved to their respective files in the last 2 commits

--- packages/hyperdrive-sdk/src/hyperdrive/HyperdriveContract.ts
+++ packages/hyperdrive-sdk/src/hyperdrive/HyperdriveContract.ts
@@ -1,14 +1,7 @@
 import { HyperdriveABI } from "@hyperdrive/core";
-import {
-  Contract,
-  ReadableContract,
-  WritableContract,
-} from "src/contract/Contract";
-
-export type ReadableHyperdriveContract = ReadableContract<typeof HyperdriveABI>;
-export type WritableHyperdriveContract = WritableContract<typeof HyperdriveABI>;
+import { Contract } from "src/contract/Contract";
 
 /**
  * Represents a contract utilizing the Hyperdrive ABI
  */
-export type HyperdriveContract = Contract<typeof HyperdriveABI>;
+export type IHyperdriveContract = Contract<typeof HyperdriveABI>;
