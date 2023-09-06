3b0e3f5fca1bf5ca1b4d66fdadd41eec98c0b624
Danny Delott <dannydelott@gmail.com>
2023-09-06 10:35:53 -0700

    Stub out methods for HyperdriveSdk

--- packages/hyperdrive-sdk/src/hyperdrive/HyperdriveSdk.ts
+++ packages/hyperdrive-sdk/src/hyperdrive/HyperdriveSdk.ts
@@ -1,4 +1,63 @@
-export class HyperdriveSdk {
-  // TODO: Implement a convenience class that allows consumers to access the
-  // read and write methods of hyperdrive in one place.
+import {
+  ContractReadOptions,
+  BlockTag,
+  ContractWriteOptions,
+} from "src/contract/Contract";
+import { IReadableHyperdrive } from "src/hyperdrive/ReadableHyperdrive";
+import { IWritableHyperdrive } from "src/hyperdrive/WritableHyperdrive";
+import { PoolConfig } from "src/pool/PoolConfig";
+import { PoolInfo } from "src/pool/PoolInfo";
+
+export class HyperdriveSdk implements IReadableHyperdrive, IWritableHyperdrive {
+  constructor() {}
+  getPoolConfig(
+    options?: ContractReadOptions | undefined,
+  ): Promise<PoolConfig> {
+    throw new Error("Method not implemented.");
+  }
+  getPoolInfo(options?: ContractReadOptions | undefined): Promise<PoolInfo> {
+    throw new Error("Method not implemented.");
+  }
+  getFixedRate(options?: ContractReadOptions | undefined): Promise<bigint> {
+    throw new Error("Method not implemented.");
+  }
+  getLiquidity(options?: ContractReadOptions | undefined): Promise<bigint> {
+    throw new Error("Method not implemented.");
+  }
+  getTradingVolume(
+    options?:
+      | {
+          fromBlock?: bigint | BlockTag | undefined;
+          toBlock?: bigint | BlockTag | undefined;
+        }
+      | undefined,
+  ): Promise<bigint> {
+    throw new Error("Method not implemented.");
+  }
+  getLongPrice(options?: ContractReadOptions | undefined): Promise<bigint> {
+    throw new Error("Method not implemented.");
+  }
+  checkpoint(
+    time: number,
+    options?: ContractWriteOptions | undefined,
+  ): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  pause(
+    paused: boolean,
+    options?: ContractWriteOptions | undefined,
+  ): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  initialize(
+    args: {
+      contribution: bigint;
+      apr: bigint;
+      destination: `0x${string}`;
+      asUnderlying?: boolean | undefined;
+    },
+    options?: ContractWriteOptions | undefined,
+  ): Promise<bigint> {
+    throw new Error("Method not implemented.");
+  }
 }
