894da5e0d62e9a48d32ce284882b413f1b6f8eb5
Danny Delott <dannydelott@gmail.com>
2023-09-13 10:09:32 -0700

    Refactor modal buttons

--- apps/hyperdrive-trading/src/ui/hyperdrive/lp/AddLiquidityModalButton/AddLiquidityModalButton.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/lp/AddLiquidityModalButton/AddLiquidityModalButton.tsx
@@ -1,6 +1,7 @@
-import { ChevronRightIcon, ClockIcon } from "@heroicons/react/24/outline";
+import { ChevronRightIcon, SquaresPlusIcon } from "@heroicons/react/24/outline";
 import { ReactElement } from "react";
 import { Hyperdrive } from "src/appconfig/types";
+import { ChecklistItem } from "src/ui/base/components/ChecklistItem/CheckedListItem";
 import { Modal } from "src/ui/base/components/Modal/Modal";
 import { Well } from "src/ui/base/components/Well/Well";
 import { AddLiquidityForm } from "src/ui/hyperdrive/lp/AddLiquidityForm/AddLiquidityForm";
@@ -19,19 +20,26 @@ export function AddLiquidityModalButton({
     >
       {({ showModal }) => (
         <Well interactive variant="primary" onClick={() => showModal()}>
-          <div className="flex w-52 flex-col gap-2 py-4 text-center">
-            <ClockIcon className="mb-2 h-16 text-primary" />
-            <p className="font-lato text-h5 font-light text-primary">
-              Add Liquidity
-            </p>
-            <p className="font-light">
-              Earn trading fees when users open longs or shorts
-            </p>
+          <div className="flex w-[272px] flex-col justify-between gap-2 p-4">
+            <SquaresPlusIcon className="mb-2 h-16 text-primary" />
+            <p className="text-h5">Add Liquidity</p>
+            <p className="font-bold">Earn trading fees and interest</p>
+            <div className="mt-4 ml-4 flex flex-col gap-2 ">
+              <ChecklistItem checked readOnly variant="primary">
+                Deposit {hyperdrive.baseToken.symbol} to back trades
+              </ChecklistItem>
+              <ChecklistItem checked readOnly variant="primary">
+                Idle capital accrues vault APY
+              </ChecklistItem>
+              <ChecklistItem checked readOnly variant="primary">
+                No rollovers, easy-to-use
+              </ChecklistItem>
+            </div>
             {/* Using a div styled as a button here just as a visual cue. Don't
             use a real button here since the Well is interactive already, and
             doing so would create invalid dom nesting of buttons. */}
             <div className="daisy-btn-primary daisy-btn-sm daisy-btn mt-2 justify-between gap-0 border-primary">
-              <span className="ml-4 flex-1 text-center">Add LP</span>
+              <span className="ml-4 flex-1 text-center">Add liquidity</span>
               <ChevronRightIcon className="h-3 text-right" />
             </div>
           </div>
--- apps/hyperdrive-trading/src/ui/hyperdrive/shorts/OpenShortModalButton/OpenShortModalButton.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/shorts/OpenShortModalButton/OpenShortModalButton.tsx
@@ -1,9 +1,11 @@
 import { BoltIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 import { ReactElement } from "react";
 import { Hyperdrive } from "src/appconfig/types";
+import { ChecklistItem } from "src/ui/base/components/ChecklistItem/CheckedListItem";
 import { Modal } from "src/ui/base/components/Modal/Modal";
 import { Well } from "src/ui/base/components/Well/Well";
 import { OpenShortForm } from "src/ui/hyperdrive/shorts/OpenShortForm/OpenShortForm";
+import { useVaultRate } from "src/ui/vaults/useVaultRate";
 
 const OPEN_SHORT_MODAL_ID = "openShortModal";
 export function OpenShortModalButton({
@@ -11,6 +13,10 @@ export function OpenShortModalButton({
 }: {
   hyperdrive: Hyperdrive;
 }): ReactElement {
+  const { vaultRate } = useVaultRate({
+    // TODO: temporary for now until this available via addresses.json
+    vaultAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+  });
   return (
     <Modal
       modalId={OPEN_SHORT_MODAL_ID}
@@ -18,19 +24,30 @@ export function OpenShortModalButton({
     >
       {({ showModal }) => (
         <Well interactive variant="accent" onClick={() => showModal()}>
-          <div className="flex w-52 flex-col gap-2 py-4 text-center">
+          <div className="flex w-[272px] flex-col justify-between gap-2 p-4">
             <BoltIcon className="mb-2 h-16 text-accent" />
-            <p className="font-lato text-h5 font-light text-accent">
-              Open a short
+            <p className="text-h5">Open a short</p>
+            <p className="font-bold">
+              Earn{" "}
+              <span className="text-accent">{vaultRate?.formatted}% APY</span>{" "}
+              on {hyperdrive.baseToken.symbol}
             </p>
-            <p className="font-light">
-              Earn yield by shorting the current bond price
-            </p>
+            <div className="mt-4 ml-4 flex flex-col gap-2 ">
+              <ChecklistItem readOnly checked variant="accent">
+                Variable rate yield
+              </ChecklistItem>
+              <ChecklistItem readOnly checked variant="accent">
+                Profit from rising fixed rates
+              </ChecklistItem>
+              <ChecklistItem readOnly checked variant="accent">
+                Redeemable before maturity
+              </ChecklistItem>
+            </div>
             {/* Using a div styled as a button here just as a visual cue. Don't
             use a real button here since the Well is interactive already, and
             doing so would create invalid dom nesting of buttons. */}
-            <div className="daisy-btn-accent daisy-btn-sm daisy-btn mt-2 justify-between gap-0">
-              <span className="ml-4 flex-1 text-center">Short</span>
+            <div className="daisy-btn-accent daisy-btn-sm daisy-btn mt-4 justify-between gap-0">
+              <span className="ml-4 flex-1 text-center">Open short</span>
               <ChevronRightIcon className="h-3 text-right" />
             </div>
           </div>
