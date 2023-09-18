b3af0a2783af136d9ab24509d0e0d4ea1c98887d
Jack Burrus <jhburrus3@gmail.com>
2023-09-18 17:48:57 -0600

    Adds createHyperdriveMathContract (#385)
    * adds create math
    
    * move out interface

---
+++ packages/hyperdrive-viem/src/utils/createHyperdriveMathContract.ts
@@ -0,0 +1,24 @@
+import { HyperdriveMathABI, SimpleCache } from "@hyperdrive/sdk";
+import { ViemCachedReadContract } from "src/contract/ViemCachedReadContract";
+import { Address, PublicClient } from "viem";
+
+interface CreateHyperdriveMathContractOptions {
+  address: Address;
+  publicClient: PublicClient;
+  cache?: SimpleCache;
+}
+export function createHyperdriveMathContract<
+  TOptions extends CreateHyperdriveMathContractOptions,
+>({ address, publicClient, cache }: TOptions): ViemHyperdriveMathContract {
+  return new ViemCachedReadContract({
+    abi: HyperdriveMathABI,
+    address,
+    publicClient,
+    cache,
+  });
+}
+
+// Defaulting to read-only contract because the Math Contract only has pure functions.
+type ViemHyperdriveMathContract = ViemCachedReadContract<
+  typeof HyperdriveMathABI
+>;
