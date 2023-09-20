bbd7a784b00b2d93fcff09af7112856aede73950
jackburrus <jhburrus3@gmail.com>
2023-09-20 13:37:18 -0600

    updates useClosedshorts

--- apps/hyperdrive-trading/src/ui/hyperdrive/shorts/hooks/useClosedShorts.ts
+++ apps/hyperdrive-trading/src/ui/hyperdrive/shorts/hooks/useClosedShorts.ts
@@ -1,11 +1,11 @@
-import { ClosedShort } from "@hyperdrive/core";
-import { getClosedShortsQuery } from "@hyperdrive/queries";
+import { ClosedShort } from "@hyperdrive/sdk/src/shorts/types";
 import { QueryStatus, useQuery } from "@tanstack/react-query";
+import { makeQueryKey } from "src/base/makeQueryKey";
+import { useReadHyperdrive } from "src/ui/hyperdrive/hooks/useReadHyperdrive";
 import { Address } from "viem";
-import { usePublicClient } from "wagmi";
 
 interface UseClosedShortsOptions {
-  account: Address | undefined;
+  account: Address;
   hyperdriveAddress: Address | undefined;
 }
 
@@ -19,14 +19,15 @@ export function useClosedShorts({
   closedShorts: ClosedShort[] | undefined;
   closedShortsStatus: QueryStatus;
 } {
-  const publicClient = usePublicClient();
-  const { data: closedShorts, status: closedShortsStatus } = useQuery(
-    getClosedShortsQuery({
-      traderAddress: account,
-      hyperdriveAddress,
-      publicClient: publicClient,
-    }),
-  );
+  const readHyperdrive = useReadHyperdrive(hyperdriveAddress);
+  const queryEnabled = !!readHyperdrive;
+  const { data: closedShorts, status: closedShortsStatus } = useQuery({
+    queryKey: makeQueryKey("closedShorts", { account, hyperdriveAddress }),
+    queryFn: queryEnabled
+      ? async () => readHyperdrive.getClosedShorts({ account })
+      : undefined,
+    enabled: queryEnabled,
+  });
 
   return { closedShorts, closedShortsStatus };
 }
