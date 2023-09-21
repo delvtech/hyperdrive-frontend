4a1ce5b7d617b31005f65cb082a2d79565b1dcc1
Danny Delott <dannydelott@gmail.com>
2023-09-21 16:42:31 -0600

    Delete shorts and amm queries

--- packages/hyperdrive-queries/src/index.ts
+++ packages/hyperdrive-queries/src/index.ts
@@ -1,16 +1,5 @@
-// AMM
-export { getFixedRateQuery } from "src/amm/getFixedRateQuery";
-
 // Vaults
 export { getVaultRateQuery } from "src/vaults/erc4626";
-
-// Shorts
-export { getOpenShortsQuery } from "src/amm/shorts/getOpenShorts";
-export { getClosedShortsQuery } from "src/amm/shorts/getClosedShorts";
-export { getMaxShortQuery } from "src/amm/shorts/getMaxShort";
-
-// Liquidity
-export { getLiquidityQuery } from "src/amm/getLiquidity";
 
 // Trading Volume
 export { getTradingVolumeQuery } from "src/amm/volume/getTradingVolume";
