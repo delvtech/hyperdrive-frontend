93a3227abcab1a6803fb1882b9f8051759688a6d
Danny Delott <dannydelott@gmail.com>
2023-09-21 16:48:45 -0600

    Remove more dead queries

--- packages/hyperdrive-queries/src/index.ts
+++ packages/hyperdrive-queries/src/index.ts
@@ -1,10 +1,2 @@
 // Vaults
 export { getVaultRateQuery } from "src/vaults/erc4626";
-
-// LP Shares
-export { getLpSharesQuery } from "src/amm/lp/getLpShares";
-export { getClosedLpSharesQuery } from "src/amm/lp/getClosedLpShares";
-
-// Withdrawal Shares
-export { getWithdrawalSharesQuery } from "src/amm/lp/getWithdrawalShares";
-export { getRedeemedWithdrawalSharesQuery } from "src/amm/lp/getRedeemedWithdrawalShares";
