3e85835b15f178dc6e01b3a708811400200a32db
jackburrus <jhburrus3@gmail.com>
2023-09-24 16:43:52 -0600

    site working

--- apps/hyperdrive-sdk-docs/src/pages/index.tsx
+++ apps/hyperdrive-sdk-docs/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
