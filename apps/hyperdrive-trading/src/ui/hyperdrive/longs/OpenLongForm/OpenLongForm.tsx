153d42d67978e45cfcd4aa420f48b85ecff222d3
Danny Delott <dannydelott@gmail.com>
2023-09-25 15:23:02 -0700

    Refactor callers of useMaxLong

--- apps/hyperdrive-trading/src/ui/hyperdrive/longs/OpenLongForm/OpenLongForm.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/longs/OpenLongForm/OpenLongForm.tsx
@@ -13,6 +13,7 @@ import { OpenLongPreview } from "src/ui/hyperdrive/longs/OpenLongPreview/OpenLon
 import { useTokenAllowance } from "src/ui/token/hooks/useTokenAllowance";
 import { useTokenApproval } from "src/ui/token/hooks/useTokenApproval";
 import { TokenInput } from "src/ui/token/TokenInput";
+import { formatUnits } from "viem";
 import { useAccount, useBalance } from "wagmi";
 
 interface OpenLongFormProps {
@@ -31,12 +32,11 @@ export function OpenLongForm({ market }: OpenLongFormProps): ReactElement {
   const { maxLong } = useMaxLong(market);
 
   let maxAmount: string | undefined;
-
   if (maxLong && baseTokenBalance) {
     maxAmount =
       maxLong.maxBaseIn > baseTokenBalance.value
         ? baseTokenBalance.formatted
-        : maxLong.formattedMaxBaseIn;
+        : formatUnits(maxLong.maxBaseIn, market.baseToken.decimals);
   }
 
   const { amount, amountAsBigInt, setAmount } = useNumericInput({
