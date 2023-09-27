8765485f9c40dbab8f2d437ce54199f2c782ed49
Danny Delott <dannydelott@gmail.com>
2023-09-27 16:36:44 -0700

    Update type, add comments

--- packages/hyperdrive-sdk/src/pool/PoolConfig.ts
+++ packages/hyperdrive-sdk/src/pool/PoolConfig.ts
@@ -1,7 +1,10 @@
 import { HyperdriveABI } from "src/abis/Hyperdrive";
 import { FunctionReturnType } from "src/base/abitype";
 
+// Given the unlikely scenario that getPoolConfig will have a second argument,
+// we can infer that the first element is always the PoolConfig. This approach
+// minimizes update points when the PoolConfig type changes in new versons.
 export type PoolConfig = FunctionReturnType<
   typeof HyperdriveABI,
   "getPoolConfig"
->;
+>[0];
--- packages/hyperdrive-sdk/src/pool/PoolInfo.ts
+++ packages/hyperdrive-sdk/src/pool/PoolInfo.ts
@@ -1,4 +1,10 @@
 import { HyperdriveABI } from "src/abis/Hyperdrive";
 import { FunctionReturnType } from "src/base/abitype";
 
-export type PoolInfo = FunctionReturnType<typeof HyperdriveABI, "getPoolInfo">;
+// Given the unlikely scenario that getPoolInfo will have a second argument,
+// we can infer that the first element is always the PoolInfo. This approach
+// minimizes update points when the PoolInfo type changes in new versons.
+export type PoolInfo = FunctionReturnType<
+  typeof HyperdriveABI,
+  "getPoolInfo"
+>[0];
