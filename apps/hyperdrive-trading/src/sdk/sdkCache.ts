6dee7397f1002fae277f7703e655219f006eb609
jackburrus <jhburrus3@gmail.com>
2023-09-28 08:04:33 -0600

    subscribe added to viem

--- apps/hyperdrive-trading/src/sdk/sdkCache.ts
+++ apps/hyperdrive-trading/src/sdk/sdkCache.ts
@@ -1,3 +1,6 @@
 import { LruSimpleCache } from "@hyperdrive/sdk";
+import { QueryCacheSdk } from "./queryCache";
 
 export const sdkCache = new LruSimpleCache({ max: 500 });
+
+export const querySdkCache = new QueryCacheSdk();
