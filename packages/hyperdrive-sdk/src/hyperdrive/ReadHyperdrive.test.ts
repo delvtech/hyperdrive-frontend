528c84ddaf10f29b22cdc7c10a30f13a1d39b748
Danny Delott <dannydelott@gmail.com>
2023-10-09 13:54:45 -0700

    Add comments, refactor test descriptions for clarity

--- packages/hyperdrive-sdk/src/hyperdrive/ReadHyperdrive.test.ts
+++ packages/hyperdrive-sdk/src/hyperdrive/ReadHyperdrive.test.ts
@@ -8,16 +8,24 @@ import { NetworkStub } from "src/network/stubs/NetworkStub";
 import { simplePoolConfig } from "src/pool/testing/simplePoolConfig";
 import { simplePoolInfo } from "src/pool/testing/simplePoolInfo";
 
-test("Should get the PoolConfig when getPoolConfig is called", async () => {
+// The sdk should return the exact PoolConfig from the contracts. It should not
+// do any conversions or transformations, eg: converting seconds to ms,
+// formatting bigints, etc..
+test("Should return the PoolConfig from the contract as-is when getPoolConfig is called", async () => {
   const { contract, readHyperdrive } = setupReadHyperdrive();
 
+  // stub out the contract call the sdk is going to make
   contract.stubRead("getPoolConfig", [simplePoolConfig]);
 
+  // The sdk should return the correct data
   const value = await readHyperdrive.getPoolConfig();
   expect(value).toBe(simplePoolConfig);
 });
 
-test("Should get the PoolInfo when getPoolInfo is called", async () => {
+// The sdk should return the exact PoolInfo from the contracts. It should not do
+// any conversions or transformations, eg: converting seconds into ms,
+// formatting bigints, etc..
+test("Should return the PoolInfo from the contract as-is when getPoolInfo is called", async () => {
   const { contract, readHyperdrive } = setupReadHyperdrive();
 
   contract.stubRead("getPoolInfo", [simplePoolInfo]);
@@ -26,25 +34,13 @@ test("Should get the PoolInfo when getPoolInfo is called", async () => {
   expect(value).toBe(simplePoolInfo);
 });
 
+// The sdk should return the exact APR from the contracts. It should not do any
+// conversions or transformations, eg: formatting bigints, etc..
 test("Should get the fixed rate when getFixedRate is called", async () => {
   const { contract, mathContract, readHyperdrive } = setupReadHyperdrive();
 
   // These are necessary to stub, but the values won't be used since we stub
   // calculateAPRFromReserves directly
-  contract.stubRead("getPoolConfig", [simplePoolConfig]);
-  contract.stubRead("getPoolInfo", [simplePoolInfo]);
-
-  mathContract.stubRead("calculateAPRFromReserves", [1n]);
-
-  const value = await readHyperdrive.getFixedRate();
-  expect(value).toBe(1n);
-});
-
-test("Should get the fixed rate when getFixedRate is called", async () => {
-  const { contract, mathContract, readHyperdrive } = setupReadHyperdrive();
-
-  // These are necessary to stub, but the values won't be used since we also
-  // have to stub calculateAPRFromReserves directly
   contract.stubRead("getPoolConfig", [simplePoolConfig]);
   contract.stubRead("getPoolInfo", [simplePoolInfo]);
 
