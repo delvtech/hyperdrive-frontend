9c740224c4fe59d662c24c10249fc23a5badae6c
jackburrus <jhburrus3@gmail.com>
2023-10-09 16:07:13 -0600

    events working

--- apps/hyperdrive-trading/src/ui/app/Table/useTransactionData.ts
+++ apps/hyperdrive-trading/src/ui/app/Table/useTransactionData.ts
@@ -1,8 +1,21 @@
 import { useQueries } from "@tanstack/react-query";
 import { Hyperdrive } from "src/appconfig/types";
 import { useReadHyperdrive } from "src/ui/hyperdrive/hooks/useReadHyperdrive";
+type TransactionData = {
+  assetId: bigint;
+  baseAmount: bigint;
+  bondAmount: bigint;
+  eventName: string;
+  timestamp: bigint;
+  trader: string;
+};
 
-export function useTransactionData(hyperdrive: Hyperdrive): any[] {
+type RowType = {
+  type: string;
+  value: string;
+  account: string;
+};
+export function useTransactionData(hyperdrive: Hyperdrive): RowType[] {
   const readHyperdrive = useReadHyperdrive(hyperdrive.address);
   const events = useQueries({
     queries: [
@@ -16,17 +29,17 @@ export function useTransactionData(hyperdrive: Hyperdrive): any[] {
       },
     ],
   });
-  console.log(events, "events");
-  return [
-    {
-      type: "Open Long",
-      value: "1000",
-      account: "0x1234",
-    },
-    {
-      type: "Open Short",
-      value: "1000",
-      account: "0x1234",
-    },
-  ];
+
+  const combinedEvents = events.flatMap<TransactionData>(
+    ({ data: eventData }) => eventData,
+  );
+  const mappedEvents = combinedEvents.map((event) => {
+    return {
+      type: event.eventName,
+      value: event.baseAmount.toString(),
+      account: event.trader,
+    };
+  });
+
+  return mappedEvents;
 }
