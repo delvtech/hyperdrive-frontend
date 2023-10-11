c45354b8e77ab9b465e004a5451cd4df4c8f1858
Danny Delott <dannydelott@gmail.com>
2023-10-11 12:16:44 -0700

    Refactor TokenInput to daisy components

--- apps/hyperdrive-trading/src/ui/token/TokenInput.tsx
+++ apps/hyperdrive-trading/src/ui/token/TokenInput.tsx
@@ -1,14 +1,28 @@
-import { ReactElement } from "react";
+import classNames from "classnames";
+import { ReactElement, ReactNode } from "react";
 import { Token } from "src/appconfig/types";
 
 interface TokenInputProps {
   token: Token;
   value: string;
   onChange: (newAmount: string) => void;
+  /**
+   * If provided, the MAX button will be shown
+   */
   maxValue?: string;
-  maxLabel?: string;
-  showMax?: boolean;
+  /**
+   * If provided, this text will be used instead for the input's label
+   */
+  inputLabel?: string;
+  /**
+   * Optional stat to show, useful for things like wallet balances
+   */
+  stat?: ReactNode;
   disabled?: boolean;
+  /**
+   * If true, this will render the input with error styling
+   */
+  hasError?: boolean;
   autoFocus?: boolean;
 }
 
@@ -17,17 +31,31 @@ export function TokenInput({
   token,
   onChange,
   maxValue,
-  maxLabel = "Max",
-  showMax = true,
+  inputLabel = "Enter amount",
+  stat,
+  hasError = false,
   disabled = false,
   autoFocus = false,
 }: TokenInputProps): ReactElement {
   return (
-    <div className=" flex flex-col gap-y-2 rounded border border-neutral-content bg-base-100 p-4">
-      <div className="flex gap-x-4">
+    <div className="daisy-form-control flex-1">
+      <label className="daisy-label flex justify-between">
+        <span className="daisy-label-text">{inputLabel}</span>
+        {stat ? <span className="daisy-label-text">{stat}</span> : null}
+      </label>
+      <label className="daisy-input-group">
+        <span>
+          {(() => {
+            if (token.iconUrl) {
+              return <img className="h-6" src={token.iconUrl} />;
+            }
+            if (token.symbol) {
+              return <span className="daisy-label-text">{token.symbol}</span>;
+            }
+          })()}
+        </span>
         <input
-          autoFocus={autoFocus}
-          className="w-full bg-transparent text-h4 font-semibold outline-neutral-content placeholder:text-neutral-content focus:outline-dashed"
+          type="number"
           // Setting step to `any` allows any number between min and max to be
           // displayed without triggering the browser's native validation
           // tooltips. We do this because by default steps are whole numbers.
@@ -35,36 +63,31 @@ export function TokenInput({
           // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/number#step
           step="any"
           min="0"
+          autoFocus={autoFocus}
           max={maxValue}
-          name="Base Token Input"
-          onChange={(event) => onChange(event.target.value)}
-          placeholder="0"
-          type="number"
-          value={value}
+          name={`${token.symbol} input`}
           disabled={disabled}
+          className={classNames("daisy-input-bordered daisy-input flex-1", {
+            "daisy-input-error text-error": hasError,
+          })}
+          value={value}
+          placeholder="0"
+          onChange={(event) => onChange(event.target.value)}
         />
-
-        <div className="flex min-w-fit items-center gap-x-2">
-          {token.iconUrl && <img className="h-6" src={token.iconUrl} />}
-          <h4 className="font-semibold uppercase">{token.symbol}</h4>
-        </div>
-      </div>
-
-      {showMax && maxValue !== undefined && (
-        <div className="flex text-neutral-content">
-          {maxValue && (
-            <span
-              onClick={() => onChange(maxValue)}
-              className="mr-auto cursor-pointer underline hover:no-underline"
-            >
-              Max
-            </span>
-          )}
-          <p>
-            {maxLabel}: {maxValue}
-          </p>
-        </div>
-      )}
+        {maxValue !== undefined ? (
+          <button
+            className={classNames("daisy-glass daisy-btn-square daisy-btn", {
+              "daisy-btn-error": hasError,
+            })}
+            onClick={(e) => {
+              e.preventDefault();
+              onChange(maxValue);
+            }}
+          >
+            Max
+          </button>
+        ) : null}
+      </label>
     </div>
   );
 }
