483dc648a5ffc9b979dc5a2c8b8ba83bc8c06ebf
Danny Delott <dannydelott@gmail.com>
2023-10-12 15:01:19 -0700

    Add comment

--- apps/hyperdrive-trading/src/ui/base/components/Well/Well.tsx
+++ apps/hyperdrive-trading/src/ui/base/components/Well/Well.tsx
@@ -19,6 +19,7 @@ export function Well({
 }: PropsWithChildren<WellProps>): ReactElement {
   const isInteractive = !disabled && (interactive || onClick);
   const className = classNames("daisy-card p-4 bg-base-200 border border-1", {
+    // Shadow style from https://manuarora.in/boxshadows
     "shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]":
       elevation === "elevated",
     "w-full": block,
