22dbb12831ef89c86bb03dc1cc974c34b8d82fa2
Danny Delott <dannydelott@gmail.com>
2023-10-17 17:58:50 -0700

    Create reusable transaction view component

---
+++ apps/hyperdrive-trading/src/ui/hyperdrive/TransactionView.tsx
@@ -0,0 +1,39 @@
+import { ReactElement, ReactNode } from "react";
+import { Well } from "src/ui/base/components/Well/Well";
+
+interface TransactionViewProps {
+  heading: string;
+  tokenInput: ReactNode;
+  transactionPreview: ReactNode;
+  disclaimer: ReactNode;
+
+  actionButton: ReactNode;
+}
+
+export function TransactionView({
+  heading,
+  tokenInput,
+  transactionPreview,
+  disclaimer,
+  actionButton,
+}: TransactionViewProps): ReactElement {
+  return (
+    <div className="flex flex-col gap-4">
+      <h5 className="font-bold">{heading}</h5>
+      {tokenInput}
+
+      <div className="mt-4 flex flex-col gap-6">
+        <Well elevation="flat">
+          <div className="space-y-4">
+            <span className="text-h6 font-bold">Preview transaction</span>
+            {transactionPreview}
+          </div>
+        </Well>
+
+        <p className="text-center text-body">{disclaimer}</p>
+
+        {actionButton}
+      </div>
+    </div>
+  );
+}
