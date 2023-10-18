4777b60d88c5bcd1bc37ee3dd7f160aa3d93dac0
Danny Delott <dannydelott@gmail.com>
2023-10-17 18:47:35 -0700

    Refactor Redeem withdrawal shares and remove lp modals

--- apps/hyperdrive-trading/src/ui/hyperdrive/lp/RemoveLiquidityForm/RemoveLiquidityForm.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/lp/RemoveLiquidityForm/RemoveLiquidityForm.tsx
@@ -2,9 +2,9 @@ import { adjustAmountByPercentage } from "@hyperdrive/sdk";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { MouseEvent, ReactElement } from "react";
 import { Hyperdrive } from "src/appconfig/types";
-import { Well } from "src/ui/base/components/Well/Well";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
 import { useNumericInput } from "src/ui/base/hooks/useNumericInput";
+import { TransactionView } from "src/ui/hyperdrive/TransactionView";
 import { usePreviewRemoveLiquidity } from "src/ui/hyperdrive/lp/hooks/usePreviewRemoveLiquidity";
 import { useRemoveLiquidity } from "src/ui/hyperdrive/lp/hooks/useRemoveLiquidity";
 import { TokenInput } from "src/ui/token/TokenInput";
@@ -71,51 +71,47 @@ export function RemoveLiquidityForm({
     : null;
 
   return (
-    <div className="flex flex-col gap-4">
-      <h5 className="font-bold">Remove liquidity</h5>
-      <TokenInput
-        token={{
-          name: "Hyperdrive LP",
-          symbol: "LP shares",
-          decimals: baseDecimals,
-          address: "0x00",
-        }}
-        value={amount ?? ""}
-        maxValue={formatUnits(lpShares, baseDecimals)}
-        stat={
-          lpShares
-            ? `Balance: ${formatBalance({
-                balance: lpShares,
-                decimals: baseDecimals,
-                places: 4,
-              })} LP shares`
-            : undefined
-        }
-        onChange={(newAmount) => setAmount(newAmount)}
-      />
-
-      {/* You receive Section */}
-      <div className="mt-4 flex flex-col gap-6">
-        <Well elevation="flat">
-          <div className="space-y-4">
-            <span className="text-h6 font-bold">Preview transaction</span>
-            <div className="flex flex-col gap-1">
-              <div className="flex justify-between">
-                <p>You receive</p>
-                <p>
-                  {formattedBaseAmountOut || 0} {hyperdrive.baseToken.symbol}
-                </p>
-              </div>
-
-              <div className="flex justify-between">
-                <p>Withdrawal shares</p>
-                <p>{formattedWithdrawalSharesOut || 0} Shares</p>
-              </div>
-            </div>
+    <TransactionView
+      heading="Remove liquidity"
+      tokenInput={
+        <TokenInput
+          token={{
+            name: "Hyperdrive LP",
+            symbol: "LP shares",
+            decimals: baseDecimals,
+            address: "0x00",
+          }}
+          value={amount ?? ""}
+          maxValue={formatUnits(lpShares, baseDecimals)}
+          stat={
+            lpShares
+              ? `Balance: ${formatBalance({
+                  balance: lpShares,
+                  decimals: baseDecimals,
+                  places: 6,
+                })} LP shares`
+              : undefined
+          }
+          onChange={(newAmount) => setAmount(newAmount)}
+        />
+      }
+      transactionPreview={
+        <div className="flex flex-col gap-1">
+          <div className="flex justify-between">
+            <p>You receive</p>
+            <p>
+              {formattedBaseAmountOut || 0} {hyperdrive.baseToken.symbol}
+            </p>
           </div>
-        </Well>
 
-        {account ? (
+          <div className="flex justify-between">
+            <p>Withdrawal shares</p>
+            <p>{formattedWithdrawalSharesOut || 0} Shares</p>
+          </div>
+        </div>
+      }
+      actionButton={
+        account ? (
           <button
             className="daisy-btn-primary daisy-btn"
             disabled={!removeLiquidity || removeLiquidityStatus === "loading"}
@@ -128,8 +124,8 @@ export function RemoveLiquidityForm({
           </button>
         ) : (
           <ConnectButton />
-        )}
-      </div>
-    </div>
+        )
+      }
+    />
   );
 }
