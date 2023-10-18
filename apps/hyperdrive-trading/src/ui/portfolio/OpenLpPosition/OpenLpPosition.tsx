9c93638769dfdbd30366467ca5f631393b7624a5
jackburrus <jhburrus3@gmail.com>
2023-10-18 09:22:43 -0600

    cleanup

--- apps/hyperdrive-trading/src/ui/portfolio/OpenLpPosition/OpenLpPosition.tsx
+++ apps/hyperdrive-trading/src/ui/portfolio/OpenLpPosition/OpenLpPosition.tsx
@@ -1,4 +1,3 @@
-import { multiplyBigInt } from "@hyperdrive/sdk";
 import {
   Row,
   createColumnHelper,
@@ -12,7 +11,6 @@ import { Hyperdrive } from "src/appconfig/types";
 import { NonIdealState } from "src/ui/base/components/NonIdealState";
 import { CellWithTooltip } from "src/ui/base/components/tables/SortableGridTable";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
-import { usePoolInfo } from "src/ui/hyperdrive/hooks/usePoolInfo";
 import { RedeemWithdrawalSharesModalButton } from "src/ui/hyperdrive/lp/RedeemWithdrawalSharesModalButton/RedeemWithdrawalSharesModalButton";
 import { RemoveLiquidityModalButton } from "src/ui/hyperdrive/lp/RemoveLiquidityModalButton/RemoveLiquidityModalButton";
 import { useLpShares } from "src/ui/hyperdrive/lp/hooks/useLpShares";
@@ -42,9 +40,8 @@ function getColumns(hyperdrive: Hyperdrive) {
           content="Position"
         />
       ),
-      cell: ({ getValue, row }) => {
+      cell: ({ getValue }) => {
         const lpShares = getValue();
-        const withdrawalShares = row.original.withdrawalShares;
 
         return (
           <span key="type" className="font-semibold uppercase">
@@ -131,7 +128,6 @@ function CurrentValueCell({
   const { address: account } = useAccount();
   const lpShares = row.original.lpShares;
   const withdrawalShares = row.original.withdrawalShares;
-  const poolInfo = usePoolInfo(hyperdrive.address);
   const { baseAmountOut: withdrawalSharesBaseWithdrawable } =
     usePreviewRedeemWithdrawalShares({
       market: hyperdrive,
@@ -197,115 +193,12 @@ export function OpenLpPosition({
       },
     ];
   }, [lpShares, withdrawalShares]);
-  const { baseAmountOut: lpBaseWithdrawable } = usePreviewRemoveLiquidity({
-    market: hyperdrive,
-    lpSharesIn: lpShares,
-    minBaseAmountOut: 1n, // TODO: slippage,
-    destination: account,
-  });
-
-  const { baseAmountOut: withdrawalSharesBaseWithdrawable } =
-    usePreviewRedeemWithdrawalShares({
-      market: hyperdrive,
-      withdrawalSharesIn: withdrawalShares,
-      minBaseAmountOutPerShare: 1n, // TODO: slippage,
-      destination: account,
-    });
-
-  const { poolInfo } = usePoolInfo(hyperdrive.address);
-
   const tableInstance = useReactTable({
     columns: getColumns(hyperdrive),
     data: memoizedData as LpRowType[],
     getCoreRowModel: getCoreRowModel(),
   });
 
-  const rows: any[] = [];
-  if (lpShares) {
-    rows.push([
-      <span key="type" className="font-semibold uppercase">
-        LP
-      </span>,
-      formatBalance({
-        balance: lpShares,
-        decimals: (hyperdrive as Hyperdrive).baseToken.decimals,
-        places: 4,
-      }),
-      <span key="value">
-        {!!poolInfo ? (
-          `${formatBalance({
-            balance: multiplyBigInt(
-              [lpShares, poolInfo.lpSharePrice],
-              hyperdrive.baseToken.decimals,
-            ),
-            decimals: hyperdrive.baseToken.decimals,
-            places: 4,
-          })} ${hyperdrive.baseToken.symbol}`
-        ) : (
-          <Skeleton />
-        )}
-      </span>,
-      <span key="withdrawable">
-        {lpBaseWithdrawable !== undefined ? (
-          `${formatBalance({
-            balance: lpBaseWithdrawable,
-            decimals: hyperdrive.baseToken.decimals,
-          })} ${hyperdrive.baseToken.symbol}`
-        ) : (
-          <Skeleton />
-        )}
-      </span>,
-      <span key="remove-liquidity" className="flex justify-end">
-        <RemoveLiquidityModalButton
-          modalId="remove-liquidity-modal"
-          hyperdrive={hyperdrive}
-          lpShares={lpShares}
-        />
-      </span>,
-    ]);
-  }
-
-  if (withdrawalShares) {
-    rows.push([
-      <span key="type" className="font-semibold uppercase">
-        Pending withdrawal
-      </span>,
-      formatBalance({
-        balance: withdrawalShares,
-        decimals: hyperdrive.baseToken.decimals,
-      }),
-      <span key="value">
-        {!!poolInfo ? (
-          `${formatBalance({
-            balance: multiplyBigInt(
-              [withdrawalShares, poolInfo.lpSharePrice],
-              hyperdrive.baseToken.decimals,
-            ),
-            decimals: hyperdrive.baseToken.decimals,
-          })} ${hyperdrive.baseToken.symbol}`
-        ) : (
-          <Skeleton />
-        )}
-      </span>,
-      <span key="withdrawable">
-        {withdrawalSharesBaseWithdrawable !== undefined ? (
-          `${formatBalance({
-            balance: withdrawalSharesBaseWithdrawable,
-            decimals: hyperdrive.baseToken.decimals,
-          })} ${hyperdrive.baseToken.symbol}`
-        ) : (
-          <Skeleton />
-        )}
-      </span>,
-      <span key="redeem-withdraw-shares" className="flex justify-end">
-        <RedeemWithdrawalSharesModalButton
-          modalId="redeem-withdrawal-shares-modal"
-          hyperdrive={hyperdrive}
-          withdrawalShares={withdrawalShares}
-        />
-      </span>,
-    ]);
-  }
   return (
     <div className="max-h-96 overflow-y-scroll">
       <table className="daisy-table-zebra daisy-table daisy-table-lg">
