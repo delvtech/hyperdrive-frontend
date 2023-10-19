23509a7783d376e56227abb6dd6dff3ef443858d
jackburrus <jhburrus3@gmail.com>
2023-10-19 15:17:09 -0600

    adds for rest

--- apps/hyperdrive-trading/src/ui/portfolio/OpenShortsTable/OpenShortsTable.tsx
+++ apps/hyperdrive-trading/src/ui/portfolio/OpenShortsTable/OpenShortsTable.tsx
@@ -157,7 +157,7 @@ export function OpenShortsTable({
           {tableInstance.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
-                <th key={header.id}>
+                <th className="sticky top-0 bg-white" key={header.id}>
                   {header.isPlaceholder
                     ? null
                     : flexRender(
