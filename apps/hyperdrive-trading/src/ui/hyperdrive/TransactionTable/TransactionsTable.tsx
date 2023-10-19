4f8da3010a365ae254412cf7545050a72e37ebd3
jackburrus <jhburrus3@gmail.com>
2023-10-19 15:13:32 -0600

    adds sticky headers

--- apps/hyperdrive-trading/src/ui/hyperdrive/TransactionTable/TransactionsTable.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/TransactionTable/TransactionsTable.tsx
@@ -110,7 +110,7 @@ export function TransactionTable({
           {tableInstance.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
-                <th key={header.id}>
+                <th className="sticky top-0 bg-white" key={header.id}>
                   {header.isPlaceholder ? null : (
                     <>
                       <div
