c140e4c05cfb2feebc3b29649ea9ad7ce2ed0dec
Ryan Goree <goree.ryan@gmail.com>
2023-10-30 12:55:24 -0400

    Add CLI package (#477)
    * Add CLI package
    
    * Add server command
    
    * Remove test script from package.json

---
+++ packages/hyperdrive-cli/src/hyperdrive.ts
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+import path from "node:path";
+import signale from "signale";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import {
+  COMMAND_FILE_EXTENSIONS,
+  selectCommandHandler,
+} from "./utils/selectCommandHandler";
+
+const commandDir = "./commands";
+
+const parser = yargs(hideBin(process.argv))
+  .commandDir(commandDir, {
+    extensions: COMMAND_FILE_EXTENSIONS,
+  })
+  .command(
+    "$0",
+    "Start a guided walkthrough",
+    () => {},
+    selectCommandHandler({
+      commandsPath: path.resolve(__dirname, commandDir),
+      message: `Hyperdrive engaged, select a command:`,
+      isRoot: true,
+    }),
+  )
+  // turn off yargs error handling
+  .fail(false);
+
+async function main() {
+  try {
+    await parser.parse();
+  } catch (err) {
+    const help = await parser.getHelp();
+    signale.error(`${err}\n\n${help}`);
+  }
+}
+
+main();
