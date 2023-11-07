df2de1ef43d9b788c47a25e47c733bc46a30b1fa
jackburrus <jhburrus3@gmail.com>
2023-11-06 17:25:51 -0700

    remove hyperdrive from evm client

--- packages/evm-client/src/contract/cached/CachedReadContract/CachedReadContract.test.ts
+++ packages/evm-client/src/contract/cached/CachedReadContract/CachedReadContract.test.ts
@@ -1,74 +1,74 @@
-import { expect, test } from "vitest";
+import { IERC20 } from "src/base/IERC20";
+import { ALICE, BOB } from "src/base/testing/accounts";
 import { ContractEvent } from "src/contract/ContractEvents";
-import { ReadContractStub } from "src/contract/stubs/ReadContractStub/ReadContractStub";
 import { CachedReadContract } from "src/contract/cached/CachedReadContract/CachedReadContract";
-import { IHyperdrive } from "@hyperdrive/artifacts/dist/IHyperdrive";
-
-const HyperdriveABI = IHyperdrive.abi;
+import { ReadContractStub } from "src/contract/stubs/ReadContractStub/ReadContractStub";
+import { expect, test } from "vitest";
+const ERC20ABI = IERC20.abi;
 
 test("It caches the read function", async () => {
-  const contract = new ReadContractStub(HyperdriveABI);
+  const contract = new ReadContractStub(ERC20ABI);
   const cachedContract = new CachedReadContract({ contract });
 
   const stubbedValue = ["0x123abc"] as const;
   contract.stubRead({
-    functionName: "baseToken",
+    functionName: "name",
     args: [],
     value: stubbedValue,
   });
 
-  const value = await cachedContract.read("baseToken", []);
+  const value = await cachedContract.read("name", []);
   expect(value).toBe(stubbedValue);
 
-  const value2 = await cachedContract.read("baseToken", []);
+  const value2 = await cachedContract.read("name", []);
   expect(value2).toBe(stubbedValue);
 
-  const stub = contract.getReadStub("baseToken");
+  const stub = contract.getReadStub("name");
   expect(stub?.callCount).toBe(1);
 });
 
 test("It caches the getEvents function", async () => {
-  const contract = new ReadContractStub(HyperdriveABI);
+  const contract = new ReadContractStub(ERC20ABI);
   const cachedContract = new CachedReadContract({ contract });
 
-  const stubbedEvents: ContractEvent<typeof HyperdriveABI, "AddLiquidity">[] = [
+  const stubbedEvents: ContractEvent<typeof ERC20ABI, "Transfer">[] = [
     {
-      eventName: "AddLiquidity",
+      eventName: "Transfer",
       args: {
-        baseAmount: 100n,
-        lpAmount: 100n,
-        provider: "0x123abc",
+        from: ALICE,
+        to: BOB,
+        value: 100n,
       },
       blockNumber: 1n,
       data: "0x123abc",
       transactionHash: "0x123abc",
     },
   ];
-  contract.stubEvents("AddLiquidity", stubbedEvents);
+  contract.stubEvents("Transfer", stubbedEvents);
 
-  const events = await cachedContract.getEvents("AddLiquidity");
+  const events = await cachedContract.getEvents("Transfer");
   expect(events).toBe(stubbedEvents);
 
-  const events2 = await cachedContract.getEvents("AddLiquidity");
+  const events2 = await cachedContract.getEvents("Transfer");
   expect(events2).toBe(stubbedEvents);
 
-  const stub = contract.getEventsStub("AddLiquidity");
+  const stub = contract.getEventsStub("Transfer");
   expect(stub?.callCount).toBe(1);
 });
 
 test("The deleteRead function deletes the cached read value", async () => {
-  const contract = new ReadContractStub(HyperdriveABI);
+  const contract = new ReadContractStub(ERC20ABI);
   const cachedContract = new CachedReadContract({ contract });
 
   const stubbedValue = [100n] as const;
   contract.stubRead({ functionName: "balanceOf", value: stubbedValue });
 
-  const value = await cachedContract.read("balanceOf", [0n, "0x123abc"]);
+  const value = await cachedContract.read("balanceOf", ["0x123abc"]);
   expect(value).toBe(stubbedValue);
 
-  cachedContract.deleteRead("balanceOf", [0n, "0x123abc"]);
+  cachedContract.deleteRead("balanceOf", ["0x123abc"]);
 
-  const value2 = await cachedContract.read("balanceOf", [0n, "0x123abc"]);
+  const value2 = await cachedContract.read("balanceOf", ["0x123abc"]);
   expect(value2).toBe(stubbedValue);
 
   const stub = contract.getReadStub("balanceOf");
@@ -76,22 +76,27 @@ test("The deleteRead function deletes the cached read value", async () => {
 });
 
 test("It clears the cache", async () => {
-  const contract = new ReadContractStub(HyperdriveABI);
+  const contract = new ReadContractStub(ERC20ABI);
   const cachedContract = new CachedReadContract({ contract });
 
   contract.stubRead({ functionName: "balanceOf", value: [100n] });
-  contract.stubRead({ functionName: "factory", value: ["0x123abc"] });
+  const stubbedValue = ["0x123abc"] as const;
+  contract.stubRead({
+    functionName: "name",
+    args: [],
+    value: stubbedValue,
+  });
 
-  await cachedContract.read("balanceOf", [0n, "0x123abc"]);
-  await cachedContract.read("factory", []);
+  await cachedContract.read("balanceOf", ["0x123abc"]);
+  await cachedContract.read("name", []);
 
   cachedContract.clearCache();
 
-  await cachedContract.read("balanceOf", [0n, "0x123abc"]);
-  await cachedContract.read("factory", []);
+  await cachedContract.read("balanceOf", ["0x123abc"]);
+  await cachedContract.read("name", []);
 
   const stubA = contract.getReadStub("balanceOf");
-  const stubB = contract.getReadStub("factory");
+  const stubB = contract.getReadStub("name");
   expect(stubA?.callCount).toBe(2);
   expect(stubB?.callCount).toBe(2);
 });
--- packages/evm-client/src/contract/stubs/ReadWriteContractStub/ReadWriteContractStub.test.ts
+++ packages/evm-client/src/contract/stubs/ReadWriteContractStub/ReadWriteContractStub.test.ts
@@ -1,16 +1,16 @@
-import { IHyperdrive } from "@hyperdrive/artifacts/dist/IHyperdrive";
+import { IERC20 } from "src/base/IERC20";
 import { ReadWriteContractStub } from "src/contract/stubs/ReadWriteContractStub/ReadWriteContractStub";
 import { expect, test } from "vitest";
-
+const ERC20ABI = IERC20.abi;
 test("It stubs the write function", async () => {
-  const contract = new ReadWriteContractStub(IHyperdrive.abi);
+  const contract = new ReadWriteContractStub(ERC20ABI);
 
-  const stubbedValue = [100n] as const;
-  contract.stubWrite("addLiquidity", stubbedValue);
+  const stubbedValue = [true] as const;
+  contract.stubWrite("transfer", stubbedValue);
 
-  const value = await contract.write("addLiquidity", []);
+  const value = await contract.write("transfer", [true]);
   expect(value).toBe(stubbedValue);
 
-  const stub = contract.getWriteStub("addLiquidity");
+  const stub = contract.getWriteStub("transfer");
   expect(stub?.callCount).toBe(1);
 });
