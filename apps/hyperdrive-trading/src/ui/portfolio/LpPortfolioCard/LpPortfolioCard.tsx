eb5d722568451240617521bae9247241e6168e7b
Danny Delott <dannydelott@gmail.com>
2023-11-10 20:53:03 -0800

    Polish Non ideal state when no LP shares

--- apps/hyperdrive-trading/src/ui/portfolio/LpPortfolioCard/LpPortfolioCard.tsx
+++ apps/hyperdrive-trading/src/ui/portfolio/LpPortfolioCard/LpPortfolioCard.tsx
@@ -1,9 +1,12 @@
+import { InboxIcon } from "@heroicons/react/24/outline";
+import classNames from "classnames";
 import * as dnum from "dnum";
 import { ReactElement } from "react";
 import Skeleton from "react-loading-skeleton";
 import { Hyperdrive } from "src/appconfig/types";
 import { formatRate } from "src/base/formatRate";
 import { Modal } from "src/ui/base/components/Modal/Modal";
+import { NonIdealState } from "src/ui/base/components/NonIdealState";
 import { Well } from "src/ui/base/components/Well/Well";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
 import { usePoolInfo } from "src/ui/hyperdrive/hooks/usePoolInfo";
@@ -53,141 +56,147 @@ export function LpPortfolioCard({
     <Well>
       <div className="flex w-80 flex-col gap-4">
         <span className="text-h6 font-bold">LP Shares</span>
-        <div className="mb-4 flex flex-col gap-2">
-          <div className="flex flex-col">
-            <div className="flex justify-between">
-              <p
-                className="daisy-tooltip inline-flex cursor-help items-center gap-1 border-b border-dashed border-current"
-                data-tip="Your share of the total liquidity in the pool"
-              >
-                Pool share
-              </p>
-              <p>
-                {!!lpShares && !!lpSharesTotalSupply ? (
-                  `${formatRate(
-                    calculateRatio({
-                      a: lpShares,
-                      b: lpSharesTotalSupply,
-                      decimals: hyperdrive.baseToken.decimals,
-                    }),
-                    hyperdrive.baseToken.decimals,
-                  )}%`
-                ) : (
-                  <Skeleton />
-                )}
+        {lpShares !== 0n ? (
+          <>
+            <div className="mb-4 flex flex-col gap-2">
+              <div className="flex flex-col">
+                <div className="flex justify-between">
+                  <p
+                    className="daisy-tooltip inline-flex cursor-help items-center gap-1 border-b border-dashed border-current"
+                    data-tip="Your share of the total liquidity in the pool"
+                  >
+                    Pool share
+                  </p>
+                  <p>
+                    {!!lpShares && !!lpSharesTotalSupply ? (
+                      `${formatRate(
+                        calculateRatio({
+                          a: lpShares,
+                          b: lpSharesTotalSupply,
+                          decimals: hyperdrive.baseToken.decimals,
+                        }),
+                        hyperdrive.baseToken.decimals,
+                      )}%`
+                    ) : (
+                      <Skeleton />
+                    )}
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col">
+                <div className="flex justify-between">
+                  <p className="">LP balance</p>
+                  <p className="">
+                    {!lpShares ? (
+                      <Skeleton />
+                    ) : (
+                      formatBalance({
+                        balance: lpShares,
+                        decimals: hyperdrive.baseToken.decimals,
+                        places: 4,
+                      })
+                    )}{" "}
+                    Shares
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col">
+                <div className="flex justify-between">
+                  <p className="">Current value</p>
+                  <p className="font-bold">
+                    {!!poolInfo && !!lpShares ? (
+                      `${formatBalance({
+                        balance: calculateLpValue({
+                          lpShares,
+                          lpSharePrice: poolInfo.lpSharePrice,
+                          decimals: hyperdrive.baseToken.decimals,
+                        }),
+                        decimals: hyperdrive.baseToken.decimals,
+                        places: 4,
+                      })} ${hyperdrive.baseToken.symbol}`
+                    ) : (
+                      <Skeleton />
+                    )}
+                  </p>
+                </div>
+              </div>
+              <div className="mt-4 flex flex-col">
+                <div className="flex justify-between">
+                  <p
+                    className="daisy-tooltip mb-1 inline-flex cursor-help items-center border-b border-dashed border-current"
+                    data-tip="Your ratio of idle capital to capital being used to back Longs and Shorts."
+                  >
+                    Utilization ratio
+                  </p>
+                  <p>
+                    {!!lpBaseWithdrawable && !!withdrawalSharesOut ? (
+                      `${formatRate(
+                        utilizationRatio,
+                        hyperdrive.baseToken.decimals,
+                      )}%`
+                    ) : (
+                      <Skeleton />
+                    )}
+                  </p>
+                </div>
+              </div>
+              <progress
+                className="daisy-progress"
+                value={
+                  // Don't let the progress bar go over 100%
+                  Math.min(
+                    +formatUnits(
+                      utilizationRatio,
+                      hyperdrive.baseToken.decimals,
+                    ) * 100,
+                    100,
+                  )
+                }
+                max="100"
+              ></progress>
+              <p className="text-center text-body">
+                Note: When you withdraw liquidity, you get back{" "}
+                {hyperdrive.baseToken.symbol} and withdrawal shares proportional
+                to your utilization ratio.
               </p>
             </div>
-          </div>
-          <div className="flex flex-col">
-            <div className="flex justify-between">
-              <p className="">LP balance</p>
-              <p className="">
-                {!lpShares ? (
-                  <Skeleton />
-                ) : (
-                  formatBalance({
-                    balance: lpShares,
-                    decimals: hyperdrive.baseToken.decimals,
-                    places: 4,
-                  })
-                )}{" "}
-                Shares
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <div className="flex justify-between">
-              <p className="">Current value</p>
-              <p className="font-bold">
-                {!!poolInfo && !!lpShares ? (
-                  `${formatBalance({
-                    balance: calculateLpValue({
-                      lpShares,
-                      lpSharePrice: poolInfo.lpSharePrice,
-                      decimals: hyperdrive.baseToken.decimals,
-                    }),
-                    decimals: hyperdrive.baseToken.decimals,
-                    places: 4,
-                  })} ${hyperdrive.baseToken.symbol}`
-                ) : (
-                  <Skeleton />
-                )}
-              </p>
-            </div>
-          </div>
-          <div className="mt-4 flex flex-col">
-            <div className="flex justify-between">
-              <p
-                className="daisy-tooltip mb-1 inline-flex cursor-help items-center border-b border-dashed border-current"
-                data-tip="Your ratio of idle capital to capital being used to back Longs and Shorts."
-              >
-                Utilization ratio
-              </p>
-              <p>
-                {!!lpBaseWithdrawable && !!withdrawalSharesOut ? (
-                  `${formatRate(
-                    utilizationRatio,
-                    hyperdrive.baseToken.decimals,
-                  )}%`
-                ) : (
-                  <Skeleton />
-                )}
-              </p>
-            </div>
-          </div>
-          <progress
-            className="daisy-progress"
-            value={
-              // The progress bar can't go past 100%, but it's possible that
-              // negative interest makes the utilization ratio go over 100%
-              Math.max(
-                +formatUnits(utilizationRatio, hyperdrive.baseToken.decimals),
-                100,
-              )
-            }
-            max="100"
-          ></progress>
-          <p className="text-center text-body">
-            Note: When you withdraw liquidity, you get back{" "}
-            {hyperdrive.baseToken.symbol} and withdrawal shares proportional to
-            your utilization ratio.
-          </p>
-        </div>
-        <div className="flex items-center justify-end">
-          <div className="flex gap-1">
-            <Modal
-              modalId={"depositLpModal"}
-              modalContent={<AddLiquidityForm market={hyperdrive} />}
-            >
-              {({ showModal }) => (
-                <button
-                  className="daisy-btn-neutral daisy-btn-sm daisy-btn rounded-r-none  rounded-l-md hover:daisy-btn-ghost"
-                  onClick={showModal}
-                >
-                  Deposit
-                </button>
-              )}
-            </Modal>
-            <Modal
-              modalId={"withdrawalLpModal"}
-              modalContent={
-                <RemoveLiquidityForm
+            <div className="flex items-center justify-end">
+              <div className="flex gap-1">
+                <DepositButton
                   hyperdrive={hyperdrive}
-                  lpShares={lpShares || 0n}
+                  className="rounded-l-md rounded-r-none"
                 />
-              }
-            >
-              {({ showModal }) => (
-                <button
-                  className="daisy-btn-neutral daisy-btn-sm daisy-btn rounded-l-none rounded-r-md hover:daisy-btn-ghost"
-                  onClick={showModal}
+                <Modal
+                  modalId={"withdrawalLpModal"}
+                  modalContent={
+                    <RemoveLiquidityForm
+                      hyperdrive={hyperdrive}
+                      lpShares={lpShares || 0n}
+                    />
+                  }
                 >
-                  Withdraw
-                </button>
-              )}
-            </Modal>
-          </div>
-        </div>
+                  {({ showModal }) => (
+                    <button
+                      className="daisy-btn-neutral daisy-btn-sm daisy-btn rounded-l-none rounded-r-md hover:daisy-btn-ghost"
+                      onClick={showModal}
+                    >
+                      Withdraw
+                    </button>
+                  )}
+                </Modal>
+              </div>
+            </div>
+          </>
+        ) : (
+          <NonIdealState
+            icon={<InboxIcon className="h-16" />}
+            heading="No LP shares found."
+            text="Add liquidity to receive LP shares."
+            action={
+              <DepositButton hyperdrive={hyperdrive} label="Add liquidity" />
+            }
+          />
+        )}
       </div>
     </Well>
   );
@@ -223,4 +232,33 @@ function calculateRatio({
     dnum.from("100", decimals),
     decimals,
   )[0];
+}
+
+function DepositButton({
+  hyperdrive,
+  label = "Deposit",
+  className,
+}: {
+  hyperdrive: Hyperdrive;
+  label?: string;
+  className?: string;
+}) {
+  return (
+    <Modal
+      modalId={"depositLpModal"}
+      modalContent={<AddLiquidityForm market={hyperdrive} />}
+    >
+      {({ showModal }) => (
+        <button
+          className={classNames(
+            "daisy-btn-neutral daisy-btn-sm daisy-btn  hover:daisy-btn-ghost",
+            className,
+          )}
+          onClick={showModal}
+        >
+          {label}
+        </button>
+      )}
+    </Modal>
+  );
 }
