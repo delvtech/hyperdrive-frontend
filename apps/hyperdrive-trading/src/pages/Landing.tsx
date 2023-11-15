66fbfd21669f937c0e974473c3832c7b5a6e4772
jackburrus <jhburrus3@gmail.com>
2023-11-15 13:45:33 -0700

    Add footer to Landing page

--- apps/hyperdrive-trading/src/pages/Landing.tsx
+++ apps/hyperdrive-trading/src/pages/Landing.tsx
@@ -27,6 +27,7 @@ export function Landing(): ReactElement | null {
       <CommonHeadTags />
       <Hero />
       <PositionCards hyperdrive={hyperdrive} />
+      <Footer />
     </div>
   );
 }
@@ -104,5 +105,49 @@ function Hero() {
         </div>
       </div>
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="daisy-footer p-10 text-base-content">
+      <aside>
+        <Link
+          to={"/"}
+          className="inline-flex items-center gap-2 font-lato text-h6 font-extrabold uppercase tracking-widest"
+        >
+          <div className="inline-flex w-full">
+            <div className="h-4 w-2.5 -skew-x-12 bg-[#007eed]"></div>
+            <div className="h-4 w-2.5 -skew-x-12 bg-[#522dae]"></div>
+            <div className="h-4 w-2.5 -skew-x-12 bg-[#f40000]"></div>
+          </div>
+          Hyperdrive
+        </Link>
+        <p className="max-w-[160px]">
+          Revolutionize DeFi with Everlasting Liquidity
+        </p>
+      </aside>
+      <nav>
+        <header className="daisy-footer-title">Services</header>
+        <Link
+          to={
+            "https://delv-tech.notion.site/Hyperdrive-Docs-6c014b9270af4483861b1c2ffa7f3a88"
+          }
+          className="link link-hover"
+        >
+          Documentation
+        </Link>
+      </nav>
+      <nav>
+        <header className="daisy-footer-title">Company</header>
+        <a className="link link-hover">About us</a>
+      </nav>
+      <nav>
+        <header className="daisy-footer-title">Legal</header>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
+    </footer>
   );
 }
