9da9fe65cbbd4f42a3ddfce206c5b3ce860832bb
jackburrus <jhburrus3@gmail.com>
2023-11-22 07:35:10 -0700

    Adds calculateeffectiveshares test

---
+++ packages/hyperdrive-sdk/src/pool/calculateEffectiveShares.test.ts
@@ -0,0 +1,11 @@
+import { expect, test } from "vitest";
+import { calculateEffectiveShareReserves } from "./calculateEffectiveShares";
+
+test("calculateEffectiveShares should return effective shares for a given position", async () => {
+  const effectiveShares = calculateEffectiveShareReserves(
+    100000119271191525861204752n,
+    -10715885395142403501106n,
+  );
+
+  expect(effectiveShares).toEqual(100010835156586668264705858n);
+});
