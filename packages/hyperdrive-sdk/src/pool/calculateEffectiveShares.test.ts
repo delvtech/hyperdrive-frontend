e2e3cd34a1b6ed1579cc5cb2e411375ab1452a96
jackburrus <jhburrus3@gmail.com>
2023-11-27 13:11:19 -0700

    updates test description

--- packages/hyperdrive-sdk/src/pool/calculateEffectiveShares.test.ts
+++ packages/hyperdrive-sdk/src/pool/calculateEffectiveShares.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "vitest";
 import { calculateEffectiveShareReserves } from "./calculateEffectiveShares";
 
-test("calculateEffectiveShares should return effective shares for a given position", async () => {
+test("calculateEffectiveShares should return `shareReserves - shareAdjustment`", async () => {
   const effectiveShares = calculateEffectiveShareReserves(
     100000119271191525861204752n,
     -10715885395142403501106n,
