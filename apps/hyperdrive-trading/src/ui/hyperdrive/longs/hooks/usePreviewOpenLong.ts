a57f1fee34f3b54be08b7b42eb9bd15cdbe17f31
Ryan Goree <goree.ryan@gmail.com>
2023-11-28 16:17:02 -0600

    Replace preview open short/long impls

--- apps/hyperdrive-trading/src/ui/hyperdrive/longs/hooks/usePreviewOpenLong.ts
+++ apps/hyperdrive-trading/src/ui/hyperdrive/longs/hooks/usePreviewOpenLong.ts
@@ -1,16 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { Hyperdrive } from "src/appconfig/types";
-import { ZERO_ADDRESS } from "src/base/constants";
 import { makeQueryKey } from "src/base/makeQueryKey";
 import { useReadWriteHyperdrive } from "src/ui/hyperdrive/hooks/useReadWriteHyperdrive";
-import { Address } from "wagmi";
 interface UsePreviewOpenLongOptions {
   market: Hyperdrive;
   baseAmount: bigint | undefined;
-  minBondAmountOut: bigint | undefined;
-  minSharePrice: bigint | undefined;
-  destination: Address | undefined;
-  asBase?: boolean;
   enabled?: boolean;
 }
 
@@ -23,38 +17,21 @@ interface UsePreviewOpenLongResult {
 export function usePreviewOpenLong({
   market,
   baseAmount,
-  minBondAmountOut,
-  minSharePrice,
-  destination,
-  asBase = true,
   enabled,
 }: UsePreviewOpenLongOptions): UsePreviewOpenLongResult {
   const readWriteHyperdrive = useReadWriteHyperdrive(market.address);
-  const queryEnabled =
-    !!readWriteHyperdrive &&
-    !!baseAmount &&
-    !!minBondAmountOut &&
-    minSharePrice !== undefined &&
-    !!destination &&
-    enabled;
+  const queryEnabled = !!readWriteHyperdrive && !!baseAmount && enabled;
 
   const { data, status } = useQuery({
     queryKey: makeQueryKey("previewOpenLong", {
       market: market.address,
       baseAmount: baseAmount?.toString(),
-      minBondAmountOut: minBondAmountOut?.toString(),
-      destination,
     }),
     enabled: queryEnabled,
     queryFn: queryEnabled
       ? () =>
           readWriteHyperdrive.previewOpenLong({
             baseAmount,
-            minBondAmountOut,
-            minSharePrice,
-            destination,
-            asBase,
-            extraData: ZERO_ADDRESS,
           })
       : undefined,
   });
