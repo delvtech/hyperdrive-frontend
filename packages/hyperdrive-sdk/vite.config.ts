fdc134348452c8e3b7ee88f5891940e62b7740e4
jackburrus <jhburrus3@gmail.com>
2023-11-30 10:38:37 -0700

    adds alias to test

--- packages/hyperdrive-sdk/vite.config.ts
+++ packages/hyperdrive-sdk/vite.config.ts
@@ -3,4 +3,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    alias: {
+      "@delvtech/hyperdrive-wasm?v0.2.0": "@delvtech/hyperdrive-wasm",
+    },
+  },
 });
