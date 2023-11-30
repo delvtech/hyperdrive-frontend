f713d80bac552717d13b40b9ec76c00adba14672
Danny Delott <dannydelott@gmail.com>
2023-11-30 09:26:21 -0800

    Closed rows cursor fix

--- apps/hyperdrive-trading/src/ui/portfolio/ClosedLpTable/ClosedLpTable.tsx
+++ apps/hyperdrive-trading/src/ui/portfolio/ClosedLpTable/ClosedLpTable.tsx
@@ -157,10 +157,7 @@ export function ClosedLpTable({
           ) : (
             tableInstance.getRowModel().rows.map((row) => {
               return (
-                <tr
-                  key={row.id}
-                  className="h-24 cursor-pointer grid-cols-4 items-center"
-                >
+                <tr key={row.id} className="h-24 grid-cols-4 items-center">
                   <>
                     {row.getVisibleCells().map((cell) => {
                       return (
