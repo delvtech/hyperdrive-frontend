f5dc2a0bc075e3c7b14cd5e7b2f45b9b91f35f3b
jackburrus <jhburrus3@gmail.com>
2023-12-01 14:01:00 -0700

    fix text opacity

--- apps/hyperdrive-trading/src/ui/base/components/Stat.tsx
+++ apps/hyperdrive-trading/src/ui/base/components/Stat.tsx
@@ -12,17 +12,19 @@ export function Stat({ label, value, description }: StatProps): ReactElement {
   return (
     <div className="group">
       <div className="whitespace-nowrap text-h6">{value}</div>
-      <p
+      <div
         data-tip={description}
         // Future note: use before: to style the text inside the tooltip
         className={classNames(
-          "daisy-tooltip flex items-center text-sm text-gray-500",
+          "daisy-tooltip flex items-center transition duration-150 ease-in-out",
           description ? "cursor-help" : "",
         )}
       >
-        {label}
-        <InformationCircleIcon className="ml-1 inline-block w-4 text-gray-400 opacity-0 transition duration-150 ease-in-out group-hover:text-gray-500 group-hover:opacity-100" />
-      </p>
+        <p className="text-sm opacity-50">
+          {label}
+          <InformationCircleIcon className="ml-1 inline-block w-4 text-gray-400 opacity-0 transition duration-150 ease-in-out group-hover:text-gray-500 group-hover:opacity-100" />
+        </p>
+      </div>
     </div>
   );
 }
