73beaf056510498347af0556b3a5852845fca01f
jackburrus <jhburrus3@gmail.com>
2023-12-05 09:32:07 -0700

    refactor faq

--- apps/hyperdrive-trading/src/ui/onboarding/FAQ/FAQ.tsx
+++ apps/hyperdrive-trading/src/ui/onboarding/FAQ/FAQ.tsx
@@ -4,39 +4,34 @@ import { faqData } from "src/ui/onboarding/FAQ/faqData";
 
 export function FAQ(): ReactElement {
   const [selectedFAQKey, setSelectedFAQKey] = useState("faq1");
+
+  // Find the selected FAQ based on the selectedFAQKey
+  const selectedFAQ = faqData.find((faq) => faq.key === selectedFAQKey);
+
   return (
     <div className="flex max-w-6xl flex-col">
       <span className="mb-2 text-h5 font-bold">Frequently Asked Questions</span>
       <div className="flex flex-row gap-6 rounded-md bg-base-200 p-12">
         <div className="w-[30%]">
-          {faqData.map((faq) => {
-            return (
-              <div
-                key={faq.key}
-                onClick={() => setSelectedFAQKey(faq.key)}
-                className={classNames("my-5 cursor-pointer", {
-                  "bg-base-200 opacity-40": selectedFAQKey !== faq.key,
-                })}
-              >
-                <div>{faq.question}</div>
-              </div>
-            );
-          })}
+          {faqData.map((faq) => (
+            <div
+              key={faq.key}
+              onClick={() => setSelectedFAQKey(faq.key)}
+              className={classNames("my-5 cursor-pointer", {
+                "bg-base-200 opacity-40": selectedFAQKey !== faq.key,
+              })}
+            >
+              <div>{faq.question}</div>
+            </div>
+          ))}
         </div>
-        <div className="flex w-[70%] flex-col items-start justify-center">
-          {faqData.map((faq) => {
-            return (
-              <div
-                key={faq.key}
-                className={classNames("mb-6", {
-                  hidden: selectedFAQKey !== faq.key,
-                })}
-              >
-                <div className="mb-1 text-h4">{faq.question}</div>
-                <span>{faq.answer}</span>
-              </div>
-            );
-          })}
+        <div className="flex min-h-[400px] w-[70%] flex-col items-start justify-center overflow-auto">
+          {selectedFAQ && (
+            <div className="mb-6">
+              <div className="mb-1 text-h4">{selectedFAQ.question}</div>
+              <span>{selectedFAQ.answer}</span>
+            </div>
+          )}
         </div>
       </div>
     </div>
