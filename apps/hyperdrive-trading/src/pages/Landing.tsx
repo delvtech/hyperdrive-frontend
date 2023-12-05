13c3efb3be4975f81aaf7902d1e248215fc3455d
Jack Burrus <jhburrus3@gmail.com>
2023-12-05 09:56:19 -0700

    Fix Homepage responsiveness (#547)
    * fix hero title
    
    * cleanup faq and add media hooks
    
    * refactor faq
    
    * remove comment

--- apps/hyperdrive-trading/src/pages/Landing.tsx
+++ apps/hyperdrive-trading/src/pages/Landing.tsx
@@ -4,14 +4,16 @@ import {
   SquaresPlusIcon,
 } from "@heroicons/react/24/outline";
 import { ReactElement } from "react";
-import { Hyperdrive } from "src/appconfig/types";
 import { CommonHeadTags } from "src/ui/app/Head/CommonHeadTags";
 import { useAppConfig } from "src/ui/appconfig/useAppConfig";
+import { useIsTailwindSmallScreen } from "src/ui/base/mediaBreakpoints";
 import { PositionCard } from "src/ui/hyperdrive/PositionCard";
 import { FAQ } from "src/ui/onboarding/FAQ/FAQ";
+import { MobileFaq } from "src/ui/onboarding/FAQ/MobileFaq";
 
 export function Landing(): ReactElement | null {
   const { appConfig } = useAppConfig();
+  const isSmallScreenView = useIsTailwindSmallScreen();
   if (appConfig) {
     // eslint-disable-next-line no-console
     console.log("appConfig", appConfig);
@@ -25,17 +27,13 @@ export function Landing(): ReactElement | null {
     <div className="flex flex-col items-center gap-10 bg-base-100 py-8 px-4">
       <CommonHeadTags />
       <Hero />
-      <PositionCards hyperdrive={hyperdrive} />
-      <FAQ />
+      <PositionCards />
+      {isSmallScreenView ? <MobileFaq /> : <FAQ />}
     </div>
   );
 }
 
-function PositionCards({
-  hyperdrive,
-}: {
-  hyperdrive: Hyperdrive;
-}): ReactElement {
+function PositionCards(): ReactElement {
   return (
     <div className="flex max-w-6xl flex-col items-center justify-evenly gap-10">
       <div className="flex flex-wrap justify-center gap-16 ">
@@ -79,7 +77,7 @@ function PositionCards({
       </div>
       <a
         href={"/markets"}
-        className="daisy-btn-neutral daisy-btn-md daisy-btn mt-10 w-60 justify-between gap-0 hover:daisy-btn-ghost"
+        className="daisy-btn-neutral daisy-btn daisy-btn-md mt-10 w-60 justify-between gap-0 hover:daisy-btn-ghost"
       >
         <span className="flex-1 text-center">View Markets</span>
       </a>
@@ -92,7 +90,7 @@ function Hero() {
     <div className="daisy-hero">
       <div className="daisy-hero-content text-center">
         <div className="max-w-2xl">
-          <span className="mb-5  text-h2 font-bold">Hyperdrive</span>
+          <span className="mb-5 text-h3 font-bold md:text-h2">Hyperdrive</span>
           <p className="mb-5">
             Hyperdrive marks a significant stride forward in exploring variable
             and fixed rate primitives. With no preset expiration dates, unified
--- apps/hyperdrive-trading/src/ui/onboarding/FAQ/FAQ.tsx
+++ apps/hyperdrive-trading/src/ui/onboarding/FAQ/FAQ.tsx
@@ -1,205 +1,34 @@
 import classNames from "classnames";
 import { ReactElement, useState } from "react";
+import { faqData } from "src/ui/onboarding/FAQ/faqData";
 
 export function FAQ(): ReactElement {
   const [selectedFAQKey, setSelectedFAQKey] = useState("faq1");
+
+  const selectedFAQ = faqData.find((faq) => faq.key === selectedFAQKey);
 
   return (
     <div className="flex max-w-6xl flex-col">
       <span className="mb-2 text-h5 font-bold">Frequently Asked Questions</span>
       <div className="flex flex-row gap-6 rounded-md bg-base-200 p-12">
         <div className="w-[30%]">
-          <div
-            onClick={() => setSelectedFAQKey("faq1")}
-            className={classNames("my-5 cursor-pointer", {
-              "bg-base-200 opacity-40": selectedFAQKey !== "faq1",
-            })}
-          >
-            <div>How can I get fixed rates?</div>
-          </div>
-          <div
-            onClick={() => setSelectedFAQKey("faq2")}
-            className={classNames("my-5 cursor-pointer", {
-              "bg-base-200 opacity-40": selectedFAQKey !== "faq2",
-            })}
-          >
-            <div>What is a short?</div>
-          </div>
-          <div
-            onClick={() => setSelectedFAQKey("faq3")}
-            className={classNames("my-5 cursor-pointer", {
-              "bg-base-200 opacity-40": selectedFAQKey !== "faq3",
-            })}
-          >
-            <div>What are the benefits of providing liquidity?</div>
-          </div>
-          <div
-            onClick={() => setSelectedFAQKey("faq4")}
-            className={classNames("my-5 cursor-pointer", {
-              "bg-base-200 opacity-40": selectedFAQKey !== "faq4",
-            })}
-          >
-            <div>Can I close my long/short/LP position before maturity?</div>
-          </div>
-          <div
-            onClick={() => setSelectedFAQKey("faq5")}
-            className={classNames("my-5 cursor-pointer", {
-              "bg-base-200 opacity-40": selectedFAQKey !== "faq5",
-            })}
-          >
-            <div>How do LP positions work?</div>
-          </div>
+          {faqData.map((faq) => (
+            <div
+              key={faq.key}
+              onClick={() => setSelectedFAQKey(faq.key)}
+              className={classNames("my-5 cursor-pointer", {
+                "bg-base-200 opacity-40": selectedFAQKey !== faq.key,
+              })}
+            >
+              <div>{faq.question}</div>
+            </div>
+          ))}
         </div>
-
-        <div className="flex w-[70%] flex-col items-start justify-center">
-          {selectedFAQKey === "faq1" && (
-            <div>
-              <div className="mb-1 text-h4">How can I get fixed rates?</div>
-              <span>
-                By opening a Long and holding it to maturity. <br />
-                <br />
-                Opening a long position in Hyperdrive implies purchasing
-                hy[Token] at a discount. hy[Token] is redeemable for its full
-                face value at maturity. The market price you pay and the
-                hy[Token]&rsquo;s value at maturity are known upfront, which
-                results in a fixed rate of return. <br />
-                <br />
-                Opening longs has an immediate impact on the market.
-                hy[Token]&rsquo;s market price increases, which means the fixed
-                rate decreases. Conversely, closing longs has the immediate
-                impact of decreasing the market price and increasing the fixed
-                rate. <br />
-                <br />
-                Users can also take speculative long positions. Read our docs to
-                learn more about{" "}
-                <a
-                  className="underline"
-                  href="https://delv-tech.notion.site/Position-Types-249d2fc0f16847c283b6f4df00df9bd5"
-                >
-                  Hyperdrive&rsquo;s Position Types
-                </a>
-                .
-              </span>
-            </div>
-          )}
-          {selectedFAQKey === "faq2" && (
-            <div>
-              <div className="mb-1 text-h4">What is a short?</div>
-              <span>
-                Simply put, a short is the opposite of a long. Opening a short
-                implies short-selling hy[Token] at its current market price,
-                thus paying an amount equivalent to the hy[Token]&rsquo;s
-                discount (i.e., the fixed rate applied to the position size)
-                upfront, while accruing the variable interest (APY) on the full
-                size of the position being shorted for the duration it&rsquo;s
-                held. <br />
-                <br />
-                Opening a short causes the market price of hy[Token] to
-                decrease, while closing a short has the inverse effect. <br />
-                <br />
-                Users can take speculative short positions or use shorts for
-                hedging other investments. Read our docs to learn more about{" "}
-                <a
-                  className="underline"
-                  href="https://delv-tech.notion.site/Position-Types-249d2fc0f16847c283b6f4df00df9bd5"
-                >
-                  Hyperdrive&rsquo;s Position Types
-                </a>
-                .
-              </span>
-            </div>
-          )}
-          {selectedFAQKey === "faq3" && (
-            <div>
-              <div className="mb-1 text-h4">
-                What are the benefits of providing liquidity?
-              </div>
-              <span>
-                Liquidity providers (LPs) take potential revenue from 3
-                different sources: <br /> - Trading fees from longs and shorts
-                being opened and closed by traders. <br /> - Variable rate on
-                their idle capital being deployed to the underlying yield
-                source. <br /> - Profit (or loss) resulting from taking the
-                other side of traders’ positions.
-                <br />
-                Furthermore, LP capital isn&rsquo;t tied to a specific maturity
-                date, so these benefits remain until the funds are withdrawn by
-                the LP. <br /> Learn more about{" "}
-                <a
-                  className="underline"
-                  href="https://delv-tech.notion.site/LP-Profitability-0acf6928b88c4a33875221aa15ca62d2"
-                >
-                  LP Profitability
-                </a>
-                .
-              </span>
-            </div>
-          )}
-          {selectedFAQKey === "faq4" && (
-            <div>
-              <div className="mb-1 text-h4">
-                Can I close my long/short/LP position before maturity?
-              </div>
-              <span>
-                Yes, longs and shorts can be closed at any time (except in rare
-                cases, learn more in the{" "}
-                <a
-                  href="https://delv-tech.notion.site/AMM-Checkpoints-11eefd1e09974551949803e16fe48afa"
-                  className="underline"
-                >
-                  docs
-                </a>
-                ). However, closing these positions before maturity implies
-                selling them to the AMM at the current market price. In other
-                words, positions that are closed early won&rsquo;t get the
-                initially expected rate of return, since their exit value
-                depends on market activity. <br />
-                <br />
-                Liquidity Provider (LP) positions don&rsquo;t have a maturity
-                date, so LP funds can be withdrawn at any time as long as
-                they&rsquo;re not currently backing other long or short
-                positions; if they are, LPs will receive Withdrawal Shares that
-                represent pending withdrawals that still share the same benefits
-                of regular LP capital. Learn more in the{" "}
-                <a
-                  href="https://delv-tech.notion.site/LP-Shares-9d3d21330909494d83608eddf7d518c5"
-                  className="underline"
-                >
-                  LP Shares
-                </a>{" "}
-                section of our docs.
-              </span>
-            </div>
-          )}
-          {selectedFAQKey === "faq5" && (
-            <div>
-              <div className="mb-1 text-h4">How do LP positions work?</div>
-              <span>
-                You open an LP position by providing base asset to the AMM. This
-                increases the AMM&rsquo;s capacity for longs and shorts. LP
-                capital is then used to back long and short positions opened by
-                traders.
-                <br />
-                <br />
-                LPs can withdraw their capital at any time, but if part of it is
-                currently backing open positions, the AMM will temporarily keep
-                that part and give Withdrawal Shares to the LP until there is
-                enough idle liquidity available to replace that capital. <br />
-                <br />
-                When long or short positions are closed, the liquidity used to
-                back them is freed up and used to buy back Withdrawal Shares, at
-                which point LPs can complete their pending withdrawal process.{" "}
-                <br />
-                <br />
-                Learn more in the{" "}
-                <a
-                  href="https://delv-tech.notion.site/LP-Shares-9d3d21330909494d83608eddf7d518c5"
-                  className="underline"
-                >
-                  LP Shares
-                </a>{" "}
-                section of our docs.
-              </span>
+        <div className="flex min-h-[400px] w-[70%] flex-col items-start justify-center overflow-auto">
+          {selectedFAQ && (
+            <div className="mb-6">
+              <div className="mb-1 text-h4">{selectedFAQ.question}</div>
+              <span>{selectedFAQ.answer}</span>
             </div>
           )}
         </div>
