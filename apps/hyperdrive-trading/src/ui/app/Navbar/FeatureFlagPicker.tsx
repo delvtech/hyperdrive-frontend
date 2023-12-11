b71f883588d8e8de81407ca61ebd3352ea578051
Danny Delott <dannydelott@gmail.com>
2023-12-11 09:19:49 -0800

    Remove feature flags from menu

--- apps/hyperdrive-trading/src/ui/app/Navbar/FeatureFlagPicker.tsx
+++ apps/hyperdrive-trading/src/ui/app/Navbar/FeatureFlagPicker.tsx
@@ -1,29 +1,23 @@
 import { Cog6ToothIcon } from "@heroicons/react/24/outline";
 import { ReactElement } from "react";
 import { useFeatureFlag } from "src/ui/base/featureFlags/featureFlags";
-import { NEW_MARKET_LAYOUT_FEATURE_FLAG } from "src/ui/markets/featureFlags";
-import { LP_CARDS_FEATURE_FLAG } from "src/ui/portfolio/featureFlags";
 export function FeatureFlagPicker(): ReactElement {
   return (
     <div className="daisy-dropdown-end daisy-dropdown">
-      <label tabIndex={0} className="daisy-btn-ghost rounded-btn daisy-btn">
+      <label tabIndex={0} className="daisy-btn-ghost daisy-btn rounded-btn">
         <Cog6ToothIcon className="h-4" /> Devtools
       </label>
       <ul
         tabIndex={0}
-        className="daisy-dropdown-content daisy-menu rounded-box z-[1] mt-4 w-52 bg-base-100 p-2 shadow"
+        className="daisy-dropdown-content daisy-menu z-[1] mt-4 w-52 rounded-box bg-base-100 p-2 shadow"
       >
         <li className="daisy-menu-title">Feature flags</li>
-        <FeatureFlagMenuItem flagName={LP_CARDS_FEATURE_FLAG}>
-          New LP cards
-        </FeatureFlagMenuItem>
-        <FeatureFlagMenuItem flagName={NEW_MARKET_LAYOUT_FEATURE_FLAG}>
-          New market details
-        </FeatureFlagMenuItem>
+        {/* Place your feature flag menu items here */}
       </ul>
     </div>
   );
 }
+
 function FeatureFlagMenuItem({
   flagName,
   children,
