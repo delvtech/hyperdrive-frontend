9491ac61ffeec5b8c3bc5ed5dba1cb707d21c96b
Danny Delott <dannydelott@gmail.com>
2023-12-12 17:20:58 -0800

    Remove gud-type in favor of figma values, specify colors and fontsizes/line heights, replace daisy styles

--- apps/hyperdrive-trading/tailwind.config.js
+++ apps/hyperdrive-trading/tailwind.config.js
@@ -1,83 +1,78 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { gudTypeScale, rounder } = require("gud-type");
-const typeScale = gudTypeScale(
-  ["xs", "sm", "body", "lg", "h6", "h5", "h4", "h3", "h2", "h1"],
-  {
-    // make "body" the 0 index
-    startingIndex: -2,
-    // set the base size (size at 0) to 13
-    base: 13,
-    // Set the scale multiplier/ratio to 2
-    multiplier: 2,
-    // Make it take 5 steps to reach each multiple
-    // for example, if starting at 4 with 5 steps to reach it's next multiple of
-    // 2 (8), the scale would look like: 4, 4.75, 5.5, 6.25, 7, 8
-    steps: 5,
-    // round each font size up to the closest whole number
-    round: rounder(1, "up"),
-    unit: "px",
-  },
-);
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}", "index.html"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,css}", "index.html"],
   theme: {
-    extend: {
-      fontFamily: {
-        lato: ["Lato"], // for headers
+    colors: {
+      // Used for primary buttons, links, tabs and states
+      lime: {
+        100: "#DFFF99",
+        DEFAULT: "#BAFF29",
+        800: "#8BCC00",
       },
-      colors: {
-        "base-300": "#212140",
-        "hyper-blue-400": "#425079",
+      // Used for interactive text and components such as tabs, tags and
+      // sliders
+      teal: {
+        100: "#E8FCFC",
+        DEFAULT: "#9BF3F0",
+        800: "#49E9E4",
+        900: "#16B6B1",
+      },
+      // Used for secondary buttons and their states
+      purple: {
+        100: "#DBCCFF",
+        DEFAULT: "#AB87FF",
+        800: "#7033FF",
+      },
+
+      gray: {
+        50: "#EFF4F6",
+        100: "#BDD2DB",
+        400: "#9CBBC9",
+        600: "#243942",
+        700: "#202F36",
+        800: "#1A272D",
+        900: "#10191D",
       },
     },
     fontSize: {
-      xs: typeScale.xs.fontSize,
-      sm: typeScale.sm.fontSize,
-      body: typeScale.body.fontSize,
-      lg: typeScale.lg.fontSize,
-      h6: typeScale.h6.fontSize,
-      h5: typeScale.h5.fontSize,
-      h4: typeScale.h4.fontSize,
-      h3: typeScale.h3.fontSize,
-      h2: typeScale.h2.fontSize,
-      h1: typeScale.h1.fontSize,
-    },
-    lineHeight: {
-      sm: typeScale.sm.lineHeight,
-      body: typeScale.body.lineHeight,
-      lg: typeScale.lg.lineHeight,
-      h6: typeScale.h6.lineHeight,
-      h5: typeScale.h5.lineHeight,
-      h4: typeScale.h4.lineHeight,
-      h3: typeScale.h3.lineHeight,
-      h2: typeScale.h2.lineHeight,
-      h1: typeScale.h1.lineHeight,
+      // body texts
+      p1: ["18px", { lineHeight: "22px" }],
+      p2: ["16px", { lineHeight: "20px" }],
+      p3: ["14px", { lineHeight: "16px" }],
+      p4: ["12px", { lineHeight: "14px" }],
+      // Headings
+      h1: ["64px", { lineHeight: "77px" }],
+      h2: ["40px", { lineHeight: "48px" }],
+      h3: ["32px", { lineHeight: "38px" }],
+      h4: ["24px", { lineHeight: "30px" }],
+      h5: ["20px", { lineHeight: "24px" }],
     },
   },
   plugins: [require("daisyui"), require("tailwindcss-debug-screens")],
   daisyui: {
+    prefix: "daisy-",
     themes: [
       {
-        lofi: {
-          // eslint-disable-next-line @typescript-eslint/no-var-requires
-          ...require("daisyui/src/theming/themes")["lofi"],
-          // Tone down the pure black
-          neutral: "#333333",
-          primary: "#333333",
-          secondary: "#333333",
-          accent: "#333333",
+        hyperdrive: {
+          primary: "#BAFF29", // lime DEFAULT
+          "primary-content": "#10191D", // Neutral-900
 
-          error: "#f40000",
-          success: "#019d60",
+          accent: "#9BF3F0", // teal DEFAULT
+          "accent-content": "#10191D", // Neutral-900
+
+          "base-100": "#10191D", // Neutral-900
+          "base-200": "#1A272D", // Neutral-800
+          "base-content": "#EFF4F6", // Neutral-50
+
+          error: "#FF5757",
+          success: "#36D399",
 
           "--tab-radius": "0.4rem",
-          "--rounded-box": "0.4rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-box": "2rem", // border radius rounded-box utility class, used in card and other large boxes
           "--rounded-btn": "0.4rem",
           "--rounded-badge": "0.4rem",
         },
       },
     ],
-    prefix: "daisy-",
   },
 };
