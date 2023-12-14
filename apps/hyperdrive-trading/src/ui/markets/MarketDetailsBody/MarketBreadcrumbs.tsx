425bd2717d8430fe4ab332ba9943e5f0c270731f
Danny Delott <dannydelott@gmail.com>
2023-12-14 15:08:33 -0800

    Retheme market details page (#564)

--- apps/hyperdrive-trading/src/ui/markets/MarketDetailsBody/MarketBreadcrumbs.tsx
+++ apps/hyperdrive-trading/src/ui/markets/MarketDetailsBody/MarketBreadcrumbs.tsx
@@ -1,21 +1,17 @@
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { ReactElement } from "react";
 import { Link } from "react-router-dom";
-import { Hyperdrive } from "src/appconfig/types";
 
-export function MarketBreadcrumbs({
-  hyperdrive,
-}: {
-  hyperdrive: Hyperdrive;
-}): ReactElement {
+export function MarketBreadcrumbs(): ReactElement {
   return (
     <div className="flex flex-1 justify-between">
-      <div className="daisy-breadcrumbs md:text-h6">
+      <div className="daisy-breadcrumbs md:text-md">
         <ul>
           <li>
-            <Link to="/markets">All Markets</Link>
-          </li>
-          <li>
-            {hyperdrive.baseToken.symbol} / {`hy${hyperdrive.baseToken.symbol}`}
+            <Link to="/markets" className="text-accent">
+              <ArrowLeftIcon className="mr-1 h-4" />
+              Back to markets
+            </Link>
           </li>
         </ul>
       </div>
--- apps/hyperdrive-trading/src/ui/markets/MarketDetailsBody/MarketHeader.tsx
+++ apps/hyperdrive-trading/src/ui/markets/MarketDetailsBody/MarketHeader.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from "react";
 import { Hyperdrive } from "src/appconfig/types";
+import { formatTermLength } from "src/ui/markets/formatTermLength";
 import { PriceBadges } from "./PriceBadges";
 
 export function MarketHeader({
@@ -9,27 +10,13 @@ export function MarketHeader({
   hyperdrive: Hyperdrive;
   longPrice: { price: bigint; formatted: string } | undefined;
 }): ReactElement {
+  const formattedTermLength = formatTermLength(hyperdrive.termLengthMS);
   return (
     <div className="my-2 flex items-center gap-2 md:my-0">
-      <div className="daisy-avatar-group shrink-0 -space-x-4 p-2 font-semibold ">
-        <div className="daisy-placeholder daisy-avatar">
-          <div className="w-16 bg-base-200">
-            <span className="text-body font-bold">
-              {hyperdrive.baseToken.symbol}
-            </span>
-          </div>
-        </div>
-        <div className="daisy-placeholder daisy-avatar">
-          <div className="w-16 bg-base-200">
-            <span className="text-body font-bold">
-              hy{hyperdrive.baseToken.symbol}
-            </span>
-          </div>
-        </div>
-      </div>
       <div className="flex w-full flex-col items-start gap-1">
-        <span className="items-center text-h4 font-semibold md:my-0 md:text-h3">
-          {hyperdrive.baseToken.symbol} / hy{hyperdrive.baseToken.symbol}
+        <span className="gradient-text items-center text-h1 md:my-0">
+          {formattedTermLength} {hyperdrive.baseToken.symbol}-
+          {hyperdrive.yieldSource}
         </span>
         {longPrice ? (
           <PriceBadges hyperdrive={hyperdrive} longPrice={longPrice} />
