77a9a5992c2ccbff9ca97c57636232b850ec1f8a
jackburrus <jhburrus3@gmail.com>
2023-12-14 13:56:55 -0700

    custom theme applied

---
+++ apps/hyperdrive-trading/src/ui/appconfig/customTheme.tsx
@@ -0,0 +1,60 @@
+// eslint-disable-next-line import/named
+import { Theme } from "@rainbow-me/rainbowkit";
+
+export function customRainbowTheme(): Theme {
+  return {
+    blurs: {
+      modalOverlay: "...",
+    },
+    colors: {
+      accentColor: "...",
+      accentColorForeground: "...",
+      actionButtonBorder: "...",
+      actionButtonBorderMobile: "...",
+      actionButtonSecondaryBackground: "...",
+      closeButton: "...",
+      closeButtonBackground: "...",
+      connectButtonBackground: "...",
+      connectButtonBackgroundError: "...",
+      connectButtonInnerBackground: "...",
+      connectButtonText: "...",
+      connectButtonTextError: "...",
+      connectionIndicator: "...",
+      downloadBottomCardBackground: "...",
+      downloadTopCardBackground: "...",
+      error: "...",
+      generalBorder: "...",
+      generalBorderDim: "...",
+      menuItemBackground: "...",
+      modalBackdrop: "...",
+      modalBackground: "#243942",
+      modalBorder: "#202F36",
+      modalText: "#EFF4F6",
+      modalTextDim: "...",
+      modalTextSecondary: "...",
+      profileAction: "...",
+      profileActionHover: "...",
+      profileForeground: "...",
+      selectedOptionBorder: "...",
+      standby: "...",
+    },
+    fonts: {
+      body: "...",
+    },
+    radii: {
+      actionButton: "...",
+      connectButton: "...",
+      menuButton: "...",
+      modal: "32px",
+      modalMobile: "...",
+    },
+    shadows: {
+      connectButton: "...",
+      dialog: "...",
+      profileDetailsAction: "...",
+      selectedOption: "...",
+      selectedWallet: "...",
+      walletLogo: "...",
+    },
+  };
+}
--- apps/hyperdrive-trading/src/ui/main.tsx
+++ apps/hyperdrive-trading/src/ui/main.tsx
@@ -1,4 +1,4 @@
-import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
+import { darkTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -9,10 +9,10 @@ import "react-loading-skeleton/dist/skeleton.css";
 import { queryClient } from "src/network/queryClient";
 import { wagmiChains, wagmiConfig } from "src/network/wagmiClient";
 import { App } from "src/ui/app/App/App";
+import { customRainbowTheme } from "src/ui/appconfig/customTheme";
+import ToastProvider from "src/ui/base/components/Toaster/ToastProvider";
 import "src/ui/globals.css";
 import { WagmiConfig } from "wagmi";
-import ToastProvider from "./base/components/Toaster/ToastProvider";
-
 const container = document.getElementById("root") as HTMLDivElement;
 const root = createRoot(container);
 
@@ -27,7 +27,10 @@ root.render(
       <RainbowKitProvider
         chains={wagmiChains}
         showRecentTransactions
-        theme={darkTheme()}
+        theme={{
+          ...darkTheme(),
+          ...customRainbowTheme(),
+        }}
       >
         <SkeletonTheme
           baseColor="hsl(var(--b1))"
