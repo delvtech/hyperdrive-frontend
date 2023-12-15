25e68965f154b484135d92b07b2009dadec42f1d
jackburrus <jhburrus3@gmail.com>
2023-12-15 09:27:27 -0700

    updates per comments

--- apps/hyperdrive-trading/src/ui/base/components/ConnectWallet.tsx
+++ apps/hyperdrive-trading/src/ui/base/components/ConnectWallet.tsx
@@ -13,14 +13,10 @@ export function ConnectWalletButton(): JSX.Element {
         const connected = mounted && account && chain;
         return (
           <div
-            {...(!mounted && {
-              "aria-hidden": true,
-              style: {
-                opacity: 0,
-                pointerEvents: "none",
-                userSelect: "none",
-              },
-            })}
+            aria-hidden={!mounted}
+            className={
+              !mounted ? "pointer-events-none invisible select-none" : ""
+            }
           >
             {(() => {
               if (!connected) {
@@ -28,7 +24,6 @@ export function ConnectWalletButton(): JSX.Element {
                   <button
                     className="daisy-btn-primary daisy-btn daisy-btn-circle w-32"
                     onClick={openConnectModal}
-                    type="button"
                   >
                     Connect Wallet
                   </button>
@@ -39,7 +34,6 @@ export function ConnectWalletButton(): JSX.Element {
                   <button
                     className="daisy-btn-primary daisy-btn daisy-btn-circle w-32"
                     onClick={openChainModal}
-                    type="button"
                   >
                     Wrong network
                   </button>
@@ -49,7 +43,6 @@ export function ConnectWalletButton(): JSX.Element {
                 <button
                   className="daisy-btn-primary daisy-btn daisy-btn-circle mx-0 w-32"
                   onClick={openAccountModal}
-                  type="button"
                 >
                   {account.displayName}
                 </button>
---
+++ apps/hyperdrive-trading/src/ui/wallet/customTheme.tsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line import/named
+import { darkTheme, Theme } from "@rainbow-me/rainbowkit";
+
+export function customRainbowTheme(): Theme {
+  const darkThemePresets = darkTheme();
+  return {
+    ...darkThemePresets,
+    colors: {
+      ...darkThemePresets.colors,
+      modalBackground: "#202F36", // gray-700
+      modalBorder: "#202F36", // gray-700
+      modalText: "#EFF4F6", // gray-100
+      accentColor: "#9BF3F0", // teal-DEFAULT
+    },
+  };
+}
