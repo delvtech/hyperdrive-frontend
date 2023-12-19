630f99bea36ad187a5798ef487d123855dca4425
Ryan Goree <goree.ryan@gmail.com>
2023-12-19 11:26:30 -0600

    Add top level option to accept all default values

--- packages/hyperdrive-cli/src/hyper.ts
+++ packages/hyperdrive-cli/src/hyper.ts
@@ -10,4 +10,28 @@ run({
       skip: (options) => !!options.help,
     }),
   ],
+
+  // Top level options applied to all commands
+  options: {
+    y: {
+      alias: ["yes"],
+      description: "Accept all default option values",
+      type: "boolean",
+      default: false,
+    },
+  },
+
+  // Apply default values to options after parsing if -y or --yes are passed
+  afterParse: async ({ parsedOptions, context }) => {
+    if (parsedOptions.yes) {
+      for (const [key, config] of Object.entries(context.options)) {
+        const isMissingValue = parsedOptions[key] === undefined;
+        const hasDefault = config.default !== undefined;
+
+        if (isMissingValue && hasDefault) {
+          (parsedOptions[key] as any) = config.default;
+        }
+      }
+    }
+  },
 });
