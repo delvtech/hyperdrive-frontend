36bce8c7579072629d146a03a0f3c0c0b42dff0f
jackburrus <jhburrus3@gmail.com>
2024-01-10 15:53:28 -0700

    tooltips added for short

--- apps/hyperdrive-trading/src/ui/hyperdrive/shorts/OpenShortPreview/OpenShortPreview.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/shorts/OpenShortPreview/OpenShortPreview.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from "react";
 import { Hyperdrive } from "src/appconfig/types";
 import { convertMillisecondsToDays } from "src/base/convertMillisecondsToDays";
+import { PreviewTransactionRow } from "src/ui/base/components/PreviewTransactionRow";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
 
 interface OpenShortPreviewProps {
@@ -16,42 +17,49 @@ export function OpenShortPreview({
 }: OpenShortPreviewProps): ReactElement {
   return (
     <div className="flex flex-col gap-1">
-      <div className="flex justify-between">
-        <p>Short size</p>
-        <p className="font-bold">
-          {shortSize
-            ? `${formatBalance({
-                balance: shortSize,
-                decimals: market.baseToken.decimals,
-                places: 6,
-              })}`
-            : "0"}{" "}
-          hy{market.baseToken.symbol}
-        </p>
-      </div>
-      <div className="flex justify-between">
-        <p>You pay</p>
-        <p className="font-bold">
-          {costBasis
-            ? `${formatBalance({
-                balance: costBasis,
-                decimals: market.baseToken.decimals,
-                places: 6,
-              })}`
-            : "0"}{" "}
-          {market.baseToken.symbol}
-        </p>
-      </div>
+      <PreviewTransactionRow
+        label="Short size"
+        value={
+          <span
+            className="daisy-tooltip daisy-tooltip-top daisy-tooltip-left cursor-help border-b border-dashed border-current before:border"
+            data-tip="The amount of pool liquidity deposited into the yield source."
+          >
+            {shortSize
+              ? `${formatBalance({
+                  balance: shortSize,
+                  decimals: market.baseToken.decimals,
+                  places: 6,
+                })} ${market.baseToken.symbol}`
+              : `0` + ` ${market.baseToken.symbol}`}
+          </span>
+        }
+      />
+      <PreviewTransactionRow
+        label="You pay"
+        value={
+          <span
+            className="daisy-tooltip daisy-tooltip-top daisy-tooltip-left cursor-help border-b border-dashed border-current before:border"
+            data-tip="The upfront cost to open a short."
+          >
+            {costBasis
+              ? `${formatBalance({
+                  balance: costBasis,
+                  decimals: market.baseToken.decimals,
+                  places: 6,
+                })} ${market.baseToken.symbol}`
+              : `0 ${market.baseToken.symbol}`}
+          </span>
+        }
+      />
 
-      <div className="flex justify-between">
-        <p>Matures in</p>
-        <p>
-          {convertMillisecondsToDays(market.termLengthMS)} days,{" "}
-          {new Date(
-            Date.now() + Number(market.termLengthMS),
-          ).toLocaleDateString()}
-        </p>
-      </div>
+      <PreviewTransactionRow
+        label="Matures in"
+        value={`${convertMillisecondsToDays(
+          market.termLengthMS,
+        )} days, ${new Date(
+          Date.now() + Number(market.termLengthMS),
+        ).toLocaleDateString()}`}
+      />
     </div>
   );
 }
