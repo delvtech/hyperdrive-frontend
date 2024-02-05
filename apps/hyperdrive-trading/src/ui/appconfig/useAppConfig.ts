912a07b3eb9008925c7521b03e50d02ab1bac140
Danny Delott <dannydelott@gmail.com>
2024-02-05 13:51:17 -0800

    Add new useAppConfig

---
+++ apps/hyperdrive-trading/src/ui/appconfig/useAppConfig.ts
@@ -0,0 +1,23 @@
+import {
+  AppConfig,
+  cloudChainAppConfig,
+  localChainAppConfig,
+} from "@hyperdrive/appconfig";
+import assertNever from "assert-never";
+import { SupportedChainId } from "src/chains/supportedChains";
+import { useChainId } from "wagmi";
+
+export function useAppConfig(): AppConfig | undefined {
+  const chainId = useChainId() as SupportedChainId;
+
+  switch (chainId) {
+    case 31337:
+      return localChainAppConfig;
+
+    case 42069:
+      return cloudChainAppConfig;
+
+    default:
+      assertNever(chainId);
+  }
+}
