dd6b5dbed9e85a2025771c27165623969508ae3b
Danny Delott <dannydelott@gmail.com>
2024-02-05 16:35:56 -0800

    Remove calls to useAppConfigOld where it wasn't being used

--- apps/hyperdrive-trading/src/ui/hyperdrive/hooks/useReadWriteHyperdrive.ts
+++ apps/hyperdrive-trading/src/ui/hyperdrive/hooks/useReadWriteHyperdrive.ts
@@ -2,20 +2,17 @@ import { ReadWriteHyperdrive } from "@hyperdrive/sdk";
 import { ViemReadWriteHyperdrive } from "@hyperdrive/sdk-viem";
 import { useMemo } from "react";
 import { querySdkCache } from "src/sdk/sdkCache";
-import { useAppConfigOld } from "src/ui/appconfig/useAppConfigOld";
 import { Address, useChainId, usePublicClient, useWalletClient } from "wagmi";
 
 export function useReadWriteHyperdrive(
   address: Address | undefined,
 ): ReadWriteHyperdrive | undefined {
-  const { appConfig } = useAppConfigOld();
-
   const chainId = useChainId();
   const publicClient = usePublicClient();
   const { data: walletClient } = useWalletClient();
 
   return useMemo(() => {
-    if (!appConfig || !walletClient || !address) {
+    if (!walletClient || !address) {
       return undefined;
     }
 
@@ -26,5 +23,5 @@ export function useReadWriteHyperdrive(
       cache: querySdkCache,
       id: chainId.toString(),
     });
-  }, [address, appConfig, chainId, publicClient, walletClient]);
+  }, [address, chainId, publicClient, walletClient]);
 }
