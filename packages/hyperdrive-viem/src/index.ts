7b2169462c163e795c370f5b38195b43cb02fe50
Danny Delott <dannydelott@gmail.com>
2024-02-13 14:24:29 -0800

    Update evm-client to fix simulateWrite not found error

--- packages/hyperdrive-viem/src/index.ts
+++ packages/hyperdrive-viem/src/index.ts
@@ -1,3 +1,6 @@
 // client
 export { ViemReadHyperdrive } from "src/client/ViemReadHyperdrive";
 export { ViemReadWriteHyperdrive } from "src/client/ViemReadWriteHyperdrive";
+
+// Re-export all of evm-client-viem
+export * from "@delvtech/evm-client-viem";
