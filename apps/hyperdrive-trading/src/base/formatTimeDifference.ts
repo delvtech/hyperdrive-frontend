14accbfd289dc3ae398727b6267c8a262adbccff
jackburrus <jhburrus3@gmail.com>
2024-02-14 15:30:27 -0700

    format time difference

---
+++ apps/hyperdrive-trading/src/base/formatTimeDifference.ts
@@ -0,0 +1,29 @@
+const SECONDS_PER_MINUTE = 60;
+const SECONDS_PER_HOUR = 3600;
+
+/**
+ * Converts seconds to a human-readable string representing the time difference in minutes or hours.
+ * @param {bigint} currentTimeStamp
+ * @param {bigint} previousTimeStamp
+ * @returns {string} A string representing how long ago the event occurred, in minutes or hours.
+ */
+export function formatTimeDifference({
+  currentTimeStamp,
+  previousTimeStamp,
+}: {
+  currentTimeStamp: bigint;
+  previousTimeStamp: bigint;
+}): string {
+  const timeDifference = Number(currentTimeStamp - previousTimeStamp);
+
+  if (timeDifference < 2 * SECONDS_PER_MINUTE) {
+    return "recently";
+  } else if (timeDifference < SECONDS_PER_HOUR) {
+    const minutes = Math.floor(timeDifference / SECONDS_PER_MINUTE);
+    return `${minutes} minutes ago`;
+  } else {
+    const hours = Math.floor(timeDifference / SECONDS_PER_HOUR);
+    const hourText = hours === 1 ? "hour" : "hours";
+    return `${hours} ${hourText} ago`;
+  }
+}
