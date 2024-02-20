4eea9c5906a3d44e6bea21c932fc72ced8ebe5b7
Danny Delott <dannydelott@gmail.com>
2024-02-20 08:50:59 -0800

    Add version file, integrate into app

---
+++ apps/hyperdrive-trading/src/ui/version/logAppVersion.ts
@@ -0,0 +1,12 @@
+import { APP_VERSION } from "src/version.output";
+
+export function logAppVersion(): void {
+  const repoUrl = "https://github.com/delvtech/hyperdrive-monorepo";
+  const versionUrl = `${repoUrl}/commit/${APP_VERSION}`;
+  const unreleasedUrl = `${repoUrl}/compare/${APP_VERSION}...main`;
+
+  // eslint-disable-next-line no-console
+  console.log(`App version: ${versionUrl}`);
+  // eslint-disable-next-line no-console
+  console.log(`Unreleased commits: ${unreleasedUrl}`);
+}
