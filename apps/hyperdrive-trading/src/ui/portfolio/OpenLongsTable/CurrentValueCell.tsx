6ff0e7a1077816570dc2c743b4a00e71c593e284
Danny Delott <dannydelott@gmail.com>
2024-02-27 15:09:02 -0800

    Convert steth shares into tokens in OpenLong table (#714)
    * Re-use CurrentValueCell in mobile and desktop open long tables
    
    * Convert steth shares to steth tokens in OpenLongs table

---
+++ apps/hyperdrive-trading/src/ui/portfolio/OpenLongsTable/CurrentValueCell.tsx
@@ -0,0 +1,97 @@
+import {
+  HyperdriveConfig,
+  findBaseToken,
+  findYieldSourceToken,
+} from "@hyperdrive/appconfig";
+import { Long } from "@hyperdrive/sdk";
+import classNames from "classnames";
+import { ReactElement } from "react";
+import { useAppConfig } from "src/ui/appconfig/useAppConfig";
+import { formatBalance } from "src/ui/base/formatting/formatBalance";
+import { usePreviewCloseLong } from "src/ui/hyperdrive/longs/hooks/usePreviewCloseLong";
+import { useConvertStethSharesToStethTokens } from "src/ui/vaults/steth/useConvertStethSharesToStethTokens";
+import { getIsSteth } from "src/vaults/isSteth";
+import { parseUnits } from "viem";
+import { useAccount } from "wagmi";
+
+export function CurrentValueCell({
+  row,
+  hyperdrive,
+}: {
+  row: Long;
+  hyperdrive: HyperdriveConfig;
+}): ReactElement {
+  const { address: account } = useAccount();
+  const appConfig = useAppConfig();
+  const baseToken = findBaseToken({
+    baseTokenAddress: hyperdrive.baseToken,
+    tokens: appConfig.tokens,
+  });
+  const sharesToken = findYieldSourceToken({
+    yieldSourceTokenAddress: hyperdrive.sharesToken,
+    tokens: appConfig.tokens,
+  });
+
+  // steth markets will close you out to steth shares, which must be converted
+  // into steth tokens
+  const isStethHyperdrive = getIsSteth(sharesToken);
+  const { amountOut, previewCloseLongStatus } = usePreviewCloseLong({
+    hyperdriveAddress: hyperdrive.address,
+    maturityTime: row.maturity,
+    bondAmountIn: row.bondAmount,
+    minOutput: parseUnits("0", baseToken.decimals),
+    destination: account,
+    // You can only withdraw as base if it's not a steth hyperdrive
+    asBase: !isStethHyperdrive,
+  });
+
+  // Steth tokens are 1:1 with eth so they can be used interchangeably with the
+  // base token
+  const { stethTokenAmount: stethTokenAmountOut } =
+    useConvertStethSharesToStethTokens({
+      lidoAddress: sharesToken.address,
+      stethShares: amountOut,
+      enabled: isStethHyperdrive,
+    });
+  const amountOutOrStethTokens = isStethHyperdrive
+    ? stethTokenAmountOut
+    : amountOut;
+
+  const currentValue =
+    amountOutOrStethTokens &&
+    formatBalance({
+      balance: amountOutOrStethTokens,
+      decimals: baseToken.decimals,
+      places: 4,
+    });
+
+  const isPositiveChangeInValue =
+    amountOutOrStethTokens && amountOutOrStethTokens > row.baseAmountPaid;
+  if (previewCloseLongStatus === "error") {
+    return <div>Insufficient Liquidity</div>;
+  }
+  return (
+    <div className="daisy-stat flex flex-row p-0 xl:flex-col">
+      <span className="daisy-stat-value text-xs font-bold md:text-md">
+        {currentValue?.toString()}
+      </span>
+      <div
+        data-tip={"Profit/Loss since open"}
+        className={classNames(
+          "daisy-stat-desc daisy-tooltip mt-1 inline-flex text-xs",
+          { "text-success": isPositiveChangeInValue },
+          { "text-error": !isPositiveChangeInValue },
+        )}
+      >
+        <span>{isPositiveChangeInValue ? "+" : ""}</span>
+        {amountOutOrStethTokens
+          ? `${formatBalance({
+              balance: amountOutOrStethTokens - row.baseAmountPaid,
+              decimals: baseToken.decimals,
+              places: 4,
+            })} ${baseToken.symbol}`
+          : undefined}
+      </div>
+    </div>
+  );
+}
--- apps/hyperdrive-trading/src/ui/portfolio/OpenLongsTable/OpenLongsTableMobile.tsx
+++ apps/hyperdrive-trading/src/ui/portfolio/OpenLongsTable/OpenLongsTableMobile.tsx
@@ -21,7 +21,6 @@ import {
 import classNames from "classnames";
 import { ReactElement } from "react";
 import { formatRate } from "src/base/formatRate";
-import { ETH_MAGIC_NUMBER } from "src/token/ETH_MAGIC_NUMBER";
 import { useAppConfig } from "src/ui/appconfig/useAppConfig";
 import { ConnectWalletButton } from "src/ui/base/components/ConnectWallet";
 import LoadingState from "src/ui/base/components/LoadingState";
@@ -29,9 +28,8 @@ import { NonIdealState } from "src/ui/base/components/NonIdealState";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
 import { CloseLongModalButton } from "src/ui/hyperdrive/longs/CloseLongModalButton/CloseLongModalButton";
 import { useOpenLongs } from "src/ui/hyperdrive/longs/hooks/useOpenLongs";
-import { usePreviewCloseLong } from "src/ui/hyperdrive/longs/hooks/usePreviewCloseLong";
 import { MaturesOnCell } from "src/ui/portfolio/MaturesOnCell/MaturesOnCell";
-import { parseUnits } from "viem";
+import { CurrentValueCell } from "src/ui/portfolio/OpenLongsTable/CurrentValueCell";
 import { useAccount } from "wagmi";
 
 export function OpenLongsTableMobile({
@@ -248,69 +246,6 @@ function getMobileColumns({
       },
     }),
   ];
-}
-
-function CurrentValueCell({
-  row,
-  hyperdrive,
-}: {
-  row: Long;
-  hyperdrive: HyperdriveConfig;
-}) {
-  const { address: account } = useAccount();
-  const appConfig = useAppConfig();
-  const baseToken = findBaseToken({
-    baseTokenAddress: hyperdrive.baseToken,
-    tokens: appConfig.tokens,
-  });
-  // TODO: This needs to be converted to baseAmount if called w/ asBase set to false
-  const { amountOut: baseAmountOut, previewCloseLongStatus } =
-    usePreviewCloseLong({
-      hyperdriveAddress: hyperdrive.address,
-      maturityTime: row.maturity,
-      bondAmountIn: row.bondAmount,
-      minOutput: parseUnits("0", baseToken.decimals),
-      destination: account,
-      asBase: baseToken.address !== ETH_MAGIC_NUMBER,
-    });
-
-  const currentValue =
-    baseAmountOut &&
-    formatBalance({
-      balance: baseAmountOut,
-      decimals: baseToken.decimals,
-      places: 2,
-    });
-
-  const isPositiveChangeInValue =
-    baseAmountOut && baseAmountOut > row.baseAmountPaid;
-  if (previewCloseLongStatus === "error") {
-    return <div>Insufficient Liquidity</div>;
-  }
-  return (
-    <div className="daisy-stat flex flex-row p-0 xl:flex-col">
-      <span className="daisy-stat-value text-md font-bold">
-        {currentValue?.toString()}
-      </span>
-      <div
-        data-tip={"Profit/Loss since open"}
-        className={classNames(
-          "daisy-stat-desc daisy-tooltip mt-1 inline-flex text-xs",
-          { "text-success": isPositiveChangeInValue },
-          { "text-error": !isPositiveChangeInValue },
-        )}
-      >
-        <span>{isPositiveChangeInValue ? "+" : ""}</span>
-        {baseAmountOut
-          ? `${formatBalance({
-              balance: baseAmountOut - row.baseAmountPaid,
-              decimals: baseToken.decimals,
-              places: 4,
-            })} ${baseToken.symbol}`
-          : undefined}
-      </div>
-    </div>
-  );
 }
 
 function FixedRateCell({
