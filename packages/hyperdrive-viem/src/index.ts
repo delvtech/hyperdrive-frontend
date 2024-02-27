0a95117edff29fc1087606f58138c68c6045aab6
Ryan Goree <goree.ryan@gmail.com>
2024-02-27 16:51:54 -0600

    Re-export core SDK from viem

--- packages/hyperdrive-viem/src/index.ts
+++ packages/hyperdrive-viem/src/index.ts
@@ -2,5 +2,8 @@
 export { createReadHyperdrive } from "src/factories/createReadHyperdrive";
 export { createReadWriteHyperdrive } from "src/factories/createReadWriteHyperdrive";
 
+// Re-export all of hyperdrive-sdk
+export * from "@hyperdrive/sdk";
+
 // Re-export all of evm-client-viem
 export * from "@delvtech/evm-client-viem";
