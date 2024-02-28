94b87a64ea5c77fc940d026bdb0d9a7bae6d3719
Jack Burrus <jhburrus3@gmail.com>
2024-02-28 13:10:18 -0700

    Adds token balance to openshort form (#721)
    * adds token balance
    
    * update classnames
    
    * keep gap

--- apps/hyperdrive-trading/src/ui/token/TokenChoices.tsx
+++ apps/hyperdrive-trading/src/ui/token/TokenChoices.tsx
@@ -1,6 +1,7 @@
 import { TokenConfig } from "@hyperdrive/appconfig";
 import classNames from "classnames";
 import { ReactElement } from "react";
+import { formatBalance } from "src/ui/base/formatting/formatBalance";
 import { Address } from "viem";
 
 export function TokenChoices({
@@ -11,7 +12,11 @@ export function TokenChoices({
   vertical = false,
 }: {
   label: string;
-  tokens: { disabled?: boolean; tokenConfig: TokenConfig<any> }[];
+  tokens: {
+    disabled?: boolean;
+    tokenConfig: TokenConfig<any>;
+    tokenBalance?: bigint;
+  }[];
   selectedTokenAddress: Address;
   onTokenChange: (tokenAddress: Address) => void;
   vertical?: boolean;
@@ -19,42 +24,64 @@ export function TokenChoices({
   return (
     <div
       className={classNames("flex justify-between", {
-        "flex-col gap-1": vertical,
+        "flex-col gap-2": vertical,
+        "flex-row items-center gap-8": !vertical,
       })}
     >
       <span className="daisy-label-text">{label}</span>
-      <div className="flex gap-8">
-        {tokens.map(({ disabled, tokenConfig: { symbol, address } }) => {
-          const checked = selectedTokenAddress === address;
-          return (
-            <label
-              key={address}
-              className={classNames(
-                "inline-flex items-center justify-end gap-2",
-                {
-                  "cursor-not-allowed text-gray-100": disabled,
-                  "cursor-pointer": !disabled,
-                },
-              )}
-            >
-              <input
-                type="radio"
-                name={symbol}
-                disabled={!!disabled}
-                checked={checked}
-                className={classNames("daisy-radio daisy-radio-xs", {
-                  // show as primary when disabled otherwise the radio button
-                  // doesn't look right (it's the same color as the background)
-                  "daisy-radio-primary": checked || disabled,
-                })}
-                onChange={() => {
-                  onTokenChange(address);
-                }}
-              />
-              {symbol}
-            </label>
-          );
+      <div
+        className={classNames("flex", {
+          "flex-col gap-2": vertical,
+          "gap-4": !vertical,
         })}
+      >
+        {tokens.map(
+          ({
+            disabled,
+            tokenConfig: { symbol, address, decimals },
+            tokenBalance,
+          }) => {
+            const checked = selectedTokenAddress === address;
+            return (
+              <label
+                key={address}
+                className={classNames(
+                  "inline-flex items-center justify-start gap-2",
+                  {
+                    "cursor-not-allowed text-gray-100": disabled,
+                    "cursor-pointer": !disabled,
+                  },
+                )}
+              >
+                <input
+                  type="radio"
+                  name={symbol}
+                  disabled={!!disabled}
+                  checked={checked}
+                  className={classNames("daisy-radio daisy-radio-xs", {
+                    // show as primary when disabled otherwise the radio button
+                    // doesn't look right (it's the same color as the background)
+                    "daisy-radio-primary": checked || disabled,
+                  })}
+                  onChange={() => {
+                    onTokenChange(address);
+                  }}
+                />
+                {symbol}{" "}
+                <span className="text-xs">
+                  {tokenBalance
+                    ? `(Balance: ${formatBalance({
+                        balance: tokenBalance,
+                        decimals,
+                        places: 2,
+                        includeCommas: true,
+                      })})`
+                    : null}
+                </span>
+              </label>
+            );
+          },
+        )}
       </div>
     </div>
   );
