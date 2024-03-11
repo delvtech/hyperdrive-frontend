9243fe84dc6f1541de51fbf85687b36876e32786
Danny Delott <dannydelott@gmail.com>
2024-03-11 13:00:29 -0700

    Add HyperdriveModel and StethHyperdriveModel

---
+++ apps/hyperdrive-trading/src/hyperdrive/BaseHyperdriveModel.ts
@@ -0,0 +1,193 @@
+import {
+  ContractReadOptions,
+  ReadWriteHyperdrive,
+  createReadWriteHyperdrive,
+} from "@delvtech/hyperdrive-viem";
+import {
+  AppConfig,
+  EmptyExtensions,
+  HyperdriveConfig,
+  TokenConfig,
+  YieldSourceExtensions,
+  findBaseToken,
+  findHyperdriveConfig,
+  findYieldSourceToken,
+} from "@hyperdrive/appconfig";
+import { WalletClient } from "node_modules/viem/_types/clients/createWalletClient";
+import { sdkCache } from "src/sdk/sdkCache";
+import { Address, Hash, PublicClient } from "viem";
+
+/**
+ * The stateless model for the Hyperdrive UI so that it's easy to implement new
+ * hyperdrives by adding them to appconfig and implementing the standard model.
+ */
+export interface IHyperdriveModel {
+  baseToken: TokenConfig<EmptyExtensions>;
+  sharesToken: TokenConfig<YieldSourceExtensions>;
+  hyperdriveConfig: HyperdriveConfig;
+
+  publicClient: PublicClient;
+  walletClient: WalletClient;
+
+  previewOpenLongWithBase(args: {
+    baseAmount: bigint;
+    options?: ContractReadOptions;
+  }): Promise<{
+    bondProceeds: bigint;
+    maturityTime: bigint;
+    spotPriceAfterOpen: bigint;
+    spotRateAfterOpen: bigint;
+    curveFee: bigint;
+  }>;
+  previewOpenLongWithShares(args: {
+    sharesAmount: bigint;
+    options?: ContractReadOptions;
+  }): Promise<{
+    bondProceeds: bigint;
+    maturityTime: bigint;
+    spotPriceAfterOpen: bigint;
+    spotRateAfterOpen: bigint;
+    curveFee: bigint;
+  }>;
+  openLongWithBase(args: {
+    destination: Address;
+    minSharePrice: bigint;
+    minBondsOut: bigint;
+    baseAmount: bigint;
+    ethValue?: bigint;
+  }): Promise<Hash>;
+  openLongWithShares(args: {
+    sharesAmount: bigint;
+    destination: Address;
+    minSharePrice: bigint;
+    minBondsOut: bigint;
+    ethValue?: bigint;
+  }): Promise<Hash>;
+}
+
+export class BaseHyperdriveModel implements IHyperdriveModel {
+  hyperdriveConfig: HyperdriveConfig;
+  baseToken: TokenConfig<EmptyExtensions>;
+  sharesToken: TokenConfig<YieldSourceExtensions>;
+  readWriteHyperdrive: ReadWriteHyperdrive;
+  publicClient: PublicClient;
+  walletClient: WalletClient;
+
+  constructor({
+    hyperdriveAddress,
+    appConfig,
+    publicClient,
+    walletClient,
+  }: {
+    publicClient: PublicClient;
+    walletClient: WalletClient;
+    hyperdriveAddress: Address;
+    appConfig: AppConfig;
+  }) {
+    this.hyperdriveConfig = findHyperdriveConfig({
+      hyperdriveAddress,
+      hyperdrives: appConfig.hyperdrives,
+    });
+    this.publicClient = publicClient;
+    this.walletClient = walletClient;
+    this.readWriteHyperdrive = createReadWriteHyperdrive({
+      address: hyperdriveAddress,
+      publicClient: this.publicClient,
+      walletClient: this.walletClient,
+      cache: sdkCache,
+      namespace: appConfig.chainId.toString(),
+    });
+    this.baseToken = findBaseToken({
+      baseTokenAddress: this.hyperdriveConfig.baseToken,
+      tokens: appConfig.tokens,
+    });
+    this.sharesToken = findYieldSourceToken({
+      yieldSourceTokenAddress: this.hyperdriveConfig.sharesToken,
+      tokens: appConfig.tokens,
+    });
+  }
+  openLongWithBase({
+    destination,
+    minBondsOut,
+    minSharePrice,
+    baseAmount,
+    ethValue,
+  }: {
+    destination: `0x${string}`;
+    minSharePrice: bigint;
+    minBondsOut: bigint;
+    baseAmount: bigint;
+    ethValue?: bigint | undefined;
+  }): Promise<Hash> {
+    return this.readWriteHyperdrive.openLong({
+      amount: baseAmount,
+      destination,
+      minBondsOut,
+      minSharePrice,
+      asBase: true,
+      options: { value: ethValue },
+    });
+  }
+  openLongWithShares({
+    destination,
+    minBondsOut,
+    minSharePrice,
+    sharesAmount,
+    ethValue,
+  }: {
+    sharesAmount: bigint;
+    destination: `0x${string}`;
+    minSharePrice: bigint;
+    minBondsOut: bigint;
+    ethValue?: bigint | undefined;
+  }): Promise<Hash> {
+    return this.readWriteHyperdrive.openLong({
+      amount: sharesAmount,
+      destination,
+      minBondsOut,
+      minSharePrice,
+      asBase: false,
+      options: { value: ethValue },
+    });
+  }
+  async previewOpenLongWithBase({
+    baseAmount,
+    options,
+  }: {
+    baseAmount: bigint;
+    options: ContractReadOptions;
+  }): Promise<{
+    bondProceeds: bigint;
+    maturityTime: bigint;
+    spotPriceAfterOpen: bigint;
+    spotRateAfterOpen: bigint;
+    curveFee: bigint;
+  }> {
+    return this.readWriteHyperdrive.previewOpenLong({
+      amountIn: baseAmount,
+      asBase: true,
+      decimals: this.hyperdriveConfig.decimals,
+      options,
+    });
+  }
+  async previewOpenLongWithShares({
+    sharesAmount,
+    options,
+  }: {
+    sharesAmount: bigint;
+    options: ContractReadOptions;
+  }): Promise<{
+    bondProceeds: bigint;
+    maturityTime: bigint;
+    spotPriceAfterOpen: bigint;
+    spotRateAfterOpen: bigint;
+    curveFee: bigint;
+  }> {
+    return this.readWriteHyperdrive.previewOpenLong({
+      amountIn: sharesAmount,
+      asBase: true,
+      decimals: this.hyperdriveConfig.decimals,
+      options,
+    });
+  }
+}
---
+++ apps/hyperdrive-trading/src/hyperdrive/steth/StethHyperdriveModel.ts
@@ -0,0 +1,87 @@
+import { MockLido } from "@delvtech/hyperdrive-artifacts/MockLido";
+import { ContractReadOptions } from "@delvtech/hyperdrive-viem";
+import { AppConfig } from "@hyperdrive/appconfig";
+import { BaseHyperdriveModel } from "src/hyperdrive/BaseHyperdriveModel";
+import { Address, Hash, PublicClient, WalletClient } from "viem";
+
+export class StethHyperdriveModel extends BaseHyperdriveModel {
+  lidoAddress: Address;
+  constructor({
+    hyperdriveAddress,
+    appConfig,
+    publicClient,
+    walletClient,
+  }: {
+    publicClient: PublicClient;
+    walletClient: WalletClient;
+    hyperdriveAddress: Address;
+    appConfig: AppConfig;
+  }) {
+    super({ hyperdriveAddress, appConfig, publicClient, walletClient });
+
+    this.lidoAddress = this.sharesToken.address;
+  }
+
+  /**
+   * The `sharesAmount` input is denominated in steth tokens since that is what
+   * is shown to the user. This preview function handles converting the steth
+   * tokens into steth shares so that hyperdrive can preview the trade
+   * correctly.
+   */
+  async previewOpenLongWithShares({
+    sharesAmount,
+    options,
+  }: {
+    sharesAmount: bigint;
+    options: ContractReadOptions;
+  }): Promise<{
+    bondProceeds: bigint;
+    maturityTime: bigint;
+    spotPriceAfterOpen: bigint;
+    spotRateAfterOpen: bigint;
+    curveFee: bigint;
+  }> {
+    const convertedSharesAmount = await this.convertStethTokensToShares(
+      sharesAmount,
+    );
+
+    return super.previewOpenLongWithShares({
+      sharesAmount: convertedSharesAmount,
+      options,
+    });
+  }
+
+  async openLongWithShares({
+    destination,
+    minBondsOut,
+    minSharePrice,
+    sharesAmount,
+    ethValue,
+  }: {
+    sharesAmount: bigint;
+    destination: `0x${string}`;
+    minSharePrice: bigint;
+    minBondsOut: bigint;
+    ethValue?: bigint | undefined;
+  }): Promise<Hash> {
+    const convertedSharesAmount = await this.convertStethTokensToShares(
+      sharesAmount,
+    );
+    return super.openLongWithShares({
+      destination,
+      minBondsOut,
+      minSharePrice,
+      sharesAmount: convertedSharesAmount,
+      ethValue,
+    });
+  }
+
+  private convertStethTokensToShares(sharesAmount: bigint) {
+    return this.publicClient.readContract({
+      abi: MockLido.abi,
+      address: this.lidoAddress,
+      functionName: "getSharesByPooledEth",
+      args: [sharesAmount],
+    });
+  }
+}
