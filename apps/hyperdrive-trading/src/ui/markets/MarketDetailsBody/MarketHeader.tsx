b6c769ac6ba4c92cf2050168ec696d84104fe2dc
Jack Burrus <jhburrus3@gmail.com>
2024-03-19 15:17:05 -0600

    adds skeleton for price badges in market header (#786)
    * adds skeleton
    
    * move hook logic into price badges

--- apps/hyperdrive-trading/src/ui/markets/MarketDetailsBody/MarketHeader.tsx
+++ apps/hyperdrive-trading/src/ui/markets/MarketDetailsBody/MarketHeader.tsx
@@ -1,7 +1,5 @@
-import { HyperdriveConfig, findBaseToken } from "@hyperdrive/appconfig";
+import { HyperdriveConfig } from "@hyperdrive/appconfig";
 import { ReactElement } from "react";
-import { useAppConfig } from "src/ui/appconfig/useAppConfig";
-import { useCurrentLongPrice } from "src/ui/hyperdrive/longs/hooks/useCurrentLongPrice";
 import { PriceBadges } from "src/ui/markets/MarketDetailsBody/PriceBadges";
 
 export function MarketHeader({
@@ -9,21 +7,13 @@ export function MarketHeader({
 }: {
   hyperdrive: HyperdriveConfig;
 }): ReactElement {
-  const appConfig = useAppConfig();
-  const { longPrice } = useCurrentLongPrice(hyperdrive.address);
-  const baseToken = findBaseToken({
-    baseTokenAddress: hyperdrive.baseToken,
-    tokens: appConfig.tokens,
-  });
   return (
     <div className="my-2 flex items-center gap-2 md:my-0">
       <div className="flex w-full flex-col items-start gap-1">
         <h1 className="gradient-text items-center md:my-0">
           {hyperdrive.name}
         </h1>
-        {longPrice ? (
-          <PriceBadges longPrice={longPrice} baseToken={baseToken} />
-        ) : null}
+        <PriceBadges hyperdrive={hyperdrive} />
       </div>
     </div>
   );
