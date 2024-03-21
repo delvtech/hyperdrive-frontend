ee42d578bcf5f6e14b4cc327973c453a76013574
jackburrus <jhburrus3@gmail.com>
2024-03-21 14:13:26 -0600

    approvals added to modal

--- apps/hyperdrive-trading/src/ui/hyperdrive/lp/AddLiquidityForm/AddLiquidityForm.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/lp/AddLiquidityForm/AddLiquidityForm.tsx
@@ -25,7 +25,7 @@ import { useLpShares } from "src/ui/hyperdrive/lp/hooks/useLpShares";
 import { useLpSharesTotalSupply } from "src/ui/hyperdrive/lp/hooks/useLpSharesTotalSupply";
 import { usePreviewAddLiquidity } from "src/ui/hyperdrive/lp/hooks/usePreviewAddLiquidity";
 import { TransactionView } from "src/ui/hyperdrive/TransactionView";
-import ApproveTokenButton from "src/ui/token/ApproveTokenButton";
+import ApproveTokenChoices from "src/ui/token/ApproveTokenChoices";
 import { useActiveToken } from "src/ui/token/hooks/useActiveToken";
 import { useTokenAllowance } from "src/ui/token/hooks/useTokenAllowance";
 import { TokenInput } from "src/ui/token/TokenInput";
@@ -213,10 +213,9 @@ export function AddLiquidityForm({
 
         if (!hasEnoughAllowance) {
           return (
-            <ApproveTokenButton
+            <ApproveTokenChoices
               spender={hyperdrive.address}
               token={activeToken}
-              tokenBalance={activeTokenBalance}
               amountAsBigInt={depositAmountAsBigInt}
               amount={depositAmount}
             />
