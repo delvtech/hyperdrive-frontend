ce553280357a1466131187f5f03b1ab859e8f72d
Ryan Goree <goree.ryan@gmail.com>
2024-03-21 18:45:21 -0500

    Add erc4626 token

---
+++ packages/hyperdrive-js-core/src/token/erc4626/ReadWriteErc4626.ts
@@ -0,0 +1,16 @@
+import { CachedReadWriteContract } from "@delvtech/evm-client";
+import { ReadWriteContractModelOptions } from "src/model/ReadWriteModel";
+import { writeErc20Mixin } from "src/token/erc20/ReadWriteErc20";
+import { ReadErc4626 } from "src/token/erc4626/ReadErc4626";
+import { Erc4626Abi } from "src/token/erc4626/abi";
+
+export interface ReadWriteErc4626Options
+  extends ReadWriteContractModelOptions {}
+
+export class ReadWriteErc4626 extends writeErc20Mixin(ReadErc4626) {
+  declare stEthContract: CachedReadWriteContract<Erc4626Abi>;
+
+  constructor(options: ReadWriteErc4626Options) {
+    super(options);
+  }
+}
