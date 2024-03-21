a7bf207006cfa184d0c031525f9c02c9edc3b112
Danny Delott <dannydelott@gmail.com>
2024-03-21 11:41:15 -0700

    Add token icon to tokenpicker

--- apps/hyperdrive-trading/src/ui/token/TokenPicker.tsx
+++ apps/hyperdrive-trading/src/ui/token/TokenPicker.tsx
@@ -1,3 +1,4 @@
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import { TokenConfig } from "@hyperdrive/appconfig";
 import { ReactElement } from "react";
 import { Address } from "viem";
@@ -11,24 +12,48 @@ export function TokenPicker({
   activeTokenAddress: Address;
   onChange: (tokenAddress: Address) => void;
 }): ReactElement {
+  // A single element doesn't need a dropdown
   if (tokens.length === 1) {
     return (
-      <div className="daisy-join-item flex h-12 items-center border border-neutral-content/30 bg-base-100 px-4">
-        {tokens[0].symbol}
+      <div className="daisy-join-item flex h-12 shrink-0 items-center gap-1.5 border border-neutral-content/30 bg-base-100 px-4">
+        <img src={tokens[0].iconUrl} className="h-5 " />{" "}
+        <span className="text-sm font-semibold">{tokens[0].symbol}</span>
       </div>
     );
   }
+  const activeToken = tokens.find(
+    ({ address }) => address === activeTokenAddress,
+  ) as TokenConfig<any>;
   return (
-    <select
-      className="daisy-join-item daisy-select flex h-12 items-center border border-neutral-content/30 bg-base-100"
-      onChange={(e) => onChange(e.target.value as Address)}
-      value={activeTokenAddress}
-    >
-      {tokens.map((token) => (
-        <option key={token.address} value={token.address}>
-          {token.symbol}
-        </option>
-      ))}
-    </select>
+    <div className={"daisy-dropdown daisy-dropdown-bottom shrink-0"}>
+      <button
+        className="daisy-btn flex h-12 items-center rounded-r-none border border-neutral-content/30 bg-base-100 px-4 hover:border-neutral-content/30"
+        onClick={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <img src={activeToken.iconUrl} className="h-5 " /> {activeToken.symbol}
+        <ChevronDownIcon className="ml-2 h-3" />
+      </button>
+      <ul
+        tabIndex={0}
+        className="daisy-menu daisy-dropdown-content z-[1] w-40 rounded-box bg-base-100 p-4 shadow"
+      >
+        {[
+          tokens.map((token) => (
+            <li key={token.address}>
+              <button
+                onClick={(e) => {
+                  onChange(token.address);
+                  e.preventDefault();
+                }}
+              >
+                <img src={token.iconUrl} className="h-5 " /> {token.symbol}
+              </button>
+            </li>
+          )),
+        ]}
+      </ul>
+    </div>
   );
 }
