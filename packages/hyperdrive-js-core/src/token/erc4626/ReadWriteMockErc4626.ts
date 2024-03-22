1f5a646adf2234aa23c2be7c291eec3e765f3ad4
Ryan Goree <goree.ryan@gmail.com>
2024-03-22 10:52:37 -0500

    Fix readWrite model

--- packages/hyperdrive-js-core/src/token/erc4626/ReadWriteMockErc4626.ts
+++ packages/hyperdrive-js-core/src/token/erc4626/ReadWriteMockErc4626.ts
@@ -3,13 +3,14 @@ import {
   ContractWriteOptions,
 } from "@delvtech/evm-client";
 import { MockErc4626Abi } from "src/token/erc4626/abi";
+import { writeErc20Mixin } from "src/token/erc20/ReadWriteErc20";
+import { ReadMockErc4626 } from "./ReadMockErc4626";
 import { ReadWriteContractModelOptions } from "src/model/ReadWriteModel";
-import { ReadWriteErc4626 } from "src/token/erc4626/ReadWriteErc4626";
 
 export interface ReadWriteMockErc4626Options
   extends ReadWriteContractModelOptions {}
 
-export class ReadWriteMockErc4626 extends ReadWriteErc4626 {
+export class ReadWriteMockErc4626 extends writeErc20Mixin(ReadMockErc4626) {
   declare mockErc4626Contract: CachedReadWriteContract<MockErc4626Abi>;
 
   constructor(options: ReadWriteMockErc4626Options) {
