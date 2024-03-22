290c6117b410dc09c670c9906f1b4173d7bafde0
Ryan Goree <goree.ryan@gmail.com>
2024-03-22 10:44:31 -0500

    Simplify models

--- packages/hyperdrive-js-core/src/hyperdrive/erc4626/ReadMockErc4626Hyperdrive.ts
+++ packages/hyperdrive-js-core/src/hyperdrive/erc4626/ReadMockErc4626Hyperdrive.ts
@@ -1,33 +1,16 @@
-import { CachedReadContract, ContractReadOptions } from "@delvtech/evm-client";
-import { ReadContractModelOptions } from "src/model/ReadModel";
+import { ContractReadOptions } from "@delvtech/evm-client";
 import {
-  mockErc4626HyperdriveAbi,
-  MockErc4626HyperdriveAbi,
-} from "src/hyperdrive/erc4626/abi";
-import { ReadErc4626Hyperdrive } from "src/hyperdrive/erc4626/ReadErc4626Hyperdrive";
+  ReadErc4626Hyperdrive,
+  ReadErc4626HyperdriveOptions,
+} from "src/hyperdrive/erc4626/ReadErc4626Hyperdrive";
 import { ReadMockErc4626 } from "src/token/erc4626/ReadMockErc4626";
 
-export interface ReadMockErc4626HyperdriveOptions
-  extends ReadContractModelOptions {}
-
 export class ReadMockErc4626Hyperdrive extends ReadErc4626Hyperdrive {
-  mockErc4626HyperdriveContract: CachedReadContract<MockErc4626HyperdriveAbi>;
-
   constructor({
     name = "Mock ERC-4626 Hyperdrive",
-    address,
-    contractFactory,
-    network,
-    cache,
-    namespace,
-  }: ReadMockErc4626HyperdriveOptions) {
-    super({ address, contractFactory, network, cache, name, namespace });
-    this.mockErc4626HyperdriveContract = contractFactory({
-      abi: mockErc4626HyperdriveAbi,
-      address,
-      cache,
-      namespace,
-    });
+    ...options
+  }: ReadErc4626HyperdriveOptions) {
+    super({ name, ...options });
   }
 
   async getSharesToken(
--- packages/hyperdrive-js-core/src/hyperdrive/erc4626/ReadWriteMockErc4626Hyperdrive.ts
+++ packages/hyperdrive-js-core/src/hyperdrive/erc4626/ReadWriteMockErc4626Hyperdrive.ts
@@ -1,24 +1,8 @@
-import {
-  CachedReadWriteContract,
-  ContractReadOptions,
-} from "@delvtech/evm-client";
-import { MockErc4626HyperdriveAbi } from "src/hyperdrive/erc4626/abi";
-import {
-  ReadWriteErc4626Hyperdrive,
-  ReadWriteErc4626HyperdriveOptions,
-} from "src/hyperdrive/erc4626/ReadWriteErc4626Hyperdrive";
+import { ContractReadOptions } from "@delvtech/evm-client";
+import { ReadWriteErc4626Hyperdrive } from "src/hyperdrive/erc4626/ReadWriteErc4626Hyperdrive";
 import { ReadWriteMockErc4626 } from "src/token/erc4626/ReadWriteMockErc4626";
 
-export interface ReadWriteMockErc4626HyperdriveOptions
-  extends ReadWriteErc4626HyperdriveOptions {}
-
 export class ReadWriteMockErc4626Hyperdrive extends ReadWriteErc4626Hyperdrive {
-  declare mockErc4626HyperdriveContract: CachedReadWriteContract<MockErc4626HyperdriveAbi>;
-
-  constructor(options: ReadWriteMockErc4626HyperdriveOptions) {
-    super(options);
-  }
-
   async getSharesToken(
     options?: ContractReadOptions,
   ): Promise<ReadWriteMockErc4626> {
