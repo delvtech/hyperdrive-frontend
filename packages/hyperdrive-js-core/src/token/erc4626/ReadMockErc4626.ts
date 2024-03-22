33494117b9473e46eac23e61dcad9bb763e67303
Ryan Goree <goree.ryan@gmail.com>
2024-03-22 10:51:51 -0500

    Simplify model

--- packages/hyperdrive-js-core/src/token/erc4626/ReadMockErc4626.ts
+++ packages/hyperdrive-js-core/src/token/erc4626/ReadMockErc4626.ts
@@ -1,9 +1,6 @@
-import { ReadContractModelOptions } from "src/model/ReadModel";
 import { CachedReadContract, ContractReadOptions } from "@delvtech/evm-client";
 import { mockErc4626Abi, MockErc4626Abi } from "src/token/erc4626/abi";
-import { ReadErc4626 } from "src/token/erc4626/ReadErc4626";
-
-export interface ReadMockErc4626Options extends ReadContractModelOptions {}
+import { ReadErc4626, ReadErc4626Options } from "src/token/erc4626/ReadErc4626";
 
 export class ReadMockErc4626 extends ReadErc4626 {
   mockErc4626Contract: CachedReadContract<MockErc4626Abi>;
@@ -15,7 +12,7 @@ export class ReadMockErc4626 extends ReadErc4626 {
     network,
     cache,
     namespace,
-  }: ReadMockErc4626Options) {
+  }: ReadErc4626Options) {
     super({
       address,
       contractFactory,
