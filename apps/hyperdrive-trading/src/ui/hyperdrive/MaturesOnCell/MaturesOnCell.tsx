dd560d8231da4de214c93f4199aa790b61d40b13
Jack Burrus <jhburrus3@gmail.com>
2024-03-25 13:16:28 -0600

    updates format date (#813)

--- apps/hyperdrive-trading/src/ui/hyperdrive/MaturesOnCell/MaturesOnCell.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/MaturesOnCell/MaturesOnCell.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from "react";
 import { convertMillisecondsToDays } from "src/base/convertMillisecondsToDays";
+import { formatDate } from "src/ui/base/formatting/formatDate";
 import { useBlock } from "wagmi";
 
 export function MaturesOnCell({
@@ -10,7 +11,6 @@ export function MaturesOnCell({
   const { data: currentBlock } = useBlock();
   const isTermComplete = maturity < (currentBlock?.timestamp || 0n);
   const maturityDateMS = maturity * 1000n;
-  const termEndDate = new Date(Number(maturityDateMS));
   const daysLeft = convertMillisecondsToDays(
     Number(maturityDateMS - (currentBlock?.timestamp || 0n) * 1000n),
   );
@@ -18,11 +18,7 @@ export function MaturesOnCell({
   return (
     <div className="daisy-stat flex flex-row p-0 xl:flex-col">
       <span className="daisy-stat-value text-xs font-normal lg:text-md">
-        {termEndDate.toLocaleDateString("en-US", {
-          month: "short",
-          day: "numeric",
-          year: "numeric",
-        })}
+        {formatDate(Number(maturityDateMS))}
       </span>
       {isTermComplete ? (
         <div className={"daisy-stat-desc inline-flex w-32 text-xs"}>
