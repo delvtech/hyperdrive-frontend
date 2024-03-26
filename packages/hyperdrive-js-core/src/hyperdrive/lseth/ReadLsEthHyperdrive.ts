4c626165688b70eb0203cbfd6a128923d3a42d47
Ryan Goree <goree.ryan@gmail.com>
2024-03-26 10:27:03 -0500

    Add LsETH Hyperdrive models

---
+++ packages/hyperdrive-js-core/src/hyperdrive/lseth/ReadLsEthHyperdrive.ts
@@ -0,0 +1,83 @@
+import { CachedReadContract, ContractReadOptions } from "@delvtech/evm-client";
+import { Constructor } from "src/base/types";
+import {
+  lsEthHyperdriveAbi,
+  LsEthHyperdriveAbi,
+} from "src/hyperdrive/lseth/abi";
+import {
+  ReadHyperdrive,
+  ReadHyperdriveOptions,
+} from "src/hyperdrive/ReadHyperdrive/ReadHyperdrive";
+import { ReadEth } from "src/token/eth/ReadEth";
+import { ReadLsEth } from "src/token/lseth/ReadLsEth";
+
+export class ReadLsEthHyperdrive extends readLsEthHyperdriveMixin(
+  ReadHyperdrive,
+) {}
+
+/**
+ * @internal
+ */
+export interface ReadLsEthHyperdriveMixin {
+  lsEthHyperdriveContract: CachedReadContract<LsEthHyperdriveAbi>;
+
+  /**
+   * Get a model of ETH, the base token for this Hyperdrive instance.
+   */
+  getBaseToken(options?: ContractReadOptions): Promise<ReadEth>;
+
+  /**
+   * Get a model of the LsETH token for this Hyperdrive instance.
+   */
+  getSharesToken(options?: ContractReadOptions): Promise<ReadLsEth>;
+}
+
+/**
+ * @internal
+ */
+export function readLsEthHyperdriveMixin<T extends Constructor<ReadHyperdrive>>(
+  Base: T,
+): T & Constructor<ReadLsEthHyperdriveMixin> {
+  return class extends Base implements ReadLsEthHyperdriveMixin {
+    lsEthHyperdriveContract: CachedReadContract<LsEthHyperdriveAbi>;
+
+    constructor(...[options]: any[]) {
+      const {
+        name = "lsETH Hyperdrive",
+        address,
+        contractFactory,
+        network,
+        cache,
+        namespace,
+      } = options as ReadHyperdriveOptions;
+      super({ address, contractFactory, network, cache, name, namespace });
+      this.lsEthHyperdriveContract = contractFactory({
+        abi: lsEthHyperdriveAbi,
+        address,
+        cache,
+        namespace,
+      });
+    }
+
+    async getBaseToken(): Promise<ReadEth> {
+      return new ReadEth({
+        contractFactory: this.contractFactory,
+        network: this.network,
+      });
+    }
+
+    async getSharesToken(options?: ContractReadOptions): Promise<ReadLsEth> {
+      const address = await this.lsEthHyperdriveContract.read(
+        "lsEth",
+        {},
+        options,
+      );
+      return new ReadLsEth({
+        address,
+        contractFactory: this.contractFactory,
+        namespace: this.contract.namespace,
+        network: this.network,
+      });
+    }
+  };
+}
