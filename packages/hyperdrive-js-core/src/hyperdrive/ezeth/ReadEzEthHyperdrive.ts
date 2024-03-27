9f689550ee6ca453b67acc4ef546076976100e1c
Ryan Goree <goree.ryan@gmail.com>
2024-03-27 14:58:48 -0500

    Add ezETH Hyperdrive models

---
+++ packages/hyperdrive-js-core/src/hyperdrive/ezeth/ReadEzEthHyperdrive.ts
@@ -0,0 +1,72 @@
+import { CachedReadContract, ContractReadOptions } from "@delvtech/evm-client";
+import { Constructor } from "src/base/types";
+import { ReadHyperdrive } from "src/hyperdrive/ReadHyperdrive/ReadHyperdrive";
+import {
+  EzEthHyperdriveAbi,
+  ezEthHyperdriveAbi,
+} from "src/hyperdrive/ezeth/abi";
+import { ReadErc20 } from "src/token/erc20/ReadErc20";
+import { ReadEth } from "src/token/eth/ReadEth";
+
+/**
+ * @internal
+ */
+export interface ReadEzEthHyperdriveMixin {
+  ezEthHyperdriveContract: CachedReadContract<EzEthHyperdriveAbi>;
+
+  /**
+   * Get a model of ETH, the base token for this Hyperdrive instance.
+   */
+  getBaseToken(options?: ContractReadOptions): Promise<ReadEth>;
+
+  /**
+   * Get a model of the EzETH token for this Hyperdrive instance.
+   */
+  getSharesToken(options?: ContractReadOptions): Promise<ReadErc20>;
+}
+
+/**
+ * @internal
+ */
+export function readEzEthHyperdriveMixin<T extends Constructor<ReadHyperdrive>>(
+  Base: T,
+): Constructor<ReadEzEthHyperdriveMixin> & T {
+  return class extends Base implements ReadEzEthHyperdriveMixin {
+    ezEthHyperdriveContract: CachedReadContract<EzEthHyperdriveAbi>;
+
+    constructor(...[options]: any[]) {
+      const {
+        name = "EzETH Hyperdrive",
+        address,
+        contractFactory,
+        network,
+        cache,
+        namespace,
+      } = options as any;
+      super({ address, contractFactory, network, cache, name, namespace });
+      this.ezEthHyperdriveContract = contractFactory({
+        abi: ezEthHyperdriveAbi,
+        address,
+        cache,
+        namespace,
+      });
+    }
+
+    async getBaseToken(): Promise<ReadEth> {
+      return new ReadEth({
+        contractFactory: this.contractFactory,
+        network: this.network,
+      });
+    }
+
+    async getSharesToken(): Promise<ReadErc20> {
+      const address = await this.ezEthHyperdriveContract.read("ezETH");
+      return new ReadErc20({
+        address,
+        contractFactory: this.contractFactory,
+        namespace: this.contract.namespace,
+        network: this.network,
+      });
+    }
+  };
+}
---
+++ packages/hyperdrive-js-core/src/hyperdrive/ezeth/ReadWriteEzEthHyperdrive.ts
@@ -0,0 +1,36 @@
+import {
+  CachedReadWriteContract,
+  ContractReadOptions,
+} from "@delvtech/evm-client";
+import { ReadWriteHyperdrive } from "src/hyperdrive/ReadWriteHyperdrive";
+import { readEzEthHyperdriveMixin } from "src/hyperdrive/ezeth/ReadEzEthHyperdrive";
+import { EzEthHyperdriveAbi } from "src/hyperdrive/ezeth/abi";
+import { ReadWriteErc20 } from "src/token/erc20/ReadWriteErc20";
+import { ReadWriteEth } from "src/token/eth/ReadWriteEth";
+
+export class ReadWriteEzEthHyperdrive extends readEzEthHyperdriveMixin(
+  ReadWriteHyperdrive,
+) {
+  declare lsEthHyperdriveContract: CachedReadWriteContract<EzEthHyperdriveAbi>;
+
+  async getSharesToken(options?: ContractReadOptions): Promise<ReadWriteErc20> {
+    const address = await this.lsEthHyperdriveContract.read(
+      "ezETH",
+      {},
+      options,
+    );
+    return new ReadWriteErc20({
+      address,
+      contractFactory: this.contractFactory,
+      namespace: this.contract.namespace,
+      network: this.network,
+    });
+  }
+
+  async getBaseToken(): Promise<ReadWriteEth> {
+    return new ReadWriteEth({
+      contractFactory: this.contractFactory,
+      network: this.network,
+    });
+  }
+}
