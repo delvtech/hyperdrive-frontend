bbc98f075f712432707fcbc204f0d40659e7f261
Danny Delott <dannydelott@gmail.com>
2024-03-28 08:06:37 -0700

    Organize file

--- apps/hyperdrive-trading/src/ui/hyperdrive/TransactionTable/TransactionsTable.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/TransactionTable/TransactionsTable.tsx
@@ -54,306 +54,7 @@ const eventMap = {
 } as const;
 type EventName = keyof typeof eventMap;
 
-function FilterSelect({
-  header,
-}: {
-  header: Header<TransactionData, unknown>;
-}) {
-  const isTailwindSmallScreen = useIsTailwindSmallScreen();
-  if (isTailwindSmallScreen) {
-    return (
-      <div className="daisy-dropdown flex flex-row rounded border">
-        <label tabIndex={0} className="daisy-btn daisy-btn-ghost rounded-btn">
-          {(header.column.getFilterValue() as string) || "All"}
-          <ChevronDownIcon className="h-4" />
-        </label>
-        <ul
-          tabIndex={0}
-          className="daisy-menu daisy-dropdown-content mt-14 rounded-md bg-base-200 p-2 shadow"
-        >
-          {["All", "Longs", "Shorts", "LP"].map((filter) => (
-            <li key={filter} className="daisy-menu-title">
-              <a onClick={() => header.column.setFilterValue(filter)}>
-                {filter}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-  return (
-    <div className="flex flex-wrap">
-      {["All", "Longs", "Shorts", "LP"].map((filter) => (
-        <a
-          key={filter}
-          className={`${
-            header.column.getFilterValue() === filter ||
-            (!header.column.getFilterValue() && filter === "All")
-              ? "daisy-tab daisy-tab-active text-sm md:text-lg"
-              : "daisy-tab text-sm  font-normal opacity-80 hover:opacity-100 md:text-lg"
-          }`}
-          onClick={() => header.column.setFilterValue(filter)}
-        >
-          {filter}
-        </a>
-      ))}
-    </div>
-  );
-}
-
-function formatTransactionTableMobileData(
-  row: TransactionData,
-  hyperdrive: HyperdriveConfig,
-  appConfig: AppConfig,
-) {
-  const baseToken = findBaseToken({
-    baseTokenAddress: hyperdrive.baseToken,
-    tokens: appConfig.tokens,
-  });
-  const size = dnum.format(
-    [
-      row.eventName === "OpenShort" || row.eventName === "CloseShort"
-        ? row.bondAmount || 0n
-        : row.baseAmount || 0n,
-      18,
-    ],
-    { digits: 2 },
-  );
-
-  let baseQueuedForWithdrawalLabel;
-  if (
-    row.eventName === "RemoveLiquidity" &&
-    row.withdrawalShares &&
-    row.lpSharePrice
-  ) {
-    const baseQueuedForWithdrawal = dnum.multiply(
-      [row.withdrawalShares, hyperdrive.decimals],
-      [row.lpSharePrice, hyperdrive.decimals],
-    )[0];
-
-    baseQueuedForWithdrawalLabel = formatBaseQueuedForWithdrawalLabel(
-      baseQueuedForWithdrawal,
-      hyperdrive.decimals,
-      baseToken.symbol,
-    );
-  }
-  return [
-    {
-      name: "Event",
-      value: (
-        <EventNameCell
-          name={eventMap[row.eventName as EventName] || row.eventName}
-          blockNumber={row.blockNumber || 0n}
-        />
-      ),
-    },
-    {
-      name: "Size",
-      value: (
-        <div className="flex flex-col items-end gap-1">
-          <span className="flex">
-            {size} {baseToken.symbol}
-          </span>
-          <span className="daisy-label-text text-neutral-content ">
-            {baseQueuedForWithdrawalLabel}
-          </span>
-        </div>
-      ),
-    },
-    {
-      name: "Account",
-      value: <AccountCell account={row.trader} />,
-    },
-    {
-      name: "Time",
-      value: <BlockInfo blockNumber={row.blockNumber} />,
-    },
-  ];
-}
-
 const columnHelper = createColumnHelper<TransactionData>();
-
-function getMobileColumns(hyperdrive: HyperdriveConfig, appConfig: AppConfig) {
-  return [
-    columnHelper.accessor("eventName", {
-      id: "eventName",
-      enableSorting: false,
-      enableColumnFilter: true,
-      header: () => null,
-      cell: ({ row }) => {
-        const data = formatTransactionTableMobileData(
-          row.original,
-          hyperdrive,
-          appConfig,
-        );
-        return (
-          <ul className="flex flex-col items-start gap-4 text-neutral-content">
-            {data.map((column) => (
-              <li key={column.name}>{column.name}</li>
-            ))}
-          </ul>
-        );
-      },
-      filterFn: (row, _, filterValue) => {
-        const type = row.getValue("eventName") as string;
-        const filters = {
-          All: true,
-          Longs: ["OpenLong", "CloseLong"].includes(type),
-          Shorts: ["OpenShort", "CloseShort"].includes(type),
-          LP: [
-            "AddLiquidity",
-            "RemoveLiquidity",
-            "RedeemWithdrawalShares",
-          ].includes(type),
-        } as const;
-        return filters[filterValue as keyof typeof filters];
-      },
-    }),
-    columnHelper.display({
-      id: "ColumnValues",
-      cell: ({ row }) => {
-        const data = formatTransactionTableMobileData(
-          row.original,
-          hyperdrive,
-          appConfig,
-        );
-        return (
-          <ul className="flex flex-col items-end gap-4">
-            {data.map((column) => (
-              <li key={column.name}>{column.value}</li>
-            ))}
-          </ul>
-        );
-      },
-    }),
-  ];
-}
-
-function getColumns(hyperdrive: HyperdriveConfig, appConfig: AppConfig) {
-  const baseToken = findBaseToken({
-    baseTokenAddress: hyperdrive.baseToken,
-    tokens: appConfig.tokens,
-  });
-  return [
-    columnHelper.accessor("eventName", {
-      id: "eventName",
-      enableSorting: false,
-      enableColumnFilter: true,
-      header: () => null,
-      cell: ({ getValue, row }) => {
-        return (
-          <EventNameCell
-            name={eventMap[getValue() as EventName] || getValue()}
-            blockNumber={row.original.blockNumber || 0n}
-          />
-        );
-      },
-      filterFn: (row, _, filterValue) => {
-        const type = row.getValue("eventName") as string;
-        const filters = {
-          All: true,
-          Longs: ["OpenLong", "CloseLong"].includes(type),
-          Shorts: ["OpenShort", "CloseShort"].includes(type),
-          LP: [
-            "AddLiquidity",
-            "RemoveLiquidity",
-            "RedeemWithdrawalShares",
-          ].includes(type),
-        } as const;
-        return filters[filterValue as keyof typeof filters];
-      },
-    }),
-    columnHelper.accessor("bondAmount", {
-      id: "bondAmount",
-      header: `Size`,
-      cell: ({ getValue, row }) => {
-        const bondAmount = getValue();
-        const size = dnum.format(
-          [
-            row.original.eventName === "OpenShort" ||
-            row.original.eventName === "CloseShort"
-              ? bondAmount || 0n
-              : row.original.baseAmount || 0n,
-            18,
-          ],
-          { digits: 2 },
-        );
-        const isLpRow =
-          row.getValue("eventName") === "AddLiquidity" ||
-          row.getValue("eventName") === "RemoveLiquidity" ||
-          row.getValue("eventName") === "RedeemWithdrawalShares";
-
-        // show withdrawal shares for remove liquidity
-        if (
-          row.getValue("eventName") === "RemoveLiquidity" &&
-          row.original.withdrawalShares &&
-          row.original.lpSharePrice
-        ) {
-          const baseQueuedForWithdrawal = dnum.multiply(
-            [row.original.withdrawalShares, hyperdrive.decimals],
-            [row.original.lpSharePrice, hyperdrive.decimals],
-          )[0];
-
-          const baseQueuedForWithdrawalLabel =
-            formatBaseQueuedForWithdrawalLabel(
-              baseQueuedForWithdrawal,
-              hyperdrive.decimals,
-              baseToken.symbol,
-            );
-
-          return (
-            <div className="flex flex-col gap-1">
-              <span>
-                {size} {baseToken.symbol}
-              </span>
-              <span className="daisy-label-text text-neutral-content ">
-                {baseQueuedForWithdrawalLabel}
-              </span>
-            </div>
-          );
-        }
-
-        return (
-          <span>
-            {size} {isLpRow ? baseToken.symbol : `hy${baseToken.symbol}`}
-          </span>
-        );
-      },
-      enableColumnFilter: false,
-      sortingFn: (a, b) =>
-        Number(a?.getValue("value") ?? 0) - Number(b?.getValue("value") ?? 0),
-    }),
-    columnHelper.accessor("trader", {
-      header: "Account",
-      enableColumnFilter: false,
-      cell: (account) => <AccountCell account={account.getValue()} />,
-    }),
-    columnHelper.accessor("blockNumber", {
-      header: "Time",
-      enableColumnFilter: false,
-      cell: (blockNumber) => <BlockInfo blockNumber={blockNumber.getValue()} />,
-    }),
-  ];
-}
-
-function formatBaseQueuedForWithdrawalLabel(
-  baseQueuedForWithdrawal: bigint,
-  decimals: number,
-  baseSymbol: string,
-) {
-  const baseQueuedForWithdrawalLabel = formatBalance({
-    balance: baseQueuedForWithdrawal,
-    places: 2,
-    decimals,
-  });
-  const floorOrAmount =
-    baseQueuedForWithdrawal < parseUnits("0.001", decimals)
-      ? "<0.001"
-      : baseQueuedForWithdrawalLabel;
-
-  return `+${floorOrAmount} ${baseSymbol} queued for withdrawal`;
-}
 
 export function TransactionTable({
   hyperdrive,
@@ -476,6 +177,305 @@ export function TransactionTable({
       {tableInstance.getFilteredRowModel().rows.length && !isLoading ? (
         <Pagination tableInstance={tableInstance} />
       ) : null}
+    </div>
+  );
+}
+
+function getColumns(hyperdrive: HyperdriveConfig, appConfig: AppConfig) {
+  const baseToken = findBaseToken({
+    baseTokenAddress: hyperdrive.baseToken,
+    tokens: appConfig.tokens,
+  });
+  return [
+    columnHelper.accessor("eventName", {
+      id: "eventName",
+      enableSorting: false,
+      enableColumnFilter: true,
+      header: () => null,
+      cell: ({ getValue, row }) => {
+        return (
+          <EventNameCell
+            name={eventMap[getValue() as EventName] || getValue()}
+            blockNumber={row.original.blockNumber || 0n}
+          />
+        );
+      },
+      filterFn: (row, _, filterValue) => {
+        const type = row.getValue("eventName") as string;
+        const filters = {
+          All: true,
+          Longs: ["OpenLong", "CloseLong"].includes(type),
+          Shorts: ["OpenShort", "CloseShort"].includes(type),
+          LP: [
+            "AddLiquidity",
+            "RemoveLiquidity",
+            "RedeemWithdrawalShares",
+          ].includes(type),
+        } as const;
+        return filters[filterValue as keyof typeof filters];
+      },
+    }),
+    columnHelper.accessor("bondAmount", {
+      id: "bondAmount",
+      header: `Size`,
+      cell: ({ getValue, row }) => {
+        const bondAmount = getValue();
+        const size = dnum.format(
+          [
+            row.original.eventName === "OpenShort" ||
+            row.original.eventName === "CloseShort"
+              ? bondAmount || 0n
+              : row.original.baseAmount || 0n,
+            18,
+          ],
+          { digits: 2 },
+        );
+        const isLpRow =
+          row.getValue("eventName") === "AddLiquidity" ||
+          row.getValue("eventName") === "RemoveLiquidity" ||
+          row.getValue("eventName") === "RedeemWithdrawalShares";
+
+        // show withdrawal shares for remove liquidity
+        if (
+          row.getValue("eventName") === "RemoveLiquidity" &&
+          row.original.withdrawalShares &&
+          row.original.lpSharePrice
+        ) {
+          const baseQueuedForWithdrawal = dnum.multiply(
+            [row.original.withdrawalShares, hyperdrive.decimals],
+            [row.original.lpSharePrice, hyperdrive.decimals],
+          )[0];
+
+          const baseQueuedForWithdrawalLabel =
+            formatBaseQueuedForWithdrawalLabel(
+              baseQueuedForWithdrawal,
+              hyperdrive.decimals,
+              baseToken.symbol,
+            );
+
+          return (
+            <div className="flex flex-col gap-1">
+              <span>
+                {size} {baseToken.symbol}
+              </span>
+              <span className="daisy-label-text text-neutral-content ">
+                {baseQueuedForWithdrawalLabel}
+              </span>
+            </div>
+          );
+        }
+
+        return (
+          <span>
+            {size} {isLpRow ? baseToken.symbol : `hy${baseToken.symbol}`}
+          </span>
+        );
+      },
+      enableColumnFilter: false,
+      sortingFn: (a, b) =>
+        Number(a?.getValue("value") ?? 0) - Number(b?.getValue("value") ?? 0),
+    }),
+    columnHelper.accessor("trader", {
+      header: "Account",
+      enableColumnFilter: false,
+      cell: (account) => <AccountCell account={account.getValue()} />,
+    }),
+    columnHelper.accessor("blockNumber", {
+      header: "Time",
+      enableColumnFilter: false,
+      cell: (blockNumber) => <BlockInfo blockNumber={blockNumber.getValue()} />,
+    }),
+  ];
+}
+
+function formatTransactionTableMobileData(
+  row: TransactionData,
+  hyperdrive: HyperdriveConfig,
+  appConfig: AppConfig,
+) {
+  const baseToken = findBaseToken({
+    baseTokenAddress: hyperdrive.baseToken,
+    tokens: appConfig.tokens,
+  });
+  const size = dnum.format(
+    [
+      row.eventName === "OpenShort" || row.eventName === "CloseShort"
+        ? row.bondAmount || 0n
+        : row.baseAmount || 0n,
+      18,
+    ],
+    { digits: 2 },
+  );
+
+  let baseQueuedForWithdrawalLabel;
+  if (
+    row.eventName === "RemoveLiquidity" &&
+    row.withdrawalShares &&
+    row.lpSharePrice
+  ) {
+    const baseQueuedForWithdrawal = dnum.multiply(
+      [row.withdrawalShares, hyperdrive.decimals],
+      [row.lpSharePrice, hyperdrive.decimals],
+    )[0];
+
+    baseQueuedForWithdrawalLabel = formatBaseQueuedForWithdrawalLabel(
+      baseQueuedForWithdrawal,
+      hyperdrive.decimals,
+      baseToken.symbol,
+    );
+  }
+  return [
+    {
+      name: "Event",
+      value: (
+        <EventNameCell
+          name={eventMap[row.eventName as EventName] || row.eventName}
+          blockNumber={row.blockNumber || 0n}
+        />
+      ),
+    },
+    {
+      name: "Size",
+      value: (
+        <div className="flex flex-col items-end gap-1">
+          <span className="flex">
+            {size} {baseToken.symbol}
+          </span>
+          <span className="daisy-label-text text-neutral-content ">
+            {baseQueuedForWithdrawalLabel}
+          </span>
+        </div>
+      ),
+    },
+    {
+      name: "Account",
+      value: <AccountCell account={row.trader} />,
+    },
+    {
+      name: "Time",
+      value: <BlockInfo blockNumber={row.blockNumber} />,
+    },
+  ];
+}
+
+function getMobileColumns(hyperdrive: HyperdriveConfig, appConfig: AppConfig) {
+  return [
+    columnHelper.accessor("eventName", {
+      id: "eventName",
+      enableSorting: false,
+      enableColumnFilter: true,
+      header: () => null,
+      cell: ({ row }) => {
+        const data = formatTransactionTableMobileData(
+          row.original,
+          hyperdrive,
+          appConfig,
+        );
+        return (
+          <ul className="flex flex-col items-start gap-4 text-neutral-content">
+            {data.map((column) => (
+              <li key={column.name}>{column.name}</li>
+            ))}
+          </ul>
+        );
+      },
+      filterFn: (row, _, filterValue) => {
+        const type = row.getValue("eventName") as string;
+        const filters = {
+          All: true,
+          Longs: ["OpenLong", "CloseLong"].includes(type),
+          Shorts: ["OpenShort", "CloseShort"].includes(type),
+          LP: [
+            "AddLiquidity",
+            "RemoveLiquidity",
+            "RedeemWithdrawalShares",
+          ].includes(type),
+        } as const;
+        return filters[filterValue as keyof typeof filters];
+      },
+    }),
+    columnHelper.display({
+      id: "ColumnValues",
+      cell: ({ row }) => {
+        const data = formatTransactionTableMobileData(
+          row.original,
+          hyperdrive,
+          appConfig,
+        );
+        return (
+          <ul className="flex flex-col items-end gap-4">
+            {data.map((column) => (
+              <li key={column.name}>{column.value}</li>
+            ))}
+          </ul>
+        );
+      },
+    }),
+  ];
+}
+
+function formatBaseQueuedForWithdrawalLabel(
+  baseQueuedForWithdrawal: bigint,
+  decimals: number,
+  baseSymbol: string,
+) {
+  const baseQueuedForWithdrawalLabel = formatBalance({
+    balance: baseQueuedForWithdrawal,
+    places: 2,
+    decimals,
+  });
+  const floorOrAmount =
+    baseQueuedForWithdrawal < parseUnits("0.001", decimals)
+      ? "<0.001"
+      : baseQueuedForWithdrawalLabel;
+
+  return `+${floorOrAmount} ${baseSymbol} queued for withdrawal`;
+}
+
+function FilterSelect({
+  header,
+}: {
+  header: Header<TransactionData, unknown>;
+}) {
+  const isTailwindSmallScreen = useIsTailwindSmallScreen();
+  if (isTailwindSmallScreen) {
+    return (
+      <div className="daisy-dropdown flex flex-row rounded border">
+        <label tabIndex={0} className="daisy-btn daisy-btn-ghost rounded-btn">
+          {(header.column.getFilterValue() as string) || "All"}
+          <ChevronDownIcon className="h-4" />
+        </label>
+        <ul
+          tabIndex={0}
+          className="daisy-menu daisy-dropdown-content mt-14 rounded-md bg-base-200 p-2 shadow"
+        >
+          {["All", "Longs", "Shorts", "LP"].map((filter) => (
+            <li key={filter} className="daisy-menu-title">
+              <a onClick={() => header.column.setFilterValue(filter)}>
+                {filter}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  return (
+    <div className="flex flex-wrap">
+      {["All", "Longs", "Shorts", "LP"].map((filter) => (
+        <a
+          key={filter}
+          className={`${
+            header.column.getFilterValue() === filter ||
+            (!header.column.getFilterValue() && filter === "All")
+              ? "daisy-tab daisy-tab-active text-sm md:text-lg"
+              : "daisy-tab text-sm  font-normal opacity-80 hover:opacity-100 md:text-lg"
+          }`}
+          onClick={() => header.column.setFilterValue(filter)}
+        >
+          {filter}
+        </a>
+      ))}
     </div>
   );
 }
