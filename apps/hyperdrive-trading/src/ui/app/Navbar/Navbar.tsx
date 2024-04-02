b5b645fc865073eed60511dbf82699399b97b5e3
jackburrus <jhburrus3@gmail.com>
2024-04-02 13:02:35 -0600

    remove feature flex picker on mobile

--- apps/hyperdrive-trading/src/ui/app/Navbar/Navbar.tsx
+++ apps/hyperdrive-trading/src/ui/app/Navbar/Navbar.tsx
@@ -1,18 +1,22 @@
 import { Link } from "@tanstack/react-router";
 import { ReactElement } from "react";
-// import { FeatureFlagPicker } from "src/ui/app/Navbar/FeatureFlagPicker";
+import { FeatureFlagPicker } from "src/ui/app/Navbar/FeatureFlagPicker";
 import { HyperdriveLogo } from "src/ui/app/Navbar/HyperdriveLogo";
 import { ConnectWalletButton } from "src/ui/base/components/ConnectWallet";
+import { useIsTailwindSmallScreen } from "src/ui/base/mediaBreakpoints";
 export function Navbar(): ReactElement {
+  const isTailwindSmallScreen = useIsTailwindSmallScreen();
   return (
-    <div className="daisy-navbar flex">
+    <div className="daisy-navbar">
       <div className="daisy-navbar-start ml-2">
         <Link to={"/"} className="inline-flex items-center ">
           <HyperdriveLogo />
         </Link>
       </div>
       <div className="daisy-navbar-end gap-8">
-        {/* {import.meta.env.DEV ? <FeatureFlagPicker /> : null} */}
+        {import.meta.env.DEV && !isTailwindSmallScreen ? (
+          <FeatureFlagPicker />
+        ) : null}
         <ConnectWalletButton />
       </div>
     </div>
