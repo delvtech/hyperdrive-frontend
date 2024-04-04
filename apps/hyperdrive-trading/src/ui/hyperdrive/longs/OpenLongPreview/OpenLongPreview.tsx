623c5e3808e3d063018e543352d858b5abd3a425
Danny Delott <dannydelott@gmail.com>
2024-04-03 17:46:39 -0700

    Add You Spend

--- apps/hyperdrive-trading/src/ui/hyperdrive/longs/OpenLongPreview/OpenLongPreview.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/longs/OpenLongPreview/OpenLongPreview.tsx
@@ -2,7 +2,11 @@ import {
   calculateFixedRateFromOpenLong,
   Long,
 } from "@delvtech/hyperdrive-viem";
-import { findBaseToken, HyperdriveConfig } from "@hyperdrive/appconfig";
+import {
+  findBaseToken,
+  HyperdriveConfig,
+  TokenConfig,
+} from "@hyperdrive/appconfig";
 import classNames from "classnames";
 import * as dnum from "dnum";
 import { ReactElement } from "react";
@@ -18,6 +22,7 @@ interface OpenLongPreviewProps {
   hyperdrive: HyperdriveConfig;
   long: Long;
   spotRateAfterOpen: bigint | undefined;
+  activeToken: TokenConfig<any>;
   curveFee: bigint | undefined;
 }
 
@@ -25,6 +30,7 @@ export function OpenLongPreview({
   hyperdrive,
   long,
   spotRateAfterOpen,
+  activeToken,
   curveFee,
 }: OpenLongPreviewProps): ReactElement {
   const appConfig = useAppConfig();
@@ -34,19 +40,21 @@ export function OpenLongPreview({
   });
   const { fixedAPR } = useCurrentFixedAPR(hyperdrive.address);
 
-  let changeInFixedApr = 0n;
-  if (spotRateAfterOpen && fixedAPR) {
-    changeInFixedApr = dnum.subtract(
-      [fixedAPR.apr, 18],
-      [spotRateAfterOpen, 18],
-    )[0];
-  }
-
   const termLengthMS = Number(hyperdrive.poolConfig.positionDuration * 1000n);
   const numDays = convertMillisecondsToDays(termLengthMS);
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-col gap-3">
+        <LabelValue
+          label="You spend"
+          value={
+            <span>{`${formatBalance({
+              balance: long.baseAmountPaid,
+              decimals: baseToken.decimals,
+              places: 4,
+            })} ${activeToken.symbol}`}</span>
+          }
+        />
         <LabelValue
           label="You receive"
           value={
@@ -76,7 +84,7 @@ export function OpenLongPreview({
         />
       </div>
       <div className="flex flex-col gap-3">
-        <h6 className="font-medium">Interest Rate</h6>
+        <h6 className="font-medium">Fixed Rate</h6>
         <LabelValue
           label="Net fixed rate"
           value={
@@ -100,7 +108,7 @@ export function OpenLongPreview({
           }
         />
         <LabelValue
-          label="Fixed APR after open"
+          label="Market rate after open"
           value={
             <span
               className={classNames(
