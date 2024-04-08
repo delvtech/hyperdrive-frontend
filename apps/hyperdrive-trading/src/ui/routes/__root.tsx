7cce1565a1290ef7e66ff71ec2fca2ee028a3611
Ryan Goree <goree.ryan@gmail.com>
2024-04-08 14:11:59 -0500

    Fix min page size

--- apps/hyperdrive-trading/src/ui/routes/__root.tsx
+++ apps/hyperdrive-trading/src/ui/routes/__root.tsx
@@ -1,22 +1,26 @@
-import { Outlet, RootRoute } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import Footer from "src/ui/app/Footer/Footer";
 import { Navbar } from "src/ui/app/Navbar/Navbar";
 import { IneligibleAccountDialog } from "src/ui/compliance/IneligibleAccountMessage";
-export const Route = new RootRoute({
+export const Route = createRootRoute({
   component: () => <RootComponent />,
 });
 
 function RootComponent() {
   return (
-    <div className="flex h-full flex-col items-center">
-      <div className="flex flex-col gap-9">
+    <div className="flex min-h-screen flex-col items-center justify-between">
+      <div className="flex w-full grow flex-col gap-9">
         <Navbar />
         <IneligibleAccountDialog className="grow" />
         <Outlet />
       </div>
       <Footer />
-      {import.meta.env.DEV ? <TanStackRouterDevtools /> : null}
+      {import.meta.env.DEV ? (
+        <div className="fixed">
+          <TanStackRouterDevtools />
+        </div>
+      ) : null}
     </div>
   );
 }
