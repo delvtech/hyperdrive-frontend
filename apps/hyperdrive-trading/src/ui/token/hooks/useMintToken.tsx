1eeaaef9d32f89026b603a5010b1613f1335e9a5
Danny Delott <dannydelott@gmail.com>
2024-04-09 14:49:58 -0500

    Fix minting hook (#916)

---
+++ apps/hyperdrive-trading/src/ui/token/hooks/useMintToken.tsx
@@ -0,0 +1,82 @@
+import { ERC20Mintable } from "@delvtech/hyperdrive-artifacts/ERC20Mintable";
+import { TokenConfig } from "@hyperdrive/appconfig";
+import { useAddRecentTransaction } from "@rainbow-me/rainbowkit";
+import toast from "react-hot-toast";
+import { queryClient } from "src/network/queryClient";
+import { waitForTransactionAndInvalidateCache } from "src/network/waitForTransactionAndInvalidateCache";
+import TransactionToast from "src/ui/base/components/Toaster/TransactionToast";
+import { Address, formatUnits } from "viem";
+import { usePublicClient, useReadContract, useWriteContract } from "wagmi";
+
+export function useMintToken({
+  token,
+  destination,
+  amount,
+}: {
+  token: TokenConfig<any>;
+  destination: Address | undefined;
+  amount: bigint;
+}): { mint: (() => void) | undefined } {
+  const addRecentTransaction = useAddRecentTransaction();
+  const publicClient = usePublicClient();
+  const { data: maxMintAmount } = useReadContract({
+    abi: [
+      {
+        type: "function",
+        name: "maxMintAmount",
+        inputs: [],
+        outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
+        stateMutability: "view",
+      },
+    ],
+    functionName: "maxMintAmount",
+    address: token.address,
+    args: [],
+  });
+
+  const isEnabled =
+    !!destination && !!amount && !!publicClient && !!maxMintAmount;
+  const { writeContract } = useWriteContract();
+
+  const mint = isEnabled
+    ? () => {
+        return writeContract(
+          {
+            address: token.address,
+            abi: ERC20Mintable.abi,
+            functionName: "mint",
+            args: [destination, maxMintAmount],
+          },
+          {
+            onSuccess: async (hash) => {
+              toast.loading(
+                <TransactionToast
+                  message={`Minting ${token.symbol}`}
+                  txHash={hash}
+                />,
+                { id: hash },
+              );
+              addRecentTransaction({
+                hash: hash,
+                description: `Mint ${formatUnits(amount, token.decimals)} ${token.symbol}`,
+              });
+              await waitForTransactionAndInvalidateCache({
+                publicClient,
+                queryClient,
+                hash,
+              });
+              toast.success(
+                <TransactionToast
+                  message={`Minted ${token.symbol}`}
+                  txHash={hash}
+                />,
+                { id: hash },
+              );
+            },
+          },
+        );
+      }
+    : undefined;
+
+  return { mint };
+}
