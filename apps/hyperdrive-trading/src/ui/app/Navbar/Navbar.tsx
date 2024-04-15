ecc25e3d4d0181490c659b64da8c1686853048bc
Cash Baller <cashd@users.noreply.github.com>
2024-04-15 12:35:44 -0500

    Update Navbar.tsx

--- apps/hyperdrive-trading/src/ui/app/Navbar/Navbar.tsx
+++ apps/hyperdrive-trading/src/ui/app/Navbar/Navbar.tsx
@@ -23,8 +23,8 @@ export function Navbar(): ReactElement {
           href="https://warpcast.com/~/compose?text=Can%20you%20outtrade%20me%20on%20Hyperdrive%3F%20Warp%20your%20best%20Hyperdrive%20trade%20screenshot%20below%20%F0%9F%93%88%20%2Fdelv&embeds[]=https://frames.hyperdrive.trade"
           className="text-white daisy-btn rounded-full bg-warpcast-purple hover:bg-farcaster-purple"
         >
-          <span className="hidden md:inline">Cast on Warpcast</span>
-          <span className="inline md:hidden">Cast</span>
+          <span className="hidden sm:inline">Cast on Warpcast</span>
+          <span className="inline sm:hidden">Cast</span>
           <img
             className="h-4"
             src="https://raw.githubusercontent.com/vrypan/farcaster-brand/ea95543f6d1308dd52631f5e81f395a60447df0b/icons/icon-transparent/transparent-white.svg"
