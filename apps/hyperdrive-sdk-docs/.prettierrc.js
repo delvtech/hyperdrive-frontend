cf4686d949874753701e446630bcb20c31bebbbf
jackburrus <jhburrus3@gmail.com>
2024-04-17 10:37:37 +0530

    fix docs build

--- apps/hyperdrive-sdk-docs/.prettierrc.js
+++
@@ -1,7 +0,0 @@
-import repoConfig from "@hyperdrive/prettier-config";
-
-/** @type {import("prettier").Config} */
-export default {
-  ...repoConfig,
-  plugins: ["prettier-plugin-organize-imports"],
-};
