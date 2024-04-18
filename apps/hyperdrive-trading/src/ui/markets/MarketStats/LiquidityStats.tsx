e2e7410455eba8fa0c970f71f716733a5bfafa9a
Danny Delott <dannydelott@gmail.com>
2024-04-18 13:02:09 -0700

    Refactor stats into groupings

--- apps/hyperdrive-trading/src/ui/markets/MarketStats/LiquidityStats.tsx
+++ apps/hyperdrive-trading/src/ui/markets/MarketStats/LiquidityStats.tsx
@@ -6,13 +6,10 @@ import { Stat } from "src/ui/base/components/Stat";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
 import { formatCompact } from "src/ui/base/formatting/formatCompact";
 import { useIsTailwindSmallScreen } from "src/ui/base/mediaBreakpoints";
-import { useCurrentFixedAPR } from "src/ui/hyperdrive/hooks/useCurrentFixedAPR";
 import { useLiquidity } from "src/ui/hyperdrive/hooks/useLiquidity";
-import { useLpApy } from "src/ui/hyperdrive/hooks/useLpApy";
 import { useTradingVolume } from "src/ui/hyperdrive/hooks/useTradingVolume";
-import { useYieldSourceRate } from "src/ui/vaults/useYieldSourceRate";
 import { useBlockNumber } from "wagmi";
-export function MarketStats({
+export function LiquidityStats({
   hyperdrive,
 }: {
   hyperdrive: HyperdriveConfig;
@@ -32,89 +29,31 @@ export function MarketStats({
     hyperdriveAddress: hyperdrive.address,
     decimals: baseToken.decimals,
   });
-  const { fixedAPR, fixedAPRStatus } = useCurrentFixedAPR(hyperdrive.address);
-  const { lpApy, lpApyStatus } = useLpApy(hyperdrive.address);
-
-  const { vaultRate, vaultRateStatus } = useYieldSourceRate({
-    hyperdriveAddress: hyperdrive.address,
-  });
 
   return (
-    <div className="grid grid-cols-2 gap-6 border-y border-neutral-content/20 py-8 sm:grid-cols-3 lg:grid-cols-5">
-      <Stat
-        label="Yield Source APY"
+    <div className="flex gap-16">
+      {/* TODO: Add this stat in next PR */}
+      {/* <Stat
+        label="Total (DAI)"
         value={
-          vaultRateStatus === "loading" && vaultRate === undefined ? (
-            <Skeleton className="w-20" />
-          ) : (
-            <div className="flex flex-row">{vaultRate?.formatted || 0}%</div>
-          )
-        }
-        description={`The yield source backing the hy${baseToken.symbol} in this pool.`}
-        tooltipPosition={"right"}
-      />
-      <Stat
-        label="Fixed APR"
-        value={
-          fixedAPRStatus === "loading" && fixedAPR === undefined ? (
-            <Skeleton className="w-20" />
-          ) : (
-            <span className="flex items-center gap-1.5">
-              {fixedAPR?.formatted || "0"}%
-            </span>
-          )
-        }
-        description="Fixed rate earned from opening longs, before fees and slippage are applied."
-      />
-      <Stat
-        label="LP APY (7d)"
-        value={
-          lpApyStatus !== "loading" ? (
-            <span className="flex items-center gap-1.5">
-              {lpApy === undefined
-                ? "no data"
-                : `${(lpApy * 100).toFixed(2) === "-0.00" ? "0.00" : (lpApy * 100).toFixed(2)}%`}{" "}
-            </span>
-          ) : (
-            <Skeleton className="w-20" />
-          )
-        }
-        description={`The LP's annual return projection assuming the past 7-day performance rate continues for a year.`}
-        tooltipPosition={isTailwindSmallScreen ? "right" : "bottom"}
-      />
-      <Stat
-        description={`The amount of hy${
-          baseToken.symbol
-        } (either longs or shorts) that have been traded in the last 24 hours.\n\nLong volume: ${formatBalance(
-          {
-            balance: longVolume || 0n,
-            decimals: baseToken.decimals,
-            places: baseToken.places,
-          },
-        )} hy${baseToken.symbol} \nShort volume: ${formatBalance({
-          balance: shortVolume || 0n,
-          decimals: baseToken.decimals,
-          places: baseToken.places,
-        })} hy${baseToken.symbol}`}
-        tooltipPosition={isTailwindSmallScreen ? "left" : "bottom"}
-        label="Volume (24h)"
-        value={
-          tradingVolumeStatus === "loading" && totalVolume === undefined ? (
-            <Skeleton className="w-20" />
-          ) : (
-            <AmountLabel
-              symbol={`hy${baseToken.symbol}`}
-              value={formatCompact({
-                value: totalVolume || 0n,
+          <AmountLabel
+            icon={baseToken.iconUrl || ""}
+            symbol={baseToken.symbol}
+            value={
+              // TODO: Wire this up
+              formatCompact({
+                value: parseUnits("517826", 18) || 0n,
                 decimals: baseToken.decimals,
-              })}
-            />
-          )
+              })
+            }
+          />
         }
-      />
+        description={`The present value in the pool`}
+        tooltipPosition={isTailwindSmallScreen ? "right" : "bottom"}
+      /> */}
       <Stat
-        label="Available Liquidity"
-        description={`The amount of capital that has been deployed by LPs in the pool.`}
+        label={`Available (${baseToken.symbol})`}
+        description={`The amount of liquidity available for trading`}
         value={
           liquidityStatus === "loading" && liquidity === undefined ? (
             <Skeleton className="w-20" />
@@ -131,13 +70,42 @@ export function MarketStats({
         }
         tooltipPosition={isTailwindSmallScreen ? "right" : "left"}
       />
+      <Stat
+        description={`The amount of hy${
+          baseToken.symbol
+        } (either longs or shorts) that have been traded in the last 24 hours.\n\nLong volume: ${formatBalance(
+          {
+            balance: longVolume || 0n,
+            decimals: baseToken.decimals,
+            places: baseToken.places,
+          },
+        )} hy${baseToken.symbol} \nShort volume: ${formatBalance({
+          balance: shortVolume || 0n,
+          decimals: baseToken.decimals,
+          places: baseToken.places,
+        })} hy${baseToken.symbol}`}
+        tooltipPosition={isTailwindSmallScreen ? "left" : "bottom"}
+        label={`hy${baseToken.symbol} Volume (24h)`}
+        value={
+          tradingVolumeStatus === "loading" && totalVolume === undefined ? (
+            <Skeleton className="w-20" />
+          ) : (
+            <AmountLabel
+              symbol={`hy${baseToken.symbol}`}
+              value={formatCompact({
+                value: totalVolume || 0n,
+                decimals: baseToken.decimals,
+              })}
+            />
+          )
+        }
+      />
     </div>
   );
 }
 
 function AmountLabel({
   value,
-  symbol,
   icon,
   tooltip,
 }: {
@@ -154,11 +122,8 @@ function AmountLabel({
         "daisy-tooltip daisy-tooltip-bottom flex flex-row items-center justify-start before:whitespace-pre-wrap before:font-normal before:content-[attr(data-tip)]"
       }
     >
+      {icon ? <img src={icon} className="h-7 rounded-full  p-1" /> : undefined}
       {value}
-      <span className="ml-1">{symbol}</span>
-      {icon ? (
-        <img src={icon} className="ml-1 h-8 rounded-full  p-1" />
-      ) : undefined}
     </p>
   );
 }
