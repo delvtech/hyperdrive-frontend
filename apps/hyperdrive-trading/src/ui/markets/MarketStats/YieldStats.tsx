6da11d7e9533bf6d3ce81cf1593d1fe9d4067a5f
Danny Delott <dannydelott@gmail.com>
2024-04-25 08:32:58 -0700

    Show vault rate in a badge on the Yield section

--- apps/hyperdrive-trading/src/ui/markets/MarketStats/YieldStats.tsx
+++ apps/hyperdrive-trading/src/ui/markets/MarketStats/YieldStats.tsx
@@ -1,8 +1,14 @@
-import { HyperdriveConfig, findBaseToken } from "@hyperdrive/appconfig";
+import {
+  HyperdriveConfig,
+  findBaseToken,
+  findYieldSourceToken,
+} from "@hyperdrive/appconfig";
 import { ReactElement } from "react";
 import Skeleton from "react-loading-skeleton";
 import { useAppConfig } from "src/ui/appconfig/useAppConfig";
+import { Badge } from "src/ui/base/components/Badge";
 import { Stat } from "src/ui/base/components/Stat";
+import { Well } from "src/ui/base/components/Well/Well";
 import { useIsTailwindSmallScreen } from "src/ui/base/mediaBreakpoints";
 import { useCurrentFixedAPR } from "src/ui/hyperdrive/hooks/useCurrentFixedAPR";
 import { useLpApy } from "src/ui/hyperdrive/hooks/useLpApy";
@@ -18,6 +24,10 @@ export function YieldStats({
     baseTokenAddress: hyperdrive.baseToken,
     tokens: appConfig.tokens,
   });
+  const sharesToken = findYieldSourceToken({
+    yieldSourceTokenAddress: hyperdrive.sharesToken,
+    tokens: appConfig.tokens,
+  });
 
   const { fixedAPR, fixedAPRStatus } = useCurrentFixedAPR(hyperdrive.address);
   const { lpApy, lpApyStatus } = useLpApy(hyperdrive.address);
@@ -27,48 +37,59 @@ export function YieldStats({
   });
 
   return (
-    <div className="flex flex-wrap gap-16">
-      <Stat
-        label="Fixed APR"
-        value={
-          fixedAPRStatus === "loading" && fixedAPR === undefined ? (
-            <Skeleton className="w-20" />
-          ) : (
-            <span className="flex items-center gap-1.5">
-              {fixedAPR?.formatted || "0"}%
-            </span>
-          )
-        }
-        description="Fixed rate earned from opening longs, before fees and slippage are applied."
-      />
-      <Stat
-        label="Yield Source APY"
-        value={
-          vaultRateStatus === "loading" && vaultRate === undefined ? (
-            <Skeleton className="w-20" />
-          ) : (
-            <div className="flex flex-row">{vaultRate?.formatted || 0}%</div>
-          )
-        }
-        description={`The yield source backing the hy${baseToken.symbol} in this pool.`}
-        tooltipPosition={"right"}
-      />
-      <Stat
-        label="LP APY (7d)"
-        value={
-          lpApyStatus !== "loading" ? (
-            <span className="flex items-center gap-1.5">
-              {lpApy === undefined
-                ? "no data"
-                : `${(lpApy * 100).toFixed(2) === "-0.00" ? "0.00" : (lpApy * 100).toFixed(2)}%`}{" "}
-            </span>
-          ) : (
-            <Skeleton className="w-20" />
-          )
-        }
-        description={`The LP's annual return projection assuming the past 7-day performance rate continues for a year.`}
-        tooltipPosition={isTailwindSmallScreen ? "right" : "bottom"}
-      />
-    </div>
+    <Well transparent>
+      <div className="space-y-8">
+        <div className="flex justify-between">
+          <h5 className="flex items-center gap-2 text-gray-400">Yield</h5>
+          <div className="font-dmMono text-neutral-content">
+            {vaultRateStatus === "loading" && vaultRate === undefined ? (
+              <Skeleton className="w-20" />
+            ) : (
+              <Badge>
+                {sharesToken.extensions.shortName} @ {vaultRate?.formatted || 0}
+                % APY
+              </Badge>
+            )}
+          </div>
+        </div>
+        <div className="flex flex-wrap gap-16">
+          <Stat
+            label="Fixed APR"
+            value={
+              fixedAPRStatus === "loading" && fixedAPR === undefined ? (
+                <Skeleton className="w-20" />
+              ) : (
+                <span className="flex items-center gap-1.5">
+                  {fixedAPR?.formatted || "0"}%
+                </span>
+              )
+            }
+            description="Fixed rate earned from opening longs, before fees and slippage are applied."
+          />
+          <Stat
+            label="Variable APY"
+            value={`${vaultRate?.formatted || 0}%`}
+            description={`The yield source backing the hy${baseToken.symbol} in this pool.`}
+            tooltipPosition={"right"}
+          />
+          <Stat
+            label="LP APY (7d)"
+            value={
+              lpApyStatus !== "loading" ? (
+                <span className="flex items-center gap-1.5">
+                  {lpApy === undefined
+                    ? "no data"
+                    : `${(lpApy * 100).toFixed(2) === "-0.00" ? "0.00" : (lpApy * 100).toFixed(2)}%`}{" "}
+                </span>
+              ) : (
+                <Skeleton className="w-20" />
+              )
+            }
+            description={`The LP's annual return projection assuming the past 7-day performance rate continues for a year.`}
+            tooltipPosition={isTailwindSmallScreen ? "right" : "bottom"}
+          />
+        </div>
+      </div>
+    </Well>
   );
 }
