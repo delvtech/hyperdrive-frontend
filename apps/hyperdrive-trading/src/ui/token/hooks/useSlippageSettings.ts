5f013db4b39b49eb6b3d985c4f4877afc7ff66e2
jackburrus <jhburrus3@gmail.com>
2024-04-30 19:39:21 +1000

    slippage updated to use strings

--- apps/hyperdrive-trading/src/ui/token/hooks/useSlippageSettings.ts
+++ apps/hyperdrive-trading/src/ui/token/hooks/useSlippageSettings.ts
@@ -1,18 +1,22 @@
+import * as dnum from "dnum";
 import { useState } from "react";
 import { AUTO_SLIPPAGE_AMOUNT } from "src/ui/token/SlippageSettings";
 export function useSlippageSettings({ decimals }: { decimals: number }): {
-  slippage: bigint;
-  setSlippage: (slippage: bigint) => void;
+  slippage: string;
+  slippageAsBigInt: bigint;
+  setSlippage: (slippage: string) => void;
   activeOption: "auto" | "custom";
   setActiveOption: (activeOption: "auto" | "custom") => void;
 } {
-  const [slippage, setSlippage] = useState<bigint>(
-    AUTO_SLIPPAGE_AMOUNT(decimals),
-  );
+  const [slippage, setSlippage] = useState<string>(AUTO_SLIPPAGE_AMOUNT);
+  const slippageAsBigInt = slippage
+    ? dnum.from(slippage, decimals)[0]
+    : dnum.from(AUTO_SLIPPAGE_AMOUNT, decimals)[0];
   const [activeOption, setActiveOption] = useState<"auto" | "custom">("auto");
 
   return {
     slippage,
+    slippageAsBigInt,
     setSlippage,
     activeOption,
     setActiveOption,
