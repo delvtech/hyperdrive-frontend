6a6b1f4e4f4c0b53c9476d8eac61b10223e39a56
jackburrus <jhburrus3@gmail.com>
2024-05-06 07:23:02 -0700

    right align all markets table

--- apps/hyperdrive-trading/src/ui/markets/AllMarketsTable/LpApyCell.tsx
+++ apps/hyperdrive-trading/src/ui/markets/AllMarketsTable/LpApyCell.tsx
@@ -1,5 +1,7 @@
+import classNames from "classnames";
 import { ReactElement } from "react";
 import Skeleton from "react-loading-skeleton";
+import { useIsTailwindSmallScreen } from "src/ui/base/mediaBreakpoints";
 import { useLpApy } from "src/ui/hyperdrive/hooks/useLpApy";
 import { Address } from "viem";
 
@@ -9,11 +11,20 @@ export function LpApyCell({
   hyperdriveAddress: Address;
 }): ReactElement {
   const { lpApy, lpApyStatus } = useLpApy(hyperdriveAddress);
+  const isTailwindSmallScreen = useIsTailwindSmallScreen();
   if (lpApyStatus === "loading") {
     return <Skeleton className="h-6 w-20" />;
   }
   if (!lpApy) {
     return <span>no data</span>;
   }
-  return <span>{(lpApy * 100).toFixed(2)}%</span>;
+  return (
+    <span
+      className={classNames({
+        "flex w-14 justify-end": !isTailwindSmallScreen,
+      })}
+    >
+      {(lpApy * 100).toFixed(2)}%
+    </span>
+  );
 }
