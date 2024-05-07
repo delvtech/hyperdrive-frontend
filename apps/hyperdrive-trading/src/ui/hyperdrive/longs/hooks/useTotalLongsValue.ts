fc34ec718c57ffd485ac5376ccd42f7889a18827
jackburrus <jhburrus3@gmail.com>
2024-05-07 14:19:33 -0600

    useQueries working

---
+++ apps/hyperdrive-trading/src/ui/hyperdrive/longs/hooks/useTotalLongsValue.ts
@@ -0,0 +1,64 @@
+import { Long, PoolInfo, ReadHyperdrive } from "@delvtech/hyperdrive-viem";
+import { HyperdriveConfig } from "@hyperdrive/appconfig";
+import { useQueries } from "@tanstack/react-query";
+import { makeQueryKey } from "src/base/makeQueryKey";
+import { parseUnits } from "src/base/parseUnits";
+import { convertSharesToBase } from "src/hyperdrive/convertSharesToBase";
+import { Address } from "viem";
+
+export function useTotalLongsValue({
+  hyperdrive,
+  account,
+  openLongs,
+  readHyperdrive,
+  poolInfo,
+}: {
+  hyperdrive: HyperdriveConfig;
+  account: Address;
+  openLongs: Long[] | undefined;
+  readHyperdrive: ReadHyperdrive | undefined;
+  poolInfo: PoolInfo | undefined;
+}): { totalLongsValue: bigint | undefined; isLoading: boolean } {
+  let totalLongsValue = 0n;
+  let isLoading = false;
+  const queryEnabled =
+    !!account && !!openLongs && !!readHyperdrive && !!poolInfo;
+  const previewCloseLongQueries = useQueries({
+    queries:
+      openLongs?.map((long) => ({
+        queryKey: makeQueryKey("previewCloseLong", {
+          hyperdriveAddress: hyperdrive.address,
+          bondAmountIn: long.bondAmount?.toString(),
+          minOutput: parseUnits("0", 18).toString(),
+          destination: account,
+          asBase: false,
+        }),
+        enabled: queryEnabled,
+        queryFn: async () =>
+          readHyperdrive?.previewCloseLong({
+            maturityTime: long.maturity,
+            bondAmountIn: long.bondAmount,
+            destination: account,
+            asBase: false,
+            minAmountOut: parseUnits("0", 18),
+            options: {
+              from: account,
+            },
+          }),
+      })) ?? [],
+  });
+  previewCloseLongQueries.forEach((query) => {
+    if (query.status === "success") {
+      const amountOutInBase = convertSharesToBase({
+        decimals: hyperdrive.decimals,
+        sharesAmount: query.data,
+        vaultSharePrice: poolInfo?.vaultSharePrice,
+      });
+      totalLongsValue += amountOutInBase || 0n;
+    }
+    if (query.status === "loading") {
+      isLoading = true;
+    }
+  });
+  return { totalLongsValue, isLoading };
+}
--- apps/hyperdrive-trading/src/ui/markets/LongsTab/LongsTab.tsx
+++ apps/hyperdrive-trading/src/ui/markets/LongsTab/LongsTab.tsx
@@ -1,31 +1,66 @@
-import { HyperdriveConfig } from "@hyperdrive/appconfig";
+import { Long, PoolInfo, ReadHyperdrive } from "@delvtech/hyperdrive-js-core";
+import { HyperdriveConfig, findBaseToken } from "@hyperdrive/appconfig";
+import { format as dnFormat } from "dnum";
 import { ReactElement } from "react";
+import Skeleton from "react-loading-skeleton";
+import { parseUnits } from "src/base/parseUnits";
+import { convertSharesToBase } from "src/hyperdrive/convertSharesToBase";
+import { useAppConfig } from "src/ui/appconfig/useAppConfig";
+import { usePoolInfo } from "src/ui/hyperdrive/hooks/usePoolInfo";
+import { useReadHyperdrive } from "src/ui/hyperdrive/hooks/useReadHyperdrive";
 import { ClosedLongsTable } from "src/ui/hyperdrive/longs/ClosedLongsTable/ClosedLongsTable";
 import { OpenLongModalButton } from "src/ui/hyperdrive/longs/OpenLongModalButton/OpenLongModalButton";
 import { OpenLongsTable } from "src/ui/hyperdrive/longs/OpenLongsTable/OpenLongsTable";
 import { useOpenLongs } from "src/ui/hyperdrive/longs/hooks/useOpenLongs";
+import { useTotalLongsValue } from "src/ui/hyperdrive/longs/hooks/useTotalLongsValue";
 import { MarketDetailsTab } from "src/ui/markets/MarketDetailsTab/MarketDetailsTab";
 import { OpenClosedFilter } from "src/ui/markets/OpenClosedFilter/OpenClosedFilter";
 import { useOpenOrClosedSearchParam } from "src/ui/markets/hooks/useOpenOrClosedSearchParam";
+import { Address } from "viem";
 import { useAccount } from "wagmi";
-
 export function LongsTab({
   hyperdrive,
 }: {
   hyperdrive: HyperdriveConfig;
 }): ReactElement {
   const activeOpenOrClosedTab = useOpenOrClosedSearchParam();
+  const readHyperdrive = useReadHyperdrive(hyperdrive.address);
   const { address: account } = useAccount();
+  const appConfig = useAppConfig();
   const { openLongs } = useOpenLongs({
     account,
     hyperdriveAddress: hyperdrive.address,
   });
+  const { poolInfo } = usePoolInfo({ hyperdriveAddress: hyperdrive.address });
+  const { totalLongsValue, isLoading } = useTotalLongsValue({
+    hyperdrive,
+    account: account as Address,
+    openLongs,
+    readHyperdrive,
+    poolInfo,
+  });
+  const baseToken = findBaseToken({
+    baseTokenAddress: hyperdrive.baseToken,
+    tokens: appConfig.tokens,
+  });
+
   return (
     <MarketDetailsTab
       positions={
         <div className="flex flex-col">
           <div className="flex flex-wrap items-center justify-between gap-4 p-8">
-            <h5 className="font-medium">Long Positions</h5>
+            <div className="flex flex-col items-start gap-2">
+              <h5 className="font-medium">Long Positions</h5>
+              {!isLoading ? (
+                <p className="text-xs text-neutral-content">
+                  Total Value:{" "}
+                  {Number(dnFormat([totalLongsValue || 0n, 18])).toFixed(4)}{" "}
+                  {baseToken.symbol}
+                </p>
+              ) : (
+                <Skeleton width={100} />
+              )}
+            </div>
             <div className="flex items-center gap-4">
               {account && openLongs?.length ? (
                 <OpenLongModalButton
@@ -45,4 +80,50 @@ export function LongsTab({
       }
     />
   );
+}
+
+function calculateTotalLongsValue({
+  hyperdrive,
+  account,
+  openLongs,
+  readHyperdrive,
+  poolInfo,
+}: {
+  hyperdrive: HyperdriveConfig;
+  account: Address | undefined;
+  openLongs: Long[] | undefined;
+  readHyperdrive: ReadHyperdrive | undefined;
+  poolInfo: PoolInfo | undefined;
+}) {
+  return new Promise(async (resolve) => {
+    if (!readHyperdrive) {
+      resolve(0);
+    }
+    let totalValue = 0n;
+    if (openLongs && account && readHyperdrive && poolInfo) {
+      const promises = openLongs.map(async (long) => {
+        const sharesAmountOut = await readHyperdrive.previewCloseLong({
+          maturityTime: long.maturity,
+          bondAmountIn: long.bondAmount,
+          destination: account,
+          asBase: false,
+          minAmountOut: parseUnits("0", 18),
+          options: {
+            from: account,
+          },
+        });
+        const amountOutInBase = convertSharesToBase({
+          decimals: hyperdrive.decimals,
+          sharesAmount: sharesAmountOut,
+          vaultSharePrice: poolInfo?.vaultSharePrice,
+        });
+
+        return amountOutInBase;
+      });
+
+      const results = await Promise.all(promises);
+      totalValue = results.reduce((acc, val) => acc + val, 0n);
+    }
+    resolve(totalValue);
+  });
 }
