16ac1e29249dfeb229caa0833d757bc8a8d03a99
Jack Burrus <jhburrus3@gmail.com>
2024-05-08 08:17:56 +1000

    Add total long value to long tab (#1039)
    * useQueries working
    
    * cleanup types
    
    * cleanup is loading
    
    * move pool info into hook
    
    * remove if statement
    
    * change to 4 digits to match table
    
    * fix per comments

--- apps/hyperdrive-trading/src/ui/markets/LongsTab/LongsTab.tsx
+++ apps/hyperdrive-trading/src/ui/markets/LongsTab/LongsTab.tsx
@@ -1,31 +1,61 @@
-import { HyperdriveConfig } from "@hyperdrive/appconfig";
+import { HyperdriveConfig, findBaseToken } from "@hyperdrive/appconfig";
+import { format as dnFormat } from "dnum";
 import { ReactElement } from "react";
+import Skeleton from "react-loading-skeleton";
+import { useAppConfig } from "src/ui/appconfig/useAppConfig";
 import { ClosedLongsTable } from "src/ui/hyperdrive/longs/ClosedLongsTable/ClosedLongsTable";
 import { OpenLongModalButton } from "src/ui/hyperdrive/longs/OpenLongModalButton/OpenLongModalButton";
 import { OpenLongsTable } from "src/ui/hyperdrive/longs/OpenLongsTable/OpenLongsTable";
 import { useOpenLongs } from "src/ui/hyperdrive/longs/hooks/useOpenLongs";
+import { useTotalLongsValue } from "src/ui/hyperdrive/longs/hooks/useTotalLongsValue";
 import { MarketDetailsTab } from "src/ui/markets/MarketDetailsTab/MarketDetailsTab";
 import { OpenClosedFilter } from "src/ui/markets/OpenClosedFilter/OpenClosedFilter";
 import { useOpenOrClosedSearchParam } from "src/ui/markets/hooks/useOpenOrClosedSearchParam";
 import { useAccount } from "wagmi";
-
 export function LongsTab({
   hyperdrive,
 }: {
   hyperdrive: HyperdriveConfig;
 }): ReactElement {
   const activeOpenOrClosedTab = useOpenOrClosedSearchParam();
+  const appConfig = useAppConfig();
   const { address: account } = useAccount();
   const { openLongs } = useOpenLongs({
     account,
     hyperdriveAddress: hyperdrive.address,
+  });
+  const { totalLongsValue, isLoading } = useTotalLongsValue({
+    hyperdrive,
+    account,
+    openLongs,
+  });
+  const baseToken = findBaseToken({
+    baseTokenAddress: hyperdrive.baseToken,
+    tokens: appConfig.tokens,
   });
   return (
     <MarketDetailsTab
       positions={
         <div className="flex flex-col">
           <div className="flex flex-wrap items-center justify-between gap-4 p-8">
-            <h5 className="font-medium">Long Positions</h5>
+            <div className="flex flex-col items-start gap-2">
+              <h5 className="font-medium">Long Positions</h5>
+              {!isLoading ? (
+                <>
+                  {openLongs?.length ? (
+                    <p className="text-xs text-neutral-content">
+                      Total Value:{" "}
+                      {dnFormat([totalLongsValue || 0n, baseToken.decimals], {
+                        digits: 4,
+                      })}{" "}
+                      {baseToken.symbol}
+                    </p>
+                  ) : undefined}
+                </>
+              ) : (
+                <Skeleton width={100} />
+              )}
+            </div>
             <div className="flex items-center gap-4">
               {account && openLongs?.length ? (
                 <OpenLongModalButton
