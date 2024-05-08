2bc27ca3d9b5921674003d5c382f1a57515fd582
Jack Burrus <jhburrus3@gmail.com>
2024-05-09 07:17:31 +1000

    swap to single usequery (#1044)
    * swap to single usequery
    
    * updates per comments

--- apps/hyperdrive-trading/src/ui/hyperdrive/shorts/hooks/useTotalShortsValue.ts
+++ apps/hyperdrive-trading/src/ui/hyperdrive/shorts/hooks/useTotalShortsValue.ts
@@ -1,6 +1,6 @@
 import { Short } from "@delvtech/hyperdrive-viem";
 import { HyperdriveConfig } from "@hyperdrive/appconfig";
-import { useQueries } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { makeQueryKey } from "src/base/makeQueryKey";
 import { parseUnits } from "src/base/parseUnits";
 import { convertSharesToBase } from "src/hyperdrive/convertSharesToBase";
@@ -20,54 +20,46 @@ export function useTotalShortsValue({
   const readHyperdrive = useReadHyperdrive(hyperdrive.address);
   const { poolInfo } = usePoolInfo({ hyperdriveAddress: hyperdrive.address });
 
-  let isLoading = true;
   const queryEnabled =
     !!account && !!openShorts && !!readHyperdrive && !!poolInfo;
-  const previewCloseShortQueries = useQueries({
-    queries:
-      openShorts?.map((short) => ({
-        queryKey: makeQueryKey("previewCloseShort", {
-          hyperdriveAddress: hyperdrive.address,
-          maturityTime: short.maturity?.toString(),
-          shortAmountIn: short.bondAmount?.toString(),
-          minAmountOut: parseUnits("0", 18).toString(),
-          destination: account,
-          asBase: false,
-        }),
-        enabled: queryEnabled,
-        queryFn: queryEnabled
-          ? () =>
-              readHyperdrive?.previewCloseShort({
-                maturityTime: short.maturity,
-                shortAmountIn: short.bondAmount,
-                destination: account,
-                asBase: false,
-                minAmountOut: parseUnits("0", 18),
-                options: {
-                  from: account,
-                },
-              })
-          : undefined,
-      })) ?? [],
-  });
-  const allQueriesSucceeded = previewCloseShortQueries.every(
-    (query) => query.status === "success",
-  );
-  let totalShortsValue = 0n;
-  if (allQueriesSucceeded) {
-    previewCloseShortQueries.forEach((query) => {
-      const amountOutInBase = convertSharesToBase({
-        decimals: hyperdrive.decimals,
-        sharesAmount: query.data,
-        vaultSharePrice: poolInfo?.vaultSharePrice,
-      });
-      totalShortsValue += amountOutInBase || 0n;
-    });
-  }
 
-  isLoading = previewCloseShortQueries.some(
-    (query) => query.status === "loading",
-  );
+  const { data: totalShortsValue, isLoading } = useQuery({
+    queryKey: makeQueryKey("totalShortsValue", {
+      hyperdriveAddress: hyperdrive.address,
+      account,
+    }),
+    enabled: queryEnabled,
+    queryFn: queryEnabled
+      ? async () => {
+          const promises = openShorts.map((short) =>
+            readHyperdrive.previewCloseShort({
+              maturityTime: short.maturity,
+              shortAmountIn: short.bondAmount,
+              destination: account,
+              asBase: false,
+              minAmountOut: parseUnits("0", 18),
+              options: {
+                from: account,
+              },
+            }),
+          );
+
+          const results = await Promise.all(promises);
+
+          let totalShortsValue = 0n;
+          results.forEach((result) => {
+            const amountOutInBase = convertSharesToBase({
+              decimals: hyperdrive.decimals,
+              sharesAmount: result,
+              vaultSharePrice: poolInfo?.vaultSharePrice,
+            });
+            totalShortsValue += amountOutInBase || 0n;
+          });
+
+          return totalShortsValue;
+        }
+      : undefined,
+  });
 
   return { totalShortsValue, isLoading };
 }
