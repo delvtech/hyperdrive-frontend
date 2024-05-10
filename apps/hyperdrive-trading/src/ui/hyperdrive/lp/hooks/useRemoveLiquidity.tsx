79d76d70950847c93d2479ce28a476e0aa62216d
Jack Burrus <jhburrus3@gmail.com>
2024-05-10 10:10:14 -0600

    adds error parsing (#1047)

--- apps/hyperdrive-trading/src/ui/hyperdrive/lp/hooks/useRemoveLiquidity.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/lp/hooks/useRemoveLiquidity.tsx
@@ -5,6 +5,7 @@ import {
   useQueryClient,
 } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { parseError } from "src/network/parseError";
 import TransactionToast from "src/ui/base/components/Toaster/TransactionToast";
 import { SUCCESS_TOAST_DURATION } from "src/ui/base/toasts";
 import { useReadWriteHyperdrive } from "src/ui/hyperdrive/hooks/useReadWriteHyperdrive";
@@ -82,6 +83,12 @@ export function useRemoveLiquidity({
           description: "Remove Liquidity",
         });
       }
+    },
+    onError(error) {
+      const message = parseError({
+        error,
+      });
+      toast.error(message);
     },
   });
   return {
--- apps/hyperdrive-trading/src/ui/hyperdrive/shorts/hooks/useCloseShort.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/shorts/hooks/useCloseShort.tsx
@@ -5,6 +5,7 @@ import {
   useQueryClient,
 } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { parseError } from "src/network/parseError";
 import TransactionToast from "src/ui/base/components/Toaster/TransactionToast";
 import { SUCCESS_TOAST_DURATION } from "src/ui/base/toasts";
 import { useReadWriteHyperdrive } from "src/ui/hyperdrive/hooks/useReadWriteHyperdrive";
@@ -86,6 +87,12 @@ export function useCloseShort({
           description: "Close Short",
         });
       }
+    },
+    onError(error) {
+      const message = parseError({
+        error,
+      });
+      toast.error(message);
     },
   });
 
