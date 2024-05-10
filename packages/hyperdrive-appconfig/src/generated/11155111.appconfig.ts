c167c95e2481325475a2ea2f66f5832c358440c4
Danny Delott <dannydelott@gmail.com>
2024-05-10 09:08:25 -0700

    Pluck out renzo markets

--- packages/hyperdrive-appconfig/src/generated/11155111.appconfig.ts
+++ packages/hyperdrive-appconfig/src/generated/11155111.appconfig.ts
@@ -55,20 +55,6 @@ export const sepoliaAppConfig: AppConfig = {
       iconUrl: "https://cryptologos.cc/logos/ethereum-eth-logo.png?v=029",
     },
     {
-      address: "0x63E77f68fA4774A0e2Fc0a085f02150746dd8230",
-      decimals: 18,
-      places: 2,
-      name: "Renzo ezETH",
-      symbol: "ezETH",
-      iconUrl:
-        "https://file.notion.so/f/f/8918534a-08b2-4581-8604-0c70c8725edf/00c378ba-134a-4438-8064-a7541364b9af/ezeth_token_icon_light_background.svg?id=0ab06ed9-9568-42e1-bd75-936b076e5289&table=block&spaceId=8918534a-08b2-4581-8604-0c70c8725edf&expirationTimestamp=1715385600000&signature=ZW87EbyS8prbj3gd4vzqcBQ36PlTfRM6dKUYfsan3zM&downloadName=ezeth+token+icon+light+background.svg",
-      tags: ["yieldSource", "ezeth"],
-      extensions: {
-        shortName: "Renzo ezETH",
-        protocol: "renzo",
-      },
-    },
-    {
       address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
       decimals: 18,
       places: 2,
@@ -428,82 +414,6 @@ export const sepoliaAppConfig: AppConfig = {
         },
       },
     },
-    {
-      address: "0xA955F5d36D3a8Af3739600e49376d65c4fa99412",
-      name: "14d ETH-Renzo ezETH",
-      decimals: 18,
-      baseToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-      sharesToken: "0x63E77f68fA4774A0e2Fc0a085f02150746dd8230",
-      depositOptions: {
-        isBaseTokenDepositEnabled: false,
-        isShareTokenDepositsEnabled: true,
-      },
-      withdrawOptions: {
-        isBaseTokenWithdrawalEnabled: false,
-        isShareTokenWithdrawalEnabled: true,
-      },
-      poolConfig: {
-        baseToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-        vaultSharesToken: "0x63E77f68fA4774A0e2Fc0a085f02150746dd8230",
-        linkerFactory: "0xf039Fd46fE8d81c116Cf4bdC787B3272A4C52815",
-        linkerCodeHash:
-          "0x9a69ff3b9a110deacb7d941d71222642f269891c0da9fcc15ec7c63fbd216b3f",
-        initialVaultSharePrice: 199960202403963n,
-        minimumShareReserves: 1000000000000000n,
-        minimumTransactionAmount: 1000000000000000n,
-        circuitBreakerDelta: 600000000000000000n,
-        positionDuration: 1209600n,
-        checkpointDuration: 86400n,
-        timeStretch: 1746050381163618n,
-        governance: "0xc187a246Ee5A4Fe4395a8f6C0f9F2AA3A5a06e9b",
-        feeCollector: "0xc187a246Ee5A4Fe4395a8f6C0f9F2AA3A5a06e9b",
-        sweepCollector: "0xc187a246Ee5A4Fe4395a8f6C0f9F2AA3A5a06e9b",
-        fees: {
-          curve: 10000000000000000n,
-          flat: 19230769230769n,
-          governanceLP: 150000000000000000n,
-          governanceZombie: 30000000000000000n,
-        },
-      },
-    },
-    {
-      address: "0xA2Ad31DaEbfE222dc96810898EF7FC239daAb580",
-      name: "30d ETH-Renzo ezETH",
-      decimals: 18,
-      baseToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-      sharesToken: "0x63E77f68fA4774A0e2Fc0a085f02150746dd8230",
-      depositOptions: {
-        isBaseTokenDepositEnabled: false,
-        isShareTokenDepositsEnabled: true,
-      },
-      withdrawOptions: {
-        isBaseTokenWithdrawalEnabled: false,
-        isShareTokenWithdrawalEnabled: true,
-      },
-      poolConfig: {
-        baseToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-        vaultSharesToken: "0x63E77f68fA4774A0e2Fc0a085f02150746dd8230",
-        linkerFactory: "0xf039Fd46fE8d81c116Cf4bdC787B3272A4C52815",
-        linkerCodeHash:
-          "0x9a69ff3b9a110deacb7d941d71222642f269891c0da9fcc15ec7c63fbd216b3f",
-        initialVaultSharePrice: 199960123842816n,
-        minimumShareReserves: 1000000000000000n,
-        minimumTransactionAmount: 1000000000000000n,
-        circuitBreakerDelta: 600000000000000000n,
-        positionDuration: 2592000n,
-        checkpointDuration: 86400n,
-        timeStretch: 3737448720271234n,
-        governance: "0xc187a246Ee5A4Fe4395a8f6C0f9F2AA3A5a06e9b",
-        feeCollector: "0xc187a246Ee5A4Fe4395a8f6C0f9F2AA3A5a06e9b",
-        sweepCollector: "0xc187a246Ee5A4Fe4395a8f6C0f9F2AA3A5a06e9b",
-        fees: {
-          curve: 10000000000000000n,
-          flat: 41666666666666n,
-          governanceLP: 150000000000000000n,
-          governanceZombie: 30000000000000000n,
-        },
-      },
-    },
   ],
   protocols: {
     maker: {
@@ -527,12 +437,6 @@ export const sepoliaAppConfig: AppConfig = {
       name: "Rocket Pool",
       iconUrl:
         "https://assets.coingecko.com/coins/images/2090/large/rocket_pool_%28RPL%29.png?1696503058",
-    },
-    renzo: {
-      id: "renzo",
-      name: "Renzo",
-      iconUrl:
-        "https://file.notion.so/f/f/8918534a-08b2-4581-8604-0c70c8725edf/8104f9b0-980c-4e9e-a9a3-49f0b741d99e/renzo_logo_green_on_transparent.svg?id=9000598a-f94c-492d-8f5a-ac09e441319d&table=block&spaceId=8918534a-08b2-4581-8604-0c70c8725edf&expirationTimestamp=1715385600000&signature=jn2uDGG1CTn2NjRZ4IC8hOxp7uXEcxcl56PrQN7Ly2U&downloadName=renzo+logo+green+on+transparent.svg",
     },
   },
 };
