32b3f527514c9059ee90f12c112eafd5d6c6138f
jackburrus <jhburrus3@gmail.com>
2024-05-22 10:58:02 -0600

    remove params from read steth

--- packages/hyperdrive-js-core/src/hyperdrive/steth/ReadStEthHyperdrive.ts
+++ packages/hyperdrive-js-core/src/hyperdrive/steth/ReadStEthHyperdrive.ts
@@ -184,9 +184,7 @@ export function readStEthHyperdriveMixin<T extends Constructor<ReadHyperdrive>>(
 
     async previewCloseShort({
       asBase,
-      destination,
       maturityTime,
-      minAmountOut: _minAmountOut,
       shortAmountIn,
       extraData,
       options,
@@ -194,21 +192,10 @@ export function readStEthHyperdriveMixin<T extends Constructor<ReadHyperdrive>>(
       ReadHyperdrive["previewCloseShort"]
     > {
       const requiresConversion = !asBase && !this.isUsingSharesAccounting;
-      let minAmountOut = _minAmountOut;
-
-      if (requiresConversion) {
-        // Using the latest block since the preview will simulate the transaction
-        // using the latest state of the contract.
-        minAmountOut = await this.convertToShares(minAmountOut, {
-          blockTag: "latest",
-        });
-      }
 
       const { maxAmountOut, flatPlusCurveFee } = await super.previewCloseShort({
         asBase,
-        destination,
         maturityTime,
-        minAmountOut,
         shortAmountIn,
         extraData,
         options,
