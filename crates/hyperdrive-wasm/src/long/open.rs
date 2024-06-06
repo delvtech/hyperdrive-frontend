3cdc379b223c10b2c0cd8c6fdb6aced9ab0f95b5
Ryan Goree <goree.ryan@gmail.com>
2024-06-06 18:36:05 -0500

    Finish error management

--- crates/hyperdrive-wasm/src/long/open.rs
+++ crates/hyperdrive-wasm/src/long/open.rs
@@ -1,11 +1,10 @@
-use ethers::types::U256;
-use fixed_point::FixedPoint;
 use hyperdrive_math::State;
-use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
 use crate::{
+    error::ToJsResult,
     types::{JsPoolConfig, JsPoolInfo},
-    utils::set_panic_hook,
+    utils::{ToFixedPoint, ToU256},
 };
 
 /// Calculates the long amount that will be opened for a given base amount.
@@ -16,17 +15,20 @@ use crate::{
 ///
 /// @param baseAmount - The amount of base tokens to open a long for
 #[wasm_bindgen(skip_jsdoc)]
-pub fn calcOpenLong(poolInfo: &JsPoolInfo, poolConfig: &JsPoolConfig, baseAmount: &str) -> String {
-    set_panic_hook();
+pub fn calcOpenLong(
+    poolInfo: &JsPoolInfo,
+    poolConfig: &JsPoolConfig,
+    baseAmount: &str,
+) -> Result<String, JsValue> {
     let state = State {
-        info: poolInfo.into(),
-        config: poolConfig.into(),
+        info: poolInfo.try_into()?,
+        config: poolConfig.try_into()?,
     };
-    let base_amount: FixedPoint = FixedPoint::from(U256::from_dec_str(baseAmount).unwrap());
+    let base_amount = baseAmount.to_fixed_point()?;
 
-    let result_fp = state.calculate_open_long(base_amount).unwrap();
+    let result_fp = state.calculate_open_long(base_amount).to_js_result()?;
 
-    U256::from(result_fp).to_string()
+    Ok(result_fp.to_u256()?.to_string())
 }
 
 /// Calculates the spot price after opening a Hyperdrive long.
@@ -41,17 +43,16 @@ pub fn spotPriceAfterLong(
     poolInfo: &JsPoolInfo,
     poolConfig: &JsPoolConfig,
     baseAmount: &str,
-) -> String {
-    set_panic_hook();
+) -> Result<String, JsValue> {
     let state = State {
-        info: poolInfo.into(),
-        config: poolConfig.into(),
+        info: poolInfo.try_into()?,
+        config: poolConfig.try_into()?,
     };
-    let base_amount = FixedPoint::from(U256::from_dec_str(baseAmount).unwrap());
+    let base_amount = baseAmount.to_fixed_point()?;
 
     let result_fp = state
         .calculate_spot_price_after_long(base_amount, None)
-        .unwrap();
+        .to_js_result()?;
 
-    U256::from(result_fp).to_string()
+    Ok(result_fp.to_u256()?.to_string())
 }
