9e3bf21097394a8cf2ada11ee5b98e0127245455
Ryan Goree <goree.ryan@gmail.com>
2024-06-11 19:49:30 -0500

    Use app config in sandbox, reduce args

--- packages/hyperdrive-appconfig/src/appconfig/helpers/findYieldSourceHyperdrives.ts
+++ packages/hyperdrive-appconfig/src/appconfig/helpers/findYieldSourceHyperdrives.ts
@@ -1,16 +1,14 @@
+import { AppConfig } from "src/appconfig/AppConfig";
 import { findYieldSourceToken } from "src/appconfig/helpers/findYieldSourceToken";
 import { HyperdriveConfig } from "src/hyperdrives/HyperdriveConfig";
-import { TokenConfig } from "src/tokens/getTokenConfig";
 import { YieldSourceExtensions } from "src/yieldSources/YieldSourceTokenConfig";
 
 export function findYieldSourceHyperdrives({
   yieldSourceId,
-  hyperdrives,
-  tokens,
+  appConfig: { hyperdrives, tokens },
 }: {
   yieldSourceId: YieldSourceExtensions["protocol"];
-  hyperdrives: HyperdriveConfig[];
-  tokens: TokenConfig<any>[];
+  appConfig: AppConfig;
 }): HyperdriveConfig[] {
   return hyperdrives.filter((hyperdrive) => {
     const sharesToken = findYieldSourceToken({
