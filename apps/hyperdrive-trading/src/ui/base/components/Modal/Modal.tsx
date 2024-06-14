919d9f13fad948195f98aaf574cf86d8c20368d1
Danny Delott <dannydelott@gmail.com>
2024-06-14 16:02:20 -0700

    Add modalHeader prop to Modal

--- apps/hyperdrive-trading/src/ui/base/components/Modal/Modal.tsx
+++ apps/hyperdrive-trading/src/ui/base/components/Modal/Modal.tsx
@@ -4,6 +4,7 @@ import { ReactElement, ReactNode, useRef } from "react";
 interface ModalProps {
   modalId: string;
   modalContent: ReactNode;
+  modalHeader?: ReactNode;
   children?: (options: ModalChildrenOptions) => ReactNode;
   className?: string;
   forceOpen?: boolean;
@@ -16,6 +17,7 @@ interface ModalChildrenOptions {
 export function Modal({
   modalId,
   modalContent,
+  modalHeader,
   children,
   className,
   forceOpen = false,
@@ -34,9 +36,14 @@ export function Modal({
       >
         <form
           method="dialog"
-          className={classNames("daisy-modal-box bg-base-200", className)}
+          className={classNames("daisy-modal-box bg-base-200 p-0", className)}
         >
-          {modalContent}
+          <div className="flex flex-col gap-4">
+            <div className="h-30 flex items-center bg-base-100 bg-[url('/sine-wave.svg')] bg-cover bg-no-repeat px-8 pb-4 pt-8">
+              {modalHeader}
+            </div>
+            <div className="px-2 pb-6 pt-0 sm:px-10">{modalContent}</div>
+          </div>
         </form>
         <form method="dialog" className="daisy-modal-backdrop">
           <button>close</button>
