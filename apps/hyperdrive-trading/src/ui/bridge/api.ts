4fdd41ec9fb04a632f8d8b3331de729c5e769996
Matthew Brown <mattfdbrown@gmail.com>
2024-06-26 11:37:10 -0500

    Add Gopher Stubs (#1207)
    * add gopher stubs
    
    * add comment

--- apps/hyperdrive-trading/src/ui/bridge/api.ts
+++ apps/hyperdrive-trading/src/ui/bridge/api.ts
@@ -1,4 +1,9 @@
-import { Api } from "@delvtech/gopher";
+import {
+  Api,
+  ServerGetSupportedFungibleTokensResponse,
+} from "@delvtech/gopher";
+import { HttpResponse as HttpResponseMSW, http } from "msw";
+import { setupWorker } from "msw/browser";
 
 export const BASE_URL = "https://gopher.test.buildwithsygma.com";
 export const SWAGGER_JSON_URL = `${BASE_URL}/swagger/doc.json`;
@@ -6,3 +11,73 @@ export const SWAGGER_JSON_URL = `${BASE_URL}/swagger/doc.json`;
 export const gopher = new Api({
   baseUrl: BASE_URL,
 });
+
+export const worker = setupWorker(
+  http.get(`${BASE_URL}/assets/fungible`, ({ request, params, cookies }) => {
+    const response: ServerGetSupportedFungibleTokensResponse = {
+      data: [
+        {
+          addresses: {},
+          decimals: 9,
+          logoURI:
+            "https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png",
+          symbol: "SOL",
+          name: "Solana",
+        },
+        {
+          name: "USDC",
+          decimals: 6,
+          symbol: "USDC",
+          logoURI:
+            "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png",
+          addresses: {
+            "1": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+            "10": "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
+            "100": "0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83",
+            "137": "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359",
+            "8453": "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+            "42161": "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
+            "43114": "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
+          },
+        },
+        {
+          name: "TetherUSD",
+          decimals: 6,
+          symbol: "USDT",
+          logoURI:
+            "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png",
+          addresses: {
+            "1": "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+            "10": "0x94b008aA00579c1307B0EF2c499aD98a8ce58e58",
+            "100": "0x4ECaBa5870353805a9F068101A40E0f32ed605C6",
+            "137": "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
+            "8453": "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2",
+            "42161": "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+            "43114": "0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7",
+          },
+        },
+        {
+          name: "DAI Stablecoin",
+          decimals: 18,
+          symbol: "DAI",
+          logoURI:
+            "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png",
+          addresses: {
+            "1": "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+            "10": "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+            "137": "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",
+            "8453": "0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb",
+            "42161": "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+            "43114": "0xd586e7f844cea2f87f50152665bcbc2c279d8d70",
+          },
+        },
+      ],
+    };
+    const status = {
+      status: 202,
+      statusText: "Mocked status",
+    };
+
+    return HttpResponseMSW.json(response, status);
+  }),
+);
--- apps/hyperdrive-trading/src/ui/main.tsx
+++ apps/hyperdrive-trading/src/ui/main.tsx
@@ -14,6 +14,7 @@ import { queryClient } from "src/network/queryClient";
 import { wagmiConfig } from "src/network/wagmiClient";
 import { App } from "src/ui/app/App/App";
 import ToastProvider from "src/ui/base/components/Toaster/ToastProvider";
+import { worker } from "src/ui/bridge/api";
 import "src/ui/globals.css";
 import { logAppVersion } from "src/ui/version/logAppVersion";
 import { customRainbowTheme } from "src/ui/wallet/customTheme";
@@ -28,51 +29,64 @@ if (import.meta.env.DEV) {
 
 logAppVersion();
 
-root.render(
-  <WagmiProvider config={wagmiConfig}>
-    <ToastProvider />
-    <QueryClientProvider client={queryClient}>
-      <RainbowKitProvider
-        appInfo={{
-          appName: "Hyperdrive",
-          disclaimer: () => (
-            <span className="text-xs text-neutral-content">
-              Note: If you choose to log in with Capsule, please review their{" "}
-              <a
-                href="https://capsule-org.notion.site/Capsule-Labs-Inc-Privacy-Policy-7421971b985d4320abfe8454c4ba185c"
-                target="_blank"
-                rel="noreferrer"
-                className="underline"
-              >
-                privacy
-              </a>{" "}
-              policy.{" "}
-            </span>
-          ),
-        }}
-        showRecentTransactions
-        theme={customRainbowTheme()}
-      >
-        <SkeletonTheme baseColor="#202F36" highlightColor="#243942">
-          <RollbarProvider
-            config={{
-              accessToken: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN as string,
-              environment: import.meta.env.VITE_ROLLBAR_ENV as string,
-              captureIp: "anonymize",
-              captureUncaught: true,
-              captureUnhandledRejections: true,
-            }}
-          >
-            <RollbarErrorBoundary>
-              <App />
-            </RollbarErrorBoundary>
-          </RollbarProvider>
-        </SkeletonTheme>
-      </RainbowKitProvider>
-    </QueryClientProvider>
-  </WagmiProvider>,
-);
+// TODO: remove this when we no longer need to stub the gopher API.  Currently
+// there is no support for any testnets so we have to stub out the responses for
+// now.
+if (import.meta.env.DEV) {
+  worker.start().then(() => {
+    root.render(<Root />);
+  });
+} else {
+  root.render(<Root />);
+}
 
+function Root() {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <ToastProvider />
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          appInfo={{
+            appName: "Hyperdrive",
+            disclaimer: () => (
+              <span className="text-xs text-neutral-content">
+                Note: If you choose to log in with Capsule, please review their{" "}
+                <a
+                  href="https://capsule-org.notion.site/Capsule-Labs-Inc-Privacy-Policy-7421971b985d4320abfe8454c4ba185c"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="underline"
+                >
+                  privacy
+                </a>{" "}
+                policy.{" "}
+              </span>
+            ),
+          }}
+          showRecentTransactions
+          theme={customRainbowTheme()}
+        >
+          <SkeletonTheme baseColor="#202F36" highlightColor="#243942">
+            <RollbarProvider
+              config={{
+                accessToken: import.meta.env
+                  .VITE_ROLLBAR_ACCESS_TOKEN as string,
+                environment: import.meta.env.VITE_ROLLBAR_ENV as string,
+                captureIp: "anonymize",
+                captureUncaught: true,
+                captureUnhandledRejections: true,
+              }}
+            >
+              <RollbarErrorBoundary>
+                <App />
+              </RollbarErrorBoundary>
+            </RollbarProvider>
+          </SkeletonTheme>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
 /**
  * Enable Tailwind Debug Screens plugin in development mode for showing active
  * breakpoints in bottom left corner, ie: 'screen: sm', 'screen: 2xl', etc..
