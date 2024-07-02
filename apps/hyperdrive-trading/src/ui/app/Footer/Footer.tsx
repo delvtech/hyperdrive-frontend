3dd2d38b70f6db24be8074697728448bedd1e28d
Jack Burrus <jhburrus3@gmail.com>
2024-07-02 15:20:23 -0600

    Final footer (#1232)
    * adds new links to footer
    
    * comment out bug bounty
    
    * updates to docs.hyperdrive.box
    
    * remove todos

--- apps/hyperdrive-trading/src/ui/app/Footer/Footer.tsx
+++ apps/hyperdrive-trading/src/ui/app/Footer/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer(): ReactElement {
   const footerTitleClassName =
     "daisy-footer-title text-neutral-content opacity-100"; // need to set opacity here to override daisy-footer-title
   return (
-    <footer className="daisy-footer max-w-6xl grid-cols-2 p-10 text-base-content sm:grid-cols-4">
+    <footer className="daisy-footer max-w-6xl grid-cols-2 p-10 text-base-content sm:grid-cols-5">
       <aside>
         <Link
           to="/"
@@ -51,7 +51,7 @@ export default function Footer(): ReactElement {
         <a
           target="_blank"
           rel="noreferrer"
-          href="https://hyperdrive.box/docs"
+          href="https://docs.hyperdrive.box"
           className="daisy-link-hover daisy-link"
         >
           Documentation
@@ -67,7 +67,7 @@ export default function Footer(): ReactElement {
         <a
           target="_blank"
           rel="noreferrer"
-          href="https://docs-delv.gitbook.io/hyperdrive/security/security-for-the-hyperdrive-protocol"
+          href="https://docs.hyperdrive.box/security/security-for-the-hyperdrive-protocol"
           className="daisy-link-hover daisy-link"
         >
           Security
@@ -75,11 +75,55 @@ export default function Footer(): ReactElement {
         <a
           target="_blank"
           rel="noreferrer"
-          href="https://docs-delv.gitbook.io/hyperdrive/hyperdrive-trading-bots/quickstart"
+          href="https://arxiv.org/pdf/2404.05036"
+          className="daisy-link-hover daisy-link"
+        >
+          Whitepaper
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://testnet.hyperdrive.box/"
+          className="daisy-link-hover daisy-link"
+        >
+          Testnet
+        </a>
+      </nav>
+      <nav>
+        <header className={footerTitleClassName}>Developers</header>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="daisy-link-hover daisy-link"
+          href="https://github.com/delvtech"
+        >
+          GitHub
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://docs.hyperdrive.box/hyperdrive-trading-bots/quickstart"
           className="daisy-link-hover daisy-link"
         >
           Trading Bots
         </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/delvtech/hyperdrive-rs"
+          className="daisy-link-hover daisy-link"
+        >
+          SDK
+        </a>
+        {/* TODO: Add this when the bug bounty program goes live */}
+        {/* <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://cantina.xyz/bounties/7fa99194-3a19-476d-92dd-f66289baf95e"
+          className="daisy-link-hover daisy-link"
+        >
+          Bug Bounty
+        </a> */}
       </nav>
       <nav>
         <header className={footerTitleClassName}>Ecosystem</header>
@@ -109,7 +153,6 @@ export default function Footer(): ReactElement {
         </a>
       </nav>
       <nav>
-        {/* TODO: Determine if company information is needed here */}
         <header className={footerTitleClassName}>About Us</header>
         <a
           target="_blank"
@@ -127,17 +170,8 @@ export default function Footer(): ReactElement {
         >
           Blog
         </a>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          className="daisy-link-hover daisy-link"
-          href="https://github.com/delvtech"
-        >
-          GitHub
-        </a>
       </nav>
       <nav>
-        {/* TODO: Add links to legal pages */}
         <header className={footerTitleClassName}>Legal</header>
         <a className="daisy-link-hover daisy-link" href={termsOfUseUrl}>
           Terms of Use
