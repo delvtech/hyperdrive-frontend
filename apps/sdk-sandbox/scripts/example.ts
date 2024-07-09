4fd5ae0feb7dbf5b14f283ed0adcc730db1ab5c5
Danny Delott <dannydelott@gmail.com>
2024-07-09 12:23:29 -0700

    Example - maxShort and calcOpenShort discrepency

--- apps/sdk-sandbox/scripts/example.ts
+++ apps/sdk-sandbox/scripts/example.ts
@@ -1,9 +1,18 @@
 import { ReadHyperdrive } from "@delvtech/hyperdrive-viem";
 import {
+  calcOpenShort,
+  initSync,
+  maxShort,
+  wasmBuffer,
+} from "@delvtech/hyperdrive-wasm";
+
+initSync(wasmBuffer);
+
+import {
   findYieldSourceHyperdrives,
   mainnetAppConfig,
 } from "@hyperdrive/appconfig";
-import { formatEther } from "viem";
+import { formatEther, formatUnits } from "viem";
 import { publicClient } from "../client";
 
 const makerDeployments = findYieldSourceHyperdrives({
@@ -16,8 +25,45 @@ const hyperdrive = new ReadHyperdrive({
   publicClient,
 });
 
-const info = await hyperdrive.getPoolInfo();
-const rate = await hyperdrive.getFixedApr();
+// const rate = await hyperdrive.getFixedApr();
 
-console.log(`Current pool info for ${hyperdrive.address}:\n`, info);
-console.log(`Current Fixed Rate: ${formatEther(rate)}`);
+// const bondAmountForShorting = await hyperwasm.console.log(
+//   `Current pool info for ${hyperdrive.address}:\n`,
+//   info,
+// );
+// console.log(`Current Fixed Rate: ${formatEther(rate)}`);
+
+const poolInfo = await hyperdrive.getPoolInfo();
+const poolConfig = await hyperdrive.getPoolConfig();
+const { vaultSharePrice } = await hyperdrive.getCheckpoint();
+const checkpointExposure = await hyperdrive.getCheckpointExposure();
+
+const budget = BigInt(157e18);
+
+// user types in amount of base to calc the amount of bonds to short
+const expectedBonds = maxShort({
+  budget: budget,
+  checkpointExposure,
+  openVaultSharePrice: vaultSharePrice,
+  poolConfig,
+  poolInfo,
+});
+
+// Feed the bond amount from maxShort into calcOpenShort to see if the trader
+// deposit returned matches the original input to maxShort
+const traderDeposit = calcOpenShort({
+  bondAmount: expectedBonds,
+  openVaultSharePrice: vaultSharePrice,
+  poolConfig,
+  poolInfo,
+});
+
+console.log("budget:                            ", formatEther(budget));
+console.log(
+  "expected bonds given budget:       ",
+  formatUnits(expectedBonds, 18),
+);
+console.log(
+  "traderDeposit given expected bonds:",
+  formatUnits(traderDeposit, 18),
+);
