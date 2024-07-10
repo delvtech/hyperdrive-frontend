863a008c1781b1e56571cd6bb686ef6d5862ada1
Ryan Goree <goree.ryan@gmail.com>
2024-07-10 17:07:41 -0500

    Add non-ideal states

--- apps/hyperdrive-trading/src/ui/chainlog/PoolsTable.tsx
+++ apps/hyperdrive-trading/src/ui/chainlog/PoolsTable.tsx
@@ -7,6 +7,7 @@ import {
 } from "@tanstack/react-table";
 import classNames from "classnames";
 import { ReactElement } from "react";
+import { NonIdealState } from "src/ui/base/components/NonIdealState";
 import { TableSkeleton } from "src/ui/base/components/TableSkeleton";
 import { useReadRegistry } from "src/ui/registry/hooks/useReadRegistry";
 import { Address } from "viem";
@@ -23,63 +24,72 @@ export function PoolsTable(): ReactElement {
 
   return (
     <div className="w-full overflow-x-auto">
-      <table className="daisy-table daisy-table-zebra daisy-table-lg h-fit">
-        <thead>
-          {tableInstance.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th
-                  className={classNames(
-                    "sticky top-0 z-10 h-10 rounded-box text-sm",
-                    {
-                      "min-w-52": header.id === "name",
-                    },
-                  )}
-                  key={header.id}
-                >
-                  <div
-                    className="font-normal text-neutral-content"
-                    onClick={header.column.getToggleSortingHandler()}
-                  >
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext(),
+      {!isFetching && !data.length ? (
+        <div className="px-10 py-20">
+          <NonIdealState
+            heading="No pools found"
+            text="There are no pools registered on this chain."
+          />
+        </div>
+      ) : (
+        <table className="daisy-table daisy-table-zebra daisy-table-lg h-fit">
+          <thead>
+            {tableInstance.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th
+                    className={classNames(
+                      "sticky top-0 z-10 h-10 rounded-box text-sm",
+                      {
+                        "min-w-52": header.id === "name",
+                      },
                     )}
-                  </div>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody
-          className={classNames({
-            "relative h-52": !tableInstance.getFilteredRowModel().rows.length,
-          })}
-        >
-          {isFetching ? (
-            <TableSkeleton numColumns={poolCols.length} numRows={5} />
-          ) : (
-            tableInstance.getRowModel().rows.map((row) => {
-              return (
-                <tr key={row.id} className="h-20">
-                  <>
-                    {row.getVisibleCells().map((cell) => {
-                      return (
-                        <td key={cell.id}>
-                          {flexRender(
-                            cell.column.columnDef.cell,
-                            cell.getContext(),
-                          )}
-                        </td>
-                      );
-                    })}
-                  </>
-                </tr>
-              );
-            })
-          )}
-        </tbody>
-      </table>
+                    key={header.id}
+                  >
+                    <div
+                      className="font-normal text-neutral-content"
+                      onClick={header.column.getToggleSortingHandler()}
+                    >
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                    </div>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody
+            className={classNames({
+              "relative h-52": !tableInstance.getFilteredRowModel().rows.length,
+            })}
+          >
+            {isFetching ? (
+              <TableSkeleton numColumns={poolCols.length} numRows={5} />
+            ) : (
+              tableInstance.getRowModel().rows.map((row) => {
+                return (
+                  <tr key={row.id} className="h-20">
+                    <>
+                      {row.getVisibleCells().map((cell) => {
+                        return (
+                          <td key={cell.id}>
+                            {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext(),
+                            )}
+                          </td>
+                        );
+                      })}
+                    </>
+                  </tr>
+                );
+              })
+            )}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
