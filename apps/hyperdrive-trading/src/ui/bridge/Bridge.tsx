c86b9c557a13aacd532a7bf82e96c9ddeaa6f9e7
Matthew Brown <mattfdbrown@gmail.com>
2024-07-15 16:32:44 -0500

    add UI to bridge page

--- apps/hyperdrive-trading/src/ui/bridge/Bridge.tsx
+++ apps/hyperdrive-trading/src/ui/bridge/Bridge.tsx
@@ -1,14 +1,35 @@
 import { ReactElement } from "react";
-import Chains from "src/ui/bridge/Chains/Chains";
-import TokenBalances from "src/ui/bridge/TokenBalances/TokenBalances";
-import Tokens from "src/ui/bridge/Tokens/Tokens";
+import { BridgeAssetsForm } from "src/ui/bridge/BridgeAssetsForm/BridgeAssetsForm";
+import { useBridgeTokenBalances } from "src/ui/bridge/hooks/useBridgeTokenBalances";
+import { useBridgeTokens } from "src/ui/bridge/hooks/useBridgeTokens";
+import { useAccount } from "wagmi";
 
 export function Bridge(): ReactElement {
+  const { address: accountAddress } = useAccount();
+  const { tokens } = useBridgeTokens();
+  const { balances = [[]] } = useBridgeTokenBalances(
+    accountAddress,
+    tokens?.map((t) => t.symbol) || [],
+  );
+
+  const firstTokenWithBalance = tokens?.find((token, index) => {
+    return balances[index]?.some(
+      (chainBalance) => Number(chainBalance.balance) > 0,
+    );
+  });
+
+  if (!firstTokenWithBalance) {
+    return <div>No tokens to bridge</div>;
+  }
+
   return (
     <div className="m-6 flex flex-col space-y-6">
-      <TokenBalances />
-      <Tokens />
-      <Chains />
+      {/* TODO: Remove hardcoded Sepolia chain id */}
+      <div>{"Destination Chain: Sepolia"}</div>
+      <BridgeAssetsForm
+        destinationChainId={11155111}
+        token={firstTokenWithBalance}
+      />
     </div>
   );
 }
--- apps/hyperdrive-trading/src/ui/routes/bridge.tsx
+++ apps/hyperdrive-trading/src/ui/routes/bridge.tsx
@@ -1,7 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Page } from "src/ui/app/Page";
 import Bridge from "src/ui/bridge/Bridge";
 import { BRIDGE_ROUTE } from "src/ui/bridge/routes";
 
 export const Route = createFileRoute(BRIDGE_ROUTE)({
-  component: () => <Bridge />,
+  component: () => (
+    <Page>
+      <Bridge />
+    </Page>
+  ),
 });
