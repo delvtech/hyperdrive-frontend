c3c47fdfd809bf8308ed45588e2b6bcc17a54698
jackburrus <jhburrus3@gmail.com>
2024-07-15 13:49:33 -0600

    check if is new openlong form enabled

--- apps/hyperdrive-trading/src/ui/hyperdrive/longs/OpenLongPreview/OpenLongPreview.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/longs/OpenLongPreview/OpenLongPreview.tsx
@@ -1,3 +1,4 @@
+import { calculateAprFromPrice } from "@delvtech/hyperdrive-viem";
 import { ArrowRightIcon } from "@heroicons/react/16/solid";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { ClockIcon } from "@heroicons/react/24/outline";
@@ -18,6 +19,7 @@ import { useAppConfig } from "src/ui/appconfig/useAppConfig";
 import { CollapseSection } from "src/ui/base/components/CollapseSection/CollapseSection";
 import { LabelValue } from "src/ui/base/components/LabelValue";
 import { useFeatureFlag } from "src/ui/base/featureFlags/featureFlags";
+import { formatBalance } from "src/ui/base/formatting/formatBalance";
 import { formatDate } from "src/ui/base/formatting/formatDate";
 import { useFixedRate } from "src/ui/hyperdrive/longs/hooks/useFixedRate";
 interface OpenLongPreviewProps {
@@ -66,18 +68,186 @@ export function OpenLongPreview({
   const termLengthMS = Number(hyperdrive.poolConfig.positionDuration * 1000n);
   const { isFlagEnabled: isNewOpenLongFormEnabled } =
     useFeatureFlag("new-open-long-form");
-  // if (isNewOpenLongFormEnabled) {
+  if (isNewOpenLongFormEnabled) {
+    return (
+      <div className="flex flex-col gap-3.5 px-2">
+        <CollapseSection
+          heading={
+            <div className="flex w-full justify-between text-neutral-content">
+              <p>Transaction Details</p>
+              <div className="flex items-center gap-1">
+                <ClockIcon className="size-5 text-[#4E6A77]" />
+                <p>{formatDate(Date.now() + termLengthMS)}</p>
+                <ChevronDownIcon className="ml-1 size-6" />
+              </div>
+            </div>
+          }
+        >
+          <LabelValue
+            size="small"
+            label="Fixed APR after open"
+            value={
+              openLongPreviewStatus === "loading" ? (
+                <Skeleton width={100} />
+              ) : (
+                <span
+                  className={classNames({
+                    "text-base-content/80": !spotRateAfterOpen,
+                  })}
+                >
+                  {spotRateAfterOpen ? (
+                    <span className="flex gap-2">
+                      <span className="text-base-content/80">{`${fixedApr?.formatted}% `}</span>
+                      <ArrowRightIcon className="h-4 text-neutral-content" />
+                      {formatRate(spotRateAfterOpen)}%
+                    </span>
+                  ) : (
+                    "-"
+                  )}
+                </span>
+              )
+            }
+          />
+          <LabelValue
+            label="Fixed APR impact"
+            size="small"
+            value={
+              openLongPreviewStatus === "loading" ? (
+                <Skeleton width={100} />
+              ) : (
+                <span
+                  className={classNames({
+                    "text-error": spotRateAfterOpen,
+                    "text-base-content/80": !spotRateAfterOpen,
+                  })}
+                >
+                  {getMarketImpactLabel(fixedApr?.apr, spotRateAfterOpen)}
+                </span>
+              )
+            }
+          />
+        </CollapseSection>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col gap-3.5 px-2">
+      <div className="flex flex-col gap-3">
+        <LabelValue
+          label="You spend"
+          value={
+            <span
+              className={classNames({
+                "text-base-content/80": !amountPaid,
+              })}
+            >{`${formatBalance({
+              balance: amountPaid,
+              decimals: baseToken.decimals,
+              places: baseToken.places,
+            })} ${activeToken.symbol}`}</span>
+          }
+        />
+        <LabelValue
+          label="You receive"
+          value={
+            openLongPreviewStatus === "loading" ? (
+              <Skeleton width={100} />
+            ) : (
+              <span
+                className={classNames({
+                  "text-base-content/80": !bondAmount,
+                })}
+              >{`${formatBalance({
+                balance: bondAmount,
+                decimals: baseToken.decimals,
+                places: baseToken.places,
+              })} hy${baseToken.symbol}`}</span>
+            )
+          }
+        />
+        <LabelValue
+          label="Pool fee"
+          tooltipContent="Total combined fee paid to LPs and governance to open the long."
+          tooltipPosition="right"
+          tooltipSize="small"
+          value={
+            openLongPreviewStatus === "loading" ? (
+              <Skeleton width={100} />
+            ) : (
+              <span
+                className={classNames({
+                  "text-base-content/80": !curveFee,
+                })}
+              >
+                {curveFee
+                  ? `${formatBalance({
+                      balance: curveFee,
+                      decimals: baseToken.decimals,
+                      places: baseToken.places,
+                    })} hy${baseToken.symbol}`
+                  : `0 hy${baseToken.symbol}`}
+              </span>
+            )
+          }
+        />
+        <LabelValue
+          label="Fixed APR"
+          tooltipContent="Your net fixed rate after pool fees and slippage are applied."
+          tooltipPosition="right"
+          tooltipSize="small"
+          value={
+            openLongPreviewStatus === "loading" ? (
+              <Skeleton width={100} />
+            ) : (
+              <span className="gradient-text">
+                {bondAmount > 0
+                  ? `${formatRate(
+                      calculateAprFromPrice({
+                        positionDuration:
+                          hyperdrive.poolConfig.positionDuration || 0n,
+                        baseAmount: amountPaidInBase,
+                        bondAmount: bondAmount,
+                      }),
+                      baseToken.decimals,
+                    )}%`
+                  : `${fixedApr?.formatted}%`}
+              </span>
+            )
+          }
+        />
+        <LabelValue
+          label="Yield at maturity"
+          tooltipContent={`Total ${baseToken.symbol} expected in return at the end of the term, excluding fees.`}
+          tooltipPosition="right"
+          tooltipSize="small"
+          value={
+            openLongPreviewStatus === "loading" ? (
+              <Skeleton width={100} />
+            ) : bondAmount > 0 ? (
+              <span className="text-success">
+                {yieldAtMaturity
+                  ? // TODO: Add ROI here in parenthesis after the yield amount
+                    `+${formatBalance({
+                      balance: yieldAtMaturity,
+                      decimals: baseToken.decimals,
+                      places: baseToken.places,
+                    })} ${baseToken.symbol}`
+                  : undefined}
+              </span>
+            ) : (
+              <span className={"text-base-content/80"}>
+                0 {baseToken.symbol}
+              </span>
+            )
+          }
+        />
+      </div>
+
       <CollapseSection
         heading={
-          <div className="flex w-full justify-between text-neutral-content">
-            <p>Transaction Details</p>
-            <div className="flex items-center gap-1">
-              <ClockIcon className="size-5 text-[#4E6A77]" />
-              <p>{formatDate(Date.now() + termLengthMS)}</p>
-              <ChevronDownIcon className="ml-1 size-6" />
-            </div>
+          <div className="flex w-full justify-between">
+            <p>Market Impact</p>
+            <ChevronDownIcon className="ml-1 size-6" />
           </div>
         }
       >
@@ -127,7 +297,6 @@ export function OpenLongPreview({
       </CollapseSection>
     </div>
   );
-  // }
 }
 
 function getMarketImpactLabel(
