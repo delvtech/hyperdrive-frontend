819c7679263e9cc2164c883f2e1219fabb0a546f
Ryan Goree <goree.ryan@gmail.com>
2024-07-15 02:12:05 -0500

    Use string template in fixed-point example

--- apps/sdk-sandbox/scripts/fixed-point.ts
+++ apps/sdk-sandbox/scripts/fixed-point.ts
@@ -23,5 +23,5 @@ const amount = parseFixed("1_000.123456789012345678e18");
 const fee = parseFixed("0.025e18");
 const feeAmount = amount.mulUp(fee);
 
-console.log("Fee amount:", feeAmount.toString());
+console.log(`Fee amount: ${feeAmount}`);
 // => 25.003086419725308642
