70a4868612fc0906cc000aea681a778c66e1dd86
Jack Burrus <jhburrus3@gmail.com>
2024-07-15 15:51:05 -0600

    Add transaction details dropdown (#1274)
    * adds feature flag logic
    
    * check if is new openlong form enabled
    
    * changes label values to small
    
    * get rid of duplicate info
    
    * swap to old market impact label
    
    * make market impact identical
    
    * move transaction details closer
    
    * adds optoinal string
    
    * adjust per comments

--- apps/hyperdrive-trading/src/ui/hyperdrive/TransactionView.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/TransactionView.tsx
@@ -1,4 +1,5 @@
 import { ReactElement, ReactNode } from "react";
+import { useFeatureFlag } from "src/ui/base/featureFlags/featureFlags";
 
 interface TransactionViewProps {
   heading?: string;
@@ -21,6 +22,8 @@ export function TransactionView({
   disclaimer,
   actionButton,
 }: TransactionViewProps): ReactElement {
+  const { isFlagEnabled: isNewOpenLongFormEnabled } =
+    useFeatureFlag("new-open-long-form");
   return (
     <div className="flex w-full flex-col gap-4">
       {heading !== undefined && <h5>{heading}</h5>}
@@ -30,13 +33,18 @@ export function TransactionView({
       </div>
       <div className="flex flex-col gap-8">
         {primaryStats}
-        <div>
-          <h6 className="mb-4">Preview transaction</h6>
-          {transactionPreview}
-        </div>
+        {/* If the new open long form is not enabled, show the transaction preview above the action button */}
+        {!isNewOpenLongFormEnabled ? (
+          <div>
+            <h6 className="mb-4">Preview transaction</h6>
+            {transactionPreview}
+          </div>
+        ) : null}
         <div className="text-center">{actionButton}</div>
         {disclaimer ? <div>{disclaimer}</div> : null}
+        {/* If the new open long form is enabled, show the transaction preview below the action button */}
       </div>
+      {isNewOpenLongFormEnabled ? transactionPreview : null}
     </div>
   );
 }
--- apps/hyperdrive-trading/src/ui/hyperdrive/longs/OpenLongPreview/OpenLongPreview.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/longs/OpenLongPreview/OpenLongPreview.tsx
@@ -1,5 +1,7 @@
 import { calculateAprFromPrice } from "@delvtech/hyperdrive-viem";
 import { ArrowRightIcon } from "@heroicons/react/16/solid";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { ClockIcon } from "@heroicons/react/24/outline";
 import {
   HyperdriveConfig,
   TokenConfig,
@@ -16,9 +18,10 @@ import { convertSharesToBase } from "src/hyperdrive/convertSharesToBase";
 import { useAppConfig } from "src/ui/appconfig/useAppConfig";
 import { CollapseSection } from "src/ui/base/components/CollapseSection/CollapseSection";
 import { LabelValue } from "src/ui/base/components/LabelValue";
+import { useFeatureFlag } from "src/ui/base/featureFlags/featureFlags";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
+import { formatDate } from "src/ui/base/formatting/formatDate";
 import { useFixedRate } from "src/ui/hyperdrive/longs/hooks/useFixedRate";
-
 interface OpenLongPreviewProps {
   hyperdrive: HyperdriveConfig;
   bondAmount: bigint;
@@ -62,7 +65,97 @@ export function OpenLongPreview({
         decimals: baseToken.decimals,
       });
   const yieldAtMaturity = bondAmount - amountPaidInBase;
-
+  const termLengthMS = Number(hyperdrive.poolConfig.positionDuration * 1000n);
+  const { isFlagEnabled: isNewOpenLongFormEnabled } =
+    useFeatureFlag("new-open-long-form");
+  if (isNewOpenLongFormEnabled) {
+    return (
+      <div className="flex flex-col gap-3.5 px-2">
+        <CollapseSection
+          heading={
+            <div className="flex w-full items-center justify-between text-neutral-content">
+              <p>Transaction Details</p>
+              <div className="flex items-center gap-1">
+                <ClockIcon className="size-5 text-gray-500" />
+                <p>{formatDate(Date.now() + termLengthMS)}</p>
+                <ChevronDownIcon className="ml-1 size-6" />
+              </div>
+            </div>
+          }
+        >
+          <LabelValue
+            label="Pool fee"
+            tooltipContent="Total combined fee paid to LPs and governance to open the long."
+            tooltipPosition="right"
+            tooltipSize="small"
+            size="small"
+            value={
+              openLongPreviewStatus === "loading" ? (
+                <Skeleton width={100} />
+              ) : (
+                <span
+                  className={classNames({
+                    "text-base-content/80": !curveFee,
+                  })}
+                >
+                  {curveFee
+                    ? `${formatBalance({
+                        balance: curveFee,
+                        decimals: baseToken.decimals,
+                        places: baseToken.places,
+                      })} hy${baseToken.symbol}`
+                    : `0 hy${baseToken.symbol}`}
+                </span>
+              )
+            }
+          />
+          <LabelValue
+            size="small"
+            label="Fixed APR after open"
+            value={
+              openLongPreviewStatus === "loading" ? (
+                <Skeleton width={100} />
+              ) : (
+                <span
+                  className={classNames({
+                    "text-base-content/80": !spotRateAfterOpen,
+                  })}
+                >
+                  {spotRateAfterOpen ? (
+                    <span className="flex gap-2">
+                      <span className="text-base-content/80">{`${fixedApr?.formatted}% `}</span>
+                      <ArrowRightIcon className="h-4 text-neutral-content" />
+                      {formatRate(spotRateAfterOpen)}%
+                    </span>
+                  ) : (
+                    "-"
+                  )}
+                </span>
+              )
+            }
+          />
+          <LabelValue
+            label="Fixed APR impact"
+            size="small"
+            value={
+              openLongPreviewStatus === "loading" ? (
+                <Skeleton width={100} />
+              ) : (
+                <span
+                  className={classNames({
+                    "text-error": spotRateAfterOpen,
+                    "text-base-content/80": !spotRateAfterOpen,
+                  })}
+                >
+                  {getMarketImpactLabel(fixedApr?.apr, spotRateAfterOpen)}
+                </span>
+              )
+            }
+          />
+        </CollapseSection>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col gap-3.5 px-2">
       <div className="flex flex-col gap-3">
@@ -176,7 +269,17 @@ export function OpenLongPreview({
         />
       </div>
 
-      <CollapseSection heading="Market Impact">
+      <CollapseSection
+        heading={
+          <div className="flex items-center gap-4">
+            <span>Market Impact</span>
+            <span className="text-xs">
+              {/* Click to expand */}
+              <ChevronDownIcon className="h-4 focus:rotate-180 focus:transition" />
+            </span>
+          </div>
+        }
+      >
         <LabelValue
           size="small"
           label="Fixed APR after open"
