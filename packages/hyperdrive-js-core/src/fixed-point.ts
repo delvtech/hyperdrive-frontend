ef4667c5fcbe6a18dc6a9b14381db2f28e390d1f
Ryan Goree <goree.ryan@gmail.com>
2024-07-23 07:04:14 -0500

    Update sdk imports

--- packages/hyperdrive-js-core/src/fixed-point.ts
+++ packages/hyperdrive-js-core/src/fixed-point.ts
@@ -3,4 +3,4 @@ import * as fixedPoint from "@delvtech/fixed-point-wasm";
 fixedPoint.initSync(fixedPoint.wasmBuffer);
 
 export { fixedPoint };
-export const { Fixed, fixed, ln, randInRange } = fixedPoint;
+export const { fixed, FixedPoint, ln, parseFixed, randomFixed } = fixedPoint;
