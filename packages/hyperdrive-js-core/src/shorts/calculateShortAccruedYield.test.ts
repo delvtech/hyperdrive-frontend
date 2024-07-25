53f907e311e51c58be90cd49f6bb526f978153da
Ryan Goree <goree.ryan@gmail.com>
2024-07-24 20:43:44 -0500

    Refactor `calculateShortAccruedYield` to fixed-point

--- packages/hyperdrive-js-core/src/shorts/calculateShortAccruedYield.test.ts
+++ packages/hyperdrive-js-core/src/shorts/calculateShortAccruedYield.test.ts
@@ -1,19 +1,16 @@
-import * as dnum from "dnum";
+import { parseFixed } from "src/fixed-point";
 import { calculateShortAccruedYield } from "src/shorts/calculateShortAccruedYield";
 import { expect, test } from "vitest";
 
 test("calculateShortAccruedYield should return the yield a short has accrued since it was opened", async () => {
-  const [sharePriceAtCheckpointOpened] = dnum.from("1.008", 18);
-  const [currentSharePrice] = dnum.from("1.01", 18);
-
   const value = calculateShortAccruedYield({
-    bondAmount: dnum.from("100", 18)[0],
-    openVaultSharePrice: sharePriceAtCheckpointOpened,
-    endingVaultSharePrice: currentSharePrice,
+    bondAmount: parseFixed(100).bigint,
+    openVaultSharePrice: parseFixed(1.008).bigint,
+    endingVaultSharePrice: parseFixed(1.01).bigint,
     decimals: 18,
   });
   // If you opened a short position on 100 bonds at a previous checkpoint price
   // of 1.008 and the current checkpoint price is 1.01, your accrued profit would
   // be 0.20.
-  expect(value).toEqual(dnum.from("0.20", 18)[0]);
+  expect(value).toEqual(parseFixed(0.2).bigint);
 });
