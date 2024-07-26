d4d5a8f91cb513c11545cff4bf06934399841540
Ryan Goree <goree.ryan@gmail.com>
2024-07-25 23:48:32 -0500

    Refactor `calculateValueFromPrice` to fixed-point

--- apps/hyperdrive-trading/src/base/calculateValueFromPrice.test.ts
+++ apps/hyperdrive-trading/src/base/calculateValueFromPrice.test.ts
@@ -1,17 +1,12 @@
-import * as dnum from "dnum";
 import { calculateValueFromPrice } from "src/base/calculateValueFromPrice";
 import { expect, test } from "vitest";
-/**
- * Calculates how much an amount of shares are worth, give the price, example:
- *
- * Example: 10 shares each worth $2 each:
- *   calculateShareValue({ amount 10, price: 2, decimals: 2 }) // $20
- */
+
 test("calculateShareValue should return the value of shares given a certain price", async () => {
+  // 10 shares at $1.25 each should be $12.50
   const value = calculateValueFromPrice({
-    amount: dnum.from("10", 18)[0],
-    unitPrice: dnum.from("2", 18)[0],
-    decimals: 18,
+    amount: 10_00n,
+    unitPrice: 1_25n,
+    decimals: 2,
   });
-  expect(value).toEqual(dnum.from("20.0", 18)[0]);
+  expect(value).toEqual(12_50n);
 });
