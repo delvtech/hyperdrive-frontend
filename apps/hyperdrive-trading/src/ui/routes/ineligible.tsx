d1110b47284b1934b8e4fcc79292d6a0997dd2d4
Ryan Goree <goree.ryan@gmail.com>
2024-07-30 03:13:53 -0500

    Remove comment

--- apps/hyperdrive-trading/src/ui/routes/ineligible.tsx
+++ apps/hyperdrive-trading/src/ui/routes/ineligible.tsx
@@ -2,9 +2,6 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Page } from "src/ui/app/Page";
 import { IneligibleAccountMessage } from "src/ui/compliance/IneligibleAccountMessage";
 
-/**
- * An empty route that does nothing.
- */
 export const Route = createFileRoute("/ineligible")({
   component: () => (
     <Page>
