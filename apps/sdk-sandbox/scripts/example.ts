18d5bf0f8265a848d1cf31ffe119e8ee39b8ca08
jackburrus <jhburrus3@gmail.com>
2024-08-01 13:05:08 -0600

    adds example

--- apps/sdk-sandbox/scripts/example.ts
+++ apps/sdk-sandbox/scripts/example.ts
@@ -1,13 +1,13 @@
 import { ReadHyperdrive } from "@delvtech/hyperdrive-viem";
 import {
   findYieldSourceHyperdrives,
-  mainnetAppConfig,
+  sepoliaAppConfig,
 } from "@hyperdrive/appconfig";
-import { formatEther } from "viem";
+import { formatEther, maxUint256 } from "viem";
 import { publicClient } from "../client";
 
 const makerDeployments = findYieldSourceHyperdrives({
-  appConfig: mainnetAppConfig,
+  appConfig: sepoliaAppConfig,
   yieldSourceId: "maker",
 });
 
@@ -16,8 +16,37 @@ const hyperdrive = new ReadHyperdrive({
   publicClient,
 });
 
-const info = await hyperdrive.getPoolInfo();
-const rate = await hyperdrive.getFixedApr();
+// const info = await hyperdrive.getPoolInfo();
+// const rate = await hyperdrive.getFixedApr();
 
-console.log(`Current pool info for ${hyperdrive.address}:\n`, info);
-console.log(`Current Fixed Rate: ${formatEther(rate)}`);
+// console.log(`Current pool info for ${hyperdrive.address}:\n`, info);
+// console.log(`Current Fixed Rate: ${formatEther(rate)}`);
+
+const maxBonds = await hyperdrive.getMaxShort({
+  budget: maxUint256,
+});
+console.log(
+  "Given infinite budget, the max bonds you can short is ",
+  formatEther(maxBonds.maxBondsOut),
+  " which will actually cost ",
+  formatEther(maxBonds.maxBaseIn),
+  " base tokens",
+);
+
+const maxBudget = await hyperdrive.previewOpenShort({
+  amountOfBondsToShort: maxBonds.maxBondsOut,
+  asBase: true,
+});
+
+console.log(
+  `This is confirmed if you take the max short and calculate the traderDeposit via "previewOpenShort". `,
+  `If you try to short ${formatEther(maxBonds.maxBondsOut)} bonds, it will cost you ${formatEther(maxBudget.traderDeposit)} base tokens`,
+);
+
+const estimatedBondsOut = await hyperdrive.getMaxShort({
+  budget: maxBonds.maxBaseIn,
+});
+console.log(
+  `However, if you specify the budget (${formatEther(maxBonds.maxBaseIn)}) as the max base in, you get significantly fewer bonds out`,
+  formatEther(estimatedBondsOut.maxBondsOut),
+);
