a9133e6cf67692d566be851cfade685c61463ec4
Danny Delott <dannydelott@gmail.com>
2024-08-01 08:11:23 -0700

    Use default rainbowkit button (#1321)

--- apps/hyperdrive-trading/src/ui/wallet/customTheme.tsx
+++ apps/hyperdrive-trading/src/ui/wallet/customTheme.tsx
@@ -3,8 +3,17 @@ import { darkTheme, type Theme } from "@rainbow-me/rainbowkit";
 const darkThemePresets = darkTheme();
 export const customRainbowTheme: Theme = {
   ...darkThemePresets,
+  fonts: {
+    body: "Inter",
+  },
+  radii: {
+    ...darkThemePresets.radii,
+    connectButton: "100px",
+    actionButton: "100px",
+  },
   colors: {
     ...darkThemePresets.colors,
+    connectButtonBackground: "#202F36", // gray-700
     modalBackground: "#202F36", // gray-700
     modalBorder: "#202F36", // gray-700
     modalText: "#EFF4F6", // gray-100
