63aee1c32963d3395779c1ff36fae2259f5a8535
jackburrus <jhburrus3@gmail.com>
2024-08-12 15:45:36 -0600

    useportfolio longs working

--- apps/hyperdrive-trading/src/ui/portfolio/Portfolio.tsx
+++ apps/hyperdrive-trading/src/ui/portfolio/Portfolio.tsx
@@ -1,6 +1,9 @@
 import { ReactElement } from "react";
+import { usePortfolioLongsData } from "./usePortfolioLongsData";
 
 export function Portfolio(): ReactElement {
+  const { data } = usePortfolioLongsData();
+  // console.log(data, "data");
   return (
     <div className="flex min-h-screen w-full flex-col items-center bg-base-100 py-8">
       <h1>Hello Portfolio</h1>
