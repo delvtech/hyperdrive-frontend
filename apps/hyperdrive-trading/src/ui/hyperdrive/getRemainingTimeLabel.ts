f718679ccc5b98bd325ecde38c4b766d8eabf981
Jack Burrus <jhburrus3@gmail.com>
2024-08-12 17:01:14 -0600

    Update close long design (#1348)
    * primary stats looking good
    
    * close long form updated
    
    * adds fiat prices
    
    * revert primary stat changes
    
    * final cleanup

--- apps/hyperdrive-trading/src/ui/hyperdrive/getRemainingTimeLabel.ts
+++ apps/hyperdrive-trading/src/ui/hyperdrive/getRemainingTimeLabel.ts
@@ -3,14 +3,17 @@ import { calculateTimeLeft } from "src/base/calculateTimeLeft";
 /**
  * @param options.maturitySeconds The timestamp in seconds when time runs out
  * @param options.condensed Show condensed labels when showing both hours and minutes
+ * @param options.showLeftSuffix Whether to include "left" in the output string
  * @returns string
  */
 export function getRemainingTimeLabel({
   maturitySeconds,
   condensed = false,
+  showLeftSuffix = true,
 }: {
   maturitySeconds: number;
   condensed?: boolean;
+  showLeftSuffix?: boolean;
 }): string {
   const nowSeconds = Date.now() / 1000;
   const isTermComplete = maturitySeconds < nowSeconds;
@@ -23,21 +26,23 @@ export function getRemainingTimeLabel({
     return "Term complete";
   }
 
+  const suffix = showLeftSuffix ? " left" : "";
+
   if (days > 0) {
-    return `${days} days left`;
+    return `${days} days${suffix}`;
   }
 
   // Condensed only needed when showing both hours and minutes.
   if (hours > 0 && condensed) {
-    return `${hours}h ${minutes}m left`;
+    return `${hours}h ${minutes}m${suffix}`;
   }
 
   if (hours > 0) {
-    return `${hours} hours, ${minutes} minutes left`;
+    return `${hours} hours, ${minutes} minutes${suffix}`;
   }
 
   if (minutes > 0) {
-    return `${minutes} minutes left`;
+    return `${minutes} minutes${suffix}`;
   }
 
   return "-";
--- apps/hyperdrive-trading/src/ui/hyperdrive/longs/CloseLongModalButton/CloseLongModalButton.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/longs/CloseLongModalButton/CloseLongModalButton.tsx
@@ -1,19 +1,14 @@
 import { Long } from "@delvtech/hyperdrive-viem";
-import { CheckIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import {
   EmptyExtensions,
   HyperdriveConfig,
   TokenConfig,
   findBaseToken,
 } from "@hyperdrive/appconfig";
-import classNames from "classnames";
 import { ReactElement } from "react";
 import { useAppConfig } from "src/ui/appconfig/useAppConfig";
 import { Modal } from "src/ui/base/components/Modal/Modal";
 import { ModalHeader } from "src/ui/base/components/Modal/ModalHeader";
-import { Stat } from "src/ui/base/components/Stat";
-import { formatDate } from "src/ui/base/formatting/formatDate";
-import { getRemainingTimeLabel } from "src/ui/hyperdrive/getRemainingTimeLabel";
 import { CloseLongForm } from "src/ui/hyperdrive/longs/CloseLongForm/CloseLongForm";
 
 export interface CloseLongModalButtonProps {
@@ -26,10 +21,6 @@ export function CloseLongModalButton({
   long,
   hyperdrive,
 }: CloseLongModalButtonProps): ReactElement {
-  function closeModal() {
-    (window as any)[modalId].close();
-  }
-
   const appConfig = useAppConfig();
   const baseToken = findBaseToken({
     baseTokenAddress: hyperdrive.baseToken,
@@ -42,58 +33,13 @@ export function CloseLongModalButton({
   const subHeading = sharesToken
     ? getSubHeadingLabel(baseToken, hyperdrive, sharesToken)
     : "";
-  const maturityMilliseconds = Number(long.maturity * 1000n);
-  const isMature = Date.now() > maturityMilliseconds;
 
   return (
     <Modal
       modalId={modalId}
-      modalHeader={
-        <ModalHeader heading="Close Long" subHeading={subHeading}>
-          <div className="mt-5 flex w-full flex-wrap justify-between gap-4">
-            <div
-              className={classNames("daisy-badge daisy-badge-lg", {
-                "daisy-badge-neutral text-success": isMature,
-              })}
-            >
-              <Stat
-                horizontal
-                size="small"
-                label={isMature ? undefined : "Term:"}
-                value={
-                  <span
-                    className={classNames("flex items-center", {
-                      "font-normal": isMature,
-                    })}
-                  >
-                    {isMature ? <CheckIcon className="mr-2 h-4" /> : undefined}
-                    {getRemainingTimeLabel({
-                      maturitySeconds: Number(long.maturity),
-                      condensed: true,
-                    })}
-                  </span>
-                }
-              />
-            </div>
-            <div className="daisy-badge daisy-badge-lg">
-              <Stat
-                horizontal
-                size="small"
-                label="Maturity Date:"
-                value={formatDate(maturityMilliseconds)}
-              />
-            </div>
-          </div>
-        </ModalHeader>
-      }
+      modalHeader={<ModalHeader heading="Close Long" subHeading={subHeading} />}
       modalContent={
         <div>
-          <button
-            className="daisy-btn daisy-btn-circle daisy-btn-ghost daisy-btn-sm absolute right-4 top-4"
-            onClick={closeModal}
-          >
-            <XMarkIcon className="w-6" title="Close position" />
-          </button>
           <CloseLongForm
             hyperdrive={hyperdrive}
             long={long}
