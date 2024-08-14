13100d9dfc9529bd17e4be7275925b75574d3ede
Danny Delott <dannydelott@gmail.com>
2024-08-14 15:07:52 -0700

    Calculate lp apy and vault rate using a configurable number of days

--- apps/hyperdrive-trading/src/hyperdrive/getYieldSourceRate.ts
+++ apps/hyperdrive-trading/src/hyperdrive/getYieldSourceRate.ts
@@ -1,15 +1,23 @@
 import { Block, ReadHyperdrive } from "@delvtech/hyperdrive-viem";
+import { AppConfig, findHyperdriveConfig } from "@hyperdrive/appconfig";
+import { DAILY_AVERAGE_BLOCK_TOTAL } from "src/base/constants";
 
 export async function getYieldSourceRate(
   readHyperdrive: ReadHyperdrive,
+  appConfig: AppConfig,
 ): Promise<bigint> {
-  // 1 day ago in blocks
-  const oneDayInBlocks = 86400n / 12n;
+  const hyperdrive = findHyperdriveConfig({
+    hyperdriveAddress: readHyperdrive.address,
+    hyperdrives: appConfig.hyperdrives,
+  });
+  const numBlocksForHistoricalRate =
+    DAILY_AVERAGE_BLOCK_TOTAL *
+    BigInt(appConfig.yieldSources[hyperdrive.yieldSource].historicalRatePeriod);
 
   return (
     readHyperdrive
       .getYieldSourceRate({
-        blockRange: oneDayInBlocks,
+        blockRange: numBlocksForHistoricalRate,
       })
       // If the 24 hour rate doesn't exist, assume the pool was initialized less
       // than 24 hours ago and try to get the all-time rate
