dd6ee0bbce5ef9f28d7a5c8f46985c519c75288a
jackburrus <jhburrus3@gmail.com>
2024-08-15 15:22:21 -0600

    check for chain

--- apps/hyperdrive-trading/src/ui/hyperdrive/longs/OpenLongsTable/TotalOpenLongsValue.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/longs/OpenLongsTable/TotalOpenLongsValue.tsx
@@ -1,9 +1,14 @@
+import { fixed } from "@delvtech/fixed-point-wasm";
 import { HyperdriveConfig } from "@hyperdrive/appconfig";
 import { ReactElement } from "react";
+import { isTestnetChain } from "src/chains/isTestnetChain";
+import { useAppConfig } from "src/ui/appconfig/useAppConfig";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
 import { useOpenLongs } from "src/ui/hyperdrive/longs/hooks/useOpenLongs";
 import { useTotalOpenLongsValue } from "src/ui/hyperdrive/longs/hooks/useTotalOpenLongsValue";
-import { useAccount } from "wagmi";
+import { useTokenFiatPrices } from "src/ui/token/hooks/useTokenFiatPrices";
+import { Address } from "viem";
+import { useAccount, useChainId } from "wagmi";
 
 export function TotalOpenLongsValue({
   hyperdrive,
@@ -11,24 +16,53 @@ export function TotalOpenLongsValue({
   hyperdrive: HyperdriveConfig;
 }): ReactElement {
   const { address: account } = useAccount();
+  const appConfig = useAppConfig();
+  const chainId = useChainId();
   const { openLongs, openLongsStatus } = useOpenLongs({
     account,
     hyperdriveAddress: hyperdrive.address,
   });
-  const { totalOpenLongsValue } = useTotalOpenLongsValue({
+
+  const { totalOpenLongsValue, isLoading } = useTotalOpenLongsValue({
     account,
     longs: openLongs,
     enabled: openLongsStatus === "success",
     hyperdrive,
   });
-  return (
-    <p>
-      Total Value:{" "}
+  const baseToken = appConfig.tokens.find(
+    (token) => token.address === hyperdrive.baseToken,
+  );
+  const tokenPrices = useTokenFiatPrices([baseToken?.address as Address]);
+
+  //   const baseTokenPrice =
+  //     tokenPrices?.[baseToken?.address.toLowerCase() as Address];
+  // TODO: Replace this before merging.
+  const baseTokenPrice = 999363000000000000n;
+  return !isTestnetChain(chainId) ? (
+    <p className="text-h4">
+      {"$"}
+      {formatBalance({
+        balance:
+          totalOpenLongsValue && !isLoading && baseTokenPrice
+            ? fixed(totalOpenLongsValue || 0n, baseToken?.decimals).mul(
+                baseTokenPrice,
+                baseToken?.decimals,
+              ).bigint
+            : 0n,
+        decimals: baseToken?.decimals || 18,
+        places: 2,
+        includeCommas: true,
+      })}{" "}
+    </p>
+  ) : (
+    <p className="text-h4">
       {formatBalance({
         balance: totalOpenLongsValue || 0n,
-        decimals: 18,
-        places: 4,
+        decimals: baseToken?.decimals || 18,
+        places: 2,
+        includeCommas: true,
       })}{" "}
+      {baseToken?.symbol}
     </p>
   );
 }
