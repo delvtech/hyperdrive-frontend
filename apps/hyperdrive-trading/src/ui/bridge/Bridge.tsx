482da6f55abf7798d29c86dfb17bc964e77f9529
Matthew Brown <mattfdbrown@gmail.com>
2024-08-15 15:27:58 -0500

    update Bridge to 3.0 config

--- apps/hyperdrive-trading/src/ui/bridge/Bridge.tsx
+++ apps/hyperdrive-trading/src/ui/bridge/Bridge.tsx
@@ -6,39 +6,43 @@ const integratorId = import.meta.env.VITE_SQUID_ID as string;
 
 export function Bridge(): ReactElement {
   return (
-    <div className="m-6 flex flex-col space-y-6">
+    <div className="m-6 flex flex-col items-center justify-center space-y-6">
       <SquidWidget
         config={{
           integratorId,
-          companyName: "Custom",
-          style: {
-            widgetContainer: {
-              className: "squid-widget-animated-container",
-            },
-            neutralContent: "#959BB2",
-            baseContent: "#eff4f6",
-            base100: "#1a1b1f",
-            base200: "#1b272c",
-            base300: "#263138",
-            error: "#ED6A5E",
-            warning: "#FFB155",
-            success: "#2EAEB0",
-            primary: "#15ffab",
-            secondary: "#71B4BD",
-            secondaryContent: "#191c29",
-            neutral: "#1a1b1f",
-            roundedBtn: "25px",
-            roundedCornerBtn: "999px",
-            roundedBox: "6px",
-            roundedDropDown: "6px",
-          },
+          companyName: "Delv",
           slippage: 1.5,
-          infiniteApproval: false,
+          theme: {
+            // borders
+            "content-100": "#1a1b1f",
+            "content-200": "#ff0000", // 🤷‍♂️
+            // text
+            "content-300": "#ffffff",
+            "content-400": "#ff0000", // 🤷‍♂️
+            // sub text
+            "content-500": "#959bb2",
+            // placeholder
+            "content-600": "#959BB2",
+            "content-700": "#ff0000", // 🤷‍♂️
+            // lighter background
+            "content-800": "#1b272c",
+            // background
+            "content-900": "#12191d",
+            // label text
+            "accent-400": "#959BB2",
+            // aacent text
+            "accent-500": "#15ffab",
+            // button color
+            "status-positive": "#15ffab",
+            "status-negative": "#FF494A",
+            "status-warning": "#FFB155",
+          },
           enableExpress: true,
-          apiUrl: "https://api.squidrouter.com",
+          apiUrl: "https://v2.api.squidrouter.com",
           comingSoonChainIds: [],
           titles: {
             swap: "Swap",
+            swapDetails: "Swap Details",
             settings: "Settings",
             wallets: "Wallets",
             tokens: "Select Token",
