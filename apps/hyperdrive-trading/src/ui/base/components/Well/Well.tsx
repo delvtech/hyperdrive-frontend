c8d790bc181b55ad84844f01b8aabecfe6c4ab25
Danny Delott <dannydelott@gmail.com>
2024-08-17 09:19:03 -0700

    Place app on top of the background squiggle svg, tweak colors to match new design

--- apps/hyperdrive-trading/src/ui/base/components/Well/Well.tsx
+++ apps/hyperdrive-trading/src/ui/base/components/Well/Well.tsx
@@ -21,10 +21,10 @@ export function Well({
 }: PropsWithChildren<WellProps>): ReactElement {
   const isInteractive = !disabled && (interactive || onClick);
   const className = classNames(
-    "daisy-card p-7 border border-1 border-neutral-content/20",
+    "daisy-card p-8 border border-1 border-neutral-content/20",
     {
       "shadow-md": elevation === "elevated",
-      "bg-base-200": !transparent,
+      "bg-gray-800": !transparent,
       "w-full": block,
       "hover:-translate-y-0.5 hover:scale-[1.01] transition duration-300 hover:shadow-xl ease-in-out hover:shadow-primary/5":
         isInteractive,
