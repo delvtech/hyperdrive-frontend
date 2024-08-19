9943cd94de6afd1e07e3c08c5894a6d681f957a5
Danny Delott <dannydelott@gmail.com>
2024-08-19 14:26:06 -0700

    Add development mode build for staging

--- apps/hyperdrive-trading/scripts/build-app.sh
+++ apps/hyperdrive-trading/scripts/build-app.sh
@@ -3,4 +3,9 @@
 yarn run gen:version
 yarn run gen:walletconnect
 
-tsc && vite build --config vite.config.build.ts
+if [ "$1" == "development" ]; then
+  echo "Building development version..."
+  tsc && vite build --config vite.config.build.ts --mode development
+else
+  tsc && vite build --config vite.config.build.ts
+fi
