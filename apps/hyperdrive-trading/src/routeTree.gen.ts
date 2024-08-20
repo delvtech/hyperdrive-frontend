09ca915ab0c10a1b13787af9cb172fcf19a1ffdc
Matthew Brown <mattfdbrown@gmail.com>
2024-08-20 13:19:40 -0500

    route tree changes

--- apps/hyperdrive-trading/src/routeTree.gen.ts
+++ apps/hyperdrive-trading/src/routeTree.gen.ts
@@ -10,104 +10,114 @@
 
 // Import Routes
 
-import { Route as rootRoute } from './ui/routes/__root'
-import { Route as VpnImport } from './ui/routes/vpn'
-import { Route as RestrictedcountriesImport } from './ui/routes/restricted_countries'
-import { Route as MarketsImport } from './ui/routes/markets'
-import { Route as IneligibleImport } from './ui/routes/ineligible'
-import { Route as ErrorImport } from './ui/routes/error'
-import { Route as ChainlogImport } from './ui/routes/chainlog'
-import { Route as BridgeImport } from './ui/routes/bridge'
-import { Route as IndexImport } from './ui/routes/index'
-import { Route as MarketAddressImport } from './ui/routes/market.$address'
+import { Route as rootRoute } from "./ui/routes/__root";
+import { Route as VpnImport } from "./ui/routes/vpn";
+import { Route as RestrictedcountriesImport } from "./ui/routes/restricted_countries";
+import { Route as PortfolioImport } from "./ui/routes/portfolio";
+import { Route as MarketsImport } from "./ui/routes/markets";
+import { Route as IneligibleImport } from "./ui/routes/ineligible";
+import { Route as ErrorImport } from "./ui/routes/error";
+import { Route as ChainlogImport } from "./ui/routes/chainlog";
+import { Route as BridgeImport } from "./ui/routes/bridge";
+import { Route as IndexImport } from "./ui/routes/index";
+import { Route as MarketAddressImport } from "./ui/routes/market.$address";
 
 // Create/Update Routes
 
 const VpnRoute = VpnImport.update({
-  path: '/vpn',
+  path: "/vpn",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 const RestrictedcountriesRoute = RestrictedcountriesImport.update({
-  path: '/restricted_countries',
+  path: "/restricted_countries",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
+
+const PortfolioRoute = PortfolioImport.update({
+  path: "/portfolio",
+  getParentRoute: () => rootRoute,
+} as any);
 
 const MarketsRoute = MarketsImport.update({
-  path: '/markets',
+  path: "/markets",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 const IneligibleRoute = IneligibleImport.update({
-  path: '/ineligible',
+  path: "/ineligible",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 const ErrorRoute = ErrorImport.update({
-  path: '/error',
+  path: "/error",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 const ChainlogRoute = ChainlogImport.update({
-  path: '/chainlog',
+  path: "/chainlog",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 const BridgeRoute = BridgeImport.update({
-  path: '/bridge',
+  path: "/bridge",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 const IndexRoute = IndexImport.update({
-  path: '/',
+  path: "/",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 const MarketAddressRoute = MarketAddressImport.update({
-  path: '/market/$address',
+  path: "/market/$address",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 // Populate the FileRoutesByPath interface
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
-    '/': {
-      preLoaderRoute: typeof IndexImport
-      parentRoute: typeof rootRoute
-    }
-    '/bridge': {
-      preLoaderRoute: typeof BridgeImport
-      parentRoute: typeof rootRoute
-    }
-    '/chainlog': {
-      preLoaderRoute: typeof ChainlogImport
-      parentRoute: typeof rootRoute
-    }
-    '/error': {
-      preLoaderRoute: typeof ErrorImport
-      parentRoute: typeof rootRoute
-    }
-    '/ineligible': {
-      preLoaderRoute: typeof IneligibleImport
-      parentRoute: typeof rootRoute
-    }
-    '/markets': {
-      preLoaderRoute: typeof MarketsImport
-      parentRoute: typeof rootRoute
-    }
-    '/restricted_countries': {
-      preLoaderRoute: typeof RestrictedcountriesImport
-      parentRoute: typeof rootRoute
-    }
-    '/vpn': {
-      preLoaderRoute: typeof VpnImport
-      parentRoute: typeof rootRoute
-    }
-    '/market/$address': {
-      preLoaderRoute: typeof MarketAddressImport
-      parentRoute: typeof rootRoute
-    }
+    "/": {
+      preLoaderRoute: typeof IndexImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/bridge": {
+      preLoaderRoute: typeof BridgeImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/chainlog": {
+      preLoaderRoute: typeof ChainlogImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/error": {
+      preLoaderRoute: typeof ErrorImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/ineligible": {
+      preLoaderRoute: typeof IneligibleImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/markets": {
+      preLoaderRoute: typeof MarketsImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/portfolio": {
+      preLoaderRoute: typeof PortfolioImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/restricted_countries": {
+      preLoaderRoute: typeof RestrictedcountriesImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/vpn": {
+      preLoaderRoute: typeof VpnImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/market/$address": {
+      preLoaderRoute: typeof MarketAddressImport;
+      parentRoute: typeof rootRoute;
+    };
   }
 }
 
@@ -120,9 +130,10 @@ export const routeTree = rootRoute.addChildren([
   ErrorRoute,
   IneligibleRoute,
   MarketsRoute,
+  PortfolioRoute,
   RestrictedcountriesRoute,
   VpnRoute,
   MarketAddressRoute,
-])
+]);
 
 /* prettier-ignore-end */
