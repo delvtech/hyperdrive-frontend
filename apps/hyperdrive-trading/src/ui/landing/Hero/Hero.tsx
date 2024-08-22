089df774ff6e04f81832253c643bd9e3302ffb56
Danny Delott <dannydelott@gmail.com>
2024-08-22 07:57:26 -0700

    Remove feature flag on the new Pools page (#1393)
    * Remove pools-v2 feature flag
    
    * Delete unused components

--- apps/hyperdrive-trading/src/ui/landing/Hero/Hero.tsx
+++ apps/hyperdrive-trading/src/ui/landing/Hero/Hero.tsx
@@ -1,47 +1,23 @@
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import classNames from "classnames";
 import { ReactElement } from "react";
-import { useFeatureFlag } from "src/ui/base/featureFlags/featureFlags";
 
 export function Hero(): ReactElement {
-  const { isFlagEnabled: isNewPoolsView } = useFeatureFlag("pools-view");
   return (
     <div className="daisy-hero">
-      <div
-        className={classNames(
-          "daisy-hero-content",
-          isNewPoolsView ? null : "text-center",
-        )}
-      >
+      <div className={classNames("daisy-hero-content")}>
         <div className="max-w-4xl">
           <h1
             className={classNames(
-              "gradient-text mb-5 text-h3 font-medium",
-              isNewPoolsView ? "md:text-h2" : "md:text-h1",
+              "gradient-text mb-5 text-h3 font-medium md:text-h2",
             )}
           >
             Fixed and variable yields at your control.
           </h1>
-          <h5
-            className={classNames(
-              "font-inter text-neutral-content",
-              isNewPoolsView ? "text-lg" : "mb-5",
-            )}
-          >
-            {isNewPoolsView
-              ? "Open a Long to lock in a predictable fixed rate, open a Short to get multiplied exposure to the variable rate, or become a liquidity provider to earn the variable rate plus trading fees."
-              : "Open a Long to lock in a predictable fixed rate, open a Short to get multiplied exposure to the variable rate, or LP to profit from rates volatility."}
+          <h5 className={classNames("font-inter text-lg text-neutral-content")}>
+            Open a Long to lock in a predictable fixed rate, open a Short to get
+            multiplied exposure to the variable rate, or become a liquidity
+            provider to earn the variable rate plus trading fees
           </h5>
-          {isNewPoolsView ? null : (
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://docs.hyperdrive.box"
-              className="daisy-btn daisy-btn-ghost rounded-full"
-            >
-              Read the Hyperdrive docs <ArrowRightIcon className="inline h-4" />
-            </a>
-          )}
         </div>
       </div>
     </div>
