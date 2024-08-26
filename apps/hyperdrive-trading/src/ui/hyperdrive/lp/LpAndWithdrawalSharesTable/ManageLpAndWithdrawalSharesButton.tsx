0e3f94db866149930e5d974d3afa2f7eb80bb82e
jackburrus <jhburrus3@gmail.com>
2024-08-26 17:52:24 -0600

    adds add and remove liquidity buttons

--- apps/hyperdrive-trading/src/ui/hyperdrive/lp/LpAndWithdrawalSharesTable/ManageLpAndWithdrawalSharesButton.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/lp/LpAndWithdrawalSharesTable/ManageLpAndWithdrawalSharesButton.tsx
@@ -1,13 +1,19 @@
-import { Cog8ToothIcon } from "@heroicons/react/24/solid";
+import { Cog8ToothIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import { findBaseToken, HyperdriveConfig } from "@hyperdrive/appconfig";
 import { ReactElement } from "react";
 import Skeleton from "react-loading-skeleton";
 import { useAppConfig } from "src/ui/appconfig/useAppConfig";
+import { Modal } from "src/ui/base/components/Modal/Modal";
+import { ModalHeader } from "src/ui/base/components/Modal/ModalHeader";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
 import { usePoolInfo } from "src/ui/hyperdrive/hooks/usePoolInfo";
+import { AddLiquidityForm } from "src/ui/hyperdrive/lp/AddLiquidityForm/AddLiquidityForm";
+import { RemoveLiquidityForm } from "src/ui/hyperdrive/lp/RemoveLiquidityForm/RemoveLiquidityForm";
+import { useLpShares } from "src/ui/hyperdrive/lp/hooks/useLpShares";
 import { usePreviewRedeemWithdrawalShares } from "src/ui/hyperdrive/lp/hooks/usePreviewRedeemWithdrawalShares";
 import { useWithdrawalShares } from "src/ui/hyperdrive/lp/hooks/useWithdrawalShares";
 import { getWithdrawalSharesCurrentValue } from "src/ui/hyperdrive/withdrawalShares/OpenWithdrawalSharesCard/OpenWithdrawalSharesCard";
+import { RedeemWithdrawalSharesForm } from "src/ui/hyperdrive/withdrawalShares/RedeemWithdrawalSharesForm/RedeemWithdrawalSharesForm";
 import { useAccount } from "wagmi";
 
 export function ManageLpAndWithdrawalSharesButton({
@@ -21,6 +27,11 @@ export function ManageLpAndWithdrawalSharesButton({
     baseTokenAddress: hyperdrive.poolConfig.baseToken,
     tokens: appConfig.tokens,
   });
+  const { lpShares, lpSharesStatus } = useLpShares({
+    hyperdriveAddress: hyperdrive.address,
+    account,
+  });
+  const yieldSource = appConfig.yieldSources[hyperdrive.yieldSource];
   const { poolInfo } = usePoolInfo({ hyperdriveAddress: hyperdrive.address });
   const { withdrawalShares: balanceOfWithdrawalShares } = useWithdrawalShares({
     hyperdriveAddress: hyperdrive.address,
@@ -60,29 +71,101 @@ export function ManageLpAndWithdrawalSharesButton({
       >
         {balanceOfWithdrawalShares ? (
           <>
-            <button className="flex w-full flex-row justify-start border-b-2 border-b-neutral-content/20 px-0 py-4 hover:bg-neutral hover:text-neutral-content">
-              <span>Claim from queue</span>
-              <span className="ml-6 rounded-md border border-success/20 bg-success/20 px-1 text-success">
-                {baseProceedsFromPreview !== undefined ? (
-                  `+ ${formatBalance({
-                    balance: baseProceedsFromPreview,
-                    decimals: baseToken.decimals,
-                    places: baseToken.places,
-                  })}`
-                ) : (
-                  <Skeleton />
-                )}
-              </span>
-            </button>
+            <Modal
+              modalId="redeemWithdrawalShares"
+              modalHeader={
+                <ModalHeader
+                  heading="Confirm Withdraw"
+                  subHeading={"Remove liquidity that is queued for withdrawal"}
+                />
+              }
+              modalContent={
+                <RedeemWithdrawalSharesForm hyperdrive={hyperdrive} />
+              }
+            >
+              {({ showModal }) => (
+                <button
+                  onClick={showModal}
+                  className="flex w-full flex-row justify-start border-b-2 border-b-neutral-content/20 px-0 py-4 hover:bg-neutral hover:text-neutral-content"
+                >
+                  <span>Claim from queue</span>
+                  <span className="ml-6 rounded-md border border-success/20 bg-success/20 px-1 text-success">
+                    {baseProceedsFromPreview !== undefined ? (
+                      `+ ${formatBalance({
+                        balance: baseProceedsFromPreview,
+                        decimals: baseToken.decimals,
+                        places: baseToken.places,
+                      })}`
+                    ) : (
+                      <Skeleton />
+                    )}
+                  </span>
+                </button>
+              )}
+            </Modal>
           </>
         ) : undefined}
-        <button className="m-0 flex h-[52px] w-full flex-row items-center justify-start border-b-2 border-b-neutral-content/20 p-0 text-start hover:bg-neutral hover:text-neutral-content">
-          Add Liquidity
-        </button>
+        <Modal
+          modalId={"add-lp"}
+          modalHeader={
+            <ModalHeader
+              heading="Add Liquidity"
+              subHeading={`Earn yield by providing liquidity for Longs and
+          Shorts. Your liquidity also earns the ${yieldSource.shortName}
+          rate when not in use.
+          `}
+            />
+          }
+          modalContent={
+            <AddLiquidityForm
+              hyperdrive={hyperdrive}
+              onAddLiquidity={(e) => {
+                // preventDefault since we don't want to close the modal while the
+                // tx is temporarily pending the user's signature in their wallet.
+                e.preventDefault();
+              }}
+            />
+          }
+        >
+          {({ showModal }) => (
+            <button
+              onClick={showModal}
+              className="m-0 flex h-[52px] w-full flex-row items-center justify-start border-b-2 border-b-neutral-content/20 p-0 text-start hover:bg-neutral hover:text-neutral-content"
+            >
+              Add Liquidity
+            </button>
+          )}
+        </Modal>
 
-        <button className="m-0 flex h-[52px] w-full flex-row items-center justify-start p-0 text-start hover:bg-neutral hover:text-neutral-content">
-          Remove Liquidity
-        </button>
+        <Modal
+          modalId="withdrawalLpModal"
+          modalHeader={
+            <ModalHeader heading="Remove Liquidity" subHeading={"TODO"} />
+          }
+          modalContent={
+            <div>
+              <button
+                className="daisy-btn daisy-btn-circle daisy-btn-ghost daisy-btn-sm absolute right-4 top-4"
+                onClick={() => (window as any)["withdrawalLpModal"].close()}
+              >
+                <XMarkIcon className="w-6" title="Close" />
+              </button>
+              <RemoveLiquidityForm
+                hyperdrive={hyperdrive}
+                lpShares={lpShares || 0n}
+              />
+            </div>
+          }
+        >
+          {({ showModal }) => (
+            <button
+              onClick={showModal}
+              className="m-0 flex h-[52px] w-full flex-row items-center justify-start p-0 text-start hover:bg-neutral hover:text-neutral-content"
+            >
+              Remove Liquidity
+            </button>
+          )}
+        </Modal>
       </ul>
     </div>
   );
