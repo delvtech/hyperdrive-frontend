2dd552e8aef37044bc9f27f7aabad2823ec55aeb
jackburrus <jhburrus3@gmail.com>
2024-08-27 19:00:36 -0600

    remove unused configs

--- apps/hyperdrive-trading/src/ui/appconfig/useAppConfig.ts
+++ apps/hyperdrive-trading/src/ui/appconfig/useAppConfig.ts
@@ -1,32 +1,8 @@
-import { AppConfig, appConfig, protocols } from "@hyperdrive/appconfig";
-import { ZERO_ADDRESS } from "src/base/constants";
+import { AppConfig, appConfig } from "@hyperdrive/appconfig";
 import { isTestnetChain } from "src/chains/isTestnetChain";
 import { SupportedChainId } from "src/chains/supportedChains";
-import { b3Sepolia } from "src/network/b3Sepolia";
-import { baseSepolia } from "viem/chains";
 import { useChainId } from "wagmi";
 
-const baseSepoliaAppConfig: AppConfig = {
-  chains: {},
-  registries: {
-    [baseSepolia.id]: ZERO_ADDRESS,
-  },
-  hyperdrives: [],
-  tokens: [],
-  protocols: protocols,
-  yieldSources: {} as any,
-};
-
-const b3SepoliaAppConfig: AppConfig = {
-  chains: {},
-  registries: {
-    [b3Sepolia.id]: ZERO_ADDRESS,
-  },
-  hyperdrives: [],
-  tokens: [],
-  protocols: protocols,
-  yieldSources: {} as any,
-};
 export function useAppConfig(): AppConfig {
   const chainId = useChainId() as SupportedChainId;
   if (isTestnetChain(chainId)) {
