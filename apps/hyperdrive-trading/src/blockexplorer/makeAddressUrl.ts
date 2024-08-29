37c9c51685c29767ccd2035272b8a3312ac49911
Danny Delott <dannydelott@gmail.com>
2024-08-29 12:19:35 -0700

    Add public gnosis chain support

--- apps/hyperdrive-trading/src/blockexplorer/makeAddressUrl.ts
+++ apps/hyperdrive-trading/src/blockexplorer/makeAddressUrl.ts
@@ -1,8 +1,7 @@
 import { cloudChain } from "src/chains/cloudChain";
 import { gnosisFork } from "src/chains/gnosisFork";
 import { SupportedChainId } from "src/chains/supportedChains";
-import { b3Sepolia } from "src/network/b3Sepolia";
-import { baseSepolia, foundry, mainnet, sepolia } from "viem/chains";
+import { foundry, gnosis, mainnet, sepolia } from "viem/chains";
 
 export function makeAddressUrl(
   address: string,
@@ -13,16 +12,12 @@ export function makeAddressUrl(
       return `https://etherscan.io/address/${address}`;
     case sepolia.id:
       return `https://sepolia.etherscan.io/address/${address}`;
-    case baseSepolia.id:
-      return `https://sepolia.basescan.org/address/${address}`;
-    case b3Sepolia.id:
-      return `https://sepolia.explorer.b3.fun/address/${address}`;
+    case gnosis.id:
+      return `https://gnosisscan.io/address/${address}`;
+
     case foundry.id:
-      return `#`;
-    case cloudChain.id: // Cloudchain
-      return `#`;
-    case gnosisFork.id: // Cloudchain
-      return `#`;
+    case cloudChain.id:
+    case gnosisFork.id:
     default:
       return `#`;
   }
