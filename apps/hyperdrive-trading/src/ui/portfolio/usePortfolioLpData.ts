0e8932a3591e89f62945c0c33ad760a4171ee623
jackburrus <jhburrus3@gmail.com>
2024-08-30 16:27:49 -0600

    cleanup hook

--- apps/hyperdrive-trading/src/ui/portfolio/usePortfolioLpData.ts
+++ apps/hyperdrive-trading/src/ui/portfolio/usePortfolioLpData.ts
@@ -7,86 +7,84 @@ import { useAppConfig } from "src/ui/appconfig/useAppConfig";
 import { Address, PublicClient } from "viem";
 import { useAccount } from "wagmi";
 
+// Move type definitions to the top
 type OpenLpPositionsData = Record<
   Address,
   { lpShares: bigint; withdrawalShares: bigint }
 >;
+
+type ClientMap = Map<number, { publicClient: PublicClient }>;
 
 export function usePortfolioLpData(): {
   openLpPositions: OpenLpPositionsData | undefined;
   openLpPositionStatus: "error" | "success" | "loading";
 } {
   const appConfig = useAppConfig();
-  const chainIds = Object.keys(appConfig.registries).map(Number);
   const { address: account } = useAccount();
-  const queryEnabled = !!account && !!appConfig;
-  const clients: Map<
-    number,
-    {
-      publicClient: PublicClient;
-    }
-  > = new Map();
+  const queryEnabled = Boolean(account && appConfig);
 
-  for (const chainId of chainIds) {
-    // TODO: Cleanup type casting
-    const publicClient = getPublicClient(wagmiConfig as any, {
-      chainId,
-    }) as PublicClient;
-
-    clients.set(chainId, {
-      publicClient,
-    });
-  }
-
-  const { data, error, status } = useQuery({
+  const { data, status } = useQuery({
     queryKey: makeQueryKey("portfolioLp", { account }),
-    placeholderData: [],
+    placeholderData: {},
     queryFn: queryEnabled
-      ? async () => {
-          const result: OpenLpPositionsData = {};
-          const results = await Promise.all(
-            appConfig.hyperdrives.map(async (hyperdrive) => {
-              const { publicClient } = clients.get(hyperdrive.chainId) || {};
-
-              // Return available static data if no registry is found
-              if (!publicClient) {
-                console.error(
-                  `No public client found for chainId ${hyperdrive.chainId}`,
-                );
-                return undefined;
-              }
-
-              const readHyperdrive = await getReadHyperdrive({
-                appConfig,
-                hyperdriveAddress: hyperdrive.address,
-                publicClient,
-              });
-
-              const lpShares = await readHyperdrive.getLpShares({
-                account: account,
-              });
-
-              const withdrawalShares = await readHyperdrive.getWithdrawalShares(
-                {
-                  account: account,
-                },
-              );
-              // console.log({ lpShares, withdrawalShares }, "all");
-              result[hyperdrive.address] = {
-                lpShares,
-                withdrawalShares,
-              };
-
-              return result;
-            }),
-          );
-          return Object.assign({}, ...results.filter(Boolean));
-        }
+      ? () => fetchPortfolioLpData(account as Address, appConfig)
       : undefined,
+    enabled: queryEnabled,
   });
 
   return {
     openLpPositions: data,
     openLpPositionStatus: status,
   };
+}
+
+// Helper functions
+function initializeClients(chainIds: number[]): ClientMap {
+  const clients: ClientMap = new Map();
+
+  for (const chainId of chainIds) {
+    const publicClient = getPublicClient(wagmiConfig as any, {
+      chainId,
+    }) as PublicClient;
+    clients.set(chainId, { publicClient });
+  }
+
+  return clients;
+}
+
+async function fetchPortfolioLpData(
+  account: Address,
+  appConfig: ReturnType<typeof useAppConfig>,
+): Promise<OpenLpPositionsData> {
+  const clients = initializeClients(
+    Object.keys(appConfig.registries).map(Number),
+  );
+
+  const results = await Promise.all(
+    appConfig.hyperdrives.map(async (hyperdrive) => {
+      const { publicClient } = clients.get(hyperdrive.chainId) || {};
+
+      if (!publicClient) {
+        console.error(
+          `No public client found for chainId ${hyperdrive.chainId}`,
+        );
+        return undefined;
+      }
+
+      const readHyperdrive = await getReadHyperdrive({
+        appConfig,
+        hyperdriveAddress: hyperdrive.address,
+        publicClient,
+      });
+
+      const [lpShares, withdrawalShares] = await Promise.all([
+        readHyperdrive.getLpShares({ account }),
+        readHyperdrive.getWithdrawalShares({ account }),
+      ]);
+
+      return { [hyperdrive.address]: { lpShares, withdrawalShares } };
+    }),
+  );
+
+  return Object.assign({}, ...results.filter(Boolean));
 }
