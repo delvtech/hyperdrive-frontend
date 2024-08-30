d0f7c5907b95ba6caf03fe833a96960b34f23389
Ryan Goree <goree.ryan@gmail.com>
2024-08-29 21:44:12 -0500

    Show all configured pools

--- apps/hyperdrive-trading/src/ui/chainlog/PoolsTable.tsx
+++ apps/hyperdrive-trading/src/ui/chainlog/PoolsTable.tsx
@@ -1,3 +1,4 @@
+import { ReadRegistry } from "@delvtech/hyperdrive-viem";
 import { UseQueryResult, useQuery } from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
 import {
@@ -6,19 +7,23 @@ import {
   getCoreRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+import { getPublicClient } from "@wagmi/core";
 import classNames from "classnames";
 import { ReactElement } from "react";
 import { makeQueryKey } from "src/base/makeQueryKey";
+import { getReadHyperdrive } from "src/hyperdrive/getReadHyperdrive";
+import { wagmiConfig } from "src/network/wagmiClient";
 import { Status, decodeInstanceData } from "src/registry/data";
+import { sdkCache } from "src/sdk/sdkCache";
+import { useAppConfig } from "src/ui/appconfig/useAppConfig";
 import { NonIdealState } from "src/ui/base/components/NonIdealState";
 import { TableSkeleton } from "src/ui/base/components/TableSkeleton";
 import { AddressCell } from "src/ui/chainlog/AddressCell";
 import { ChainCell } from "src/ui/chainlog/ChainCell";
 import { PausedCell } from "src/ui/chainlog/PausedCell";
 import { StatusCell } from "src/ui/chainlog/StatusCell";
-import { useReadRegistry } from "src/ui/registry/hooks/useReadRegistry";
-import { Address } from "viem";
-import { useChainId } from "wagmi";
+import { Address, PublicClient } from "viem";
+import { usePublicClient } from "wagmi";
 
 export function PoolsTable(): ReactElement {
   const { data = [], isFetching } = usePoolsQuery();
@@ -126,7 +131,9 @@ const poolCols = [
   }),
   poolColHelper.accessor((row) => row.address, {
     header: "Address",
-    cell: ({ getValue }) => <AddressCell address={getValue()} />,
+    cell: ({ getValue, row }) => (
+      <AddressCell address={getValue()} chainId={row.original.chainId} />
+    ),
   }),
   poolColHelper.accessor((row) => row.version, {
     header: "Version",
@@ -141,19 +148,27 @@ const poolCols = [
   }),
   poolColHelper.accessor((row) => row.factoryAddress, {
     header: "Factory",
-    cell: ({ getValue }) => <AddressCell address={getValue()} />,
+    cell: ({ getValue, row }) => (
+      <AddressCell address={getValue()} chainId={row.original.chainId} />
+    ),
   }),
   poolColHelper.accessor((row) => row.deployerCoordinatorAddress, {
     header: "Deployer Coordinator",
-    cell: ({ getValue }) => <AddressCell address={getValue()} />,
+    cell: ({ getValue, row }) => (
+      <AddressCell address={getValue()} chainId={row.original.chainId} />
+    ),
   }),
   poolColHelper.accessor((row) => row.baseToken, {
     header: "Base Token",
-    cell: ({ getValue }) => <AddressCell address={getValue()} />,
+    cell: ({ getValue, row }) => (
+      <AddressCell address={getValue()} chainId={row.original.chainId} />
+    ),
   }),
   poolColHelper.accessor((row) => row.vaultToken, {
     header: "Vault Token",
-    cell: ({ getValue }) => <AddressCell address={getValue()} />,
+    cell: ({ getValue, row }) => (
+      <AddressCell address={getValue()} chainId={row.original.chainId} />
+    ),
   }),
 ];
 
@@ -171,41 +186,96 @@ interface Pool {
 }
 
 function usePoolsQuery(): UseQueryResult<Pool[], any> {
-  const chainId = useChainId();
-  const registry = useReadRegistry(chainId);
-  const queryEnabled = !!registry;
+  const appConfig = useAppConfig();
+  const publicClient = usePublicClient();
+  const queryEnabled = !!publicClient;
 
   return useQuery({
-    queryKey: makeQueryKey("chainlog", {
-      tab: "pools",
-      registry: registry?.address,
-      chainId,
-    }),
-    enabled: queryEnabled,
+    queryKey: makeQueryKey("chainlog", { tab: "pools" }),
     placeholderData: [],
+    enabled: queryEnabled,
     queryFn: queryEnabled
       ? async () => {
-          const pools = await registry.getInstances();
-          const metas = await registry.getInstanceInfos(
-            pools.map((pool) => pool.address),
-          );
+          // registries and clients for each chain
+          const chains: Map<
+            number,
+            {
+              registry: ReadRegistry;
+              publicClient: PublicClient;
+            }
+          > = new Map();
+
+          for (const [chainIdString, address] of Object.entries(
+            appConfig.registries,
+          )) {
+            const chainId = +chainIdString;
+
+            // TODO: Cleanup type casting
+            const publicClient = getPublicClient(wagmiConfig as any, {
+              chainId,
+            }) as PublicClient;
+
+            chains.set(chainId, {
+              publicClient,
+              registry: new ReadRegistry({
+                address,
+                publicClient,
+                cache: sdkCache,
+                namespace: chainIdString,
+              }),
+            });
+          }
 
           return Promise.all(
-            pools.map(async (pool, i): Promise<Pool> => {
-              const { data, factory, name, version } = metas[i];
+            appConfig.hyperdrives.map(async (hyperdrive): Promise<Pool> => {
+              const { registry, publicClient } =
+                chains.get(hyperdrive.chainId) || {};
+
+              // Return available static data if no registry is found
+              if (!registry || !publicClient) {
+                console.error(
+                  `No registry found for chainId ${hyperdrive.chainId}`,
+                );
+                return {
+                  name: hyperdrive.name,
+                  address: hyperdrive.address,
+                  chainId: hyperdrive.chainId,
+                  version: hyperdrive.version,
+                  isPaused: false,
+                  status: "active",
+                  factoryAddress: "0x",
+                  deployerCoordinatorAddress: "0x",
+                  baseToken: hyperdrive.poolConfig.baseToken,
+                  vaultToken: hyperdrive.poolConfig.vaultSharesToken,
+                };
+              }
+
+              const { data, factory, name, version } =
+                await registry.getInstanceInfo(hyperdrive.address);
+              const readHyperdrive = await getReadHyperdrive({
+                appConfig,
+                hyperdriveAddress: hyperdrive.address,
+                publicClient,
+              });
+
               const { baseToken, vaultSharesToken: vaultToken } =
-                await pool.getPoolConfig();
-              const { isPaused } = await pool.getMarketState();
+                await readHyperdrive.getPoolConfig();
+
+              const { isPaused } = await readHyperdrive.getMarketState();
               const { status } = decodeInstanceData(data);
               const [deployerCoordinatorAddress] =
                 await factory.getDeployerCoordinatorAddresses({
-                  instances: [pool.address],
+                  instances: [hyperdrive.address],
                 });
+              // console.log(
+              //   "deployerCoordinatorAddress",
+              //   deployerCoordinatorAddress,
+              // );
 
-              return {
+              const result = {
                 name,
-                address: pool.address,
-                chainId,
+                address: hyperdrive.address,
+                chainId: hyperdrive.chainId,
                 version,
                 isPaused,
                 status,
@@ -214,6 +284,8 @@ function usePoolsQuery(): UseQueryResult<Pool[], any> {
                 baseToken,
                 vaultToken,
               };
+
+              return result;
             }),
           );
         }
