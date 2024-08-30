13816dccf66805f061de2c690e9c258d5f528111
Ryan Goree <goree.ryan@gmail.com>
2024-08-29 23:39:33 -0500

    Filter `useAppConfig` further

--- apps/hyperdrive-trading/src/ui/appconfig/useAppConfig.ts
+++ apps/hyperdrive-trading/src/ui/appconfig/useAppConfig.ts
@@ -1,22 +1,71 @@
 import { AppConfig, appConfig } from "@hyperdrive/appconfig";
+import { useMemo } from "react";
 import { isTestnetChain } from "src/chains/isTestnetChain";
-import { SupportedChainId } from "src/chains/supportedChains";
 import { useChainId } from "wagmi";
 
 export function useAppConfig(): AppConfig {
-  const chainId = useChainId() as SupportedChainId;
-  if (isTestnetChain(chainId)) {
-    return {
-      ...appConfig,
-      hyperdrives: appConfig.hyperdrives.filter((hyperdrive) =>
-        isTestnetChain(hyperdrive.chainId),
-      ),
+  const chainId = useChainId();
+
+  // split config into mainnet and testnet
+  const { testnetConfig, mainnetConfig } = useMemo(() => {
+    const { chains, hyperdrives, registries, tokens, ...nonSpecific } =
+      appConfig;
+
+    const testnetConfig: AppConfig = {
+      ...nonSpecific,
+      chains: {},
+      hyperdrives: [],
+      registries: {},
+      tokens: [],
     };
-  }
-  return {
-    ...appConfig,
-    hyperdrives: appConfig.hyperdrives.filter(
-      (hyperdrive) => !isTestnetChain(hyperdrive.chainId),
-    ),
-  };
+    const mainnetConfig: AppConfig = {
+      ...nonSpecific,
+      chains: {},
+      hyperdrives: [],
+      registries: {},
+      tokens: [],
+    };
+
+    // chains
+    for (const [chainIdString, chain] of Object.entries(chains)) {
+      const chainId = +chainIdString;
+      if (isTestnetChain(chainId)) {
+        testnetConfig.chains[chainId] = chain;
+      } else {
+        mainnetConfig.chains[chainId] = chain;
+      }
+    }
+
+    // registries
+    for (const [chainIdString, registry] of Object.entries(registries)) {
+      const chainId = +chainIdString;
+      if (isTestnetChain(chainId)) {
+        testnetConfig.registries[chainId] = registry;
+      } else {
+        mainnetConfig.registries[chainId] = registry;
+      }
+    }
+
+    // hyperdrives
+    for (const hyperdrive of hyperdrives) {
+      if (isTestnetChain(hyperdrive.chainId)) {
+        testnetConfig.hyperdrives.push(hyperdrive);
+      } else {
+        mainnetConfig.hyperdrives.push(hyperdrive);
+      }
+    }
+
+    // tokens
+    for (const token of tokens) {
+      if (isTestnetChain(token.chainId)) {
+        testnetConfig.tokens.push(token);
+      } else {
+        mainnetConfig.tokens.push(token);
+      }
+    }
+
+    return { testnetConfig, mainnetConfig };
+  }, []);
+
+  return isTestnetChain(chainId) ? testnetConfig : mainnetConfig;
 }
