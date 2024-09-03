1a87c6f3f4a623c32e1165cf59b404c1fd6ea795
jackburrus <jhburrus3@gmail.com>
2024-09-03 16:24:23 -0600

    profit loss working, but need to handle withdrawal shares

--- apps/hyperdrive-trading/src/ui/hyperdrive/lp/LpAndWithdrawalSharesTable/LpCurrentValueCell.tsx
+++ apps/hyperdrive-trading/src/ui/hyperdrive/lp/LpAndWithdrawalSharesTable/LpCurrentValueCell.tsx
@@ -1,5 +1,6 @@
 import { fixed, parseFixed } from "@delvtech/fixed-point-wasm";
 import { HyperdriveConfig } from "@hyperdrive/appconfig";
+import classNames from "classnames";
 import { ReactElement } from "react";
 import Skeleton from "react-loading-skeleton";
 import { calculateRatio } from "src/base/calculateRatio";
@@ -34,14 +35,15 @@ export function LpCurrentValueCell({
     },
   );
 
-  const { proceeds, withdrawalShares } = usePreviewRemoveLiquidity({
-    destination: account,
-    lpSharesIn: lpShares,
-    hyperdriveAddress: hyperdrive.address,
-    minOutputPerShare: 1n,
-    asBase: hyperdrive.withdrawOptions.isBaseTokenWithdrawalEnabled,
-    chainId: hyperdrive.chainId,
-  });
+  const { proceeds, withdrawalShares, previewRemoveLiquidityStatus } =
+    usePreviewRemoveLiquidity({
+      destination: account,
+      lpSharesIn: lpShares,
+      hyperdriveAddress: hyperdrive.address,
+      minOutputPerShare: 1n,
+      asBase: hyperdrive.withdrawOptions.isBaseTokenWithdrawalEnabled,
+      chainId: hyperdrive.chainId,
+    });
 
   // make sure proceeds from withdrawal are always denominated in base
   let baseProceeds = proceeds || 0n;
@@ -66,15 +68,52 @@ export function LpCurrentValueCell({
     }
   }
 
+  const profitLoss =
+    previewRemoveLiquidityStatus === "success" ? (
+      formatBalance({
+        balance:
+          proceeds !== undefined
+            ? proceeds > baseAmountPaid
+              ? proceeds - baseAmountPaid
+              : baseAmountPaid - proceeds
+            : 0n,
+        decimals: baseToken?.decimals || 18,
+        places: baseToken?.places,
+      })
+    ) : (
+      <Skeleton />
+    );
+
+  const isPositiveChangeInValue = (proceeds ?? 0n) > baseAmountPaid;
+
   return (
     <div className="flex flex-col">
       {!!poolInfo && !!lpShares ? (
         <>
-          {`${formatBalance({
-            balance: baseValue,
-            decimals: baseToken?.decimals || 18,
-            places: baseToken?.places,
-          })}`}
+          <span className="flex items-center justify-end gap-2 text-md">
+            {`${formatBalance({
+              balance: baseValue,
+              decimals: baseToken?.decimals || 18,
+              places: baseToken?.places,
+            })}`}
+            <div
+              data-tip={"Profit/Loss since open, after closing fees."}
+              className={classNames(
+                "daisy-tooltip daisy-tooltip-left flex text-xs before:border before:font-inter",
+                {
+                  "rounded-md border border-success/20 bg-success/20 px-1 text-success":
+                    isPositiveChangeInValue,
+                  "rounded-md border border-error/20 bg-error/20 px-1 text-error":
+                    !isPositiveChangeInValue && profitLoss !== "0",
+                },
+              )}
+            >
+              <span>{isPositiveChangeInValue ? "+" : "-"}</span>
+              {baseProceeds
+                ? `${profitLoss === "0" ? "0" : profitLoss}`
+                : undefined}
+            </div>
+          </span>
           <span className="text-sm text-gray-500">
             {`${withdrawablePercent.format({ decimals: 2 })}% withdrawable`}
           </span>
