36ff9d8ff5d7445bc045ef00ba88a6567875e47b
Danny Delott <dannydelott@gmail.com>
2024-09-09 12:31:15 -0700

    Simpler utils

--- apps/hyperdrive-trading/src/blockexplorer/makeAddressUrl.ts
+++ apps/hyperdrive-trading/src/blockexplorer/makeAddressUrl.ts
@@ -1,14 +1,5 @@
 import { ChainConfig } from "@hyperdrive/appconfig";
-import { gnosis, mainnet, sepolia } from "viem/chains";
 
 export function makeAddressUrl(address: string, chain: ChainConfig): string {
-  switch (chain.id) {
-    case mainnet.id:
-    case sepolia.id:
-    case gnosis.id:
-      return `${chain.blockExplorerUrl}/address/${address}`;
-
-    default:
-      return `#`;
-  }
+  return `${chain.blockExplorerUrl}/address/${address}`;
 }
--- apps/hyperdrive-trading/src/blockexplorer/makeTransactionUrl.ts
+++ apps/hyperdrive-trading/src/blockexplorer/makeTransactionUrl.ts
@@ -1,17 +1,8 @@
 import { ChainConfig } from "@hyperdrive/appconfig";
-import { gnosis, mainnet, sepolia } from "viem/chains";
 
 export function makeTransactionURL(
   transactionHash: string | undefined,
   chain: ChainConfig,
 ): string {
-  switch (chain.id) {
-    case mainnet.id:
-    case sepolia.id:
-    case gnosis.id:
-      return `${chain.blockExplorerUrl}/tx/${transactionHash}`;
-
-    default:
-      return `#`;
-  }
+  return `${chain.blockExplorerUrl}/tx/${transactionHash}`;
 }
