71bf6a9e5c40f966954e4d62e21d309310f223b8
Danny Delott <dannydelott@gmail.com>
2024-09-09 15:06:58 -0700

    Add chain config for Linea (#1470)
    * Can't as const here
    
    * Add linea chain to appconfig
    
    * Refactor wagmiClient to use resolver object, add linea env variables

--- apps/hyperdrive-trading/src/network/wagmiClient.ts
+++ apps/hyperdrive-trading/src/network/wagmiClient.ts
@@ -11,7 +11,7 @@ import { gnosisFork } from "src/chains/gnosisFork";
 import { CreateWalletFn } from "src/wallets/CreateWalletFn";
 import { capsuleWallet } from "src/wallets/capsule";
 import { Chain, http, Transport } from "viem";
-import { foundry, gnosis, mainnet, sepolia } from "wagmi/chains";
+import { foundry, gnosis, linea, mainnet, sepolia } from "wagmi/chains";
 
 const {
   VITE_LOCALHOST_NODE_RPC_URL,
@@ -20,94 +20,139 @@ const {
   VITE_WALLET_CONNECT_PROJECT_ID,
   VITE_SEPOLIA_RPC_URL,
   VITE_MAINNET_RPC_URL,
+  VITE_LINEA_RPC_URL,
   VITE_GNOSIS_FORK_NODE_RPC_URL,
   VITE_GNOSIS_FORK_CHAIN_ID,
   VITE_GNOSIS_NODE_RPC_URL,
 } = import.meta.env;
 
-export const chains: Chain[] = [];
-const transports: Record<string, Transport> = {};
+interface WagmiClientConfig {
+  rpcUrl: string;
+  chain: Chain;
+  wallets?: (CreateWalletFn | undefined)[];
+  /**
+   * used for fork chains only
+   */
+  chainId?: number;
+}
 
+/* To add a new chain to this file, follow these steps:
+
+* 1. Import the new chain from "wagmi/chains" or define it in "src/chains" if it's a custom chain.
+* 2. Add a new property to the `chainConfigs` object with the chain's name as the key.
+* 3. Define the chain's configuration, including the `rpcUrl` and chain properties.
+* 4. Optionally, add a `wallets` property to specify the wallets that should be supported on this chain.
+* 5. If the chain is a fork chain, add a `chainId` property to specify the chain's ID.
+
+* Example:
+*   myChain: {
+*     rpcUrl: VITE_MY_CHAIN_RPC_URL,
+*     chain: myChain,
+*     wallets: [capsuleWallet],
+*     chainId: 123, // only required for fork chains
+*   }
+*/
+
+const chainConfigs: Record<string, WagmiClientConfig> = {
+  mainnet: {
+    rpcUrl: VITE_MAINNET_RPC_URL,
+    chain: mainnet,
+    wallets: [capsuleWallet],
+  },
+  gnosis: {
+    rpcUrl: VITE_GNOSIS_NODE_RPC_URL,
+    chain: gnosis,
+  },
+  linea: {
+    rpcUrl: VITE_LINEA_RPC_URL,
+    chain: linea,
+  },
+  foundry: {
+    rpcUrl: VITE_LOCALHOST_NODE_RPC_URL,
+    chain: foundry,
+  },
+  gnosisFork: {
+    rpcUrl: VITE_GNOSIS_FORK_NODE_RPC_URL,
+    chain: gnosisFork,
+    chainId: VITE_GNOSIS_FORK_CHAIN_ID,
+  },
+  cloudChain: {
+    rpcUrl: VITE_CUSTOM_CHAIN_NODE_RPC_URL,
+    chain: cloudChain,
+    chainId: VITE_CUSTOM_CHAIN_CHAIN_ID,
+  },
+  sepolia: {
+    rpcUrl: VITE_SEPOLIA_RPC_URL,
+    chain: sepolia,
+    wallets: [capsuleWallet],
+  },
+};
+
+const chains: Chain[] = [];
+const transports: Record<string, Transport> = {};
+const customWallets: CreateWalletFn[] = [];
 const recommendedWallets = [
   injectedWallet,
   safeWallet,
   rainbowWallet,
   metaMaskWallet,
 ];
-const customWallets: CreateWalletFn[] = [];
 
 // WalletConnect
 if (VITE_WALLET_CONNECT_PROJECT_ID) {
   recommendedWallets.push(walletConnectWallet);
 }
 
-// mainnet
+// Handle mainnet first to ensure it's always the first chain. This ensures
+// wagmi falls back to mainnet if the user isn't connected.
 if (VITE_MAINNET_RPC_URL) {
   chains.push(mainnet);
   transports[mainnet.id] = http(VITE_MAINNET_RPC_URL);
 
-  // TODO: push this into the custom wallets for local and cloudchain once
-  // capsule support is verified
-  if (capsuleWallet && !customWallets.includes(capsuleWallet)) {
+  if (
+    capsuleWallet &&
+    chainConfigs["mainnet"].wallets?.includes(capsuleWallet)
+  ) {
     customWallets.push(capsuleWallet);
   }
 }
 
-// Local docker anvil node
-if (VITE_LOCALHOST_NODE_RPC_URL && VITE_LOCALHOST_NODE_RPC_URL) {
-  chains.push(foundry);
-  transports[foundry.id] = http(VITE_LOCALHOST_NODE_RPC_URL);
-}
-
-// Gnosis Fork
-if (VITE_GNOSIS_FORK_NODE_RPC_URL && VITE_GNOSIS_FORK_CHAIN_ID) {
-  chains.push(gnosisFork);
-  transports[gnosisFork.id] = http(VITE_GNOSIS_FORK_NODE_RPC_URL);
-}
-
-// CloudChain
-if (VITE_CUSTOM_CHAIN_NODE_RPC_URL && VITE_CUSTOM_CHAIN_CHAIN_ID) {
-  chains.push(cloudChain);
-  transports[cloudChain.id] = http(VITE_CUSTOM_CHAIN_NODE_RPC_URL);
-}
-
-// Sepolia
-if (VITE_SEPOLIA_RPC_URL) {
-  chains.push(sepolia);
-  transports[sepolia.id] = http(VITE_SEPOLIA_RPC_URL);
-  // TODO: push this into the custom wallets for local and cloudchain once
-  // capsule support is verified
-  if (capsuleWallet) {
-    customWallets.push(capsuleWallet);
+// Build the rest of the chains dynamically, excluding mainnet
+Object.values(chainConfigs).forEach(({ rpcUrl, chain, wallets }) => {
+  // Exclude mainnet here
+  if (chain.id === mainnet.id) {
+    return;
   }
-}
 
-// Gnosis
-if (VITE_GNOSIS_NODE_RPC_URL) {
-  chains.push(gnosis);
-  transports[gnosis.id] = http(VITE_GNOSIS_NODE_RPC_URL);
-}
+  chains.push(chain);
+  transports[chain.id] = http(rpcUrl);
 
+  // Add custom wallets if they exist
+  if (wallets) {
+    wallets.forEach((wallet) => {
+      if (wallet && !customWallets.includes(wallet)) {
+        customWallets.push(wallet);
+      }
+    });
+  }
+});
+
+// Prepare wallet list
 const wallets: WalletList = [
-  {
-    groupName: "Other",
-    wallets: recommendedWallets,
-  },
+  { groupName: "Other", wallets: recommendedWallets },
 ];
-// If Capsule is not configured in your .env, then don't include it in the
-// custom wallets
+
 if (customWallets.length) {
   wallets.push({
     groupName: "Log In or Sign Up with Capsule",
     wallets: customWallets,
   });
 }
+
 export const wagmiConfig = getDefaultConfig({
   appName: "Hyperdrive",
   projectId: VITE_WALLET_CONNECT_PROJECT_ID || "0",
   transports,
-  // Viem's type for `chains` requires at least one item in the array, but since
-  // we build the list up programmatically we must cast.
   chains: chains as [Chain, ...restChains: Chain[]],
   wallets,
 });
