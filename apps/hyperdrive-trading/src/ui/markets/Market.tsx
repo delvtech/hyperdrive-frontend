f37aa1d752c222053405ab9d09d894812b8e501f
Danny Delott <dannydelott@gmail.com>
2024-09-11 15:19:42 -0700

    Remove pool-details-v2 feature flag

--- apps/hyperdrive-trading/src/ui/markets/Market.tsx
+++ apps/hyperdrive-trading/src/ui/markets/Market.tsx
@@ -3,8 +3,6 @@ import { useParams } from "@tanstack/react-router";
 import { ReactElement } from "react";
 import { Helmet } from "react-helmet";
 import { useAppConfig } from "src/ui/appconfig/useAppConfig";
-import { useFeatureFlag } from "src/ui/base/featureFlags/featureFlags";
-import { MarketDetailsBody } from "src/ui/markets/MarketDetailsBody/MarketDetailsBody";
 import { PoolDetails } from "src/ui/markets/PoolDetails/PoolDetails";
 import { MARKET_DETAILS_ROUTE } from "src/ui/markets/routes";
 import { Address } from "viem";
@@ -12,7 +10,6 @@ import { Address } from "viem";
 export function Market(): ReactElement {
   const { address, chainId } = useParams({ from: MARKET_DETAILS_ROUTE });
   const appConfig = useAppConfig();
-  const { isFlagEnabled: isPoolDetailsV2 } = useFeatureFlag("pool-details-v2");
 
   const hyperdrive = findHyperdriveConfig({
     hyperdriveChainId: Number(chainId),
@@ -28,11 +25,7 @@ export function Market(): ReactElement {
         </title>
       </Helmet>
 
-      {isPoolDetailsV2 ? (
-        <PoolDetails hyperdrive={hyperdrive} />
-      ) : (
-        <MarketDetailsBody hyperdrive={hyperdrive} />
-      )}
+      <PoolDetails hyperdrive={hyperdrive} />
     </div>
   );
 }
--- apps/hyperdrive-trading/src/ui/token/SlippageSettingsTwo.tsx
+++ apps/hyperdrive-trading/src/ui/token/SlippageSettingsTwo.tsx
@@ -5,7 +5,6 @@ import {
 import classNames from "classnames";
 import { ReactElement, useState } from "react";
 import { PercentInput } from "src/ui/base/components/PercentInput";
-import { useFeatureFlag } from "src/ui/base/featureFlags/featureFlags";
 
 export const DEFAULT_SLIPPAGE_AMOUNT = "0.5";
 
@@ -23,7 +22,6 @@ export function SlippageSettingsTwo({
   tooltip?: string;
 }): ReactElement {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const { isFlagEnabled: isPoolDetailsV2 } = useFeatureFlag("pool-details-v2");
 
   return (
     <div className="daisy-dropdown daisy-dropdown-bottom flex justify-end">
@@ -35,17 +33,12 @@ export function SlippageSettingsTwo({
           setIsDropdownOpen((prevState) => !prevState);
         }}
         className={
-          isPoolDetailsV2
-            ? "daisy-btn daisy-btn-md h-9 min-h-9 rounded-full text-xs font-normal text-neutral-content"
-            : "daisy-btn daisy-btn-ghost daisy-btn-sm flex flex-row items-center justify-end gap-2 text-xs font-light text-neutral-content"
+          "daisy-btn daisy-btn-md h-9 min-h-9 rounded-full text-xs font-normal text-neutral-content"
         }
       >
         {`${slippage || DEFAULT_SLIPPAGE_AMOUNT}%`} slippage
         <Cog6ToothIcon
-          className={classNames(
-            "h-4 stroke-2",
-            isPoolDetailsV2 ? "test-neutral-content" : "text-base-content",
-          )}
+          className={classNames("h-4 stroke-2", "test-neutral-content")}
         />
       </button>
       {isDropdownOpen && (
