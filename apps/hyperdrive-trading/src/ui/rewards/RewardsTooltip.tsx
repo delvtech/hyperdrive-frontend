c0e175c24fc5b08754b175964d35a130687be434
Danny Delott <dannydelott@gmail.com>
2024-09-12 09:19:02 -0700

    Add rewards for linena rseth

--- apps/hyperdrive-trading/src/ui/rewards/RewardsTooltip.tsx
+++ apps/hyperdrive-trading/src/ui/rewards/RewardsTooltip.tsx
@@ -1,6 +1,7 @@
-import { findHyperdriveConfig } from "@hyperdrive/appconfig";
+import { appConfig, findHyperdriveConfig } from "@hyperdrive/appconfig";
 import * as Tooltip from "@radix-ui/react-tooltip";
 import { PropsWithChildren, ReactNode } from "react";
+import { assertNever } from "src/base/assertNever";
 import { useAppConfig } from "src/ui/appconfig/useAppConfig";
 import { useRewards } from "src/ui/rewards/useRewards";
 import { Address } from "viem";
@@ -40,36 +41,55 @@ export function RewardsTooltip({
             sideOffset={5}
             collisionPadding={12}
           >
-            <div className="p- flex justify-between border-b border-neutral-content/30 p-3">
+            <div className="flex justify-between border-b border-neutral-content/30 p-3">
               <p className="gradient-text text-lg">Rewards</p>
             </div>
 
             {rewards?.map((reward) => {
-              if (reward.id === "MorphoFlatRate") {
-                return (
-                  <div
-                    key={reward.id}
-                    className="flex items-center justify-between border-b border-neutral-content/30 p-3 [&:nth-last-child(2)]:border-none"
-                  >
-                    <div className="flex items-center gap-1">
-                      <img
-                        src="https://cdn.morpho.org/assets/logos/morpho.svg"
-                        alt="Morpho logo"
-                        className="h-4"
-                      />
-                      {reward.name}
-                    </div>
+              switch (reward.id) {
+                case "MorphoFlatRate":
+                  return (
+                    <div
+                      key={reward.id}
+                      className="flex items-center justify-between border-b border-neutral-content/30 p-3 [&:nth-last-child(2)]:border-none"
+                    >
+                      <div className="flex items-center gap-1">
+                        <img
+                          src={appConfig.protocols.morpho.iconUrl}
+                          alt="Morpho logo"
+                          className="h-4"
+                        />
+                        {reward.name}
+                      </div>
 
-                    <div className="grid justify-items-end">
-                      <p className="flex items-center gap-1">
-                        +{reward.amount}
-                      </p>
-                      <p className="text-2xs text-neutral-content">
-                        per $1000 / yr
-                      </p>
+                      <div className="grid justify-items-end">
+                        <p className="flex items-center gap-1">
+                          +{reward.amount}
+                        </p>
+                        <p className="text-2xs text-neutral-content">
+                          per $1000 / yr
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                );
+                  );
+                case "LineaLXPL":
+                  return (
+                    <div
+                      key={reward.id}
+                      className="flex flex-col items-start justify-start gap-2 border-b border-neutral-content/30 p-3 [&:nth-last-child(2)]:border-none"
+                    >
+                      <div className="flex items-center gap-4">
+                        <img
+                          src={appConfig.chains[hyperdrive.chainId].iconUrl}
+                          alt="KelpDAO logo"
+                          className="h-8"
+                        />
+                        <p>This pool is eligible for XLP-L rewards.</p>
+                      </div>
+                    </div>
+                  );
+                default:
+                  assertNever(reward.id);
               }
             })}
           </Tooltip.Content>
