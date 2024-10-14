10b530f30056c8a11cb9b71ea079b1b9c38a6f13
Ryan Goree <goree.ryan@gmail.com>
2024-10-14 15:40:47 -0500

    normalize casing

--- apps/hyperdrive-trading/src/ui/compliance/hooks/useIsReadOnlyRegion.ts
+++ apps/hyperdrive-trading/src/ui/compliance/hooks/useIsReadOnlyRegion.ts
@@ -1,9 +1,11 @@
-const READONLY_REGIONS = ["US"];
+const READONLY_REGIONS = ["us"];
 
 /**
  * A hook for determining if the user is in a read-only region.
  */
 export function useIsReadOnlyRegion(): boolean | undefined {
-  const urlRegion = new URL(window.location.href).searchParams.get("region");
+  const urlRegion = new URL(window.location.href).searchParams
+    .get("region")
+    ?.toLocaleLowerCase();
   return urlRegion ? READONLY_REGIONS.includes(urlRegion) : undefined;
 }
