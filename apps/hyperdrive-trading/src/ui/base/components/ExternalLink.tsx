c3718d23960b9ac3fb957a29dc22c25d6ffaaf00
Ryan Goree <goree.ryan@gmail.com>
2024-10-29 16:37:01 -0500

    Add `ExternalLink` component

---
+++ apps/hyperdrive-trading/src/ui/base/components/ExternalLink.tsx
@@ -0,0 +1,44 @@
+import { ComponentPropsWithoutRef, ReactElement, useRef } from "react";
+
+interface ExternalLinkProps extends ComponentPropsWithoutRef<"a"> {
+  href: string;
+  /**
+   * A convenience prop to set the `target` attribute to `_blank` if `true`.
+   */
+  newTab?: boolean;
+}
+
+/**
+ * A link to an external site that tracks clicks with Plausible and ensures the
+ * `rel` attribute includes `noopener`.
+ *
+ * @see [MDN - rel=noopener](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel/noopener)
+ */
+export function ExternalLink({
+  target,
+  newTab = target === "_blank",
+  rel = "",
+  onClick,
+  children,
+  ...rest
+}: ExternalLinkProps): ReactElement {
+  const ref = useRef<HTMLAnchorElement>(null);
+
+  return (
+    <a
+      {...rest}
+      ref={ref}
+      target={newTab ? "_blank" : target}
+      rel={`${rel} noopener`}
+      onClick={(e) => {
+        window.plausible("externalLinkClick", {
+          name: ref.current?.textContent ?? "",
+          url: rest.href,
+        });
+        onClick?.(e);
+      }}
+    >
+      {children}
+    </a>
+  );
+}
