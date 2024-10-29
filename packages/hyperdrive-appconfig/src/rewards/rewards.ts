231f50eb710d2afa8e7c1475d2affd48bfc752f8
jackburrus <jhburrus3@gmail.com>
2024-10-29 12:43:57 -0600

    remove moonwell

--- packages/hyperdrive-appconfig/src/rewards/rewards.ts
+++ packages/hyperdrive-appconfig/src/rewards/rewards.ts
@@ -1,11 +1,9 @@
 import { fetchEtherfiRewards } from "src/rewards/etherfi";
 import { fetchLineaRewards } from "src/rewards/linea";
-import { fetchMoonwellRewards } from "src/rewards/moonwell";
 import { fetchMorphoRewards } from "src/rewards/morpho";
 
 export const rewardFunctions = {
   fetchMorphoRewards,
   fetchLineaRewards,
   fetchEtherfiRewards,
-  fetchMoonwellRewards,
 } as const;
