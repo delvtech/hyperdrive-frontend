ee5dfde2f15b1cb3f5dfe1fc8f8fa31cf02a6c95
Danny Delott <dannydelott@gmail.com>
2024-11-15 12:14:20 -0800

    Add dependency-cruiser, fix depcruise violations

--- apps/hyperdrive-trading/src/base/ExtractMethodParams.ts
+++
@@ -1,6 +0,0 @@
-export type ExtractMethodParams<
-  Interface extends Record<any, any>,
-  MethodName extends keyof Interface,
-> = Interface[MethodName] extends (...args: any[]) => any
-  ? Parameters<Interface[MethodName]>[0]
-  : never;
