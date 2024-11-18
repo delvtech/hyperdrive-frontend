f130069deb44576841bcb6abddfc15caa8e19627
Matthew Brown <mattfdbrown@gmail.com>
2024-11-18 12:37:04 -0600

    remove initSync

--- packages/hyperdrive-js-core/src/hyperwasm.ts
+++ packages/hyperdrive-js-core/src/hyperwasm.ts
@@ -3,8 +3,7 @@
 // invalidated and the latest version of the wasm package will be loaded.
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
+// eslint-disable-next-line import/no-unresolved
 import * as hyperwasm from "@delvtech/hyperdrive-wasm?v0.14.2";
-
-hyperwasm.initSync(hyperwasm.wasmBuffer);
 
 export { hyperwasm };
