f259ae949a09576a9655d9dd301d1d6c26c1be6b
jackburrus <jhburrus3@gmail.com>
2024-12-03 17:15:22 -0700

    Adds position table heading

--- apps/hyperdrive-trading/src/ui/portfolio/lp/LpAndWithdrawalSharesTable/LpAndWithdrawalSharesTable.tsx
+++ apps/hyperdrive-trading/src/ui/portfolio/lp/LpAndWithdrawalSharesTable/LpAndWithdrawalSharesTable.tsx
@@ -21,6 +21,7 @@ import { ManageLpAndWithdrawalSharesButton } from "src/ui/portfolio/lp/LpAndWith
 import { SizeAndPoolShareCell } from "src/ui/portfolio/lp/LpAndWithdrawalSharesTable/SizeAndPoolShareCell";
 import { WithdrawalQueueCell } from "src/ui/portfolio/lp/LpAndWithdrawalSharesTable/WithdrawalQueueCell";
 import { usePortfolioLpDataTwo } from "src/ui/portfolio/lp/usePortfolioLpData";
+import { PositionTableHeading } from "src/ui/portfolio/PositionTableHeading";
 import { useAccount } from "wagmi";
 
 export function OpenLpTableDesktopTwo({
@@ -37,11 +38,17 @@ export function OpenLpTableDesktopTwo({
     [hyperdrives],
   );
 
+  const tableData = useMemo(
+    () =>
+      (openLpPositions ?? []).filter(
+        (position) => position.lpShares > 0n || position.withdrawalShares > 0n,
+      ),
+    [openLpPositions],
+  );
+
   const tableInstance = useReactTable({
     columns,
-    data: (openLpPositions ?? []).filter(
-      (position) => position.lpShares > 0n || position.withdrawalShares > 0n,
-    ),
+    data: tableData,
     getCoreRowModel: getCoreRowModel(),
   });
   if (!account) {
@@ -77,81 +84,93 @@ export function OpenLpTableDesktopTwo({
   }
 
   return (
-    <div className="daisy-card overflow-x-clip rounded-box bg-gray-750 pt-3">
-      <table className="daisy-table daisy-table-lg">
-        <thead>
-          {tableInstance.getHeaderGroups().map((headerGroup) => (
-            <tr className="border-b-0" key={headerGroup.id}>
-              {headerGroup.headers.map((header, headerIndex) => (
-                <th
-                  key={header.id}
-                  className={classNames(
-                    "relative z-10 text-sm font-normal text-neutral-content/70",
-                  )}
-                >
-                  <div
-                    className={classNames({
-                      "px-4": headerIndex === 0,
-                      // Right align the headers in the Size, Value, and Withdrawal Queue columns
-                      "flex justify-end": [1, 2, 3].includes(headerIndex),
+    <>
+      <PositionTableHeading
+        hyperdrive={hyperdrives[0]}
+        rightElement={null}
+        hyperdriveName={
+          // This regex removes the term (eg: "30d") from the hyperdrive
+          // name since it's already shown in the table.
+          // https://regex101.com/r/f4A3th/1
+          hyperdrives[0].name.replace(/\d{1,3}d/, "")
+        }
+      />
+      <div className="daisy-card overflow-x-clip rounded-box bg-gray-750 pt-3">
+        <table className="daisy-table daisy-table-lg">
+          <thead>
+            {tableInstance.getHeaderGroups().map((headerGroup) => (
+              <tr className="border-b-0" key={headerGroup.id}>
+                {headerGroup.headers.map((header, headerIndex) => (
+                  <th
+                    key={header.id}
+                    className={classNames(
+                      "relative z-10 text-sm font-normal text-neutral-content/70",
+                    )}
+                  >
+                    <div
+                      className={classNames({
+                        "px-4": headerIndex === 0,
+                        // Right align the headers in the Size, Value, and Withdrawal Queue columns
+                        "flex justify-end": [1, 2, 3].includes(headerIndex),
+                      })}
+                    >
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                    </div>
+                    {/* Custom border with inset for the first and last header cells */}
+                    <span
+                      className={classNames(
+                        "absolute bottom-0 border-b border-neutral-content/20",
+                        {
+                          "left-6 right-0": headerIndex === 0, // Inset border only on the left side for the first header cell
+                          "left-0 right-6":
+                            headerIndex === headerGroup.headers.length - 1, // Inset border only on the right side for the last header cell
+                          "left-0 right-0":
+                            headerIndex !== 0 &&
+                            headerIndex !== headerGroup.headers.length - 1, // Full-width border for other header cells
+                        },
+                      )}
+                    />
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {tableInstance.getRowModel().rows.map((row, index) => (
+              <tr
+                key={row.id}
+                className={classNames("h-32 !border-b-0 font-dmMono")}
+              >
+                {row.getVisibleCells().map((cell, cellIndex) => (
+                  <td
+                    key={cell.id}
+                    className={classNames("text-xs md:text-md", {
+                      "px-10 pb-10": cellIndex === 0, // Add padding only to the first cell to align with header and the top row data of the next column over
+                      "rounded-b-none":
+                        index === tableInstance.getRowModel().rows.length - 1,
+                      "rounded-bl-box":
+                        index === tableInstance.getRowModel().rows.length - 1 &&
+                        cellIndex === 0,
+                      "rounded-br-box":
+                        index === tableInstance.getRowModel().rows.length - 1 &&
+                        cellIndex === row.getVisibleCells().length - 1,
+                      // Right align the values in the Size, Value, and Withdrawal Queue columns
+                      "text-end":
+                        [1, 2, 3].includes(cellIndex) && cellIndex !== 0,
                     })}
                   >
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext(),
-                    )}
-                  </div>
-                  {/* Custom border with inset for the first and last header cells */}
-                  <span
-                    className={classNames(
-                      "absolute bottom-0 border-b border-neutral-content/20",
-                      {
-                        "left-6 right-0": headerIndex === 0, // Inset border only on the left side for the first header cell
-                        "left-0 right-6":
-                          headerIndex === headerGroup.headers.length - 1, // Inset border only on the right side for the last header cell
-                        "left-0 right-0":
-                          headerIndex !== 0 &&
-                          headerIndex !== headerGroup.headers.length - 1, // Full-width border for other header cells
-                      },
-                    )}
-                  />
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {tableInstance.getRowModel().rows.map((row, index) => (
-            <tr
-              key={row.id}
-              className={classNames("h-32 !border-b-0 font-dmMono")}
-            >
-              {row.getVisibleCells().map((cell, cellIndex) => (
-                <td
-                  key={cell.id}
-                  className={classNames("text-xs md:text-md", {
-                    "px-10 pb-10": cellIndex === 0, // Add padding only to the first cell to align with header and the top row data of the next column over
-                    "rounded-b-none":
-                      index === tableInstance.getRowModel().rows.length - 1,
-                    "rounded-bl-box":
-                      index === tableInstance.getRowModel().rows.length - 1 &&
-                      cellIndex === 0,
-                    "rounded-br-box":
-                      index === tableInstance.getRowModel().rows.length - 1 &&
-                      cellIndex === row.getVisibleCells().length - 1,
-                    // Right align the values in the Size, Value, and Withdrawal Queue columns
-                    "text-end":
-                      [1, 2, 3].includes(cellIndex) && cellIndex !== 0,
-                  })}
-                >
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
