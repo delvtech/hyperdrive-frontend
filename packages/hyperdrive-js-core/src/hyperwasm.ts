74cb4208c757e383e0153683326735f0d6e1c49d
Matthew Brown <mattfdbrown@gmail.com>
2024-12-06 16:22:59 -0600

    remove wasm pin

--- packages/hyperdrive-js-core/src/hyperwasm.ts
+++ packages/hyperdrive-js-core/src/hyperwasm.ts
@@ -1,11 +1,4 @@
-// NOTE: Make sure to add the hyperwasm version number to the end of the import.
-// Adding a query parameter to the import path will cause the esm cache to be
-// invalidated and the latest version of the wasm package will be loaded.
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 // eslint-disable-next-line import/no-unresolved
-import * as hyperwasm from "@delvtech/hyperdrive-wasm?v0.14.2";
-
-hyperwasm.initSync(hyperwasm.wasmBuffer);
+import * as hyperwasm from "@delvtech/hyperdrive-wasm";
 
 export { hyperwasm };
