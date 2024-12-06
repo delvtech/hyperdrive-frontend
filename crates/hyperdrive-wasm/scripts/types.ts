a1ba5b1e41b72d782db374eb12709a2546872a21
Matthew Brown <mattfdbrown@gmail.com>
2024-12-06 16:44:51 -0600

    Refactor hyperdrive-wasm build script. (#1654)
    * copy build script
    
    * add wasm-pkg-build
    
    * use async/await
    
    * elevate error handling
    
    * don't worry about backups for now
    
    * add cargo.toml
    
    * refactor build script
    
    * update package files
    
    * cleanup
    
    * pr fixes
    
    * remove initSync
    
    * use esm-sync
    
    * remove initSync
    
    * update build script
    
    * run yarn
    
    * fix import errors
    
    * remove wasm pin
    
    * add comments

---
+++ crates/hyperdrive-wasm/scripts/types.ts
@@ -0,0 +1,53 @@
+export interface CargoToml {
+  package: {
+    name: string;
+    version: string;
+    edition: string;
+    authors: string[];
+    license: string;
+  };
+  lib: {
+    "crate-type": string[];
+  };
+  features: {
+    [feature: string]: string[];
+  };
+  dependencies: {
+    [dependency: string]: string | DependencyDetails;
+  };
+  "dev-dependencies": {
+    [dependency: string]: string;
+  };
+}
+
+export interface DependencyDetails {
+  version: string;
+  optional?: boolean;
+  "default-features"?: boolean;
+  features?: string[];
+  path?: string;
+}
+
+export interface PackageJson {
+  name: string;
+  collaborators: string[];
+  version: string;
+  license: string;
+  files: string[];
+  types: string;
+  sideEffects: string[];
+  type: string;
+  main: string;
+  exports: {
+    ".": {
+      default: {
+        require: string;
+        import: string;
+      };
+      types: string;
+    };
+  };
+  publishConfig: {
+    access: string;
+  };
+}
