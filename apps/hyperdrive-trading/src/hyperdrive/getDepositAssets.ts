dd9d9c7ff43212f2425bf95c19746194760b5b5f
Danny Delott <dannydelott@gmail.com>
2024-12-11 17:43:06 -0800

    Move getDepositAssets to own file

---
+++ apps/hyperdrive-trading/src/hyperdrive/getDepositAssets.ts
@@ -0,0 +1,33 @@
+import { ZERO_ADDRESS } from "@delvtech/drift";
+import {
+  HyperdriveConfig,
+  TokenConfig,
+  appConfig,
+  getBaseToken,
+  getToken,
+} from "@delvtech/hyperdrive-appconfig";
+
+// TODO: Move this to the appconfig
+export function getDepositAssets(hyperdrive: HyperdriveConfig): TokenConfig[] {
+  const depositAssets: TokenConfig[] = [];
+  if (hyperdrive.depositOptions.isBaseTokenDepositEnabled) {
+    const baseToken = getBaseToken({
+      hyperdriveChainId: hyperdrive.chainId,
+      hyperdriveAddress: hyperdrive.address,
+      appConfig,
+    });
+    depositAssets.push(baseToken);
+  }
+
+  if (hyperdrive.depositOptions.isShareTokenDepositsEnabled) {
+    const sharesToken = getToken({
+      chainId: hyperdrive.chainId,
+      tokenAddress: hyperdrive.poolConfig.vaultSharesToken,
+      appConfig,
+    });
+    if (sharesToken && sharesToken.address !== ZERO_ADDRESS) {
+      depositAssets.push(sharesToken);
+    }
+  }
+  return depositAssets;
+}
