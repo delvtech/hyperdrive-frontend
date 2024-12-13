4e533415e6277fbbd390ad45d61a4fc47754c04a
Danny Delott <dannydelott@gmail.com>
2024-12-12 22:31:23 -0800

    Stub out components for Points Markets page (#1680)
    * wip
    
    * Stub out layout for PointsMarket card

---
+++ apps/hyperdrive-trading/src/ui/markets/PointsMarkets/PointsMarkets.tsx
@@ -0,0 +1,245 @@
+import { fixed } from "@delvtech/fixed-point-wasm";
+import {
+  getYieldSource,
+  HyperdriveConfig,
+} from "@delvtech/hyperdrive-appconfig";
+import { Link } from "@tanstack/react-router";
+import { ReactElement, ReactNode } from "react";
+import { useAppConfigForConnectedChain } from "src/ui/appconfig/useAppConfigForConnectedChain";
+import { Well } from "src/ui/base/components/Well/Well";
+import { useLpApy } from "src/ui/hyperdrive/hooks/useLpApy";
+import { AssetStack } from "src/ui/markets/AssetStack";
+import { MARKET_DETAILS_ROUTE } from "src/ui/markets/routes";
+import { useYieldSourceRate } from "src/ui/vaults/useYieldSourceRate";
+import { useAccount } from "wagmi";
+
+export function PointsMarkets(): ReactElement | null {
+  const appConfig = useAppConfigForConnectedChain();
+  const hyperdrives = appConfig.hyperdrives;
+  return (
+    <div className="mx-[2vw] mt-4 space-y-8 lg:w-[1064px]">
+      <div className="space-y-4">
+        <h1 className="gradient-text text-h4 font-medium md:text-h4">
+          Points Markets
+        </h1>
+        <p className="font-inter text-lg leading-bodyText text-neutral-content">
+          Maximize your returns on DeFi&apos;s top yield sources and boost your
+          exposure to points and rewards.
+        </p>
+      </div>
+      <div className="flex flex-wrap gap-8">
+        {hyperdrives
+          .filter((hyperdrive) => {
+            const yieldSource = getYieldSource({
+              hyperdriveChainId: hyperdrive.chainId,
+              hyperdriveAddress: hyperdrive.address,
+              appConfig,
+            });
+            return !!yieldSource.rewardsFn;
+          })
+          .map((hyperdrive) => (
+            <PointsMarketCard
+              key={hyperdrive.address}
+              hyperdrive={hyperdrive}
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
+
+function PointsMarketCard({
+  hyperdrive,
+}: {
+  hyperdrive: HyperdriveConfig;
+}): ReactElement {
+  return (
+    <Well as="div" className="w-[514px] shrink-0 gap-6">
+      <PointsMarketCardHeader hyperdrive={hyperdrive} />
+      <PointsMarketCardBanner hyperdrive={hyperdrive} />
+      <PointsMarketTable hyperdrive={hyperdrive} />
+    </Well>
+  );
+}
+function PointsMarketCardHeader({
+  hyperdrive,
+}: {
+  hyperdrive: HyperdriveConfig;
+}) {
+  const appConfig = useAppConfigForConnectedChain();
+  const chainInfo = appConfig.chains[hyperdrive.chainId];
+  const yieldSource = getYieldSource({
+    hyperdriveChainId: hyperdrive.chainId,
+    hyperdriveAddress: hyperdrive.address,
+    appConfig,
+  });
+  return (
+    <div className="flex w-full items-center justify-between gap-2">
+      {/* Icon */}
+      <AssetStack
+        hyperdriveAddress={hyperdrive.address}
+        hyperdriveChainId={hyperdrive.chainId}
+      />
+      {/* Name & Chain info */}
+      <div className="flex w-full items-center justify-between gap-6">
+        <h4 className="mb-4 text-h4 sm:mb-0 sm:text-left">
+          {yieldSource.shortName}
+        </h4>
+        <div className="flex items-center justify-end gap-1.5 text-sm">
+          <img className="size-4 rounded-full" src={chainInfo.iconUrl} />
+          <span className="text-neutral-content">{chainInfo.name}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function PointsMarketCardBanner({
+  hyperdrive,
+}: {
+  hyperdrive: HyperdriveConfig;
+}) {
+  const multipliers = [{ multiplier: "64x", label: "SPIN Rewards" }];
+  return (
+    <div className="flex w-full items-center justify-between rounded-xl bg-base-200 p-5">
+      <div className="flex w-full flex-col items-center justify-center gap-1.5">
+        <p className="font-medium text-neutral-content sm:text-sm">
+          Earn up to
+        </p>
+        <div className="flex w-full justify-between">
+          {multipliers.map(({ multiplier, label }) => (
+            <div
+              key={label}
+              className="flex w-full flex-col items-center justify-center gap-1.5"
+            >
+              <div className="font-chakraPetch text-h4 font-medium">
+                {multiplier}
+              </div>
+              <p className="font-medium text-neutral-content sm:text-sm">
+                {label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function PointsMarketTable({ hyperdrive }: { hyperdrive: HyperdriveConfig }) {
+  const { address: account } = useAccount();
+  const { lpApy } = useLpApy({
+    chainId: hyperdrive.chainId,
+    hyperdriveAddress: hyperdrive.address,
+  });
+  const { vaultRate } = useYieldSourceRate({
+    chainId: hyperdrive.chainId,
+    hyperdriveAddress: hyperdrive.address,
+  });
+  return (
+    <div className="flex flex-col gap-3">
+      <PointsMarketRow
+        col1={null}
+        col2={
+          <Link
+            to={MARKET_DETAILS_ROUTE}
+            params={{
+              address: hyperdrive.address,
+              chainId: hyperdrive.chainId.toString(),
+            }}
+            search={{ position: "short" }}
+            className="daisy-btn h-10 min-h-10 w-full rounded-full bg-gray-500 sm:daisy-btn-sm hover:bg-gray-500 sm:h-8 sm:bg-gray-600 md:w-32"
+            onClick={(e) => {
+              e.stopPropagation();
+              window.plausible("positionCtaClick", {
+                props: {
+                  chainId: hyperdrive.chainId,
+                  poolAddress: hyperdrive.address,
+                  positionType: "short",
+                  statName: "Open Short",
+                  statValue: vaultRate?.netVaultRate
+                    ? fixed(vaultRate.netVaultRate).toString()
+                    : "",
+                  connectedWallet: account,
+                },
+              });
+            }}
+          >
+            Open Short
+          </Link>
+        }
+        col3={
+          <Link
+            to={MARKET_DETAILS_ROUTE}
+            params={{
+              address: hyperdrive.address,
+              chainId: hyperdrive.chainId.toString(),
+            }}
+            search={{ position: "lp" }}
+            className="daisy-btn h-10 min-h-10 w-full rounded-full bg-gray-500 sm:daisy-btn-sm hover:bg-gray-500 sm:h-8 sm:bg-gray-600 md:w-32"
+            onClick={(e) => {
+              e.stopPropagation();
+              window.plausible("positionCtaClick", {
+                props: {
+                  chainId: hyperdrive.chainId,
+                  poolAddress: hyperdrive.address,
+                  positionType: "lp",
+                  statName: "Add Liquidity",
+                  statValue: lpApy?.netLpApy
+                    ? fixed(lpApy.netLpApy).toString()
+                    : "",
+                  connectedWallet: account,
+                },
+              });
+            }}
+          >
+            Add Liquidity
+          </Link>
+        }
+      />
+      <PointsMarketRow
+        col1={
+          <span className="text-sm text-neutral-content">Variable APY</span>
+        }
+        col2={<span className="mr-5 text-sm">13.84%</span>}
+        col3={<span className="mr-3 text-sm">13.84%</span>}
+      />
+      <PointsMarketRow
+        col1={
+          <span className="text-sm text-neutral-content">
+            Rewards Multiplier
+          </span>
+        }
+        col2={
+          <span className="gradient-text mr-5 text-sm font-medium">64x</span>
+        }
+        col3={<span className="mr-3 text-sm">Up to 64x</span>}
+      />
+      <PointsMarketRow
+        col1={<span className="text-sm text-neutral-content">Term</span>}
+        col2={
+          <span className="mr-5 text-sm text-neutral-content">182 days</span>
+        }
+        col3={<span className="mr-3 text-sm">∞</span>}
+      />
+    </div>
+  );
+}
+
+function PointsMarketRow({
+  col1,
+  col2,
+  col3,
+}: {
+  col1: ReactNode;
+  col2: ReactNode;
+  col3: ReactNode;
+}) {
+  return (
+    <div className="grid w-full grid-flow-col grid-cols-3 gap-10">
+      <div className="w-[124px]">{col1}</div>
+      <div className="w-[122px] text-end">{col2}</div>
+      <div className="w-[122px] text-end">{col3}</div>
+    </div>
+  );
+}
