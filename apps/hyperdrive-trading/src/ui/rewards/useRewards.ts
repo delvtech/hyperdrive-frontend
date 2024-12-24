cf2565aea5bbe5e39bc9be95133908af110049e9
Danny Delott <dannydelott@gmail.com>
2024-12-24 11:26:57 -0800

    Improve caching of rewards resolver functions and token fiat price

--- apps/hyperdrive-trading/src/ui/rewards/useRewards.ts
+++ apps/hyperdrive-trading/src/ui/rewards/useRewards.ts
@@ -1,48 +1,69 @@
 import {
   AnyReward,
   appConfig,
+  getHyperdriveConfig,
   getRewardsFn,
   HyperdriveConfig,
 } from "@delvtech/hyperdrive-appconfig";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 import { getPublicClient } from "@wagmi/core";
 import { makeQueryKey2 } from "src/base/makeQueryKey";
 import { wagmiConfig } from "src/network/wagmiClient";
-import { PublicClient } from "viem";
+import { Address, PublicClient } from "viem";
 
 export function useRewards(hyperdriveConfig: HyperdriveConfig): {
   rewards: AnyReward[] | undefined;
   status: "error" | "success" | "loading";
 } {
+  const { data: rewards, status } = useQuery(
+    makeRewardsQuery({
+      hyperdriveAddress: hyperdriveConfig.address,
+      chainId: hyperdriveConfig.chainId,
+    }),
+  );
+
+  return {
+    rewards,
+    status,
+  };
+}
+
+export function makeRewardsQuery({
+  hyperdriveAddress,
+  chainId,
+}: {
+  hyperdriveAddress: Address;
+  chainId: number;
+}): UseQueryOptions<AnyReward[]> {
+  const hyperdriveConfig = getHyperdriveConfig({
+    hyperdriveChainId: chainId,
+    hyperdriveAddress,
+    appConfig,
+  });
   const rewardsFn = getRewardsFn({
     yieldSourceId: hyperdriveConfig.yieldSource,
     appConfig,
   });
 
   const queryEnabled = !!rewardsFn;
-
-  const { data: rewards, status } = useQuery({
+  return {
     queryKey: makeQueryKey2({
       namespace: "rewards",
       queryId: "rewards",
       params: {
-        chainId: hyperdriveConfig.chainId,
-        hyperdriveAddress: hyperdriveConfig.address,
+        chainId,
+        hyperdriveAddress,
       },
     }),
     enabled: queryEnabled,
+    staleTime: Infinity,
     queryFn: queryEnabled
       ? async () => {
           const publicClient = getPublicClient(wagmiConfig as any, {
-            chainId: hyperdriveConfig.chainId,
+            chainId,
           }) as PublicClient;
           return rewardsFn(publicClient);
         }
       : undefined,
-  });
-
-  return {
-    rewards,
-    status,
   };
 }
