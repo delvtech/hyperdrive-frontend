65aed97593d447648b881647e2623e6dbcf5dbda
Danny Delott <dannydelott@gmail.com>
2025-01-06 12:23:27 -0800

    Remove feature flag on Points Markets page (#1715)
    * Add fade to pool form
    
    * Remove feature flag

--- apps/hyperdrive-trading/src/ui/app/Navbar/DevtoolsMenu.tsx
+++ apps/hyperdrive-trading/src/ui/app/Navbar/DevtoolsMenu.tsx
@@ -20,9 +20,6 @@ export function DevtoolsMenu(): ReactElement {
         <FeatureFlagMenuItem flagName="portfolio-rewards">
           Portfolio Rewards
         </FeatureFlagMenuItem>
-        <FeatureFlagMenuItem flagName="points-markets">
-          Points markets
-        </FeatureFlagMenuItem>
         <MenuItem
           onClick={() => {
             window.localStorage.clear();
