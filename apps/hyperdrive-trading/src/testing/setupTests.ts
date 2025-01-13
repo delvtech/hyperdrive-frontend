c605fa0d2978ed430e5457d9c2277dc286f79e0a
Ryan Goree <goree.ryan@gmail.com>
2025-01-13 11:24:34 -0600

    Fix `formatRate`, support negative exponents in `parseFixed` (#1726)
    * Fix test setup for fixed-point-wasm
    
    * Patch fixed-point options in`formatRate`
    
    * Remove unnecessary reassignment
    
    * Add support for negative exponents

--- apps/hyperdrive-trading/src/testing/setupTests.ts
+++ apps/hyperdrive-trading/src/testing/setupTests.ts
@@ -1,3 +1,8 @@
+import * as fixedPoint from "@delvtech/fixed-point-wasm";
 import { beforeEach } from "vitest";
 
-beforeEach(() => {});
+beforeEach(() => {
+  // This is safe to call multiple times. Internally it will return early if
+  // the WASM module has already been initialized.
+  fixedPoint.initSync(fixedPoint.wasmBuffer);
+});
