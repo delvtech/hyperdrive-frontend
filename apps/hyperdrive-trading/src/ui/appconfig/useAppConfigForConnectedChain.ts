bc4d76da8cd17c3131be8295ad0cb3694a78d80b
jackburrus <jhburrus3@gmail.com>
2025-01-28 11:47:52 -0800

    fix preview

--- apps/hyperdrive-trading/src/ui/appconfig/useAppConfigForConnectedChain.ts
+++ apps/hyperdrive-trading/src/ui/appconfig/useAppConfigForConnectedChain.ts
@@ -4,10 +4,29 @@ import {
   mainnetAppConfig,
   testnetAppConfig,
 } from "@delvtech/hyperdrive-appconfig";
+import { useFeatureFlag } from "src/ui/base/featureFlags/featureFlags";
+import { useTokenList } from "src/ui/tokenlist/useTokenList";
 import { useChainId } from "wagmi";
 
 export function useAppConfigForConnectedChain(): AppConfig {
   const connectedChainId = useChainId();
 
-  return isMainnetChain(connectedChainId) ? mainnetAppConfig : testnetAppConfig;
+  const appConfig = isMainnetChain(connectedChainId)
+    ? mainnetAppConfig
+    : testnetAppConfig;
+
+  // Add any zap tokens to the appConfig using uniswap's tokenlist
+  const { isFlagEnabled } = useFeatureFlag("zaps");
+  const { tokenList } = useTokenList({
+    chainId: connectedChainId,
+    enabled: isFlagEnabled,
+  });
+  if (tokenList) {
+    return {
+      ...appConfig,
+      tokens: [...appConfig.tokens, ...tokenList],
+    };
+  }
+
+  return appConfig;
 }
