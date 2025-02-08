7d8ad6f85edd095039339e1ff3ef63cf509b472f
Ryan Goree <goree.ryan@gmail.com>
2025-02-07 23:44:21 -0600

    Use public client params in getDrift

--- apps/hyperdrive-trading/src/network/drift.ts
+++ apps/hyperdrive-trading/src/network/drift.ts
@@ -1,17 +1,12 @@
-import {
-  ClientConfig,
-  createDrift,
-  Drift,
-  LruSimpleCache,
-} from "@delvtech/drift";
+import { createDrift, Drift, LruSimpleCache } from "@delvtech/drift";
 import { viemAdapter } from "@delvtech/drift-viem";
-import { getPublicClient } from "@wagmi/core";
+import { getPublicClient, GetPublicClientParameters } from "@wagmi/core";
 import { wagmiConfig } from "src/network/wagmiClient";
 
 // 1 minute TTL to match the queryClient's staleTime
 export const driftCache = new LruSimpleCache({ max: 500, ttl: 60_000 });
 
-export function getDrift(params?: ClientConfig): Drift {
+export function getDrift(params?: GetPublicClientParameters): Drift {
   const publicClient = getPublicClient(wagmiConfig, params);
 
   if (!publicClient) {
