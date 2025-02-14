cf8a01a27f33b7313368501176bd55470f65db39
Danny Delott <dannydelott@gmail.com>
2025-02-13 17:34:10 -0800

    Wire up points leaderboard

---
+++ apps/hyperdrive-trading/src/rewards/merkl.ts
@@ -0,0 +1,137 @@
+import { MerklApi } from "@merkl/api";
+import groupBy from "lodash.groupby";
+import { ClaimableReward } from "src/rewards/ClaimableReward";
+import { Address, Hash } from "viem";
+import { gnosis } from "viem/chains";
+
+/**
+ * Merkl.xyz API client
+ */
+export const merklApi = MerklApi("https://api.merkl.xyz").v4;
+
+/**
+ * Returns the list of users with Miles and their balances.
+ */
+export interface LeaderboardEntry {
+  address: Address;
+  balance: bigint;
+}
+export async function fetchMilesLeaderboard(): Promise<LeaderboardEntry[]> {
+  const opportunitiesResponse = await fetch(
+    "https://api.merkl.xyz/v4/opportunities/campaigns?tokenAddress=0x79385D4B4c531bBbDa25C4cFB749781Bd9E23039",
+  );
+  const opportunities = (await opportunitiesResponse.json()) as {
+    campaigns: { campaignId: string }[];
+  }[];
+  const campaignIds = Array.from(
+    new Set(
+      opportunities.flatMap((opportunity) =>
+        opportunity.campaigns.map((campaign) => campaign.campaignId),
+      ),
+    ),
+  );
+
+  const users = (
+    await Promise.all(
+      campaignIds.map(async (campaignId) => {
+        const rewardsResponse = await fetch(
+          `https://api.merkl.xyz/v4/rewards/?chainId=100&campaignId=${campaignId}`,
+        );
+        const rewards = (await rewardsResponse.json()) as {
+          recipient: Address;
+          amount: string;
+        }[];
+        return rewards;
+      }),
+    )
+  ).flat();
+
+  const rewardsByUser = Object.entries(groupBy(users, (user) => user.recipient))
+    .map(([user, rewards]) => {
+      const totalRewards = rewards.reduce(
+        (total, reward) => total + BigInt(reward.amount),
+        0n,
+      );
+      return { user, totalRewards };
+    })
+    .filter(({ totalRewards }) => totalRewards > 0n)
+    .map(({ user, totalRewards }) => ({
+      address: user as Address,
+      balance: totalRewards,
+    }))
+    .toSorted((a, b) => Number(b.balance - a.balance));
+
+  return rewardsByUser;
+}
+
+/**
+ * Merkl Distributor is the contract that you can claim rewards from in the
+ * Merkl.xyz ecosystem.
+ * See: https://app.merkl.xyz/status
+ */
+export const MerklDistributorsByChain: Record<number, Address> = {
+  [gnosis.id]: "0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae",
+};
+
+/**
+ * Fetches the number of Miles a user has earned
+ */
+export async function fetchMileRewards(
+  account: Address,
+): Promise<ClaimableReward[]> {
+  // Merkl.xyz accumulates Miles across all chains and hyperdrives onto Gnosis
+  // chain only. This makes things easier for turning them into HD later if
+  // they're all just on one chain.
+  const chainIds = [gnosis.id];
+
+  // Request miles earned on each chain. We have to call this once per chain
+  // since the merkl api is buggy, despite accepting an array of chain ids. If
+  // this gets fixed, we can remove the Promise.all and simplify this logic.
+  const mileRewards = (
+    await Promise.all(
+      chainIds.map(async (chainId) => {
+        const { data } = await merklApi
+          .users({
+            address: account,
+          })
+          .rewards.get({
+            query: {
+              chainId: [chainId],
+            },
+          });
+
+        return { data, chainId };
+      }),
+    )
+  )
+    .filter(
+      ({ data }) =>
+        data?.length &&
+        // since we only request a single chain id, we can just grab the first
+        // data item
+        data[0].rewards.find(
+          // Merkl.xyz has something called HYPOINTS too, but we only care about
+          // Miles
+          (d) => d.token.symbol === "Miles" && !!Number(d.amount),
+        ),
+    )
+    .map(({ data, chainId }): ClaimableReward => {
+      const rewards = data![0].rewards.find(
+        (d) => d.token.symbol === "Miles" && !!Number(d.amount),
+      );
+      return {
+        chainId,
+        merkleType: "MerklXyz",
+        merkleProof: rewards?.proofs as Hash[],
+        claimableAmount: rewards?.amount.toString() || "0",
+        pendingAmount: rewards?.pending.toString() || "0",
+        merkleProofLastUpdated: 0,
+        rewardTokenAddress: rewards?.token.address as `0x${string}`,
+        // TODO: This won't use the same abi as the hyperdrive rewards api, so
+        // we'll need to account for this somehow
+        claimContractAddress: MerklDistributorsByChain[chainId],
+      };
+    })
+    .flat();
+  return mileRewards;
+}
--- apps/hyperdrive-trading/src/ui/markets/Leaderboard.tsx
+++ apps/hyperdrive-trading/src/ui/markets/Leaderboard.tsx
@@ -1,10 +1,43 @@
+import { AppConfig, makeAddressUrl } from "@delvtech/hyperdrive-appconfig";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/outline";
+import { useQuery } from "@tanstack/react-query";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { Addreth, ThemeDeclaration } from "addreth";
+import classNames from "classnames";
 import { ReactElement } from "react";
-import { Fade } from "react-awesome-reveal";
+import { fetchMilesLeaderboard, LeaderboardEntry } from "src/rewards/merkl";
+import { useAppConfigForConnectedChain } from "src/ui/appconfig/useAppConfigForConnectedChain";
 import LoadingState from "src/ui/base/components/LoadingState";
+import { TableSkeleton } from "src/ui/base/components/TableSkeleton";
+import { formatBalance } from "src/ui/base/formatting/formatBalance";
+import { HyperVueMilesIconUrl } from "src/ui/rewards/HyperVueMilesIconUrl";
+import { gnosis } from "viem/chains";
 
 export function Leaderboard(): ReactElement | null {
-  // TODO Implement the hook for fetching leaderboard data
-  const { leaderboard, status } = { status: "loading", leaderboard: {} };
+  const { leaderboard, status } = useMilesLeaderboard();
+  const appConfig = useAppConfigForConnectedChain();
+
+  const columns = getColumns(appConfig);
+  const tableInstance = useReactTable({
+    columns,
+    data: leaderboard || [],
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    initialState: {
+      sorting: [
+        {
+          id: "balance",
+          desc: true,
+        },
+      ],
+    },
+  });
 
   return (
     <div className="mx-[2vw] mt-4 space-y-8 lg:w-[1080px]">
@@ -13,27 +46,153 @@ export function Leaderboard(): ReactElement | null {
           Miles Leaderboard
         </h1>
         <p className="font-inter text-lg leading-bodyText text-neutral-content">
-          Showing the top miles earners across all pools
+          Live rankings of the top Miles earners on Hyperdrive. Add liquidity to
+          any Hyperdrive pool to earn Miles!
         </p>
       </div>
-      <div className="flex w-full flex-wrap gap-8">
-        {status === "loading" && !leaderboard ? (
-          <div className="flex w-full justify-center">
+      <div className="w-full overflow-x-auto">
+        {status === "loading" && !leaderboard?.length ? (
+          <div className="px-10 py-20">
             <LoadingState
-              heading="Loading pools..."
-              text="Calculating yield rates, pool data, and rewards..."
+              text="Loading miles across all users and pools..."
+              heading="Loading leaderboard..."
             />
           </div>
-        ) : leaderboard ? (
-          <Fade triggerOnce duration={500}>
-            <div>leaderboard here</div>
-          </Fade>
-        ) : null}
+        ) : (
+          <table className="daisy-table daisy-table-zebra daisy-table-lg h-fit">
+            <thead>
+              {tableInstance.getHeaderGroups().map((headerGroup) => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    const sortDirection = header.column.getIsSorted();
+                    return (
+                      <th
+                        className={classNames(
+                          "sticky top-0 z-10 h-10 rounded-box text-sm",
+                          {
+                            "min-w-52": header.id === "name",
+                          },
+                        )}
+                        key={header.id}
+                      >
+                        <div
+                          className={classNames(
+                            "flex items-center gap-1 font-normal text-neutral-content",
+                            {
+                              "flex cursor-pointer select-none items-center gap-2":
+                                header.column.getCanSort(),
+                            },
+                          )}
+                          onClick={header.column.getToggleSortingHandler()}
+                        >
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                          {sortDirection === "asc" && (
+                            <ChevronUpIcon height={15} />
+                          )}
+                          {sortDirection === "desc" && (
+                            <ChevronDownIcon height={15} />
+                          )}
+                        </div>
+                      </th>
+                    );
+                  })}
+                </tr>
+              ))}
+            </thead>
+            <tbody
+              className={classNames({
+                "relative h-52":
+                  !tableInstance.getFilteredRowModel().rows.length,
+              })}
+            >
+              {status === "loading" ? (
+                <TableSkeleton numColumns={columns.length} numRows={5} />
+              ) : (
+                tableInstance.getRowModel().rows.map((row) => {
+                  return (
+                    <tr key={row.id} className="h-20">
+                      <>
+                        {row.getVisibleCells().map((cell) => {
+                          return (
+                            <td key={cell.id}>
+                              {flexRender(
+                                cell.column.columnDef.cell,
+                                cell.getContext(),
+                              )}
+                            </td>
+                          );
+                        })}
+                      </>
+                    </tr>
+                  );
+                })
+              )}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
 }
 
 function useMilesLeaderboard() {
-  return;
+  const { data: leaderboard, status } = useQuery({
+    queryKey: ["leaderboard"],
+    queryFn: async () => {
+      return fetchMilesLeaderboard();
+    },
+  });
+  return { leaderboard, status };
+}
+
+const columnHelper = createColumnHelper<LeaderboardEntry>();
+
+const addrethTheme: ThemeDeclaration = {
+  base: "dark",
+  badgeBackground: "transparent",
+  fontSize: 16,
+};
+function getColumns(appConfig: AppConfig) {
+  return [
+    columnHelper.accessor((row) => row.address, {
+      header: "Address",
+      cell: ({ getValue }) => {
+        const address = getValue();
+        return (
+          <Addreth
+            address={address}
+            ens={false /* no ENS support on Gnosischain */}
+            theme={addrethTheme}
+            explorer={(a) => ({
+              accountUrl: makeAddressUrl(a, appConfig.chains[gnosis.id]),
+              name: "Gnosisscan",
+            })}
+          />
+        );
+      },
+    }),
+    columnHelper.accessor((row) => row.balance, {
+      header: "Miles Earned",
+      id: "balance",
+      cell: ({ getValue }) => {
+        let formatted = formatBalance({
+          balance: getValue(),
+          decimals: 18,
+          places: 2,
+        });
+        if (formatted === "0.00") {
+          formatted = "<0.01";
+        }
+        return (
+          <div className="flex items-center gap-1.5">
+            <img src={HyperVueMilesIconUrl} className="size-6 rounded-full" />{" "}
+            {formatted}
+          </div>
+        );
+      },
+    }),
+  ];
 }
