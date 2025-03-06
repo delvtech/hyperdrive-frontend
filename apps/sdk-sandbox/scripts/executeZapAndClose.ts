c752a259e8fd2442e8428a054e79c34c739df143
jackburrus <jhburrus3@gmail.com>
2025-03-06 10:17:03 -0700

    adds some logs
    Co-authored-by: Danny Delott <DannyDelott@users.noreply.github.com>

--- apps/sdk-sandbox/scripts/executeZapAndClose.ts
+++ apps/sdk-sandbox/scripts/executeZapAndClose.ts
@@ -7,7 +7,7 @@ import {
   ReadWriteHyperdrive,
   zapAbi,
 } from "@delvtech/hyperdrive-js";
-import { Address, erc20Abi, maxInt256 } from "viem";
+import { Address, erc20Abi } from "viem";
 import { publicClient, walletClient } from "../client";
 
 const zapsConfig = appConfig.zaps[707];
@@ -15,11 +15,15 @@ const drift = new Drift(viemAdapter({ publicClient, walletClient }));
 
 const poolAddress = "0x324395D5d835F84a02A75Aa26814f6fD22F25698";
 const earliestBlock = 20180617n;
+const usdcAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 
 // SAMPLE ASSET ID AND MATURITY
 const assetId: bigint =
   452312848583266388373324160190187140051835877600158453279131187532667520256n;
 const maturity = 1756857600n;
+
+const defaultOpenLongAmount = BigInt(20e18);
+const defaultCloseLongAmount = BigInt(50e18);
 
 const writePool = new ReadWriteHyperdrive({
   address: poolAddress,
@@ -40,6 +44,20 @@ const poolContract = drift.contract({
   address: poolAddress,
 });
 
+const zapBondsBalance = await readPool.contract.read("balanceOf", {
+  owner: zapsConfig.address,
+  tokenId: assetId,
+});
+console.log("zapsBondBalance", zapBondsBalance);
+
+const zapsUsdcBalance = await publicClient.readContract({
+  address: usdcAddress,
+  abi: erc20Abi,
+  functionName: "balanceOf",
+  args: [zapsConfig.address],
+});
+console.log("zapsUsdcBalance", zapsUsdcBalance);
+
 export async function executeZapOpenAndClose(swapPath: `0x${string}`) {
   try {
     const account = walletClient?.account.address as Address;
@@ -53,62 +71,69 @@ export async function executeZapOpenAndClose(swapPath: `0x${string}`) {
     });
     // console.log("openLongDetails BEFORE openLong:", beforeDetails);
 
-    const beforeBalanceOf = await readPool.contract.read("balanceOf", {
+    const beforeBalanceOfBonds = await readPool.contract.read("balanceOf", {
       owner: account,
       tokenId: assetId,
     });
-    console.log("beforeBalanceOf", fixed(beforeBalanceOf).format());
+    console.log("beforeBalanceOfBonds", fixed(beforeBalanceOfBonds).format());
 
+    const beforeBalanceOfUsdc = await publicClient.readContract({
+      address: usdcAddress,
+      abi: erc20Abi,
+      functionName: "balanceOf",
+      args: [account],
+    });
+    console.log("beforeBalanceOfUsdc", fixed(beforeBalanceOfUsdc, 6).format());
     // Open Long
-    const { result, request } = await publicClient.simulateContract({
-      abi: writePool.contract.abi,
-      address: poolAddress,
-      chain: publicClient.chain,
-      functionName: "openLong",
-      account,
-      gas: 16125042n,
-      args: [
-        BigInt(200e18), // 200 base token
-        1n,
-        1n,
-        {
-          asBase: true,
-          destination: account,
-          extraData: "0x",
-        },
-      ],
-    });
+    // const { result, request } = await publicClient.simulateContract({
+    //   abi: writePool.contract.abi,
+    //   address: poolAddress,
+    //   chain: publicClient.chain,
+    //   functionName: "openLong",
+    //   account,
+    //   gas: 16125042n,
+    //   args: [
+    //     defaultOpenLongAmount, // 20 base token
+    //     1n,
+    //     1n,
+    //     {
+    //       asBase: true,
+    //       destination: account,
+    //       extraData: "0x",
+    //     },
+    //   ],
+    // });
 
-    const openTxHash = await walletClient?.writeContract({
-      ...request,
-    });
-    if (!openTxHash) throw new Error("No open transaction hash received");
-    console.log("Open Long tx hash:", openTxHash);
+    // const openTxHash = await walletClient?.writeContract({
+    //   ...request,
+    // });
+    // if (!openTxHash) throw new Error("No open transaction hash received");
+    // console.log("Open Long tx hash:", openTxHash);
 
-    const txReceipt = await publicClient.waitForTransactionReceipt({
-      hash: openTxHash,
-    });
-    await drift.cache.clear();
+    // const txReceipt = await publicClient.waitForTransactionReceipt({
+    //   hash: openTxHash,
+    // });
+    // await drift.cache.clear();
 
-    if (!txReceipt) throw new Error("No open transaction receipt received");
-    if (txReceipt.status !== "success") {
-      console.error("Open Transaction failed:", txReceipt);
-      throw new Error("Open Transaction failed");
-    }
-    console.log("Open tx receipt status:", txReceipt.status);
+    // if (!txReceipt) throw new Error("No open transaction receipt received");
+    // if (txReceipt.status !== "success") {
+    //   console.error("Open Transaction failed:", txReceipt);
+    //   throw new Error("Open Transaction failed");
+    // }
+    // console.log("Open tx receipt status:", txReceipt.status);
 
-    // Get open long details after opening
-    const afterDetails = await readPool.getOpenLongDetails({
-      account,
-      assetId,
-    });
+    // // Get open long details after opening
+    // const afterDetails = await readPool.getOpenLongDetails({
+    //   account,
+    //   assetId,
+    // });
 
-    await drift.cache.clear();
-    const afterBalanceOf = await readPool.contract.read("balanceOf", {
-      owner: account,
-      tokenId: assetId,
-    });
-    console.log("afterBalanceOf", fixed(afterBalanceOf).format());
+    // await drift.cache.clear();
+    // const afterBalanceOfBonds = await readPool.contract.read("balanceOf", {
+    //   owner: account,
+    //   tokenId: assetId,
+    // });
+    // console.log("afterBalanceOfBonds", fixed(afterBalanceOfBonds).format());
 
     // Check the USDC Balance here
     const usdcBalanceOf = await publicClient.readContract({
@@ -122,11 +147,33 @@ export async function executeZapOpenAndClose(swapPath: `0x${string}`) {
     // console.log("openLongDetails AFTER openLong:", afterDetails);
 
     // Approve zap (auxiliary) contract to manage the long position
-    const approvalReceipt = await writePool.contract.write("setApproval", {
-      amount: maxInt256,
-      tokenID: assetId,
-      operator: zapsConfig.address,
+    const approvalReceipt = await writePool.contract.write(
+      "setApprovalForAll",
+      {
+        operator: zapsConfig.address,
+        approved: true,
+      },
+    );
+
+    await publicClient.waitForTransactionReceipt({
+      hash: approvalReceipt,
     });
+
+    const allowanceOfZapsToSpend = await publicClient.readContract({
+      address: poolAddress,
+      abi: poolContract.abi,
+      functionName: "isApprovedForAll",
+      args: [account, zapsConfig.address],
+    });
+    console.log("allowanceOfZapsToSpend", allowanceOfZapsToSpend);
+
+    // const approvalReceipt = await walletClient?.writeContract({
+    //   abi: writePool.contract.abi,
+    //   address: poolAddress,
+    //   functionName: "setApproval",
+    //   args: [maxInt256, assetId, zapsConfig.address],
+    // });
+
     console.log("Approval tx hash:", approvalReceipt);
 
     // Simulate closeLong to preview base amount out
@@ -139,7 +186,7 @@ export async function executeZapOpenAndClose(swapPath: `0x${string}`) {
         account,
         args: [
           maturity,
-          BigInt(100e18), // 100 base token (example)
+          defaultCloseLongAmount, // 10 base token (example)
           1n,
           {
             asBase: true,
@@ -158,45 +205,23 @@ export async function executeZapOpenAndClose(swapPath: `0x${string}`) {
     const blockData = await publicClient.getBlock();
     const deadline = blockData.timestamp + 60n;
 
-    const simulateSwap = await publicClient.simulateContract({
-      abi: zapAbi,
+    const simulateReadNameFromZap = await publicClient.readContract({
       address: zapsConfig.address,
-      account: account,
-      functionName: "closeLongZap",
-      gas: 16125042n,
-      args: [
-        poolAddress,
-        maturity,
-        BigInt(100e18),
-        1n,
-        {
-          destination: zapsConfig.address,
-          asBase: true,
-          extraData: "0x",
-        },
-        {
-          path: swapPath,
-          recipient: account,
-          deadline,
-          amountIn: previewBaseAmountOut,
-          amountOutMinimum: 1n,
-        },
-        false,
-      ],
+      abi: zapAbi,
+      functionName: "name",
     });
-
-    console.log("simulateSwap", simulateSwap);
+    console.log("simulateReadNameFromZap", simulateReadNameFromZap);
     const swapTx = await walletClient
       ?.writeContract({
         abi: zapAbi,
         chain: publicClient.chain,
         address: zapsConfig.address,
         functionName: "closeLongZap",
-        gas: 16125042n,
+        gas: 20000000n, // Increased gas limit
         args: [
           poolAddress,
           maturity,
-          BigInt(100e18), // 100 base token
+          defaultCloseLongAmount, // 10 base token
           1n,
           {
             // hyperdriver parameters
@@ -235,11 +260,14 @@ export async function executeZapOpenAndClose(swapPath: `0x${string}`) {
     // console.log("openLongDetails AFTER zap close:", openLongDetailsAfterZap);
 
     await drift.cache.clear();
-    const afterZapBalanceOf = await readPool.contract.read("balanceOf", {
+    const afterZapBalanceOfBonds = await readPool.contract.read("balanceOf", {
       owner: account,
       tokenId: assetId,
     });
-    console.log("afterZapBalanceOf", fixed(afterZapBalanceOf).format());
+    console.log(
+      "afterZapBalanceOfBonds",
+      fixed(afterZapBalanceOfBonds).format(),
+    );
 
     const usdcBalanceAfterZap = await publicClient.readContract({
       address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
@@ -249,7 +277,7 @@ export async function executeZapOpenAndClose(swapPath: `0x${string}`) {
     });
     console.log("usdcBalanceAfterZap", fixed(usdcBalanceAfterZap, 6).format());
 
-    return txReceipt;
+    return receipt;
   } catch (error) {
     console.error("Failed to execute zap open/close:", error);
     throw error;
