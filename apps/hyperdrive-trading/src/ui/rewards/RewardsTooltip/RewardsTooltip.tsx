7599f742848d277dedfe9f7b61b7ae9aca4a7699
Danny Delott <dannydelott@gmail.com>
2025-03-10 11:04:03 -0700

    Refactor RewardsTooltip to floating-ui tooltip

--- apps/hyperdrive-trading/src/ui/rewards/RewardsTooltip/RewardsTooltip.tsx
+++ apps/hyperdrive-trading/src/ui/rewards/RewardsTooltip/RewardsTooltip.tsx
@@ -1,8 +1,12 @@
 import { getHyperdriveConfig } from "@delvtech/hyperdrive-appconfig";
-import * as Tooltip from "@radix-ui/react-tooltip";
 import { PropsWithChildren, ReactNode } from "react";
 import { calculateMarketYieldMultiplier } from "src/hyperdrive/calculateMarketYieldMultiplier";
 import { useAppConfigForConnectedChain } from "src/ui/appconfig/useAppConfigForConnectedChain";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "src/ui/base/components/Popover/Popover";
 import { useCurrentLongPrice } from "src/ui/hyperdrive/longs/hooks/useCurrentLongPrice";
 import { useAddLiquidityRewards } from "src/ui/rewards/hooks/useAddLiquidityRewards";
 import { useOpenShortRewards } from "src/ui/rewards/hooks/useOpenShortRewards";
@@ -58,27 +62,24 @@ export function RewardsTooltip({
       <div className="flex items-center whitespace-nowrap">{children}</div>
     );
   }
-
   return (
-    <Tooltip.Root>
-      <Tooltip.Trigger className="flex items-center whitespace-nowrap">
+    <Popover>
+      <PopoverTrigger className="flex items-center whitespace-nowrap">
         {children}
-      </Tooltip.Trigger>
-      <Tooltip.Portal>
-        <Tooltip.Content
-          className="z-20 h-fit w-72 rounded-box bg-base-200 px-3 py-2 shadow-2xl"
-          sideOffset={5}
-          collisionPadding={12}
-        >
-          <RewardsTooltipContent
-            chainId={chainId}
-            position={position}
-            hyperdriveAddress={hyperdriveAddress}
-            baseRate={baseRate}
-            netRate={netRate}
-          />
-        </Tooltip.Content>
-      </Tooltip.Portal>
-    </Tooltip.Root>
+      </PopoverTrigger>
+      <PopoverContent
+        className="z-20 h-fit w-72 rounded-box bg-base-200 px-3 py-2 shadow-2xl"
+        // sideOffset={5}
+        // collisionPadding={12}
+      >
+        <RewardsTooltipContent
+          chainId={chainId}
+          position={position}
+          hyperdriveAddress={hyperdriveAddress}
+          baseRate={baseRate}
+          netRate={netRate}
+        />
+      </PopoverContent>
+    </Popover>
   );
 }
