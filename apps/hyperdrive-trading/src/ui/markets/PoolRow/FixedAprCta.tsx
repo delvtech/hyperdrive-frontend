dbbd25225c59e7fc770b4de8fb8ce2ec26e2ac60
Danny Delott <dannydelott@gmail.com>
2025-03-24 11:52:31 -0700

    Make CTAs link to respective pool details sections (#1835)
    * Make CTAs link to respective pool details sections
    
    * update captial exposure -> yield exposure

--- apps/hyperdrive-trading/src/ui/markets/PoolRow/FixedAprCta.tsx
+++ apps/hyperdrive-trading/src/ui/markets/PoolRow/FixedAprCta.tsx
@@ -1,15 +1,19 @@
+import { fixed } from "@delvtech/fixed-point-wasm";
 import { HyperdriveConfig } from "@delvtech/hyperdrive-appconfig";
+import { Link } from "@tanstack/react-router";
 import { ReactElement } from "react";
 import { formatRate } from "src/base/formatRate";
 import { useFixedRate } from "src/ui/hyperdrive/longs/hooks/useFixedRate";
 import { PercentLabel } from "src/ui/markets/PoolRow/PercentLabel";
 import { PoolStat } from "src/ui/markets/PoolRow/PoolStat";
+import { useAccount } from "wagmi";
 
 interface FixedAprCtaProps {
   hyperdrive: HyperdriveConfig;
 }
 
 export function FixedAprCta({ hyperdrive }: FixedAprCtaProps): ReactElement {
+  const { address: account } = useAccount();
   const { fixedApr, fixedRateStatus } = useFixedRate({
     chainId: hyperdrive.chainId,
     hyperdriveAddress: hyperdrive.address,
@@ -18,22 +22,44 @@ export function FixedAprCta({ hyperdrive }: FixedAprCtaProps): ReactElement {
   const label = "Fixed APR";
 
   return (
-    <PoolStat
-      label={label}
-      value={
-        fixedApr ? (
-          <PercentLabel
-            value={formatRate({
-              rate: fixedApr.apr,
-              includePercentSign: false,
-            })}
-            className="text-h4"
-          />
-        ) : (
-          "-"
-        )
-      }
-      isLoading={fixedRateStatus === "loading"}
-    />
+    <Link
+      to="/market/$chainId/$address"
+      params={{
+        address: hyperdrive.address,
+        chainId: hyperdrive.chainId.toString(),
+      }}
+      search={{ position: "long" }}
+      onClick={(e) => {
+        e.stopPropagation();
+        window.plausible("positionCtaClick", {
+          props: {
+            chainId: hyperdrive.chainId,
+            poolAddress: hyperdrive.address,
+            positionType: "long",
+            statName: label,
+            statValue: fixedApr ? fixed(fixedApr.apr, 18).toString() : "",
+            connectedWallet: account,
+          },
+        });
+      }}
+    >
+      <PoolStat
+        label={label}
+        value={
+          fixedApr ? (
+            <PercentLabel
+              value={formatRate({
+                rate: fixedApr.apr,
+                includePercentSign: false,
+              })}
+              className="text-h4"
+            />
+          ) : (
+            "-"
+          )
+        }
+        isLoading={fixedRateStatus === "loading"}
+      />
+    </Link>
   );
 }
