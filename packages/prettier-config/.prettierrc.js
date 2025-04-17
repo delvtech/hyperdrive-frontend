246b0f2c6c2a29afc884fdf55b9a60019795f1ec
jonny rhea <jonathan.rhea@gmail.com>
2025-04-17 17:06:20 -0500

    Updates

---
+++ packages/prettier-config/.prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: "all",
+  singleQuote: false,
+  semi: true,
+  printWidth: 80,
+};
+
+export default config;
