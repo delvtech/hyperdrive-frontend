01b43cbdb86eeac51bc73be9380729c1cf7f5a1d
jonny rhea <jonathan.rhea@gmail.com>
2025-04-18 11:55:47 -0500

    Updates

---
+++ assets/_commonjsHelpers-1789f0cf-DbhaA-k1.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-DaWYiYoD.js";var l=typeof globalThis<"u"?globalThis:typeof window<"u"?window:typeof o<"u"?o:typeof self<"u"?self:{};function f(e){return e&&e.__esModule&&Object.prototype.hasOwnProperty.call(e,"default")?e.default:e}export{l as c,f as g};
---
+++ assets/native-gaClPkN4.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index-DaWYiYoD.js";function t(){if(typeof WebSocket<"u")return WebSocket;if(typeof e.WebSocket<"u")return e.WebSocket;if(typeof window.WebSocket<"u")return window.WebSocket;if(typeof self.WebSocket<"u")return self.WebSocket;throw new Error("`WebSocket` is not supported in this environment")}const n=t();export{n as WebSocket};
