61e013f6a555a2c4dd85226dabf45d6bf2beb504
jonny rhea <jonathan.rhea@gmail.com>
2025-04-18 11:36:02 -0500

    Updates

--- assets/native-C2Rw2NyM.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-PeIq7Hj8.js";function t(){if(typeof WebSocket<"u")return WebSocket;if(typeof e.WebSocket<"u")return e.WebSocket;if(typeof window.WebSocket<"u")return window.WebSocket;if(typeof self.WebSocket<"u")return self.WebSocket;throw new Error("`WebSocket` is not supported in this environment")}const n=t();export{n as WebSocket};
