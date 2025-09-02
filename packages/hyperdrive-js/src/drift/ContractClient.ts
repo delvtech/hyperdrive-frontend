f9a8513a9b95fe8b016f6161bc160a6f3a22b376
Ryan Goree <goree.ryan@gmail.com>
2025-09-02 15:45:52 -0500

    Update base client types, remove unnecessary `getBlockOrThrow` util

--- packages/hyperdrive-js/src/drift/ContractClient.ts
+++ packages/hyperdrive-js/src/drift/ContractClient.ts
@@ -1,27 +1,11 @@
-import { SimpleCache } from "@delvtech/drift";
-import { Address } from "abitype";
+import type { ContractBaseOptions } from "@delvtech/drift";
 import { ReadClientOptions } from "src/drift/ReadClient";
 import { ReadWriteClientOptions } from "src/drift/ReadWriteClient";
 
 /**
  * Additional options required for clients that represent a specific contract.
  */
-export interface ContractClientOptions {
-  /**
-   * The address of the contract.
-   */
-  address: Address;
-
-  /**
-   * The cache to use for the contract.
-   */
-  cache?: SimpleCache;
-
-  /**
-   * The namespace to use for the cache.
-   */
-  cacheNamespace?: PropertyKey;
-}
+export type ContractClientOptions = Omit<ContractBaseOptions, "abi">;
 
 export interface ReadContractClientOptions
   extends ReadClientOptions,
--- packages/hyperdrive-js/src/drift/ReadClient.ts
+++ packages/hyperdrive-js/src/drift/ReadClient.ts
@@ -12,11 +12,6 @@ export interface ReadClientOptions {
    * behavior.
    */
   debugName?: string;
-
-  /**
-   * The earliest block to fetch events from.
-   */
-  earliestBlock?: bigint;
 }
 
 /**
@@ -26,33 +21,8 @@ export class ReadClient {
   drift: Drift;
   debugName: string;
 
-  constructor({ debugName, drift, earliestBlock }: ReadClientOptions) {
+  constructor({ debugName, drift }: ReadClientOptions) {
     this.debugName = debugName ?? this.constructor.name;
     this.drift = drift;
-
-    // Override the contract factory to ensure that events are fetched from the
-    // earliest block if necessary.
-    if (earliestBlock) {
-      const originalContractFactory = this.drift.contract;
-      this.drift.contract = (options) => {
-        const contract = originalContractFactory(options);
-
-        // Override the getEvents method
-        const originalGetEvents = contract.getEvents;
-        contract.getEvents = async function (eventName, options) {
-          const _options = { ...options };
-          const fromBlock = _options?.fromBlock;
-          const isBeforeEarliest =
-            typeof fromBlock === "bigint" && fromBlock < earliestBlock;
-          if (!fromBlock || fromBlock === "earliest" || isBeforeEarliest) {
-            _options.fromBlock = earliestBlock;
-          }
-
-          return originalGetEvents(eventName, _options);
-        };
-
-        return contract as any;
-      };
-    }
   }
 }
